ffbf3292ea9ee6586a4f0a6097dd7d0e408a786f
Stefan Johnson <ScrimpyCat@gmail.com>
2014-01-16 14:52:42 +1100

    Silences precision warning due to 64 bit arch
    As CC_SHA1 doesn't offer a CC_LONG64 version (or equivalent
    type that would be appropriate for NSData), just assume the
    data won't exceed the length of a CC_LONG and simply cast it
    to silence the warning.

--- Wikipedia-iOS/Categories/NSString+Extras.m
+++ Wikipedia-iOS/Categories/NSString+Extras.m
@@ -21,7 +21,7 @@
     }
     
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     
     for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
