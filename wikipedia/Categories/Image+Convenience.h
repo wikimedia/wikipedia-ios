1e0505cb419640aa9709310b6b7bbb5e1cd72dfc
Brion Vibber <brion@pobox.com>
2014-06-16 11:38:35 -0700

    Initial 'garbage collection' of unused cached images when cleaning history
    Images linked from sections were saved into the database, but not deleted
    automatically when their section was removed because they might be used
    in multiple articles.
    
    Adds a convenience method [image deleteIfUnused] to kill out the old images
    if still unused after the cascading deletes remove the sections.
    
    Change-Id: I6755ec0490e16d1d46fbb34e61c4d947858989a1

--- wikipedia/Categories/Image+Convenience.h
+++ wikipedia/Categories/Image+Convenience.h
@@ -8,4 +8,10 @@
 // Retrieves the highest resolution image in the core data store with same name as this image.
 -(Image *)getHighestResolutionImageWithSameNameUsingContext:(NSManagedObjectContext *)context;
 
+/**
+ * Check if this image is still in use, and if not remove it and its backing data.
+ * You'll still have to actually save updates on the managed object context!
+ */
+-(void)deleteIfUnused;
+
 @end
