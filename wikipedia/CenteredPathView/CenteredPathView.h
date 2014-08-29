ec33a79d8b8a222688883b1ed9a3b253102b3786
Monte Hurd <mhurd@wikimedia.org>
2014-08-29 11:13:16 -0700

    Class for rendering CGPaths sized to view.
    Change-Id: I6a67e2e1ad367e47214a334a7ec71701c39e50c6

---
+++ wikipedia/CenteredPathView/CenteredPathView.h
@@ -0,0 +1,15 @@
+//  Created by Monte Hurd on 8/26/14.
+//  Copyright (c) 2014 Wikimedia Foundation. Provided under MIT-style license; please copy and modify!
+
+// Draws a CGPath scaled to fix exactly within the view.
+
+#import <UIKit/UIKit.h>
+ 
+@interface CenteredPathView : UIView
+ 
+-(id)initWithPath: (CGPathRef)newPath
+      strokeWidth: (CGFloat)strokeWidth
+      strokeColor: (UIColor *)strokeColor
+        fillColor: (UIColor *)fillColor;
+ 
+@end
