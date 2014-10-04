239ab7631f0371f741f7b6e99961da3356fb9324
Monte Hurd <mhurd@wikimedia.org>
2014-10-03 17:34:32 -0700

    Update icon script to update all icon files.
    Added beta icon svg.
    
    Change-Id: I76484ef9fa6ca17b5ba31be4469d7d04502fab9f

--- scripts/icon-svgs-to-pngs.sh
+++ scripts/icon-svgs-to-pngs.sh
@@ -2,6 +2,8 @@
 
 ruby -e '[29,40,50,57,58,72,76,80,87,90,100,114,120,144,152,180].each { |x| `/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "wikipedia/Images.xcassets/AppIcon.appiconset/icon#{x}.png" -w #{x} '"$SCRIPT_INPUT_FILE_0"'` }'
 
+ruby -e '[87,120,180].each { |x| `/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "wikipedia/Images.xcassets/AppIcon.appiconset/icon#{x}-1.png" -w #{x} '"$SCRIPT_INPUT_FILE_0"'` }'
+
 ruby -e '[120,240,360].each { |x| `/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "wikipedia/Images.xcassets/RecentPagesEmpty.imageset/recent#{x}.png" -w #{x} '"$SCRIPT_INPUT_FILE_1"'` }'
 
 ruby -e '[120,240,360].each { |x| `/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "wikipedia/Images.xcassets/SavedPagesEmpty.imageset/savedpages#{x}.png" -w #{x} '"$SCRIPT_INPUT_FILE_2"'` }'
