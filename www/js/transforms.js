71850d47873650976d82a218172fd9e0c7ba75bc
Monte Hurd <mhurd@wikimedia.org>
2014-10-10 10:40:03 -0700

    Fix for infobox relocation leaving parent table with empty space.
    Relocates the most distant ancestor table encountered before the
    content_block_0 div is encountered. If no parent tables found it
    just moves the infobox the same as before.
    
    Change-Id: Id4733126aa843680869da47cc027f161b77e7262

--- www/js/transforms.js
+++ www/js/transforms.js
@@ -34,6 +34,23 @@ transformer.register( "relocateInfobox", function( content ) {
     
     if (!soughtP) return;
 
+    /*
+    If the infobox table itself sits within a table or series of tables,
+    move the most distant ancestor table instead of just moving the 
+    infobox. Otherwise you end up with table(s) with a hole where the 
+    infobox had been. World War II article on enWiki has this issue.
+    Note that we need to stop checking ancestor tables when we hit
+    content_block_0.
+    */
+    var infoboxParentTable = null;
+    var el = infobox;
+    while (el.parentNode) {
+        el = el.parentNode;
+        if (el.id === 'content_block_0') break;
+        if (el.tagName === 'TABLE') infoboxParentTable = el;
+    }
+    if(infoboxParentTable)infobox = infoboxParentTable;
+
     // Found the first P tag whose direct parent has id of #content_block_0.
     // Now safe to detach the infobox and stick it after the P.
     soughtP.appendChild(infobox.parentNode.removeChild(infobox));
