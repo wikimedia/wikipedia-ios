bd1e5d61aa27a254671a3d3677f5bb7a592cbead
Monte Hurd <mhurd@wikimedia.org>
2014-11-18 19:39:24 -0800

    Tweaks to Recent Searches.
    Also now handles really long search terms. As much as I'm
    normally against truncation and "...", this is a good
    place for it.
    
    Change-Id: I3a0175d103f8e96d424abee604836603922d2452

--- wikipedia/View Controllers/RecentSearches/RecentSearchCell.m
+++ wikipedia/View Controllers/RecentSearches/RecentSearchCell.m
@@ -5,28 +5,57 @@
 #import "PaddedLabel.h"
 #import "NSObject+ConstraintsScale.h"
 #import "Defines.h"
+#import "WikiGlyph_Chars.h"
 
 #define FONT_SIZE (16.0f * MENUS_SCALE_MULTIPLIER)
-#define FONT_COLOR [UIColor grayColor];
+#define FONT_COLOR [UIColor grayColor]
+#define ICON_SIZE (20.0f * MENUS_SCALE_MULTIPLIER)
+#define ICON_GLYPH WIKIGLYPH_MAGNIFYING_GLASS
+#define BORDER_COLOR [UIColor colorWithWhite:0.9 alpha:1.0]
+#define MAGNIFY_ICON_COLOR [UIColor colorWithWhite:0.8 alpha:1.0]
+
+@interface RecentSearchCell()
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topBorderHeightConstraint;
+@property (weak, nonatomic) IBOutlet UIView *topBorderView;
+@property (strong, nonatomic) NSAttributedString *magnifyIconString;
+@property (weak, nonatomic) IBOutlet PaddedLabel *iconLabel;
+
+@end
 
 @implementation RecentSearchCell
 
-- (void)awakeFromNib {
-    // Initialization code
-
+- (void)awakeFromNib
+{
     self.selectionStyle = UITableViewCellSelectionStyleNone;
 
+    self.iconLabel.attributedText = [self getAttributedIconString];
+    self.topBorderView.backgroundColor = BORDER_COLOR;
+    self.topBorderHeightConstraint.constant = 1.0f / [UIScreen mainScreen].scale;
     self.label.font = [UIFont systemFontOfSize:FONT_SIZE];
     
+    self.label.numberOfLines = 1;
+    self.label.lineBreakMode = NSLineBreakByTruncatingTail; // <-- Don't make truncating a habit! :) Is bad.
+
     self.label.textColor = FONT_COLOR;
 
-    [self adjustConstraintsScaleForViews:@[self.label]];
+    [self adjustConstraintsScaleForViews:@[self.label, self.iconLabel]];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+-(NSAttributedString *)getAttributedIconString
+{
+    return [[NSAttributedString alloc] initWithString: ICON_GLYPH
+                                           attributes: @{
+                                                         NSFontAttributeName: [UIFont fontWithName:@"WikiFont-Glyphs" size:ICON_SIZE],
+                                                         NSForegroundColorAttributeName : MAGNIFY_ICON_COLOR,
+                                                         NSBaselineOffsetAttributeName: @0
+                                                         }];
 }
 
 @end
