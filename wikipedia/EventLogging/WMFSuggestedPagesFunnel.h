d74f9fd1cf47385463b15ab3d5dcd388e0c4bfea
dr0ptp4kt <abaso@wikimedia.org>
2015-02-18 09:18:54 -0800

    Event logging for suggested pages
    * See also Ie5b02c0afca51f44ff444f1759847945fb72aba6
    
    Change-Id: I5aeab571428a15eca79fb14def955a11dad471ab

---
+++ wikipedia/EventLogging/WMFSuggestedPagesFunnel.h
@@ -0,0 +1,18 @@
+//
+//  SuggestedPagesFunnel.h
+//  Wikipedia
+//
+//  Created by Adam Baso on 2/17/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "EventLoggingFunnel.h"
+
+@interface WMFSuggestedPagesFunnel : EventLoggingFunnel
+
+- (id)initWithArticle:(MWKArticle*)article
+      suggestedTitles:(NSArray*)suggestedTitles;
+- (void)logShown;
+- (void)logClickedAtIndex:(NSUInteger)index;
+
+@end
