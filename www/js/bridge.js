771148e2d6954fcf84152cc54b25672e89b5b2f2
Monte Hurd <mhurd@wikimedia.org>
2015-04-23 18:39:19 -0700

    Fix for lock edit pencil showing alert twice.
    T96822
    
    Change-Id: Idd157331df387c4b91356147ac13e9ede7569192

--- www/js/bridge.js
+++ www/js/bridge.js
@@ -23,16 +23,18 @@ Bridge.prototype.registerListener = function( messageType, callback ) {
 };
 
 Bridge.prototype.sendMessage = function( messageType, payload ) {
-    var messagePack = { type: messageType, payload: payload };
-    var url = "x-wikipedia-bridge:" + encodeURIComponent( JSON.stringify( messagePack ) );
+    setTimeout(function() { // See: https://phabricator.wikimedia.org/T96822 and http://stackoverflow.com/a/9782220/135557
+        var messagePack = { type: messageType, payload: payload };
+        var url = "x-wikipedia-bridge:" + encodeURIComponent( JSON.stringify( messagePack ) );
 
-    // quick iframe version based on http://stackoverflow.com/a/6508343/82439
-    // fixme can this be an XHR instead? check Cordova current state
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute("src", url);
-    document.documentElement.appendChild(iframe);
-    iframe.parentNode.removeChild(iframe);
-    iframe = null;
+        // quick iframe version based on http://stackoverflow.com/a/6508343/82439
+        // fixme can this be an XHR instead? check Cordova current state
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute("src", url);
+        document.documentElement.appendChild(iframe);
+        iframe.parentNode.removeChild(iframe);
+        iframe = null;
+    }, 0);
 };
 
 module.exports = new Bridge();
