montehurd/apps-ios-wikipedia

Non-native lead image.
Fixes issue where italics title was not italic in lead image overlay.

Moves face detection from display time to image save time.

No bridge messages.

Only one non-bridged js message when face offset is calculated.
When that one js message is sent the image offset is changed
via css with css animation. Note this only happens the first
time a lead image is retrieved. If the server could tell us
the face offset we wouldn't need to animate this.

Subsequently when the article is re-presented the lead image
face offset is used directly (no need to animate it sliding to
the proper offset for the face since we know right away from the
MWKImage face offset value what it should be).

Lead image, placeholder image and gradient all shown via css.

Properly displays images with alpha so placeholder image does't
bleed through.

Landscape display (title and description text color to black,
no gradient, no background image, no drop shadow around letters)
all handled in css with no js.

No separate mechanism for downloading lead image required, the
web view requests the image in same fashion as other images.

Separate mechanism no longer needed for saving lead image
binary to data store.

Fixes display jitters related to keeping a "spacer" div
in sync with the height of a native lead image component.

Change-Id: I860523e12492b6857d8246e8ee4adfcea40a5376

Wikipedia/Web Image Interception/URLCache.m
@@ -11,6 +11,8 @@ NSString* const kURLCacheKeyWidth                = @"width";
 NSString* const kURLCacheKeyHeight               = @"height";
 NSString* const kURLCacheKeyURL                  = @"url";
 NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
+NSString* const kURLCacheKeyIsLeadImage          = @"isLeadImage";
+NSString* const kURLCacheKeyYFocalOffset         = @"yFocalOffset";
 
 #if 0
 #define URLCacheLog(...) NSLog(__VA_ARGS__)
@@ -84,7 +86,6 @@ NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
     }
 
     // Placeholder record found, so route image data to article data store.
-
     NSData* imageDataToUse = cachedResponse.data;
 
     @try {
@@ -105,7 +106,9 @@ NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
          kURLCacheKeyWidth: image.width,
          kURLCacheKeyHeight: image.height,
          kURLCacheKeyURL: image.sourceURL,
-         kURLCacheKeyFileNameNoSizePrefix: image.fileNameNoSizePrefix
+         kURLCacheKeyFileNameNoSizePrefix: image.fileNameNoSizePrefix,
+         kURLCacheKeyIsLeadImage : @([image isLeadImage]),
+         kURLCacheKeyYFocalOffset : (!image.yFocalOffset) ? @(-1) : image.yFocalOffset
      }];
 }
 

wikimedia/wikipedia-ios

Wikipedia/Web Image Interception/URLCache.m
@@ -5,12 +5,17 @@
 #import "NSString+Extras.h"
 #import "SessionSingleton.h"
 
-NSString* const kURLCacheKeyFileName             = @"fileName";
-NSString* const kURLCacheKeyData                 = @"data";
-NSString* const kURLCacheKeyWidth                = @"width";
-NSString* const kURLCacheKeyHeight               = @"height";
-NSString* const kURLCacheKeyURL                  = @"url";
-NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
+NSString* const WMFURLCacheSectionImageRetrievedNotification = @"WMFSectionImageRetrieved";
+
+NSString* const kURLCacheKeyFileName                   = @"fileName";
+NSString* const kURLCacheKeyData                       = @"data";
+NSString* const kURLCacheKeyWidth                      = @"width";
+NSString* const kURLCacheKeyHeight                     = @"height";
+NSString* const kURLCacheKeyURL                        = @"url";
+NSString* const kURLCacheKeyFileNameNoSizePrefix       = @"fileNameNoSizePrefix";
+NSString* const kURLCacheKeyIsLeadImage                = @"isLeadImage";
+NSString* const kURLCacheKeyPrimaryFocalUnitRectString = @"primaryFocalUnitRectString";
+
 
 #if 0
 #define URLCacheLog(...) NSLog(__VA_ARGS__)
@@ -84,7 +89,6 @@ NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
     }
 
     // Placeholder record found, so route image data to article data store.
-
     NSData* imageDataToUse = cachedResponse.data;
 
     @try {
@@ -97,7 +101,7 @@ NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
 
     // Broadcast the image data so things like the table of contents can update
     // itself as images arrive.
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"SectionImageRetrieved"
+    [[NSNotificationCenter defaultCenter] postNotificationName:WMFURLCacheSectionImageRetrievedNotification
                                                         object:nil
                                                       userInfo:@{
          kURLCacheKeyFileName: image.fileName,
@@ -105,7 +109,9 @@ NSString* const kURLCacheKeyFileNameNoSizePrefix = @"fileNameNoSizePrefix";
          kURLCacheKeyWidth: image.width,
          kURLCacheKeyHeight: image.height,
          kURLCacheKeyURL: image.sourceURL,
-         kURLCacheKeyFileNameNoSizePrefix: image.fileNameNoSizePrefix
+         kURLCacheKeyFileNameNoSizePrefix: image.fileNameNoSizePrefix,
+         kURLCacheKeyIsLeadImage: @([image isLeadImage]),
+         kURLCacheKeyPrimaryFocalUnitRectString: NSStringFromCGRect([image primaryFocalRectNomrmalizedToImageSize:NO])
      }];
 }
 