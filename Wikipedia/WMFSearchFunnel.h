1e5207764e06f208b877811063d7960442f94deb
Corey Floyd <cfloyd@wikimedia.org>
2015-05-08 15:18:38 -0400

    Implement search analytics - T90257
    Change-Id: I4af344ff1f4511f8cb4f388a55dc3dfde4db025a

---
+++ Wikipedia/WMFSearchFunnel.h
@@ -0,0 +1,30 @@
+//
+//  WMFSearchFunnel.h
+//  Wikipedia
+//
+//  Created by Corey Floyd on 5/8/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "EventLoggingFunnel.h"
+
+typedef NS_ENUM (NSUInteger, WMFSearchType) {
+    WMFSearchTypePrefix = 0,
+    WMFSearchTypeFull
+};
+
+@interface WMFSearchFunnel : EventLoggingFunnel
+
+- (void)logSearchStart;
+- (void)logSearchAutoSwitch;
+- (void)logSearchDidYouMean;
+- (void)logSearchResultTap;
+- (void)logSearchCancel;
+
+- (void)logSearchResultsWithTypeOfSearch:(WMFSearchType)type resultCount:(NSUInteger)count elapsedTime:(NSTimeInterval)searchTime;
+
+- (void)logShowSearchErrorWithTypeOfSearch:(WMFSearchType)type elapsedTime:(NSTimeInterval)searchTime;
+
+
+
+@end
