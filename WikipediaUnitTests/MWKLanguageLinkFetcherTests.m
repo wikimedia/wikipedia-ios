910d6c5a43ca26de4a76a6cee28a2a5b4f569cd8
Brian Gerstle <brian.gerstle@gmail.com>
2015-07-22 16:45:16 -0400

    fix crash when fetching title with empty text
    pass an error instead of silently failing or crashing
    
    Bug: T106026
    Change-Id: Ie95e4410097d3597ac7f46da5d054c4334f7e33e

---
+++ WikipediaUnitTests/MWKLanguageLinkFetcherTests.m
@@ -0,0 +1,76 @@
+//
+//  MWKLanguageLinkFetcherTests.m
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 7/22/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "WMFAsyncTestCase.h"
+#import "MWKLanguageLinkFetcher.h"
+#import "WMFNetworkUtilities.h"
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
+#define MOCKITO_SHORTHAND 1
+#import <OCMockito/OCMockito.h>
+
+#define HC_SHORTHAND 1
+#import <OCHamcrest/OCHamcrest.h>
+
+@interface MWKLanguageLinkFetcherTests : WMFAsyncTestCase
+@property (nonatomic, strong) AFHTTPRequestOperationManager* mockManager;
+@property (nonatomic, strong) id<FetchFinishedDelegate> mockDelegate;
+@property (nonatomic, strong) MWKLanguageLinkFetcher* fetcher;
+@end
+
+@implementation MWKLanguageLinkFetcherTests
+
+- (void)setUp {
+    self.mockDelegate = mockProtocol(@protocol(FetchFinishedDelegate));
+    self.mockManager  = mock([AFHTTPRequestOperationManager class]);
+    self.fetcher      = [[MWKLanguageLinkFetcher alloc] initWithManager:self.mockManager
+                                                               delegate:self.mockDelegate];
+
+    [super setUp];
+}
+
+- (void)testFetchingNilTitle {
+    PushExpectation();
+    [self.fetcher fetchLanguageLinksForTitle:nil success:^(NSArray* langLinks) {
+        XCTFail(@"Expected nil title to result in failure.");
+    } failure:^(NSError* error) {
+        XCTAssertEqual(error.code, WMFNetworkingError_InvalidParameters);
+        [self popExpectationAfter:nil];
+    }];
+    WaitForExpectations();
+    [[MKTVerify(self.mockDelegate) withMatcher:equalTo(@(FETCH_FINAL_STATUS_FAILED))
+                                   forArgument:2]
+     fetchFinished:self.fetcher
+       fetchedData:nil
+            status:0
+             error:[NSError errorWithDomain:WMFNetworkingErrorDomain code:WMFNetworkingError_InvalidParameters userInfo:nil]];
+}
+
+- (void)testFetchingEmptyTitle {
+    MWKTitle* mockTitle = mock([MWKTitle class]);
+    [given([mockTitle text]) willReturn:@""];
+
+    PushExpectation();
+    [self.fetcher fetchLanguageLinksForTitle:mockTitle success:^(NSArray* langLinks) {
+        XCTFail(@"Expected empty title to result in failure.");
+    } failure:^(NSError* error) {
+        XCTAssertEqual(error.code, WMFNetworkingError_InvalidParameters);
+        [self popExpectationAfter:nil];
+    }];
+    WaitForExpectations();
+    [[MKTVerify(self.mockDelegate) withMatcher:equalTo(@(FETCH_FINAL_STATUS_FAILED))
+                                   forArgument:2]
+     fetchFinished:self.fetcher
+       fetchedData:nil
+            status:0
+             error:[NSError errorWithDomain:WMFNetworkingErrorDomain code:WMFNetworkingError_InvalidParameters userInfo:nil]];
+}
+
+@end
