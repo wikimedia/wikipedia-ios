c1153623232741949caade45adda542c1e7c32d8
Brian Gerstle <brian.gerstle@gmail.com>
2015-07-24 11:13:52 -0400

    fix gallery image owner label overflow
    Needed to add tail truncation to attributed string attributes (since
    it attributes were overriding UIButton.titleLabel.lineBreakMode), as
    well as adding a >= "standard" trailing constraint to the button.
    
    See Phab ticket for screencaps of the fix.
    
    Bug: T106174
    Change-Id: I73e51682200dea5186e9e6e0a6c5f06d79a837a5

---
+++ Wikipedia/Categories/NSParagraphStyle+WMFParagraphStyles.m
@@ -0,0 +1,36 @@
+//
+//  NSParagraphStyle+WMFParagraphStyles.m
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 3/16/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "NSParagraphStyle+WMFParagraphStyles.h"
+
+@implementation NSParagraphStyle (WMFParagraphStyles)
+
++ (NSParagraphStyle*)wmf_naturalAlignmentStyle {
+    NSParameterAssert([NSThread isMainThread]);
+    static NSParagraphStyle* naturalAlignmentStyle = nil;
+    if (!naturalAlignmentStyle) {
+        NSMutableParagraphStyle* style = [NSMutableParagraphStyle new];
+        style.alignment       = NSTextAlignmentNatural;
+        naturalAlignmentStyle = [style copy];
+    }
+    return naturalAlignmentStyle;
+}
+
++ (NSParagraphStyle*)wmf_tailTruncatingNaturalAlignmentStyle {
+    NSParameterAssert([NSThread isMainThread]);
+    static NSParagraphStyle* tailTruncatingNaturalAlignmentStyle = nil;
+    if (!tailTruncatingNaturalAlignmentStyle) {
+        NSMutableParagraphStyle* style = [NSMutableParagraphStyle new];
+        style.alignment                     = NSTextAlignmentNatural;
+        style.lineBreakMode                 = NSLineBreakByTruncatingTail;
+        tailTruncatingNaturalAlignmentStyle = [style copy];
+    }
+    return tailTruncatingNaturalAlignmentStyle;
+}
+
+@end
