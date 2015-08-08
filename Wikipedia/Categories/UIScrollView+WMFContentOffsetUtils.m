9c242f3824e423552556c2c03d084244ea75cbdd
Brian Gerstle <brian.gerstle@gmail.com>
2015-08-08 17:19:17 -0400

    clean up list transition & add comments

---
+++ Wikipedia/Categories/UIScrollView+WMFContentOffsetUtils.m
@@ -0,0 +1,21 @@
+//
+//  UIScrollView+WMFContentOffsetUtils.m
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 8/8/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "UIScrollView+WMFContentOffsetUtils.h"
+
+@implementation UIScrollView (WMFContentOffsetUtils)
+
+- (CGPoint)wmf_topContentOffset {
+    return CGPointMake(self.contentInset.left, self.contentInset.top);
+}
+
+- (void)wmf_scrollToTop:(BOOL)animated {
+    [self setContentOffset:self.wmf_topContentOffset animated:animated];
+}
+
+@end
