32b10ac658cb01596d52ea177cb6ef9972db1e27
Monte Hurd <mhurd@wikimedia.org>
2015-08-10 18:36:20 -0700

    Removed unused "leadImage.less" styles.

--- www/js/listeners.js
+++ www/js/listeners.js
@@ -131,10 +131,6 @@ function touchEndedWithoutDragging(event){
     }
 }
 
-function getUrlFromBackgroundStyle(element) {
-    return element.style.background.match(/(?:url\(['|"]?)(.*?)(?:['|"]?\))/)[1];
-}
-
 /**
  * Attempts to send a bridge message which corresponds to `hrefTarget`, based on various attributes.
  * @return `true` if a message was sent, otherwise `false`.
@@ -164,7 +160,7 @@ function maybeSendMessageForTarget(event, hrefTarget){
             document.getElementById( href.substring( 1 ) ).scrollIntoView();
         }
     } else if (typeof hrefClass === 'string' && hrefClass.indexOf('image') !== -1) {
-         var url = (event.target.id === 'lead_image_div') ? getUrlFromBackgroundStyle(event.target) : event.target.getAttribute('src');
+         var url = event.target.getAttribute('src');
         bridge.sendMessage('imageClicked', { 'url': url });
     } else if (href) {
         bridge.sendMessage( 'linkClicked', { 'href': href });
