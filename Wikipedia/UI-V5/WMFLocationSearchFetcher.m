9b73ffa2ec570362ca07f85117685c681a1fcc11
Corey Floyd <coreyfloyd@gmail.com>
2015-08-24 20:55:44 -0400

    fix merge

--- Wikipedia/UI-V5/WMFLocationSearchFetcher.m
+++ Wikipedia/UI-V5/WMFLocationSearchFetcher.m
@@ -132,20 +132,4 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 
-@implementation WMFLocationSearchResponseSerializer
-
-- (id)responseObjectForResponse:(NSURLResponse*)response
-                           data:(NSData*)data
-                          error:(NSError* __autoreleasing*)error {
-    NSDictionary* JSON                    = [super responseObjectForResponse:response data:data error:error];
-    NSDictionary* nearbyResultsDictionary = JSON[@"query"][@"pages"];
-    NSArray* nearbyResultsArray           = [nearbyResultsDictionary allValues];
-
-    NSArray* results = [MTLJSONAdapter modelsOfClass:[MWKLocationSearchResult class] fromJSONArray:nearbyResultsArray error:error];
-    return [results sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:WMF_SAFE_KEYPATH([MWKLocationSearchResult new], distanceFromQueryCoordinates) ascending:YES]]];
-}
-
-@end
-
-
 NS_ASSUME_NONNULL_END
