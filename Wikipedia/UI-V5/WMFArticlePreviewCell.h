d8a1897f33ec75032072b66ad629187ffb09cb64
Corey Floyd <coreyfloyd@gmail.com>
2015-08-25 15:24:11 -0400

    Update the cell to use attributed strings for the summary

--- Wikipedia/UI-V5/WMFArticlePreviewCell.h
+++ Wikipedia/UI-V5/WMFArticlePreviewCell.h
@@ -1,11 +1,14 @@
 
 #import "WMFShadowCell.h"
+@class MWKImage;
 
 @interface WMFArticlePreviewCell : WMFShadowCell
 
 @property (copy, nonatomic) NSURL* imageURL;
+@property (copy, nonatomic) MWKImage* image;
+
 @property (copy, nonatomic) NSString* titleText;
 @property (copy, nonatomic) NSString* descriptionText;
-@property (copy, nonatomic) NSString* summaryText;
+@property (copy, nonatomic) NSAttributedString* summaryAttributedText;
 
 @end
