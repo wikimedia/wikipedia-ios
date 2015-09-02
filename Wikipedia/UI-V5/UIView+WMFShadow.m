bccf4e5246c3ccbfcd4e69f340a2e975c24c2442
Monte Hurd <mhurd@wikimedia.org>
2015-09-01 17:53:27 -0700

    Fix for cell shadows extending down too far and not resizing properly.

--- Wikipedia/UI-V5/UIView+WMFShadow.m
+++ Wikipedia/UI-V5/UIView+WMFShadow.m
@@ -11,16 +11,11 @@
 @implementation UIView (WMFShadow)
 
 - (void)wmf_setupShadow {
-    self.clipsToBounds       = NO;
+    self.layer.masksToBounds = NO;
     self.layer.shadowColor   = [UIColor blackColor].CGColor;
-    self.layer.shadowOpacity = 0.15;
-    self.layer.shadowRadius  = 3.0;
+    self.layer.shadowOpacity = 0.2;
+    self.layer.shadowRadius  = 4.0;
     self.layer.shadowOffset  = CGSizeZero;
-    [self wmf_updateShadowPathBasedOnBounds];
-}
-
-- (void)wmf_updateShadowPathBasedOnBounds {
-    self.layer.shadowPath = [UIBezierPath bezierPathWithRect:CGRectInset(self.bounds, -1.0, -1.0)].CGPath;
 }
 
 @end
