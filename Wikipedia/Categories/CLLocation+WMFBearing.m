5b918585bddfd59a004b2ba0220457dcf517c822
Brian Gerstle <brian.gerstle@gmail.com>
2015-09-08 21:30:56 -0400

    fix location bearing calculations
    C's math.h trig function parameters are in radians, not degrees. was
    also missing a component of the reference equation for unclear reasons.

--- Wikipedia/Categories/CLLocation+WMFBearing.m
+++ Wikipedia/Categories/CLLocation+WMFBearing.m
@@ -11,10 +11,15 @@
 @implementation CLLocation (WMFBearing)
 
 - (CLLocationDegrees)wmf_bearingToLocation:(CLLocation*)destination {
-    double dy = destination.coordinate.longitude - self.coordinate.longitude;
-    double y  = sin(dy) * cos(destination.coordinate.latitude);
-    double x  = cos(self.coordinate.latitude) * sin(destination.coordinate.latitude) - sin(self.coordinate.latitude) * cos(destination.coordinate.latitude) * cos(dy);
-    return atan2(y, x);
+    double const phiOrigin   = DEGREES_TO_RADIANS(self.coordinate.latitude),
+                 phiDest     = DEGREES_TO_RADIANS(destination.coordinate.latitude),
+                 deltaLambda = DEGREES_TO_RADIANS(destination.coordinate.longitude - self.coordinate.longitude),
+                 y           = sin(deltaLambda) * cos(phiDest),
+                 x           = cos(phiOrigin) * sin(phiDest) - sin(phiOrigin) * cos(phiDest) * cos(deltaLambda),
+    // bearing in radians in range [-180, 180]
+                 veBearingRadians = atan2(y, x);
+    // convert to degrees and put in compass range [0, 360]
+    return fmod(RADIANS_TO_DEGREES(veBearingRadians) + 360.0, 360.0);
 }
 
 - (CLLocationDegrees)wmf_bearingToLocation:(CLLocation*)location
