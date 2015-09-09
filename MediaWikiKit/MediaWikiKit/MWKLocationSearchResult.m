73c78b94e274c2cf5c5dfc9a14218a45b53df8bc
Brian Gerstle <brian.gerstle@gmail.com>
2015-09-08 21:32:06 -0400

    keypath cleanup & validation for MWKLocationSearchResult

--- MediaWikiKit/MediaWikiKit/MWKLocationSearchResult.m
+++ MediaWikiKit/MediaWikiKit/MWKLocationSearchResult.m
@@ -4,12 +4,15 @@
 @implementation MWKLocationSearchResult
 
 + (NSValueTransformer*)locationJSONTransformer {
-    return [MTLValueTransformer transformerUsingForwardBlock:^id (NSArray* value, BOOL* success, NSError* __autoreleasing* error) {
+    return [MTLValueTransformer transformerUsingForwardBlock:^id (NSArray* value,
+                                                                  BOOL* success,
+                                                                  NSError* __autoreleasing* error) {
         NSDictionary* coords = [value firstObject];
         NSNumber* lat = coords[@"lat"];
         NSNumber* lon = coords[@"lon"];
 
-        if (!lat || !lon) {
+        if (![lat isKindOfClass:[NSNumber class]] || ![lon isKindOfClass:[NSNumber class]]) {
+            WMFSafeAssign(success, NO);
             return nil;
         }
 
@@ -18,18 +21,28 @@
 }
 
 + (NSValueTransformer*)distanceFromQueryCoordinatesJSONTransformer {
-    return [MTLValueTransformer transformerUsingForwardBlock:^id (NSArray* value, BOOL* success, NSError* __autoreleasing* error) {
+    return [MTLValueTransformer transformerUsingForwardBlock:^id (NSArray* value,
+                                                                  BOOL* success,
+                                                                  NSError* __autoreleasing* error) {
         NSDictionary* coords = [value firstObject];
         NSNumber* distance = coords[@"dist"];
+        if (![distance isKindOfClass:[NSNumber class]]) {
+            WMFSafeAssign(success, NO);
+            return nil;
+        }
         return distance;
     }];
 }
 
 + (NSDictionary*)JSONKeyPathsByPropertyKey {
     NSMutableDictionary* mapping = [[super JSONKeyPathsByPropertyKey] mutableCopy];
+    /*
+       coordinates is an array of objects which have lat, lon, & dist fields. pick the fist one and set the corresponding
+       properties here.
+     */
     [mapping addEntriesFromDictionary:@{
-         @"location": @"coordinates",
-         @"distanceFromQueryCoordinates": @"coordinates",
+         WMF_SAFE_KEYPATH([MWKLocationSearchResult new], location): @"coordinates",
+         WMF_SAFE_KEYPATH([MWKLocationSearchResult new], distanceFromQueryCoordinates): @"coordinates",
      }];
 
     return mapping;
