976f66eaa3bd5ef84d8769fb18369f0af07d9e0e
Corey Floyd <coreyfloyd@gmail.com>
2015-09-23 14:42:56 -0400

    Add image snapshotting method

--- Wikipedia/UI-V5/UIView+WMFShapshotting.h
+++ Wikipedia/UI-V5/UIView+WMFShapshotting.h
@@ -3,6 +3,8 @@
 
 @interface UIView (WMFShapshotting)
 
+- (UIImage *)wmf_snapshotImage;
+
 - (UIView*)wmf_addSnapshotToView:(UIView*)containerView afterScreenUpdates:(BOOL)afterUpdates;
 - (UIView*)wmf_addResizableSnapshotToView:(UIView*)containerView fromRect:(CGRect)rect afterScreenUpdates:(BOOL)afterUpdates withCapInsets:(UIEdgeInsets)capInsets;
 
--- Wikipedia/UI-V5/UIView+WMFShapshotting.m
+++ Wikipedia/UI-V5/UIView+WMFShapshotting.m
@@ -3,6 +3,15 @@
 
 @implementation UIView (WMFShapshotting)
 
+- (UIImage *)wmf_snapshotImage
+{
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, YES, 0);
+    [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:YES];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
 - (UIView*)wmf_addSnapshotToView:(UIView*)containerView afterScreenUpdates:(BOOL)afterUpdates {
     UIView* snapshot = [self snapshotViewAfterScreenUpdates:afterUpdates];
     snapshot.frame = [containerView convertRect:self.frame fromView:self.superview];
