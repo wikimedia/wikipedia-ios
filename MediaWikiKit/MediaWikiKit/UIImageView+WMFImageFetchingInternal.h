9f018250ba406c521d49be7f7e283a3fa7922b4c
Corey Floyd <coreyfloyd@gmail.com>
2015-09-30 13:03:58 -0400

    Forgot to commit the header with the other renames (duh)

--- MediaWikiKit/MediaWikiKit/UIImageView+WMFImageFetchingInternal.h
+++ MediaWikiKit/MediaWikiKit/UIImageView+WMFImageFetchingInternal.h
@@ -3,16 +3,23 @@
 
 @class MWKImage;
 @class WMFImageController;
+@class WMFFaceDetectionCache;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImageView (WMFAssociatedObjects)
 
 /**
+ *   The cache used to hold any detected faces
+ *
+ */
++ (WMFFaceDetectionCache*)faceDetectionCache;
+
+/**
  *   The image URL associated with the receiver.
  *
  */
-@property (nonatomic, strong, nullable) NSURL* wmf_imageURL;
+@property (nonatomic, strong, nullable, setter=wmf_setImageURL:) NSURL* wmf_imageURL;
 
 /**
  *  The metadata associated with the receiver.
@@ -20,14 +27,14 @@ NS_ASSUME_NONNULL_BEGIN
  *  Used to ensure that images set on the receiver aren't associated with a URL for another metadata entity.
  *
  */
-@property (nonatomic, strong, nullable) MWKImage* wmf_imageMetadata;
+@property (nonatomic, strong, nullable, setter=wmf_setImageMetadata:) MWKImage* wmf_imageMetadata;
 
 /**
  *  The image controller used to fetch image data.
  *
  *  Used to cancel the previous fetch executed by the receiver. Default is [WMFImageController sharedInstance].
  */
-@property (nonatomic, weak, nullable) WMFImageController* wmf_imageController;
+@property (nonatomic, weak, nullable, setter=wmf_setImageController:) WMFImageController* wmf_imageController;
 
 @end
 
