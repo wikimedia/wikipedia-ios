be22efbf8ba418eef2cd5072c3de0fb464f9a148
Corey Floyd <coreyfloyd@gmail.com>
2015-10-08 12:13:06 -0400

    Update home section protocol to make the footer optional
    We don't display a footer on the new continue reading section

--- Wikipedia/UI-V5/WMFHomeSectionController.h
+++ Wikipedia/UI-V5/WMFHomeSectionController.h
@@ -15,8 +15,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (NSAttributedString*)headerText;
 
-- (NSString*)footerText;
-
 - (void)registerCellsInCollectionView:(UICollectionView*)collectionView;
 
 - (UICollectionViewCell*)dequeueCellForCollectionView:(UICollectionView*)collectionView atIndexPath:(NSIndexPath*)indexPath;
@@ -27,12 +25,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (MWKTitle*)titleForItemAtIndex:(NSUInteger)index;
 
+@optional
+
+/**
+ *  @return Return the "More" footer text that prompts a user to get more items from a section.
+ *  Not implementing this method means that no footer will be displayed
+ */
+- (NSString*)footerText;
+
 /**
  *  @return A data source which will provide a larger list of items from this section.
  */
 - (SSArrayDataSource<WMFArticleListDataSource>*)extendedListDataSource;
-
-@optional
 
 - (BOOL)shouldSelectItemAtIndex:(NSUInteger)index;
 
