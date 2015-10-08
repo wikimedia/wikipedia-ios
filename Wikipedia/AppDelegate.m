73d4e3212630c7aa11d7e4704fef84b99a5c4307
Corey Floyd <coreyfloyd@gmail.com>
2015-10-08 15:58:51 -0400

    Double whoops

--- Wikipedia/AppDelegate.m
+++ Wikipedia/AppDelegate.m
@@ -69,7 +69,6 @@ static NSString* const WMFPiwikSiteID    = @"4";
 - (void)applicationWillResignActive:(UIApplication*)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [self.appViewController pauseApp];
     [[NSUserDefaults standardUserDefaults] wmf_setAppResignActiveDate:[NSDate date]];
 }
 
@@ -86,6 +85,7 @@ static NSString* const WMFPiwikSiteID    = @"4";
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 
     [[NSUserDefaults standardUserDefaults] wmf_setAppBecomeActiveDate:[NSDate date]];
+    [self.appViewController resumeApp];
 }
 
 - (void)applicationWillTerminate:(UIApplication*)application {
