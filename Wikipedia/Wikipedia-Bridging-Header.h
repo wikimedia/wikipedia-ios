c1f4b4bc13c5c63f5bb17983944303092f24062b
Brian Gerstle <brian.gerstle@gmail.com>
2015-10-09 11:35:27 -0400

    hygiene: organize bridging header

--- Wikipedia/Wikipedia-Bridging-Header.h
+++ Wikipedia/Wikipedia-Bridging-Header.h
@@ -1,26 +1,27 @@
 #import "Global.h"
 
-#import "WikipediaAppUtils.h"
+// Model
+#import "MediaWikiKit.h"
 
+// Utilities
+#import "WikipediaAppUtils.h"
 #import "WMFBlockDefinitions.h"
 #import "WMFGCDHelpers.h"
 
 #import "NSURL+Extras.h"
 #import "NSString+Extras.h"
 #import "WMFRangeUtils.h"
-
-#import "MediaWikiKit.h"
-
 #import "UIImage+ColorMask.h"
 #import "UIView+WMFDefaultNib.h"
 #import "UIColor+WMFStyle.h"
 #import "UIFont+WMFStyle.h"
 
+// Diagnostics
 #import "ToCInteractionFunnel.h"
 
+// ObjC Frameworks & Framework Extensions
 #import <SDWebImage/SDWebImageManager.h>
 #import <SDWebImage/UIImage+MultiFormat.h>
 #import "SDWebImageManager+WMFCacheRemoval.h"
 #import "SDImageCache+WMFPersistentCache.h"
-
 #import <KVOController/FBKVOController.h>
