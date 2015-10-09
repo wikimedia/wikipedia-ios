d98b51e244ff122aa3252453fd406b0ed6715e9e
Brian Gerstle <brian.gerstle@gmail.com>
2015-10-09 13:57:52 -0400

    uncrustify

--- WikipediaUnitTests/Utilities/WMFImageController+Testing.m
+++ WikipediaUnitTests/Utilities/WMFImageController+Testing.m
@@ -12,7 +12,7 @@
 @implementation WMFImageController (Testing)
 
 + (instancetype)temporaryController {
-    SDImageCache* tempCache = [[SDImageCache alloc] initWithNamespace:@"temp" inDirectory:WMFRandomTemporaryPath()];
+    SDImageCache* tempCache    = [[SDImageCache alloc] initWithNamespace:@"temp" inDirectory:WMFRandomTemporaryPath()];
     SDWebImageManager* manager = [[SDWebImageManager alloc] initWithDownloader:[[SDWebImageDownloader alloc] init]
                                                                          cache:tempCache];
     return [[WMFImageController alloc] initWithManager:manager namespace:@"temp"];
