1bd47574a623ec2b8500cbd50e67b84ecb095e77
Brian Gerstle <brian.gerstle@gmail.com>
2015-10-15 07:16:23 -0400

    add Apple System DDLogger for debugging on devices

---
+++ Wikipedia/Categories/DDLog+WMFLogger.m
@@ -0,0 +1,32 @@
+//
+//  DDLog+WMFLogger.m
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 10/15/15.
+//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "DDLog+WMFLogger.h"
+#import "WMFLogFormatter.h"
+
+@implementation DDLog (WMFLogger)
+
++ (void)load {
+    [self wmf_addLoggersForCurrentConfiguration];
+}
+
++ (void)wmf_addLoggersForCurrentConfiguration {
+    // only add TTY in debug mode
+#if DEBUG
+    [self wmf_addWMFFormattedLogger:[DDTTYLogger sharedInstance]];
+#endif
+    // always add ASLLogger
+    [self wmf_addWMFFormattedLogger:[DDASLLogger sharedInstance]];
+}
+
++ (void)wmf_addWMFFormattedLogger:(id<DDLogger>)logger {
+    [logger setLogFormatter:[WMFLogFormatter new]];
+    [DDLog addLogger:logger];
+}
+
+@end
