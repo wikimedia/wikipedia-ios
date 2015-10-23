eeb1f484b2f20c327d98296e00fe51402d91e1d8
Brian Gerstle <brian.gerstle@gmail.com>
2015-10-23 12:43:22 -0400

    implement detection of current article "view"
    first try to detect webview section, then fall back to footer (of which
    there's only one at the moment)

--- Wikipedia/View Controllers/WebView/WebViewController.h
+++ Wikipedia/View Controllers/WebView/WebViewController.h
@@ -8,28 +8,30 @@
 
 @protocol WMFWebViewControllerDelegate;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface WebViewController : PullToRefreshViewController
     <UIWebViewDelegate,
      UIScrollViewDelegate,
      UIGestureRecognizerDelegate,
      UIAlertViewDelegate>
 
-@property (nonatomic, strong) MWKArticle* article;
+@property (nonatomic, strong, nullable) MWKArticle* article;
 
-@property (nonatomic, weak) id<WMFWebViewControllerDelegate> delegate;
+@property (nonatomic, weak, nullable) id<WMFWebViewControllerDelegate> delegate;
 
-@property (nonatomic, strong, readonly) UIWebView* webView;
+@property (nonatomic, strong, nullable, readonly) UIWebView* webView;
 
 /**
  * Currently-selected text in the webview, if there is any.
  * @return The selection if it's longer than `kMinimumTextSelectionLength`, otherwise an empty string.
  */
-@property (nonatomic, strong, readonly) NSString* selectedText;
+@property (nonatomic, strong, nonnull, readonly) NSString* selectedText;
 
 - (void)scrollToFragment:(NSString*)fragment;
 
 - (void)scrollToSection:(MWKSection*)section;
-- (MWKSection*)currentVisibleSection;
+- (nullable MWKSection*)currentVisibleSection;
 
 - (void)scrollToVerticalOffset:(CGFloat)offset;
 - (CGFloat)currentVerticalOffset;
@@ -39,7 +41,7 @@
 /**
  *  An array of view controllers which will be displayed above the receiver's @c UIWebView content from top to bottom.
  */
-@property (nonatomic, strong) UIViewController* headerViewController;
+@property (nonatomic, strong, nullable) UIViewController* headerViewController;
 
 @property (nonatomic, strong) NSArray<UIViewController*>* footerViewControllers;
 
@@ -60,3 +62,4 @@
 
 @end
 
+NS_ASSUME_NONNULL_END
