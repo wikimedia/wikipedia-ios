93a41216a55a92c2fd312e2edcd42f38ac1686cc
Corey Floyd <coreyfloyd@gmail.com>
2015-10-23 16:36:07 -0400

    Adding refresh button, yo.

--- Wikipedia/UI-V5/WMFHomeSectionHeader.h
+++ Wikipedia/UI-V5/WMFHomeSectionHeader.h
@@ -5,5 +5,8 @@
 
 @property (strong, nonatomic) IBOutlet UIImageView* icon;
 @property (strong, nonatomic) IBOutlet UITextView* titleView;
+@property (strong, nonatomic) IBOutlet UIButton* rightButton;
+
+@property (assign, nonatomic) BOOL rightButtonEnabled;
 
 @end
