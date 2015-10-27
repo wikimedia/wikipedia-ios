1928db2b0f0b2fd854a7ca826e588624bb956d1e
Monte Hurd <montehurd@Montes-MacBook-Pro.local>
2015-10-27 15:49:30 -0700

    Update lang picker and settings to use new close icon.

--- Wikipedia/UI-V5/UIButton+WMFButton.m
+++ Wikipedia/UI-V5/UIButton+WMFButton.m
@@ -69,10 +69,7 @@ NS_ASSUME_NONNULL_BEGIN
             [self setImage:[UIImage imageNamed:@"toc"] forState:UIControlStateNormal];
             break;
         case WMFButtonTypeX:
-            [self wmf_setGlyphTitle:WMF_GLYPH_X color:nil forState:UIControlStateNormal];
-            break;
-        case WMFButtonTypeXWhite:
-            [self wmf_setGlyphTitle:WMF_GLYPH_X color:[UIColor whiteColor] forState:UIControlStateNormal];
+            [self setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
             break;
         case WMFButtonTypeTrash:
             [self wmf_setGlyphTitle:WMF_GLYPH_TRASH color:nil forState:UIControlStateNormal];
@@ -142,7 +139,6 @@ NS_ASSUME_NONNULL_BEGIN
     switch (type) {
         case WMFButtonTypeW:
         case WMFButtonTypeX:
-        case WMFButtonTypeXWhite:
         case WMFButtonTypeTranslate:
         case WMFButtonTypeMagnify:
         case WMFButtonTypeReload:
