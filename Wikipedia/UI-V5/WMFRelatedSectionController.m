3ad40121b866e6de41e0208e8faab2535e26a163
Corey Floyd <coreyfloyd@gmail.com>
2015-10-28 12:42:06 -0400

    Updating the search fetcher to be stateless (the purpose of the ticket)
    Some minor cleanup elsewhere

--- Wikipedia/UI-V5/WMFRelatedSectionController.m
+++ Wikipedia/UI-V5/WMFRelatedSectionController.m
@@ -83,7 +83,7 @@ static NSUInteger const WMFRelatedSectionMaxResults      = 3;
 }
 
 - (NSArray*)items {
-    return [self.relatedTitleDataSource.relatedTitleResults
+    return [self.relatedTitleDataSource.relatedSearchResults.results
             wmf_safeSubarrayWithRange:NSMakeRange(0, WMFRelatedSectionMaxResults)];
 }
 
@@ -134,7 +134,7 @@ static NSUInteger const WMFRelatedSectionMaxResults      = 3;
 }
 
 - (SSArrayDataSource<WMFArticleListDataSource>*)extendedListDataSource {
-    if (!self.relatedSearchFetcher.isFetching && !self.relatedTitleDataSource.relatedTitleResults) {
+    if (!self.relatedSearchFetcher.isFetching && !self.relatedTitleDataSource.relatedSearchResults) {
         [self.relatedTitleDataSource fetch];
     }
     return self.relatedTitleDataSource;
@@ -156,7 +156,7 @@ static NSUInteger const WMFRelatedSectionMaxResults      = 3;
     }
     @weakify(self);
     [self.relatedTitleDataSource fetch]
-    .then(^(WMFRelatedSearchResults* _){
+    .then(^(WMFRelatedSearchResults* results){
         @strongify(self);
         [self.delegate controller:self didSetItems:self.items];
     })
