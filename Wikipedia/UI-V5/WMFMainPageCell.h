f2435e07ded6fc4a25121ca3b815030bf0b06f4e
Corey Floyd <coreyfloyd@gmail.com>
2015-11-02 14:37:08 -0500

    Adding cell for main pages

---
+++ Wikipedia/UI-V5/WMFMainPageCell.h
@@ -0,0 +1,15 @@
+//
+//  WMFMainPageCell.h
+//  Wikipedia
+//
+//  Created by Corey Floyd on 11/2/15.
+//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
+//
+
+@import SSDataSources;
+
+@interface WMFMainPageCell : SSBaseCollectionCell
+
+@property (nonatomic, strong) IBOutlet UILabel* mainPageTitle;
+
+@end
