21ee1acd086e9c13d0fe94b3832c8c21ecc4b6e8
Corey Floyd <coreyfloyd@gmail.com>
2015-11-10 16:30:41 -0500

    Update article list Table view cell to highlight substrings so it can be used for search

--- Wikipedia/UI-V5/WMFArticleListTableViewCell.h
+++ Wikipedia/UI-V5/WMFArticleListTableViewCell.h
@@ -11,12 +11,19 @@
 @property (strong, nonatomic) IBOutlet UILabel* titleLabel;
 
 /**
+ *  Set the receivers @c title, optionally highlighting the a part of the title
+ *
+ *  @param text          The text of the title
+ *  @param highlightText The part of the title to highlight
+ */
+- (void)updateTitleLabelWithText:(NSString*)text highlightingText:(NSString*)highlightText;
+
+/**
  *  Label used to display the receiver's @c description.
  *
  *  Configure as needed in Interface Builder or during initialization when subclassing.
  */
 @property (nonatomic, strong) IBOutlet UILabel* descriptionLabel;
-
 
 /**
  *  The view used to display the receiver's @c image.
