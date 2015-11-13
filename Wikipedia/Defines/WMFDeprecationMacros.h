ac5737eb8248dc9361815ac6d055aa5c728443d6
Brian Gerstle <brian.gerstle@gmail.com>
2015-11-13 17:53:00 -0500

    toggle deprecation warnings based on separate compilation flag
    if WMF_DEPRECATE_BELOW_DEPLOY_TARGET is 1, then any uses of methods
    marked with that macro trigger deprecation warnings

--- Wikipedia/Defines/WMFDeprecationMacros.h
+++ Wikipedia/Defines/WMFDeprecationMacros.h
@@ -9,7 +9,11 @@
 #ifndef WMFDeprecationMacros_h
 #define WMFDeprecationMacros_h
 
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_9_0
+#ifndef WMF_DEPRECATE_BELOW_DEPLOY_TARGET
+#define WMF_DEPRECATE_BELOW_DEPLOY_TARGET 0
+#endif
+
+#if WMF_DEPRECATE_BELOW_DEPLOY_TARGET && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_9_0
 #define WMF_DEPRECATED_WHEN_DEPLOY_AT_LEAST_9 __deprecated_msg("This API was only necessary when deployment target was < 9. Please remove any code being executed when OS is less than 9.")
 #else
 #define WMF_DEPRECATED_WHEN_DEPLOY_AT_LEAST_9
