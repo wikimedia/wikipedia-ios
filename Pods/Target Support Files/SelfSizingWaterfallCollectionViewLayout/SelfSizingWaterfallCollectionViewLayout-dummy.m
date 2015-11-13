9022dbc66874e374504f76db1f23706a111bc9fd
Corey Floyd <coreyfloyd@gmail.com>
2015-11-13 17:09:23 -0500

    Remove self sizing waterfall pod

--- Pods/Target Support Files/SelfSizingWaterfallCollectionViewLayout/SelfSizingWaterfallCollectionViewLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SelfSizingWaterfallCollectionViewLayout : NSObject
-@end
-@implementation PodsDummy_SelfSizingWaterfallCollectionViewLayout
-@end
