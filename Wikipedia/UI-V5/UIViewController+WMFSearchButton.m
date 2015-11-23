0ad809ca51e8ff9c0ee41a9e46d5f3d3d529e37e
Corey Floyd <coreyfloyd@gmail.com>
2015-11-23 14:28:53 -0500

    Preserve search results during a session
    - Keep search VC as a static ivar
    - Remove it when the app backgrounds
    - Remove it if we get a memory warning
    - Don't restore if the search site has changed

--- Wikipedia/UI-V5/UIViewController+WMFSearchButton.m
+++ Wikipedia/UI-V5/UIViewController+WMFSearchButton.m
@@ -11,10 +11,26 @@
 #import <BlocksKit/UIBarButtonItem+BlocksKit.h>
 #import "SessionSingleton.h"
 #import "UIViewController+WMFArticlePresentation.h"
+#import "MWKSite.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation UIViewController (WMFSearchButton)
+
+static WMFSearchViewController * _sharedSearchViewController = nil;
+
++ (void)load {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wmfSearchButton_applicationDidEnterBackgroundWithNotification:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wmfSearchButton_applicationDidReceiveMemoryWarningWithNotification:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+}
+
++ (void)wmfSearchButton_applicationDidEnterBackgroundWithNotification:(NSNotification*)note {
+    _sharedSearchViewController = nil;
+}
+
++ (void)wmfSearchButton_applicationDidReceiveMemoryWarningWithNotification:(NSNotification*)note {
+    _sharedSearchViewController = nil;
+}
 
 - (UIBarButtonItem*)wmf_searchBarButtonItemWithDelegate:(UIViewController<WMFSearchPresentationDelegate>*)delegate {
     @weakify(self);
@@ -36,11 +52,14 @@ NS_ASSUME_NONNULL_BEGIN
             searchSite = [[SessionSingleton sharedInstance] searchSite];
         }
 
-        WMFSearchViewController* searchVC =
-            [WMFSearchViewController searchViewControllerWithSite:searchSite
-                                                        dataStore:[delegate searchDataStore]];
-        searchVC.searchResultDelegate = delegate;
-        [self presentViewController:searchVC animated:YES completion:nil];
+        if (![searchSite isEqual:_sharedSearchViewController.searchSite]) {
+            WMFSearchViewController* searchVC =
+                [WMFSearchViewController searchViewControllerWithSite:searchSite
+                                                            dataStore:[delegate searchDataStore]];
+            searchVC.searchResultDelegate = delegate;
+            _sharedSearchViewController = searchVC;
+        }
+        [self presentViewController:_sharedSearchViewController animated:YES completion:nil];
     }];
 }
 
