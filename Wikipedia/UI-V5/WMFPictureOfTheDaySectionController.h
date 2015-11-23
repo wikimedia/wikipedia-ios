8872289cc072accedae4492fea0d87f5feb4f6a1
Brian Gerstle <brian.gerstle@gmail.com>
2015-11-23 15:32:36 -0500

    add POTD section controller & cell

---
+++ Wikipedia/UI-V5/WMFPictureOfTheDaySectionController.h
@@ -0,0 +1,13 @@
+//
+//  WMFPictureOfTheDaySectionController.h
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 11/23/15.
+//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "WMFHomeSectionController.h"
+
+@interface WMFPictureOfTheDaySectionController : NSObject<WMFHomeSectionController>
+
+@end
