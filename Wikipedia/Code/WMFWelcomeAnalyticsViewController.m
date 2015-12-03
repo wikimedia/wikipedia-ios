0e27d8843e4586693ef5be506ca7b56a3469d2ff
Corey Floyd <coreyfloyd@gmail.com>
2015-12-02 20:35:34 -0500

    Complete usage report logic
    Updating switch state, etc…

--- Wikipedia/Code/WMFWelcomeAnalyticsViewController.m
+++ Wikipedia/Code/WMFWelcomeAnalyticsViewController.m
@@ -21,17 +21,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAlwaysAsk;
+    //Set state of the toggle. Also make sure crash manager setting is in sync with this setting - likely to happen on first launch or for previous users.
+    if ([[NSUserDefaults standardUserDefaults] wmf_sendUsageReports]) {
+        self.toggle.on                                                           = YES;
+        [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
+    } else {
+        self.toggle.on                                                           = NO;
+        [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAlwaysAsk;
+    }
 }
 
 - (IBAction)toggleAnalytics:(id)sender {
     if ([(UISwitch*)sender isOn]) {
         [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
-        [[NSUserDefaults standardUserDefaults] setObject:@YES forKey:@"SendUsageReports"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
+        [[NSUserDefaults standardUserDefaults] wmf_setSendUsageReports:YES];
     } else {
         [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAlwaysAsk;
-        [[NSUserDefaults standardUserDefaults] setObject:@NO forKey:@"SendUsageReports"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
+        [[NSUserDefaults standardUserDefaults] wmf_setSendUsageReports:NO];
     }
 }
 
