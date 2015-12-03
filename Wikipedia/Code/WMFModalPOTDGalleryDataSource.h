7648cd4d52500c0d336bd655b748dd510aacdfac
Brian Gerstle <brian.gerstle@gmail.com>
2015-12-03 10:13:20 -0500

    document gallery classes
    plus move around & designate some initializers

--- Wikipedia/Code/WMFModalPOTDGalleryDataSource.h
+++ Wikipedia/Code/WMFModalPOTDGalleryDataSource.h
@@ -11,9 +11,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ *  Data source of the Picture of the Day gallery.
+ *
+ *  Fetches additional metadata & high-resolution images from Wikimedia Commons' picture of the day.
+ */
 @interface WMFModalPOTDGalleryDataSource : SSArrayDataSource
     <WMFModalImageGalleryDataSource>
 
+/**
+ *  Initialize the gallery with the info retrieved for today's picture of the day.
+ *
+ *  @note Currently hard-coded to today since the app doesn't support having multiple POTD in the Home view.
+ *        Once this is supported, we will need to pass one or more info objects (and dates) and specify the info to
+ *        display (similar to <code>-[WMFModalArticleImageGallery setVisibleImage:animated:]</code>).
+ *
+ *  @param info Today's info, which will be displayed when the gallery is first presented while it fetches more metadata
+ *              and the other days.
+ *
+ *  @return A new data source which is populated with today's and the previous 15 days' picture of the day.
+ */
 - (instancetype)initWithTodaysInfo:(MWKImageInfo*)info NS_DESIGNATED_INITIALIZER;
 
 @end
--- Wikipedia/Code/WMFModalPOTDGalleryViewController.h
+++ Wikipedia/Code/WMFModalPOTDGalleryViewController.h
@@ -8,9 +8,26 @@
 
 #import "WMFModalImageGalleryViewController.h"
 
+/**
+ *  Dispays a modal gallery of images & metadata for Wikimedia Commons pictures of the day.
+ */
 @interface WMFModalPOTDGalleryViewController : WMFModalImageGalleryViewController
 
-- (instancetype)initWithTodaysInfo:(MWKImageInfo*)info;
+/**
+ *  Initalize a gallery with pre-fetched info.
+ *
+ *  @note This class is meant to be shown after today's (or another day's) POTD has already been retrieved for the Home
+ *  view.
+ *
+ *  @param info Partial info for today's POTD (lower-res placeholder image & sparse metadata).
+ *
+ *  @return A new gallery for displaying pictures of the day.
+ */
+- (instancetype)initWithTodaysInfo:(MWKImageInfo*)info NS_DESIGNATED_INITIALIZER;
+
+///
+/// @name Unsupported Initializers
+///
 
 - (instancetype)init NS_UNAVAILABLE;
 
