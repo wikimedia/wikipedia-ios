c76fe8f471f1c847486da5f262190d2256139c02
Brian Gerstle <brian.gerstle@gmail.com>
2015-12-04 12:53:46 -0500

    only update nearby title list when it is empty
    should continue to update location, but not fetch new titles

--- Wikipedia/Code/WMFNearbyViewModel.m
+++ Wikipedia/Code/WMFNearbyViewModel.m
@@ -141,6 +141,10 @@
         DDLogVerbose(@"Not fetching nearby titles for %@ since it is too close to previously fetched location: %@.",
                      location, self.locationSearchResults.location);
         return;
+    } else if ([self.delegate respondsToSelector:@selector(nearbyViewModel:shouldFetchTitlesForLocation:)]
+               && ![self.delegate nearbyViewModel:self shouldFetchTitlesForLocation:location]) {
+        DDLogInfo(@"Skipping title update for new location %@", location);
+        return;
     }
 
     [self.lastFetch cancel];
