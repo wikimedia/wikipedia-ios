9e629fcff35b4ab3abf6cb054d17005b1a0794fd
Corey Floyd <coreyfloyd@gmail.com>
2015-12-07 15:38:07 -0500

    Adding TSMessages Pod for banner alerts

---
+++ Pods/TSMessages/Pod/Classes/TSBlurView.h
@@ -0,0 +1,15 @@
+//
+//  TSBlurView.h
+//  Pods
+//
+//  Created by Felix Krause on 20.08.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSBlurView : UIView
+
+@property (nonatomic, strong) UIColor *blurTintColor;
+
+@end
