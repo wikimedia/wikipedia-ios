681c56d1070ed356a225dfc6297e30d3e113dfac
Brian Gerstle <brian.gerstle@gmail.com>
2015-12-08 20:24:42 -0500

    refactor baseGalleryVC tests to run both before & after viewDidLoad
    tests case orginally causing the crash: setting data source on iOS 8 w/
    RTL language

--- WikipediaUnitTests/Code/WMFBaseImageGalleryViewController.m
+++ WikipediaUnitTests/Code/WMFBaseImageGalleryViewController.m
@@ -10,6 +10,7 @@
 @import Nimble;
 
 #import "WMFBaseImageGalleryViewController_Testing.h"
+#import "WMFCollectionViewPageLayout.h"
 #import <SSDataSources/SSDataSources.h>
 
 @interface WMFDummyGalleryDataSource : SSArrayDataSource<WMFImageGalleryDataSource>
@@ -24,57 +25,92 @@
 
 @end
 
-
 QuickSpecBegin(WMFBaseImageGalleryViewControllerTests)
 
-static WMFBaseImageGalleryViewController * baseGalleryVC;
-static WMFDummyGalleryDataSource* dummyDataSource;
-
-
-beforeEach(^{
-    baseGalleryVC = [[WMFBaseImageGalleryViewController alloc] init];
-});
-
-afterEach(^{
-    dummyDataSource = nil;
-});
-
 describe(@"setDataSource:", ^{
+    __block WMFBaseImageGalleryViewController * baseGalleryVC;
+    __block WMFDummyGalleryDataSource* dummyDataSource;
+
     beforeEach(^{
+        baseGalleryVC = [[WMFBaseImageGalleryViewController alloc] initWithCollectionViewLayout:[WMFCollectionViewPageLayout new]];
         dummyDataSource = [[WMFDummyGalleryDataSource alloc] initWithItems:@[@0, @1, @2]];
     });
 
-    afterEach(^{
-        expect(@(baseGalleryVC.isViewLoaded)).to(beFalse());
+    context(@"before its view is loaded",^ {
+        afterEach(^{
+            expect(@(baseGalleryVC.isViewLoaded))
+            .toWithDescription(beFalse(), @"Setting the data source shouldn't cause the view to load.");
+        });
+
+        itBehavesLike(@"an RTL compliant gallery", ^{
+            return @{ @"gallery": baseGalleryVC,
+                      @"dataSource": dummyDataSource };
+        });
     });
 
-    it(@"should keep current page at default (0) in modern LTR environments", ^{
-        [baseGalleryVC setDataSource:dummyDataSource
-                shouldSetCurrentPage:NO
-                     layoutDirection:UIUserInterfaceLayoutDirectionLeftToRight];
-        expect(@(baseGalleryVC.currentPage)).to(equal(@0));
-    });
+    context(@"after its view is loaded", ^{
+        beforeEach(^{
+            [baseGalleryVC view];
+        });
 
-    it(@"should keep current page at default (0) in modern RTL environments", ^{
-        [baseGalleryVC setDataSource:dummyDataSource
-                shouldSetCurrentPage:NO
-                     layoutDirection:UIUserInterfaceLayoutDirectionRightToLeft];
-        expect(@(baseGalleryVC.currentPage)).to(equal(@0));
-    });
-
-    it(@"should keep current page at default (0) in legacy LTR environments", ^{
-        [baseGalleryVC setDataSource:dummyDataSource
-                shouldSetCurrentPage:YES
-                     layoutDirection:UIUserInterfaceLayoutDirectionLeftToRight];
-        expect(@(baseGalleryVC.currentPage)).to(equal(@0));
-    });
-
-    it(@"should set current page to last element in legacy RTL environments", ^{
-        [baseGalleryVC setDataSource:dummyDataSource
-                shouldSetCurrentPage:YES
-                     layoutDirection:UIUserInterfaceLayoutDirectionRightToLeft];
-        expect(@(baseGalleryVC.currentPage)).to(equal(@(dummyDataSource.numberOfItems - 1)));
+        itBehavesLike(@"an RTL compliant gallery", ^{
+            return @{ @"gallery": baseGalleryVC,
+                      @"dataSource": dummyDataSource };
+        });
     });
 });
 
 QuickSpecEnd
+
+QuickConfigurationBegin(WMFBaseImageGalleryViewControllerTestsConfig)
+
++ (void)configure:(Configuration*)configuration {
+    sharedExamples(@"an RTL compliant gallery", ^(QCKDSLSharedExampleContext exampleContextProvider) {
+        __block WMFBaseImageGalleryViewController* gallery;
+        __block SSBaseDataSource<WMFImageGalleryDataSource>* dataSource;
+
+        beforeEach(^{
+            NSDictionary* exampleContext = exampleContextProvider();
+            gallery = exampleContext[@"gallery"];
+            dataSource = exampleContext[@"dataSource"];
+        });
+
+        it(@"should keep current page at default (0) in modern LTR environments", ^{
+            [gallery setDataSource:dataSource
+              shouldSetCurrentPage:NO
+                   layoutDirection:UIUserInterfaceLayoutDirectionLeftToRight];
+            expect(@(gallery.currentPage)).to(equal(@0));
+        });
+
+        it(@"should keep current page at default (0) in modern RTL environments", ^{
+            NSDictionary* exampleContext = exampleContextProvider();
+            WMFBaseImageGalleryViewController* gallery = exampleContext[@"gallery"];
+            SSBaseDataSource<WMFImageGalleryDataSource>* dataSource = exampleContext[@"dataSource"];
+
+            [gallery setDataSource:dataSource
+              shouldSetCurrentPage:NO
+                   layoutDirection:UIUserInterfaceLayoutDirectionRightToLeft];
+            expect(@(gallery.currentPage)).to(equal(@0));
+        });
+
+        it(@"should keep current page at default (0) in legacy LTR environments", ^{
+            NSDictionary* exampleContext = exampleContextProvider();
+            WMFBaseImageGalleryViewController* gallery = exampleContext[@"gallery"];
+            SSBaseDataSource<WMFImageGalleryDataSource>* dataSource = exampleContext[@"dataSource"];
+
+            [gallery setDataSource:dataSource
+              shouldSetCurrentPage:YES
+                   layoutDirection:UIUserInterfaceLayoutDirectionLeftToRight];
+            expect(@(gallery.currentPage)).to(equal(@0));
+        });
+
+        it(@"should set current page to last element in legacy RTL environments", ^{
+            [gallery setDataSource:dataSource
+              shouldSetCurrentPage:YES
+                   layoutDirection:UIUserInterfaceLayoutDirectionRightToLeft];
+            expect(@(gallery.currentPage)).to(equal(@(dataSource.numberOfItems - 1)));
+        });
+    });
+}
+
+QuickConfigurationEnd
