2d1976d72a7c974de40d5e6abdf391da2f41a79d
Corey Floyd <coreyfloyd@gmail.com>
2015-12-10 10:38:40 -0500

    Refactor Alert logic
    Removed the WMFAlert in favor of just passing the arguments
    Added NSError category for the error message with localization

--- Wikipedia/Code/WMFAlertManager.swift
+++ Wikipedia/Code/WMFAlertManager.swift
@@ -2,117 +2,18 @@
 import UIKit
 import TSMessages
 
-@objc public enum WMFAlertType: Int {
+
+extension NSError {
     
-    case Error
-    case StaticError
-    case ArticleLanguageDownload
-    case NoSearchResults
-    case TestError
-    case TestWarning
-    case TestSuccess
-    case TestMessage
-    
+    public func alertMessage() -> String {
+        if(self.wmf_isNetworkConnectionError()){
+            return "The network is unreachable"
+        }else{
+            return self.localizedDescription
+        }
+    }
 }
 
-public class WMFAlert: NSObject {
-
-    public let type: WMFAlertType
-    public var error: NSError?
-
-    init(error: NSError){
-        self.error = error
-        self.type = .Error
-        super.init()
-    }
-
-    init(staticError: NSError){
-        self.error = staticError
-        self.type = .StaticError
-        super.init()
-    }
-
-    init(type: WMFAlertType){
-        self.type = type
-        super.init()
-    }
-    
-    public var messageTitle: String {
-        switch self.type {
-        case .Error:
-            if let error = self.error {
-                if(self.error!.wmf_isNetworkConnectionError()){
-                    return "The network is unreachable"
-                }else{
-                    return error.localizedDescription
-                }
-            }else{
-                return "Error"
-            }
-        case .StaticError:
-            if let error = self.error {
-                if(self.error!.wmf_isNetworkConnectionError()){
-                    return "The network is unreachable"
-                }else{
-                    return error.localizedDescription
-                }
-            }else{
-                return "Error"
-            }
-        case .ArticleLanguageDownload:
-            return localizedStringForKeyFallingBackOnEnglish("article-languages-downloading")
-        case .NoSearchResults:
-            return localizedStringForKeyFallingBackOnEnglish("search-no-matches")
-        case .TestError:
-            return "There was an error"
-        case .TestWarning:
-            return "You have been warned"
-        case .TestSuccess:
-            return "You are successful"
-        case .TestMessage:
-            return "You have been notified"
-        }
-    }
-
-    public var messageSubtitle: String? {
-        return nil
-    }
-    
-    public var messageType: TSMessageNotificationType {
-        switch self.type {
-        case .Error:
-            return .Error
-        case .TestError:
-            return .Error
-        case .TestWarning:
-            return .Warning
-        case .TestSuccess:
-            return .Success
-        default:
-            return .Message
-        }
-    }
-    
-    public var messageDuration: NSTimeInterval {
-        switch self.type {
-        case .ArticleLanguageDownload:
-            return -1
-        case .StaticError:
-            return -1
-        default:
-            return 2
-        }
-    }
-
-    public var messageUserDismissable: Bool {
-        return true
-    }
-
-    public var messageButtonDismisses: Bool {
-        return true
-    }
-
-}
 
 public class WMFAlertManager: NSObject, TSMessageViewProtocol {
     
@@ -122,21 +23,74 @@ public class WMFAlertManager: NSObject, TSMessageViewProtocol {
         TSMessage.addCustomDesignFromFileWithName("AlertDesign.json")
         super.init()
     }
-    
-    public func showAlert(alert: WMFAlert, tapCallBack: dispatch_block_t?) {
+   
+    public func showAlert(message: String, sticky:Bool, tapCallBack: dispatch_block_t?) {
         
         TSMessage.showNotificationInViewController(TSMessage.defaultViewController(),
-            title: alert.messageTitle,
-            subtitle: alert.messageSubtitle,
+            title: message,
+            subtitle: nil,
             image: nil,
-            type: alert.messageType,
-            duration: alert.messageDuration,
+            type: .Message,
+            duration: sticky ? -1 : 2,
             callback: tapCallBack,
             buttonTitle: nil,
-            buttonCallback: alert.messageButtonDismisses ? {} : nil,
+            buttonCallback: {},
             atPosition: .Top,
-            canBeDismissedByUser: alert.messageUserDismissable)
+            canBeDismissedByUser: true)
         
+    }
+
+    public func showSuccessAlert(message: String, sticky:Bool, tapCallBack: dispatch_block_t?) {
+        
+        TSMessage.showNotificationInViewController(TSMessage.defaultViewController(),
+            title: message,
+            subtitle: nil,
+            image: nil,
+            type: .Success,
+            duration: sticky ? -1 : 2,
+            callback: tapCallBack,
+            buttonTitle: nil,
+            buttonCallback: {},
+            atPosition: .Top,
+            canBeDismissedByUser: true)
+        
+    }
+
+    public func showWarningAlert(message: String, sticky:Bool, tapCallBack: dispatch_block_t?) {
+        
+        TSMessage.showNotificationInViewController(TSMessage.defaultViewController(),
+            title: message,
+            subtitle: nil,
+            image: nil,
+            type: .Warning,
+            duration: sticky ? -1 : 2,
+            callback: tapCallBack,
+            buttonTitle: nil,
+            buttonCallback: {},
+            atPosition: .Top,
+            canBeDismissedByUser: true)
+        
+    }
+
+    public func showErrorAlert(error: NSError, sticky:Bool, tapCallBack: dispatch_block_t?) {
+        
+        TSMessage.showNotificationInViewController(TSMessage.defaultViewController(),
+            title: error.alertMessage(),
+            subtitle: nil,
+            image: nil,
+            type: .Error,
+            duration: sticky ? -1 : 2,
+            callback: tapCallBack,
+            buttonTitle: nil,
+            buttonCallback: {},
+            atPosition: .Top,
+            canBeDismissedByUser: true)
+        
+    }
+    
+    public func hideAlert() {
+        
+        TSMessage.dismissActiveNotification()
     }
     
     public func customizeMessageView(messageView: TSMessageView!) {
