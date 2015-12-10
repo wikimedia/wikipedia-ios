7e35657aca34288a3f5e1fbe81aae6785ecca765
Corey Floyd <coreyfloyd@gmail.com>
2015-12-10 17:00:09 -0500

    Add an empty view and a ui view controller category to display it.

--- Wikipedia/Code/UIColor+WMFStyle.h
+++ Wikipedia/Code/UIColor+WMFStyle.h
@@ -42,6 +42,9 @@
  */
 + (instancetype)wmf_lightGrayColor;
 
+
++ (instancetype)wmf_999999Color;
+
 + (instancetype)wmf_placeholderImageTintColor;
 
 + (instancetype)wmf_placeholderImageBackgroundColor;
@@ -59,5 +62,7 @@
 + (instancetype)wmf_nearbyDistanceBackgroundColor;
 
 + (instancetype)wmf_nearbyDistanceTextColor;
+
++ (instancetype)wmf_emptyGrayTextColor;
 
 @end
