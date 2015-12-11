b792d8d79bb8cfc3585fb16559bb2b878ee3cbff
Brian Gerstle <brian.gerstle@gmail.com>
2015-12-10 22:14:55 -0500

    add searchSite tests
    and implement a custom nimble matcher 😎

---
+++ WikipediaUnitTests/Code/SessionSingletonTests.m
@@ -0,0 +1,51 @@
+//
+//  SessionSingletonTests.m
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 12/10/15.
+//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
+//
+
+@import Quick;
+@import Nimble;
+
+#import "MWKDataStore+TempDataStoreForEach.h"
+#import "MWKDataStore+TemporaryDataStore.h"
+#import "PostNotificationMatcherShorthand.h"
+
+#import "SessionSingleton.h"
+
+QuickSpecBegin(SessionSingletonTests)
+
+__block SessionSingleton* testSession;
+
+configureTempDataStoreForEach(tempDataStore, ^{
+    NSString *appDomain = [[NSBundle bundleForClass:[SessionSingleton class]] bundleIdentifier];
+    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:appDomain];
+    testSession = [[SessionSingleton alloc] initWithDataStore:tempDataStore];
+});
+
+describe(@"searchSite", ^{
+    it(@"should default to the current device language", ^{
+        expect(testSession.searchSite.language)
+        .to(equal([[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode]));
+    });
+
+    it(@"should be persistent", ^{
+        expectAction(^{
+            [testSession setSearchLanguage:@"fr"];
+        }).to(postNotification(WMFSearchLanguageDidChangeNotification, nil));
+        MWKSite* expectedSite = testSession.searchSite;
+        SessionSingleton* newSession = [[SessionSingleton alloc] initWithDataStore:[MWKDataStore temporaryDataStore]];
+        expect(newSession.searchSite).to(equal(expectedSite));
+        [newSession.dataStore removeFolderAtBasePath];
+    });
+
+    it(@"should be idempotent", ^{
+        expectAction(^{
+            [testSession setSearchLanguage:testSession.searchSite.language];
+        }).notTo(postNotification(WMFSearchLanguageDidChangeNotification, nil));
+    });
+});
+
+QuickSpecEnd
