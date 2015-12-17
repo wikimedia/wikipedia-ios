3247b74043321f8e99d450ea5fbde6508c584da3
Brian Gerstle <brian.gerstle@gmail.com>
2015-12-17 10:30:54 -0500

    remove refresh alert (make button red) & add rev model validation
    also added fetch param to stop revision list at current revision (don't
    need to know about older ones)

--- Wikipedia/Code/WMFRevisionQueryResults.m
+++ Wikipedia/Code/WMFRevisionQueryResults.m
@@ -9,7 +9,37 @@
 #import "WMFRevisionQueryResults.h"
 #import "WMFArticleRevision.h"
 
+typedef NS_ENUM(NSInteger, WMFRevisionQueryResultsError) {
+    WMFRevisionQueryResultsErrorMissingTitle = 1
+};
+
+static NSString* const WMFRevisionQueryResultsErrorDomain = @"WMFRevisionQueryResultsErrorDomain";
+
 @implementation WMFRevisionQueryResults
+
+- (BOOL)validate:(NSError *__autoreleasing *)error {
+    if (!self.titleText.length) {
+        WMFSafeAssign(error, [NSError errorWithDomain:WMFRevisionQueryResultsErrorDomain
+                                                 code:WMFRevisionQueryResultsErrorMissingTitle
+                                             userInfo:nil]);
+        return NO;
+    }
+
+    if (!self.revisions.count) {
+        // NOTE: logging this here to ensure titleText property is set. Otherwise log isn't helpful
+        DDLogWarn(@"Unexpected empty revisions list for title %@.", self.titleText);
+    }
+
+    return YES;
+}
+
+- (void)setRevisions:(NSArray<WMFArticleRevision *> *)revisions {
+    _revisions = revisions ?: @[];
+}
+
+- (void)setTitleText:(NSString *)titleText {
+    _titleText = titleText ?: @"";
+}
 
 + (NSValueTransformer*)revisionsJSONTransformer {
     return [MTLJSONAdapter arrayTransformerWithModelClass:[WMFArticleRevision class]];
