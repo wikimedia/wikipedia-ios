227be73960aa1816c957db98084af8d3862398d0
Monte Hurd <mhurd@wikimedia.org>
2015-12-18 17:36:11 -0800

    Update grunt-browserify version to 4.0.1

--- Wikipedia/assets/bundle.js
+++ Wikipedia/assets/bundle.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (module) {
 
 function Bridge() {
@@ -106,7 +106,7 @@ function getElementFromPoint(x, y){
 global.getElementFromPoint = getElementFromPoint;
 
 
-}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],3:[function(require,module,exports){
 (function () {
 var bridge = require("./bridge");
@@ -390,7 +390,7 @@ function scrollToFragment(fragmentId){
 }
 
 global.scrollToFragment = scrollToFragment;
-}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],7:[function(require,module,exports){
 (function (global){
 var utilities = require("./utilities");
@@ -429,7 +429,7 @@ exports.getSectionHeaderForId = getSectionHeaderForId;
 global.getSectionHeadersArray = getSectionHeadersArray;
 global.getSectionHeaderLocationsArray = getSectionHeaderLocationsArray;
 
-}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./utilities":17}],8:[function(require,module,exports){
 function Transformer() {
 }
@@ -1164,5 +1164,5 @@ function getPostRotationScrollOffset() {
 global.setPreRotationRelativeScrollOffset = setPreRotationRelativeScrollOffset;
 global.getPostRotationScrollOffset = getPostRotationScrollOffset;
 
-}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]);
