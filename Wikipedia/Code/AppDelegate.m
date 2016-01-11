75f898ce272b63f1a6dade3c25eb8b225d7affb6
Monte Hurd <mhurd@wikimedia.org>
2016-01-11 08:25:55 -0800

    Moved updateDynamicIconShortcutItems call to applicationDidEnterBackground.

--- Wikipedia/Code/AppDelegate.m
+++ Wikipedia/Code/AppDelegate.m
@@ -91,12 +91,12 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     [[NSUserDefaults standardUserDefaults] wmf_setAppResignActiveDate:[NSDate date]];
-    [self updateDynamicIconShortcutItems];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication*)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self updateDynamicIconShortcutItems];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication*)application {
@@ -111,7 +111,6 @@
 
 - (void)applicationWillTerminate:(UIApplication*)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    [self updateDynamicIconShortcutItems];
 }
 
 // TODO: fetch saved pages in the background
