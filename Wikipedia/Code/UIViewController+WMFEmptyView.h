9f955fb6254169b8d31bf3ce12315f86199b6175
Corey Floyd <coreyfloyd@gmail.com>
2016-01-11 21:27:45 -0500

    Support setting an empty view of type none

--- Wikipedia/Code/UIViewController+WMFEmptyView.h
+++ Wikipedia/Code/UIViewController+WMFEmptyView.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM (NSUInteger, WMFEmptyViewType) {
+    WMFEmptyViewTypeNone,
     WMFEmptyViewTypeNoFeed,
     WMFEmptyViewTypeArticleDidNotLoad,
     WMFEmptyViewTypeNoSearchResults,
