9e3b5d953da8660d37a906ea6c158354311305c7
Corey Floyd <coreyfloyd@gmail.com>
2016-01-21 15:17:57 -0500

    Change MWKBlacklist to inherit from MWKList

--- Wikipedia/Code/WMFExploreSectionSchema.m
+++ Wikipedia/Code/WMFExploreSectionSchema.m
@@ -104,7 +104,7 @@ static NSString* const WMFExploreSectionsFileExtension = @"plist";
     
     _blackList = blackList;
     
-    [self.KVOController observe:_blackList keyPath:WMF_SAFE_KEYPATH(_blackList, blackListTitles) options:0 block:^(WMFExploreSectionSchema* observer, WMFRelatedSectionBlackList* object, NSDictionary* change) {
+    [self.KVOController observe:_blackList keyPath:WMF_SAFE_KEYPATH(_blackList, entries) options:0 block:^(WMFExploreSectionSchema* observer, WMFRelatedSectionBlackList* object, NSDictionary* change) {
         [observer updateWithChangesInBlackList:object];
     }];
 }
@@ -265,7 +265,7 @@ static NSString* const WMFExploreSectionsFileExtension = @"plist";
 
 - (void)updateWithChangesInBlackList:(WMFRelatedSectionBlackList*)blackList {
     //enumerate in reverse so that indexes are always correct
-    [[blackList.blackListTitles wmf_mapAndRejectNil:^id(MWKTitle * obj) {
+    [[blackList.entries wmf_mapAndRejectNil:^id(MWKTitle * obj) {
         return [self existingSectionForTitle:obj];
     }] enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(WMFExploreSection*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         [self removeSection:obj];
