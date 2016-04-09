97edeb36fecca25e72447896760f2fd8a0bf1914
Monte Hurd <mhurd@wikimedia.org>
2016-04-08 20:04:04 -0700

    New base request serializer and serializer header category.

---
+++ Wikipedia/Code/WMFBaseRequestSerializer.m
@@ -0,0 +1,14 @@
+#import "WMFBaseRequestSerializer.h"
+#import "AFHTTPRequestSerializer+WMFRequestHeaders.h"
+
+@implementation WMFBaseRequestSerializer
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self wmf_applyAppRequestHeaders];
+    }
+    return self;
+}
+
+@end
