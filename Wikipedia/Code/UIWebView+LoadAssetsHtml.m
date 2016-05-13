ce1412479edecb0b6bf19c30f220a0d58ef3f867
montehurd <mhurd@wikimedia.org>
2016-05-13 15:11:20 -0700

    Fix article loading so css and js referenced by index.html can be seen.
    This is iOS 9 only. There is a work-around for iOS 8 detailed here:
    http://stackoverflow.com/q/24882834

--- Wikipedia/Code/UIWebView+LoadAssetsHtml.m
+++ Wikipedia/Code/UIWebView+LoadAssetsHtml.m
@@ -7,10 +7,7 @@
 @implementation WKWebView (LoadAssetsHtml)
 
 - (void)loadHTMLFromAssetsFile:(NSString*)fileName {
-    NSString* filePath = [[self getAssetsPath] stringByAppendingPathComponent:fileName];
-    NSData* fileData   = [[NSFileManager defaultManager] contentsAtPath:filePath];
-
-    [self loadData:fileData MIMEType:@"text/html" characterEncodingName:@"UTF-8" baseURL:[NSURL URLWithString:filePath]];
+    [self loadFileURLFromPath:[[self getAssetsPath] stringByAppendingPathComponent:fileName]];
 }
 
 - (void)loadHTML:(NSString*)string withAssetsFile:(NSString*)fileName {
@@ -26,8 +23,31 @@
 
     NSNumber* fontSize   = [[NSUserDefaults standardUserDefaults] wmf_readingFontSize];
     NSString* fontString = [NSString stringWithFormat:@"%ld%%", fontSize.integerValue];
-    [self loadHTMLString:[NSString stringWithFormat:fileContents, fontString, string]
-                 baseURL:[NSURL URLWithString:path]];
+
+    // index.html and preview.html have two "%@" subsitition markers. Replace both of these with actual content.
+    NSString *templateAndContent = [NSString stringWithFormat:fileContents, fontString, string];
+    
+    // Get temp file name. For a fileName of "index.html" the temp file name would be "index.temp.html"
+    NSString *tempFileName = [[[fileName stringByDeletingPathExtension] stringByAppendingPathExtension:@"temp"] stringByAppendingPathExtension:[fileName pathExtension]];
+    
+    // Get path to tempFileName
+    NSString *tempFilePath = [[[NSURL fileURLWithPath:path] URLByDeletingLastPathComponent] URLByAppendingPathComponent:tempFileName isDirectory:NO].absoluteString;
+    
+    // Remove "file://" from beginning of tempFilePath
+    tempFilePath = [tempFilePath substringFromIndex:7];
+    
+    NSError* error = nil;
+    [templateAndContent writeToFile:tempFilePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    if (!error) {
+        [self loadFileURLFromPath:tempFilePath];
+    }else{
+        NSAssert(NO, @"\nTemp file could not be written: \n%@\n", tempFilePath);
+    }
+}
+
+-(void)loadFileURLFromPath:(NSString*)filePath {
+    //TODO: add iOS 8 fallback here...
+    [self loadFileURL:[NSURL fileURLWithPath:filePath] allowingReadAccessToURL:[[NSURL fileURLWithPath:filePath] URLByDeletingLastPathComponent]];
 }
 
 - (NSString*)getAssetsPath {
