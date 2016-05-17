c6fab3efdf09b64aab5ccdb79705f21ba95f5cd7
montehurd <mhurd@wikimedia.org>
2016-05-16 21:31:07 -0700

    Remove orphaned custom url protocols.

--- Wikipedia/Code/WMFLocalizationProtocol.h
+++
@@ -1,8 +0,0 @@
-//  Created by Monte Hurd on 4/21/15.
-//  Copyright (c) 2015 Wikimedia Foundation. Provided under MIT-style license; please copy and modify!
-
-#import <Foundation/Foundation.h>
-
-@interface WMFLocalizationProtocol : NSURLProtocol
-
-@end
