418e88dd703fdc2bc31857156f485dbc944f2743
Corey Floyd <coreyfloyd@gmail.com>
2016-06-09 14:05:54 -0700

    Add new fetcher and model for auth info
    Determines whether the new auth system is available

---
+++ Wikipedia/Code/WMFAuthManagerInfoFetcher.m
@@ -0,0 +1,71 @@
+//
+//  WMFAuthManagerInfoFetcher.m
+//  Wikipedia
+//
+//  Created by Corey Floyd on 6/8/16.
+//  Copyright © 2016 Wikimedia Foundation. All rights reserved.
+//
+
+#import "WMFAuthManagerInfoFetcher.h"
+#import "AFHTTPSessionManager+WMFDesktopRetry.h"
+#import "AFHTTPSessionManager+WMFConfig.h"
+#import "MWNetworkActivityIndicatorManager.h"
+#import "WMFMantleJSONResponseSerializer.h"
+#import "WMFAuthManagerInfo.h"
+
+
+@interface WMFAuthManagerInfoFetcher ()
+@property (nonatomic, strong) AFHTTPSessionManager* operationManager;
+@end
+
+@implementation WMFAuthManagerInfoFetcher
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        AFHTTPSessionManager* manager = [AFHTTPSessionManager wmf_createDefaultManager];
+        manager.responseSerializer = [WMFMantleJSONResponseSerializer serializerForInstancesOf:[WMFAuthManagerInfo class] fromKeypath:@"query"];
+        self.operationManager      = manager;
+    }
+    return self;
+}
+
+- (BOOL)isFetching {
+    return [[self.operationManager operationQueue] operationCount] > 0;
+}
+
+- (AnyPromise*)fetchAuthManagerLoginAvailableForSite:(MWKSite*)site {
+    return [self fetchAuthManagerAvailableForSite:site type:@"login"];
+}
+
+- (AnyPromise*)fetchAuthManagerCreationAvailableForSite:(MWKSite*)site {
+    return [self fetchAuthManagerAvailableForSite:site type:@"create"];
+}
+
+- (AnyPromise*)fetchAuthManagerAvailableForSite:(MWKSite*)site type:(NSString*)type {
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve) {
+        NSDictionary* params = @{
+            @"action": @"query",
+            @"meta": @"authmanagerinfo",
+            @"format": @"json",
+            @"amirequestsfor": type
+        };
+
+        [self.operationManager wmf_GETWithSite:site
+                                    parameters:params
+                                         retry:NULL
+                                       success:^(NSURLSessionDataTask* operation, id responseObject) {
+            [[MWNetworkActivityIndicatorManager sharedManager] pop];
+//                                           if(!responseObject){
+//
+//                                           }
+            resolve(responseObject);
+        }
+                                       failure:^(NSURLSessionDataTask* operation, NSError* error) {
+            [[MWNetworkActivityIndicatorManager sharedManager] pop];
+            resolve(error);
+        }];
+    }];
+}
+
+@end
