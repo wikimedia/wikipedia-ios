3d5393a73fdf76fac30f646ff350a57cbc9dd8e9
Corey Floyd <coreyfloyd@gmail.com>
2016-06-10 08:49:49 -0700

    Add captcha url fragment to auth info
    Needed to request captcha

--- Wikipedia/Code/WMFAuthManagerInfo.h
+++ Wikipedia/Code/WMFAuthManagerInfo.h
@@ -9,6 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, assign, readonly) BOOL canCreateAccount;
 
 @property (nonatomic, copy, readonly) NSString* captchaID;
+@property (nonatomic, copy, readonly) NSString* captchaURLFragment;
 
 @end
 
