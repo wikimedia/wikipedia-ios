60231038775d931f06e081e114abb1b71f5212a5
joewalsh <joewalsh@wikimedia.org>
2016-07-07 10:30:42 -0400

    fix indentation

--- scripts/generate_dice_roll_html.rb
+++ scripts/generate_dice_roll_html.rb
@@ -6,108 +6,109 @@ output = "<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 <html>
 <head>
-	<meta name=\"viewport\" content=\"width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no\">
-	<style>
-	body {
-		background-color: rgba(0, 0, 0, 0);
-	}
-	#container {
-		position: absolute;
-		left: 20px;
-		top: 20px;
-		height: 50px;
-		width: 50px;
-		padding: 0 !important;
-		perspective: 1000px;
-		-webkit-perspective: 1000px;
-	}
+    <meta name=\"viewport\" content=\"width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no\">
+    <style>
+    	body {
+    		background-color: rgba(0, 0, 0, 0);
+    	}
+      
+    	#container {
+    		position: absolute;
+    		left: 20px;
+    		top: 20px;
+    		height: 50px;
+    		width: 50px;
+    		padding: 0 !important;
+    		perspective: 1000px;
+    		-webkit-perspective: 1000px;
+    	}
 
-	#dice {
-		cursor: pointer;
-		position: absolute;
-		transform-style: preserve-3d;
-		height: 100%;
-		width: 100%;
-		-webkit-transform: translateZ( -25px) rotateX( 0deg) rotateY( 0deg) rotateZ( 0deg);
-	}
+    	#dice {
+    		cursor: pointer;
+    		position: absolute;
+    		transform-style: preserve-3d;
+    		height: 100%;
+    		width: 100%;
+    		-webkit-transform: translateZ( -25px) rotateX( 0deg) rotateY( 0deg) rotateZ( 0deg);
+    	}
 
-	#dice > div {
-		backface-visibility: hidden;
-		height: 50px;
-		width: 50px;
-		position: absolute;
-		background: #36c;
-		border-radius: 2px;
-	}
+    	#dice > div {
+    		backface-visibility: hidden;
+    		height: 50px;
+    		width: 50px;
+    		position: absolute;
+    		background: #36c;
+    		border-radius: 2px;
+    	}
 
-	#dice > div > span { /*die dot styling */
-		position: absolute;
-		background: #fff;
-		height: 10px;
-		width: 10px;
-		border-radius: 50%;
-		-webkit-transform: translate3d(-50%, -50%, 0);
-		backface-visibility: hidden;
-	}
+    	#dice > div > span { /*die dot styling */
+    		position: absolute;
+    		background: #fff;
+    		height: 10px;
+    		width: 10px;
+    		border-radius: 50%;
+    		-webkit-transform: translate3d(-50%, -50%, 0);
+    		backface-visibility: hidden;
+    	}
 
-	.one {
-		-webkit-transform: rotateY( 0deg) translateZ( 25px);
-	}
+    	.one {
+    		-webkit-transform: rotateY( 0deg) translateZ( 25px);
+    	}
 
-	.two {
-		-webkit-transform: rotateX( 180deg) translateZ( 25px);
-	}
+    	.two {
+    		-webkit-transform: rotateX( 180deg) translateZ( 25px);
+    	}
 
-	.three {
-		-webkit-transform: rotateY( 90deg) translateZ( 25px);
-	}
+    	.three {
+    		-webkit-transform: rotateY( 90deg) translateZ( 25px);
+    	}
 
-	.four {
-		-webkit-transform: rotateY( -90deg) translateZ( 25px);
-	}
+    	.four {
+    		-webkit-transform: rotateY( -90deg) translateZ( 25px);
+    	}
 
-	.five {
-		-webkit-transform: rotateX( 90deg) translateZ( 25px);
-	}
+    	.five {
+    		-webkit-transform: rotateX( 90deg) translateZ( 25px);
+    	}
 
-	.six {
-		-webkit-transform: rotateX( -90deg) translateZ( 25px);
-	}
+    	.six {
+    		-webkit-transform: rotateX( -90deg) translateZ( 25px);
+    	}
 
-	.one span, .three span:nth-child(2), .five span:nth-child(5) {
-		top: 50%;
-		left: 50%;
-	}
+    	.one span, .three span:nth-child(2), .five span:nth-child(5) {
+    		top: 50%;
+    		left: 50%;
+    	}
 
-	.two span:nth-child(1), .three span:nth-child(1), .four span:nth-child(1), .five span:nth-child(1), .six span:nth-child(1) {
-		top: 25%;
-		left: 25%;
-	}
+    	.two span:nth-child(1), .three span:nth-child(1), .four span:nth-child(1), .five span:nth-child(1), .six span:nth-child(1) {
+    		top: 25%;
+    		left: 25%;
+    	}
 
-	.two span:nth-child(2), .three span:nth-child(3), .four span:nth-child(4), .five span:nth-child(4), .six span:nth-child(6) {
-		top: 75%;
-		left: 75%;
-	}
+    	.two span:nth-child(2), .three span:nth-child(3), .four span:nth-child(4), .five span:nth-child(4), .six span:nth-child(6) {
+    		top: 75%;
+    		left: 75%;
+    	}
 
-	.four span:nth-child(2), .five span:nth-child(2), .six span:nth-child(2) {
-		top: 25%;
-		left: 75%;
-	}
+    	.four span:nth-child(2), .five span:nth-child(2), .six span:nth-child(2) {
+    		top: 25%;
+    		left: 75%;
+    	}
 
-	.four span:nth-child(3), .five span:nth-child(3), .six span:nth-child(5)  {
-		top: 75%;
-		left: 25%;
-	}
+    	.four span:nth-child(3), .five span:nth-child(3), .six span:nth-child(5)  {
+    		top: 75%;
+    		left: 25%;
+    	}
 
-	.six span:nth-child(3) {
-		top: 50%;
-		left: 25%;
-	}   
+    	.six span:nth-child(3) {
+    		top: 50%;
+    		left: 25%;
+    	}   
 
-	.six span:nth-child(4) {
-		top: 50%;
-		left: 75%;
-	}
+    	.six span:nth-child(4) {
+    		top: 50%;
+    		left: 75%;
+    	}
 "
 
 rx = [   0,   0,   0,   0, -90,  90]
@@ -121,11 +122,9 @@ myes = [1, -1, -1, 1]
 for i in 0..5
   side = i+1
     output << "
-
-  #dice.rolled-#{side} {
-      -webkit-transform: translateZ(#{tz[i]}px) rotateX(#{rx[i]}deg) rotateY(#{ry[i]}deg) rotateZ(#{rz}deg);
-  }
-"
+        #dice.rolled-#{side} {
+            -webkit-transform: translateZ(#{tz[i]}px) rotateX(#{rx[i]}deg) rotateY(#{ry[i]}deg) rotateZ(#{rz}deg);
+        }"
   for j in 0..5
     next if i == j
     to_side = j+1
@@ -134,38 +133,39 @@ for i in 0..5
       my = myes[v]
       variant = v + 1
       output << "
-    #dice.roll-#{side}-#{to_side}-#{variant} {
-        -webkit-animation: roll-keyframes-#{side}-#{to_side}-#{variant} 1s 1 ease-in-out forwards;
-    }
-  
-    @-webkit-keyframes roll-keyframes-#{side}-#{to_side}-#{variant} {
-    	0% {
-    		-webkit-transform: translateZ(#{tz}px) rotateX(#{rx[i]}deg) rotateY(#{ry[i]}deg) rotateZ(#{rz}deg);
-    	}
-      " +
-      # 20% {
-      #   -webkit-transform: translateZ(-125px) rotateX(#{mx*90 + rx[j]}deg) rotateY(#{my*90 + ry[j]}deg) rotateZ(#{rz}deg);
-      # }
-      # 40% {
-      #   -webkit-transform: translateZ(-250px) rotateX(#{mx*180 + rx[j]}deg) rotateY(#{my*180 + ry[j]}deg) rotateZ(#{rz}deg);
-      # }
-      # 60% {
-      #   -webkit-transform: translateZ(-375px) rotateX(#{mx*270 + rx[j]}deg) rotateY(#{my*270 + ry[j]}deg) rotateZ(#{rz}deg);
-      # }
-      "80% {
-        -webkit-transform: translateZ(-500px) rotateX(#{mx*360 + rx[j]}deg) rotateY(#{my*360 + ry[j]}deg) rotateZ(#{rz}deg);
-      }
-    	100% {
-    		-webkit-transform: translateZ(#{tz}px) rotateX(#{mx*360 + rx[j]}deg) rotateY(#{my*360 + ry[j]}deg) rotateZ(#{rz}deg);
-    	}
-    }"
+
+        #dice.roll-#{side}-#{to_side}-#{variant} {
+            -webkit-animation: roll-keyframes-#{side}-#{to_side}-#{variant} 1s 1 ease-in-out forwards;
+        }
+
+        @-webkit-keyframes roll-keyframes-#{side}-#{to_side}-#{variant} {
+            0% {
+            	-webkit-transform: translateZ(#{tz}px) rotateX(#{rx[i]}deg) rotateY(#{ry[i]}deg) rotateZ(#{rz}deg);
+            }
+            80% {
+              -webkit-transform: translateZ(-500px) rotateX(#{mx*360 + rx[j]}deg) rotateY(#{my*360 + ry[j]}deg) rotateZ(#{rz}deg);
+            }
+            100% {
+            	-webkit-transform: translateZ(#{tz}px) rotateX(#{mx*360 + rx[j]}deg) rotateY(#{my*360 + ry[j]}deg) rotateZ(#{rz}deg);
+            }
+        }"
+    # 20% {
+    #   -webkit-transform: translateZ(-125px) rotateX(#{mx*90 + rx[j]}deg) rotateY(#{my*90 + ry[j]}deg) rotateZ(#{rz}deg);
+    # }
+    # 40% {
+    #   -webkit-transform: translateZ(-250px) rotateX(#{mx*180 + rx[j]}deg) rotateY(#{my*180 + ry[j]}deg) rotateZ(#{rz}deg);
+    # }
+    # 60% {
+    #   -webkit-transform: translateZ(-375px) rotateX(#{mx*270 + rx[j]}deg) rotateY(#{my*270 + ry[j]}deg) rotateZ(#{rz}deg);
+    # }
     end
 
   end
 end
 
 output << "
-</style>
+  </style>
+</head>
 <body>
 	<div id=\"container\">
 		<div id=\"dice\" class=\"rolled-1\">
