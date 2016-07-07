67e6082345feb7686fab89cd1afb33857fbc8943
Corey Floyd <coreyfloyd@gmail.com>
2016-07-07 15:03:23 -0400

    Remove the browser VC that is no longer used
    And the associated tweak

--- Wikipedia/Code/WMFArticleListTableViewController.m
+++ Wikipedia/Code/WMFArticleListTableViewController.m
@@ -13,7 +13,7 @@
 #import "UIScrollView+WMFContentOffsetUtils.h"
 #import "UITableView+WMFLockedUpdates.h"
 
-#import "WMFArticleBrowserViewController.h"
+#import "UIViewController+WMFArticlePresentation.h"
 #import "UIViewController+WMFSearch.h"
 
 #import <Masonry/Masonry.h>
