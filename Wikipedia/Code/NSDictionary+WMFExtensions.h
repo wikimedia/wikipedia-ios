d65ff36a0700b01b51c38ba691a6ca3a4450e2f1
Corey Floyd <coreyfloyd@gmail.com>
2016-07-08 13:23:56 -0400

    Add recursive null checking methods

--- Wikipedia/Code/NSDictionary+WMFExtensions.h
+++ Wikipedia/Code/NSDictionary+WMFExtensions.h
@@ -8,6 +8,14 @@
  *
  *  @return YES if any objects are [NSNUll null], otherwise NO
  */
-- (BOOL)containsNullObjects;
+- (BOOL)wmf_containsNullObjects;
+
+
+/**
+ *  Used to find dictionaries that contain Nulls or contain sub-dictionaries or arrays that contain Nulls
+ *
+ *  @return YES if any objects or sub-collection objects are [NSNUll null], otherwise NO
+ */
+- (BOOL)wmf_recursivelyContainsNullObjects;
 
 @end
--- Wikipedia/Code/NSDictionary+WMFExtensions.m
+++ Wikipedia/Code/NSDictionary+WMFExtensions.m
@@ -3,11 +3,42 @@
 
 @implementation NSDictionary (WMFExtensions)
 
-- (BOOL)containsNullObjects {
+- (BOOL)wmf_containsNullObjects {
     NSNull* null = [self bk_match:^BOOL (id key, id obj) {
         return [obj isKindOfClass:[NSNull class]];
     }];
     return (null != nil);
+}
+
+- (BOOL)wmf_recursivelyContainsNullObjects{
+    
+    if([self wmf_containsNullObjects]){
+        return YES;
+    }
+    
+    __block BOOL hasNull = NO;
+    [self enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        
+        if([obj isKindOfClass:[NSDictionary class]]){
+            
+            hasNull = [obj wmf_recursivelyContainsNullObjects];
+            if(hasNull){
+                *stop = YES;
+                return;
+            }
+        }
+        
+        if([obj isKindOfClass:[NSArray class]]){
+            
+            hasNull = [obj wmf_recursivelyContainsNullObjects];
+            if(hasNull){
+                *stop = YES;
+                return;
+            }
+        }
+        
+    }];
+    
 }
 
 @end
