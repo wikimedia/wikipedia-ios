8acbee36b52b5320467a6868c41c40226493ceea
joewalsh <joewalsh@wikimedia.org>
2016-07-11 10:28:49 -0400

    move WMFFaceDetection to the GPU, set detection quality to low

--- Wikipedia/Code/CIContext+WMFImageProcessing.m
+++ Wikipedia/Code/CIContext+WMFImageProcessing.m
@@ -1,38 +1,15 @@
-//
-//  CIContext+WMFImageProcessing.m
-//  Wikipedia
-//
-//  Created by Brian Gerstle on 7/21/15.
-//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
-//
-
-#import "CIContext+WMFImageProcessing.h"
-
 @implementation CIContext (WMFImageProcessing)
 
-+ (instancetype)wmf_sharedBackgroundContext {
-    static CIContext* sharedBackgroundContext;
++ (instancetype)wmf_sharedContext {
+    static CIContext* sharedContext;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedBackgroundContext = [self wmf_backgroundContext];
+        NSDictionary* options = @{
+            kCIContextPriorityRequestLow: @YES
+        };
+        sharedContext = [CIContext contextWithOptions:options];
     });
-    return sharedBackgroundContext;
-}
-
-+ (instancetype)wmf_backgroundContext {
-    return [CIContext contextWithOptions:[self wmf_backgroundContextOptions]];
-}
-
-+ (NSDictionary*)wmf_backgroundContextOptions {
-    static NSDictionary* backgroundContextOptions;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSMutableDictionary* options = [NSMutableDictionary new];
-        options[kCIContextUseSoftwareRenderer] = @YES;
-        options[kCIContextPriorityRequestLow] = @YES;
-        backgroundContextOptions = [options copy];
-    });
-    return backgroundContextOptions;
+    return sharedContext;
 }
 
 @end
