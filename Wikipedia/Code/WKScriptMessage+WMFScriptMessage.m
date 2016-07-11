2419c2e50db2dc62d9c710d4441bf319c47387ad
montehurd <mhurd@wikimedia.org>
2016-07-11 11:05:25 -0700

    Re-order switch statement items.
    Moved most commonly used items higher.

--- Wikipedia/Code/WKScriptMessage+WMFScriptMessage.m
+++ Wikipedia/Code/WKScriptMessage+WMFScriptMessage.m
@@ -3,24 +3,24 @@
 @implementation WKScriptMessage (WMFScriptMessage)
 
 + (WMFWKScriptMessageType)wmf_typeForMessageName:(NSString*)name {
-    if ([name isEqualToString:@"peek"]) {
-        return WMFWKScriptMessagePeek;
-    } else if ([name isEqualToString:@"sendJavascriptConsoleLogMessageToXcodeConsole"]) {
-        return WMFWKScriptMessageConsoleMessage;
+    if ([name isEqualToString:@"nonAnchorTouchEndedWithoutDragging"]) {
+        return WMFWKScriptMessageNonAnchorTouchEndedWithoutDragging;
     } else if ([name isEqualToString:@"linkClicked"]) {
         return WMFWKScriptMessageClickLink;
     } else if ([name isEqualToString:@"imageClicked"]) {
         return WMFWKScriptMessageClickImage;
+    } else if ([name isEqualToString:@"peek"]) {
+        return WMFWKScriptMessagePeek;
     } else if ([name isEqualToString:@"referenceClicked"]) {
         return WMFWKScriptMessageClickReference;
     } else if ([name isEqualToString:@"editClicked"]) {
         return WMFWKScriptMessageClickEdit;
-    } else if ([name isEqualToString:@"nonAnchorTouchEndedWithoutDragging"]) {
-        return WMFWKScriptMessageNonAnchorTouchEndedWithoutDragging;
     } else if ([name isEqualToString:@"lateJavascriptTransform"]) {
         return WMFWKScriptMessageLateJavascriptTransform;
     } else if ([name isEqualToString:@"articleState"]) {
         return WMFWKScriptMessageArticleState;
+    } else if ([name isEqualToString:@"sendJavascriptConsoleLogMessageToXcodeConsole"]) {
+        return WMFWKScriptMessageConsoleMessage;
     } else{
         return WMFWKScriptMessageUnknown;
     }
@@ -28,10 +28,7 @@
 
 + (Class)wmf_expectedMessageBodyClassForType:(WMFWKScriptMessageType)type {
     switch (type) {
-        case WMFWKScriptMessagePeek:
-            return [NSDictionary class];
-            break;
-        case WMFWKScriptMessageConsoleMessage:
+        case WMFWKScriptMessageNonAnchorTouchEndedWithoutDragging:
             return [NSDictionary class];
             break;
         case WMFWKScriptMessageClickLink:
@@ -40,13 +37,13 @@
         case WMFWKScriptMessageClickImage:
             return [NSDictionary class];
             break;
+        case WMFWKScriptMessagePeek:
+            return [NSDictionary class];
+            break;
         case WMFWKScriptMessageClickReference:
             return [NSDictionary class];
             break;
         case WMFWKScriptMessageClickEdit:
-            return [NSDictionary class];
-            break;
-        case WMFWKScriptMessageNonAnchorTouchEndedWithoutDragging:
             return [NSDictionary class];
             break;
         case WMFWKScriptMessageLateJavascriptTransform:
@@ -54,6 +51,9 @@
             break;
         case WMFWKScriptMessageArticleState:
             return [NSString class];
+            break;
+        case WMFWKScriptMessageConsoleMessage:
+            return [NSDictionary class];
             break;
         case WMFWKScriptMessageUnknown:
             return [NSNull class];
