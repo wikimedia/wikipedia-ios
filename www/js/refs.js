de4928cc579fb6a50cca071e87657ad30a25e61f
montehurd <mhurd@wikimedia.org>
2016-07-14 12:07:39 -0700

    Fix iOS link click regression. Was using JS method not present w/iOS 8.

--- www/js/refs.js
+++ www/js/refs.js
@@ -1,14 +1,14 @@
 
 function isCitation( href ) {
-    return href.includes("#cite_note");
+    return href.indexOf("#cite_note") > -1;
 }
 
 function isEndnote( href ) {
-    return href.includes("#endnote_");
+    return href.indexOf("#endnote_") > -1;
 }
 
 function isReference( href ) {
-    return href.includes("#ref_");
+    return href.indexOf("#ref_") > -1;
 }
 
 function goDown( element ) {
