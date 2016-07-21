a423f63a234e0adef73e4602572ea60405999d63
joewalsh <joewalsh@wikimedia.org>
2016-07-21 08:23:17 -0400

    wrap documents url logging in an #if DEBUG

--- Wikipedia/Code/AppDelegate.m
+++ Wikipedia/Code/AppDelegate.m
@@ -83,6 +83,11 @@
 #pragma mark - UIApplicationDelegate
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+#if DEBUG
+    NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    NSLog(@"%@", documentsURL);
+#endif
+    
     [[BITHockeyManager sharedHockeyManager] wmf_setupAndStart];
     [PiwikTracker wmf_start];
 
@@ -96,11 +101,6 @@
     self.appViewController = vc;
 
     [self updateDynamicIconShortcutItems];
-
-#if DEBUG
-    NSLog(@"\n\nSimulator documents directory:\n\t%@\n\n",
-          [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]);
-#endif
 
     return YES;
 }
