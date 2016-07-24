62920a9ff8597e238f5c3abaeece4f2c6a77e6ca
joewalsh <joewalsh@wikimedia.org>
2016-07-24 11:18:28 -0400

    WMFTaskGroup

---
+++ Wikipedia/Code/WMFTaskGroup.m
@@ -0,0 +1,50 @@
+#import "WMFTaskGroup.h"
+
+@interface WMFTaskGroup ()
+    
+@property (nonatomic) dispatch_group_t group;
+@property (nonatomic) NSInteger count;
+
+@end
+
+@implementation WMFTaskGroup
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.group = dispatch_group_create();
+    }
+    return self;
+}
+- (void)enter {
+    @synchronized (self) {
+        self.count++;
+        dispatch_group_enter(_group);
+    }
+}
+
+- (void)leave {
+    @synchronized (self) {
+        if (self.count > 0) {
+            self.count--;
+            dispatch_group_leave(self.group);
+        } else {
+            DDLogError(@"Mismatched leave for group: %@", self);
+        }
+    }
+}
+
+- (void)waitInBackgroundWithTimeout:(NSTimeInterval)timeout completion:(nonnull dispatch_block_t)completion {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        dispatch_group_wait(self.group, dispatch_time(DISPATCH_TIME_NOW, (int64_t)timeout* NSEC_PER_SEC));
+        if (completion != nil) {
+            completion();
+        }
+    });
+}
+
+- (void)waitInBackgroundWithCompletion:(nonnull dispatch_block_t)completion {
+    dispatch_group_notify(self.group, dispatch_get_main_queue(), completion);
+}
+
+@end
