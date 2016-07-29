3571426f6f0c57f480f7fe368a2723eb3d280eab
joewalsh <joewalsh@wikimedia.org>
2016-07-29 16:18:14 -0400

    make WMFImageTag initializers nullable

--- Wikipedia/Code/WMFImageTag.h
+++ Wikipedia/Code/WMFImageTag.h
@@ -14,23 +14,23 @@ static CGSize const WMFImageTagMinimumSizeForGalleryInclusion = {64, 64};
 @property (nonatomic, strong, readonly, nullable) NSNumber* dataFileWidth;
 @property (nonatomic, strong, readonly, nullable) NSNumber* dataFileHeight;
 
-@property (nonatomic, copy, readonly) NSString *imageTagContents;
+@property (nonatomic, copy, readonly) NSString* imageTagContents;
 
-- (instancetype)initWithSrc:(NSString*)src
-                     srcset:(nullable NSString*)srcset
-                        alt:(nullable NSString*)alt
-                      width:(nullable NSString*)width
-                     height:(nullable NSString*)height
-              dataFileWidth:(nullable NSString*)dataFileWidth
-             dataFileHeight:(nullable NSString*)dataFileHeight
-                    baseURL:(nullable NSURL*)baseURL NS_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithSrc:(NSString*)src
+                              srcset:(nullable NSString*)srcset
+                                 alt:(nullable NSString*)alt
+                               width:(nullable NSString*)width
+                              height:(nullable NSString*)height
+                       dataFileWidth:(nullable NSString*)dataFileWidth
+                      dataFileHeight:(nullable NSString*)dataFileHeight
+                             baseURL:(nullable NSURL*)baseURL NS_DESIGNATED_INITIALIZER;
 
 
-- (instancetype)initWithImageTagContents:(NSString *)imageTagContents baseURL:(nullable NSURL *)baseURL;
+- (nullable instancetype)initWithImageTagContents:(NSString*)imageTagContents baseURL:(nullable NSURL*)baseURL;
 
 - (BOOL)isSizeLargeEnoughForGalleryInclusion;
 
-- (void)setValue:(NSString *)value forAttribute:(NSString *)attribute; // don't use this to set any of the attributes that have properties above (src, srcset, alt, etc)
+- (void)setValue:(NSString*)value forAttribute:(NSString*)attribute;   // don't use this to set any of the attributes that have properties above (src, srcset, alt, etc)
 
 @end
 
