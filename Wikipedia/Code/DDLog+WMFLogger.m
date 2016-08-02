5830e54f80b782db903bb74211581bec6ba61c5c
Corey Floyd <coreyfloyd@gmail.com>
2016-08-02 16:33:52 -0400

    Send log files in support emails

--- Wikipedia/Code/DDLog+WMFLogger.m
+++ Wikipedia/Code/DDLog+WMFLogger.m
@@ -1,10 +1,3 @@
-//
-//  DDLog+WMFLogger.m
-//  Wikipedia
-//
-//  Created by Brian Gerstle on 10/15/15.
-//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
-//
 
 #import "DDLog+WMFLogger.h"
 #import "WMFLogFormatter.h"
--- Wikipedia/Code/WMFHelpViewController.m
+++ Wikipedia/Code/WMFHelpViewController.m
@@ -6,6 +6,7 @@
 #import "WikipediaAppUtils.h"
 #import "Wikipedia-Swift.h"
 #import "WMFLeadingImageTrailingTextButton.h"
+#import "DDLog+WMFLogger.h"
 
 @import MessageUI;
 
@@ -64,6 +65,10 @@ static NSString* const WMFSettingsEmailSubject = @"Bug:";
         [vc setSubject:[WMFSettingsEmailSubject stringByAppendingString:[WikipediaAppUtils versionedUserAgent]]];
         [vc setToRecipients:@[WMFSettingsEmailAddress]];
         [vc setMessageBody:[NSString stringWithFormat:@"\n\n\n\nVersion: %@", [WikipediaAppUtils versionedUserAgent]] isHTML:NO];
+        NSData* data = [[DDLog wmf_currentLogFile] dataUsingEncoding:NSUTF8StringEncoding];
+        if (data) {
+            [vc addAttachmentData:data mimeType:@"text/plain" fileName:@"Log Data.txt"];
+        }
         vc.mailComposeDelegate = self;
         [self presentViewController:vc animated:YES completion:NULL];
     } else {
