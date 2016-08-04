46683a84adbac3c3843ebcf230ab6f060cc2b1c5
joewalsh <joewalsh@wikimedia.org>
2016-08-04 15:49:21 -0400

    tweak layout values

--- Wikipedia/Code/WMFCVLMetrics.m
+++ Wikipedia/Code/WMFCVLMetrics.m
@@ -29,12 +29,13 @@
     WMFCVLMetrics *metrics = [[WMFCVLMetrics alloc] init];
     metrics.boundsSize = boundsSize;
     BOOL isPad = boundsSize.width >= 600;
+    BOOL isWide = boundsSize.width >= 1000;
     metrics.numberOfColumns = isPad ? 2 : 1;
     metrics.columnWeights = isPad ? @[@1.179, @0.821] : @[@1];
-    metrics.interColumnSpacing = isPad ? 22 : 0;
+    metrics.interColumnSpacing = isPad ? 20 : 0;
     metrics.interItemSpacing = 1;
-    metrics.interSectionSpacing = isPad ? 22 : 50;
-    metrics.contentInsets = isPad ? UIEdgeInsetsMake(22, 22, 22, 22) : UIEdgeInsetsMake(0, 0, 50, 0);
+    metrics.interSectionSpacing = isPad ? 20 : 50;
+    metrics.contentInsets = isPad ? isWide ? UIEdgeInsetsMake(20, 90, 20, 90) : UIEdgeInsetsMake(20, 20, 20, 20) : UIEdgeInsetsMake(0, 0, 50, 0);
     metrics.sectionInsets = UIEdgeInsetsMake(1, 0, 1, 0);
     return metrics;
 }
