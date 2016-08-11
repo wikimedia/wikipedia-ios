a3b07b2270b9cc1e873c3eab02e61b6f77898b39
Corey Floyd <coreyfloyd@gmail.com>
2016-08-11 18:00:30 -0400

    Create methods with predefined database datasources
    Marries views and predefined mappings to create datasources for history, saved, blacklist, because you read
    Also registers datasources with the datastore to that they receive notifications when the database is updated

---
+++ Wikipedia/Code/MWKDataStore+WMFDataSources.h
@@ -0,0 +1,17 @@
+
+#import "MWKDataStore.h"
+#import "WMFDataSource.h"
+
+@interface MWKDataStore (WMFDataSources)
+
+- (id<WMFDataSource>)historyDataSource;
+
+- (id<WMFDataSource>)historyGroupedByDateDataSource;
+
+- (id<WMFDataSource>)savedDataSource;
+
+- (id<WMFDataSource>)blackListDataSource;
+
+- (id<WMFDataSource>)becauseYouReadSeedsDataSource;
+
+@end
---
+++ Wikipedia/Code/MWKDataStore+WMFDataSources.m
@@ -0,0 +1,50 @@
+
+#import "MWKDataStore+WMFDataSources.h"
+#import "YapDatabase+WMFExtensions.h"
+#import "YapDatabase+WMFViews.h"
+#import "WMFDatabaseDataSource.h"
+#import "MWKHistoryEntry+WMFDatabaseStorable.h"
+
+@interface MWKDataStore (WMFDataSourcesPrivate)
+
+@property (readonly, strong, nonatomic) YapDatabase* database;
+@property (readonly, strong, nonatomic) YapDatabaseConnection* articleReferenceReadConnection;
+@property (readonly, strong, nonatomic) YapDatabaseConnection* writeConnection;
+@property (readonly, nonatomic, strong) NSPointerArray* changeHandlers;
+
+@end
+
+
+@implementation MWKDataStore (WMFDataSources)
+
+- (void)registerChangeHandler:(id<WMFDatabaseChangeHandler>)handler {
+    [self.changeHandlers addPointer:(__bridge void* _Nullable)(handler)];
+}
+
+- (WMFDatabaseDataSource*)registeredDataSourceWithMappings:(YapDatabaseViewMappings*)mappings {
+    WMFDatabaseDataSource* datasource = [[WMFDatabaseDataSource alloc] initWithReadConnection:self.articleReferenceReadConnection writeConnection:self.writeConnection mappings:mappings];
+    [self registerChangeHandler:datasource];
+    return datasource;
+}
+
+- (id<WMFDataSource>)historyDataSource {
+    return [self registeredDataSourceWithMappings:[self.database wmf_ungroupedMappingsWithView:WMFHistorySortedByDateUngroupedView]];
+}
+
+- (id<WMFDataSource>)historyGroupedByDateDataSource {
+    return [self registeredDataSourceWithMappings:[self.database wmf_groupsAsTimeIntervalsSortedDecendingMappingsWithView:WMFHistorySortedByDateGroupedByDateView]];
+}
+
+- (id<WMFDataSource>)savedDataSource {
+    return [self registeredDataSourceWithMappings:[self.database wmf_ungroupedMappingsWithView:WMFSavedSortedByDateUngroupedView]];
+}
+
+- (id<WMFDataSource>)blackListDataSource {
+    return [self registeredDataSourceWithMappings:[self.database wmf_ungroupedMappingsWithView:WMFBlackListSortedByURLUngroupedView]];
+}
+
+- (id<WMFDataSource>)becauseYouReadSeedsDataSource {
+    return [self registeredDataSourceWithMappings:[self.database wmf_ungroupedMappingsWithView:WMFHistoryOrSavedSortedByURLUngroupedFilteredBySignificnatlyViewedAndNotBlacklistedAndNotMainPageView]];
+}
+
+@end
