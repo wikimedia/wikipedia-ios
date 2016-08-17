6deeed9b69d317ea834e94a63eed3f6b9580b488
Corey Floyd <coreyfloyd@gmail.com>
2016-08-17 09:06:04 -0400

    Add continue reading widget target

---
+++ ContinueReadingWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  ContinueReadingWidget
+//
+//  Created by Corey Floyd on 8/17/16.
+//  Copyright © 2016 Wikimedia Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ ContinueReadingWidget/TodayViewController.m
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.m
+//  ContinueReadingWidget
+//
+//  Created by Corey Floyd on 8/17/16.
+//  Copyright © 2016 Wikimedia Foundation. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
