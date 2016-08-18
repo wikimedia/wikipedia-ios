016210f78e87fae14ce8af5950f9f6c1dfc9c7ab
Corey Floyd <coreyfloyd@gmail.com>
2016-08-18 09:37:26 -0400

    Update reference images for snapshot tests for iOS 10

--- WikipediaUnitTests/Code/NSUserDefaults+WMFBatchRecordMode.h
+++ WikipediaUnitTests/Code/NSUserDefaults+WMFBatchRecordMode.h
@@ -1,10 +1,3 @@
-//
-//  NSUserDefaults+WMFBatchRecordMode.h
-//  Wikipedia
-//
-//  Created by Brian Gerstle on 12/9/15.
-//  Copyright © 2015 Wikimedia Foundation. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 
