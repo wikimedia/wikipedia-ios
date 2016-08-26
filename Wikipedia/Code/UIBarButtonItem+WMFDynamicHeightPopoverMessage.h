200be63b2f3a0d4d12dfdd17819f86162de3f1e3
montehurd <mhurd@wikimedia.org>
2016-08-25 17:10:41 -0700

    Refactor DynamicHeightPopover methods to also work with CGRect.
    ...instead of just with UIBarButtonItem.

--- Wikipedia/Code/UIBarButtonItem+WMFDynamicHeightPopoverMessage.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIBarButtonItem (WMFDynamicHeightPopoverMessage)
-
-- (UIViewController*)wmf_dynamicHeightPopoverViewControllerWithTitle:(NSString*)title
-                                                             message:(NSString*)message
-                                                               width:(CGFloat)width;
-
-@end
--- Wikipedia/Code/UIViewController+WMFDynamicHeightPopoverMessage.h
+++ Wikipedia/Code/UIViewController+WMFDynamicHeightPopoverMessage.h
@@ -4,5 +4,11 @@
 
 - (void)wmf_presentDynamicHeightPopoverViewControllerForBarButtonItem:(UIBarButtonItem*)item
                                                             withTitle:(NSString*)title
-                                                              message:(NSString*)message;
+                                                              message:(NSString*)message
+                                                                width:(CGFloat)width;
+
+- (void)wmf_presentDynamicHeightPopoverViewControllerForSourceRect:(CGRect)sourceRect
+                                                         withTitle:(NSString*)title
+                                                           message:(NSString*)message
+                                                             width:(CGFloat)width;
 @end
