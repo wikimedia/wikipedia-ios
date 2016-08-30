f4acae90af3ffb885b89ae43db1d32d500a027e2
joewalsh <joewalsh@wikimedia.org>
2016-08-30 09:45:31 -0400

    fetch top read & article previews

--- TopReadWidget/WMFTodayTopReadWidgetViewController.swift
+++ TopReadWidget/WMFTodayTopReadWidgetViewController.swift
@@ -1,11 +1,18 @@
 import UIKit
 import NotificationCenter
+import WMFUI
+import WMFModel
 
 class WMFTodayTopReadWidgetViewController: UIViewController, NCWidgetProviding {
-        
+    
+    let articlePreviewFetcher = WMFArticlePreviewFetcher()
+    let mostReadFetcher = WMFMostReadTitleFetcher()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        widgetPerformUpdate { (result) in
+            
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -14,13 +21,33 @@ class WMFTodayTopReadWidgetViewController: UIViewController, NCWidgetProviding {
     }
     
     func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.NewData)
+
+        let siteURL = NSURL.wmf_URLWithDefaultSiteAndCurrentLocale()
+
+        mostReadFetcher.fetchMostReadTitlesForSiteURL(siteURL, date: NSDate().wmf_bestMostReadFetchDate()).then { (result) -> AnyPromise in
+            
+            guard let mostReadTitlesResponse = result as? WMFMostReadTitlesResponseItem else {
+                completionHandler(.NoData)
+                return AnyPromise(value: nil)
+            }
+            
+            let articleURLs = mostReadTitlesResponse.articles.map({ (article) -> NSURL in
+                return siteURL.wmf_URLWithTitle(article.titleText)
+            })
+            
+            return self.articlePreviewFetcher.fetchArticlePreviewResultsForArticleURLs(articleURLs, siteURL: siteURL, extractLength: 0, thumbnailWidth: UIScreen.mainScreen().wmf_listThumbnailWidthForScale().unsignedIntegerValue)
+        }.then { (result) -> AnyPromise in
+            guard let articlePreviewResponse = result as? [MWKSearchResult] else {
+                completionHandler(.NoData)
+                return AnyPromise(value: nil)
+            }
+            
+            print("\(articlePreviewResponse)")
+            
+            completionHandler(.NewData)
+            return AnyPromise(value: articlePreviewResponse)
+        }
+
     }
     
 }
