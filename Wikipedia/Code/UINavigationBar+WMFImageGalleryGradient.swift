7dad64b826494af7d7b60cc4d207675c8fedea57
montehurd <mhurd@wikimedia.org>
2016-09-21 14:08:08 -0700

    Revert "Adjust top gradient stops to better match mock."
    This reverts commit 9bc14a491bf5bbe2c81c6539dc334116495bf4ba.

--- Wikipedia/Code/UINavigationBar+WMFImageGalleryGradient.swift
+++ Wikipedia/Code/UINavigationBar+WMFImageGalleryGradient.swift
@@ -17,12 +17,11 @@ extension UINavigationBar {
         let context = UIGraphicsGetCurrentContext()
         
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let locations:[CGFloat] = [0.0, 0.78, 1.0]
-        let top = UIColor.blackColor().CGColor
-        let middle = UIColor.blackColor().CGColor
+        let locations:[CGFloat] = [0.0, 1.0]
         let bottom = UIColor.clearColor().CGColor
+        let top = UIColor.blackColor().CGColor
         
-        let gradient = CGGradientCreateWithColors(colorSpace, [top, middle, bottom], locations)
+        let gradient = CGGradientCreateWithColors(colorSpace, [top, bottom], locations)
         
         let start = CGPoint(x: 0, y: 0)
         let end = CGPoint(x: 0, y: CGRectGetHeight(bounds))
