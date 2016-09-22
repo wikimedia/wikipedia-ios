05f25198d19f16fcb71b87ca633d25ac82da5a3f
joewalsh <joewalsh@wikimedia.org>
2016-09-22 15:05:34 -0400

    move locale headers logic into NSLocale category

--- Wikipedia/Code/AFHTTPRequestSerializer+WMFRequestHeaders.m
+++ Wikipedia/Code/AFHTTPRequestSerializer+WMFRequestHeaders.m
@@ -1,46 +1,10 @@
 #import "AFHTTPRequestSerializer+WMFRequestHeaders.h"
 #import "SessionSingleton.h"
 #import "ReadingActionFunnel.h"
-#import "WikipediaAppUtils.h"
-#import "NSString+WMFExtras.h"
+#import "WikipediaAppUtils.h" 
+#import <WMFModel/WMFModel-Swift.h>
 
 @implementation AFHTTPRequestSerializer (WMFRequestHeaders)
-
-
-+ (NSString *)wmf_acceptLanguageHeader {
-    static NSString *acceptLanguageHeader = nil;
-    static dispatch_once_t onceToken;
-    //we can assume at this point that the app will be re-launched when [NSLocale preferredLanguages] changes, so this only needs to be done once
-    dispatch_once(&onceToken, ^{
-        NSArray *preferredLanguages = [NSLocale preferredLanguages];
-        NSUInteger count = [preferredLanguages count];
-        CGFloat q = 1.0;
-        CGFloat qDelta = q/count;
-        NSMutableString *acceptLanguageString = [NSMutableString stringWithString:@""];
-        for (NSString *preferredLanguage in preferredLanguages) {
-            NSMutableArray *components = [[preferredLanguage componentsSeparatedByString:@"-"] mutableCopy];
-            if ([components count] > 1) {
-                [components removeLastObject];
-            }
-            NSString *languageCode = [[components componentsJoinedByString:@"-"] lowercaseString];
-            if (!languageCode) {
-                continue;
-            }
-            if (q < 1.0) {
-                [acceptLanguageString appendString:@", "];
-            }
-            [acceptLanguageString appendString:languageCode];
-            if (q < 1.0) {
-                NSString *qString = [NSString stringWithFormat:@";q=%g", q];
-                [acceptLanguageString appendString:qString];
-            }
-            q -= qDelta;
-        }
-        acceptLanguageHeader = [acceptLanguageString copy];
-    });
-    
-    return acceptLanguageHeader;
-}
 
 - (void)wmf_applyAppRequestHeaders {
     [self setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
@@ -51,7 +15,7 @@
         [self setValue:funnel.appInstallID forHTTPHeaderField:@"X-WMF-UUID"];
     }
     
-    [self setValue:[AFHTTPRequestSerializer wmf_acceptLanguageHeader]  forHTTPHeaderField:@"Accept-Language"];
+    [self setValue:[NSLocale wmf_acceptLanguageHeaderForPreferredLanguages]  forHTTPHeaderField:@"Accept-Language"];
 }
 
 @end
