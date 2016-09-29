d8b2948280b6f4ef4b748a50082923df07bef4f3
joewalsh <joewalsh@wikimedia.org>
2016-09-29 10:41:08 -0400

    connect interface outlets, update cocoapods, fix build config

--- InTheNewsNotification/WMFInTheNewsNotificationViewController.swift
+++ InTheNewsNotification/WMFInTheNewsNotificationViewController.swift
@@ -1,9 +1,15 @@
 import UIKit
 import UserNotifications
 import UserNotificationsUI
+import WMFUI
 
 class WMFInTheNewsNotificationViewController: UIViewController, UNNotificationContentExtension {
+    @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var readerCountLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var sparklineView: WMFSparklineView!
+    @IBOutlet weak var imageViewWidth: NSLayoutConstraint!
     @IBOutlet var label: UILabel?
     
     override func viewDidLoad() {
@@ -15,4 +21,7 @@ class WMFInTheNewsNotificationViewController: UIViewController, UNNotificationCo
         self.label?.text = notification.request.content.body
     }
 
+    func didReceive(response: UNNotificationResponse, completionHandler completion: (UNNotificationContentExtensionResponseOption) -> Swift.Void) {
+        
+    }
 }
