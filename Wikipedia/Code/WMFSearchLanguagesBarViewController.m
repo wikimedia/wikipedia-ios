2565e7875e50d3a7b902a53ead2b1a3be585634e
montehurd <mhurd@wikimedia.org>
2016-09-30 14:11:18 -0700

    Migrate lang button configuration to new controller.

--- Wikipedia/Code/WMFSearchLanguagesBarViewController.m
+++ Wikipedia/Code/WMFSearchLanguagesBarViewController.m
@@ -1,4 +1,5 @@
 #import "WMFSearchLanguagesBarViewController.h"
+#import "UIFont+WMFStyle.h"
 
 @interface WMFSearchLanguagesBarViewController ()
 
@@ -18,6 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self.languageButtons enumerateObjectsUsingBlock:^(UIButton *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        obj.tintColor = [UIColor wmf_blueTintColor];
+    }];
+    
+    UIImage *buttonBackground = [UIImage wmf_imageFromColor:[UIColor whiteColor]];
+    UIImage *highlightedButtonBackground = [UIImage wmf_imageFromColor:[UIColor colorWithWhite:0.9 alpha:1]];
+    [self.otherLanguagesButton setBackgroundImage:buttonBackground forState:UIControlStateNormal];
+    [self.otherLanguagesButton setBackgroundImage:highlightedButtonBackground forState:UIControlStateHighlighted];
+    [self.otherLanguagesButton.layer setCornerRadius:2.0f];
+    [self.otherLanguagesButton setClipsToBounds:YES];
+    [self.otherLanguagesButton setTitle:MWLocalizedString(@"main-menu-title", nil) forState:UIControlStateNormal];
+    self.otherLanguagesButton.titleLabel.font = [UIFont wmf_subtitle];
+
+    [self updateLanguageBarLanguages];
+    
     self.hidden = YES;
 }
 
@@ -30,6 +46,30 @@
         self.view.hidden = NO;
     }
     _hidden = hidden;
+}
+
+- (NSArray<MWKLanguageLink *> *)languageBarLanguages {
+    return [[MWKLanguageLinkController sharedInstance].preferredLanguages wmf_arrayByTrimmingToLength:3];
+}
+
+- (void)updateLanguageBarLanguages {
+    [[self languageBarLanguages] enumerateObjectsUsingBlock:^(MWKLanguageLink *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        if (idx >= [self.languageButtons count]) {
+            *stop = YES;
+        }
+        UIButton *button = self.languageButtons[idx];
+        [button setTitle:[obj localizedName] forState:UIControlStateNormal];
+    }];
+    
+    [self.languageButtons enumerateObjectsUsingBlock:^(UIButton *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        if (idx >= [[self languageBarLanguages] count]) {
+            obj.enabled = NO;
+            obj.hidden = YES;
+        } else {
+            obj.enabled = YES;
+            obj.hidden = NO;
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
