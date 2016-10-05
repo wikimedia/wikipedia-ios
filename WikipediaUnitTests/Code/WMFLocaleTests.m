bea14127c8ca8164b8d8956d4f3c3b5d07e502f4
joewalsh <joewalsh@wikimedia.org>
2016-10-05 12:24:15 -0400

    add test for special case zh headers

--- WikipediaUnitTests/Code/WMFLocaleTests.m
+++ WikipediaUnitTests/Code/WMFLocaleTests.m
@@ -15,6 +15,14 @@
     NSString *header = [NSLocale wmf_acceptLanguageHeaderForLanguageCodes:languages];
     XCTAssert([header isEqualToString:@"en-us, zh-hans;q=0.8, zh-hant;q=0.6, zh-tw;q=0.4, en-gb;q=0.2"]);
 }
+
+- (void)testZHHeaders {
+    NSArray *languages = [NSLocale wmf_uniqueLanguageCodesForLanguages:@[@"zh-Hans-CN", @"zh-Hans-SG", @"zh-Hant-MO", @"zh-Hant-TW", @"zh-Hant-HK"]];
+    NSArray *expectedResult = @[@"zh-cn", @"zh-sg", @"zh-mo", @"zh-tw", @"zh-hk"];
+    XCTAssert([languages isEqualToArray:expectedResult]);
+
+    NSString *header = [NSLocale wmf_acceptLanguageHeaderForLanguageCodes:languages];
+    XCTAssert([header isEqualToString:@"zh-cn, zh-sg;q=0.8, zh-mo;q=0.6, zh-tw;q=0.4, zh-hk;q=0.2"]);
 }
 
 @end
