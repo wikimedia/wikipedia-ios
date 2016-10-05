3b5e6af65f4d6f1e26b76cf3788b96c1b6508de2
joewalsh <joewalsh@wikimedia.org>
2016-10-05 12:24:05 -0400

    fix locale header test

--- WikipediaUnitTests/Code/WMFLocaleTests.m
+++ WikipediaUnitTests/Code/WMFLocaleTests.m
@@ -8,12 +8,13 @@
 @implementation WMFLocaleTests
 
 - (void)testLocaleHeaders {
-    NSArray *languages = [NSLocale wmf_uniqueLanguageCodesForLanguages:@[@"en-US", @"zh-Hans-US", @"zh-Hant-US", @"en-GB"]];
-    NSArray *expectedResult = @[@"en", @"zh-hans", @"zh-hant"];
+    NSArray *languages = [NSLocale wmf_uniqueLanguageCodesForLanguages:@[@"en-US", @"zh-Hans-US", @"zh-Hant-US", @"zh-Hant-TW", @"en-GB"]];
+    NSArray *expectedResult = @[@"en-us", @"zh-hans", @"zh-hant", @"zh-tw", @"en-gb"];
     XCTAssert([languages isEqualToArray:expectedResult]);
 
     NSString *header = [NSLocale wmf_acceptLanguageHeaderForLanguageCodes:languages];
-    XCTAssert([header isEqualToString:@"en, zh-hans;q=0.67, zh-hant;q=0.33"]);
+    XCTAssert([header isEqualToString:@"en-us, zh-hans;q=0.8, zh-hant;q=0.6, zh-tw;q=0.4, en-gb;q=0.2"]);
+}
 }
 
 @end
