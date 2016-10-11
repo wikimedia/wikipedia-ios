9fe2ed72bc2286416bd5085b85192ddae7ce3b0d
joewalsh <joewalsh@wikimedia.org>
2016-10-11 16:14:52 -0400

    fix build

--- Pods/Target Support Files/Pods-Foundation-InTheNewsNotification/Pods-Foundation-InTheNewsNotification-resources.sh
+++ Pods/Target Support Files/Pods-Foundation-InTheNewsNotification/Pods-Foundation-InTheNewsNotification-resources.sh
@@ -97,6 +97,12 @@ fi
 if [[ "$CONFIGURATION" == "Beta" ]]; then
   install_resource "$PODS_CONFIGURATION_BUILD_DIR/HockeySDK/HockeySDKResources.bundle"
 fi
+if [[ "$CONFIGURATION" == "AlphaDebug" ]]; then
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/HockeySDK/HockeySDKResources.bundle"
+fi
+if [[ "$CONFIGURATION" == "Alpha" ]]; then
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/HockeySDK/HockeySDKResources.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
