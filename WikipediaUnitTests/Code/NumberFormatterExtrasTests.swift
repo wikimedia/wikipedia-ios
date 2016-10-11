a7d0978f385113d2df47e079c7cd7bba8953a61b
joewalsh <joewalsh@wikimedia.org>
2016-10-11 11:15:21 -0400

    fix integer overflow in tests

--- WikipediaUnitTests/Code/NumberFormatterExtrasTests.swift
+++ WikipediaUnitTests/Code/NumberFormatterExtrasTests.swift
@@ -3,24 +3,24 @@ import XCTest
 class NumberFormatterExtrasTests: XCTestCase {
     
     func testThousands() {
-        var number = 215
-        var format = NSNumberFormatter.localizedThousandsStringFromNumber(number)
+        var number: UInt64 = 215
+        var format: String = NSNumberFormatter.localizedThousandsStringFromNumber(NSNumber(unsignedLongLong: number))
         XCTAssertTrue(format.containsString("215"))
         
         number = 1500
-        format = NSNumberFormatter.localizedThousandsStringFromNumber(number)
+        format = NSNumberFormatter.localizedThousandsStringFromNumber(NSNumber(unsignedLongLong: number))
         XCTAssertTrue(format.containsString("1.5"))
         
         number = 538000
-        format = NSNumberFormatter.localizedThousandsStringFromNumber(number)
+        format = NSNumberFormatter.localizedThousandsStringFromNumber(NSNumber(unsignedLongLong: number))
         XCTAssertTrue(format.containsString("538"))
         
         number = 867530939
-        format = NSNumberFormatter.localizedThousandsStringFromNumber(number)
+        format = NSNumberFormatter.localizedThousandsStringFromNumber(NSNumber(unsignedLongLong: number))
         XCTAssertTrue(format.containsString("867.5"))
         
         number = 312490123456
-        format = NSNumberFormatter.localizedThousandsStringFromNumber(number)
+        format = NSNumberFormatter.localizedThousandsStringFromNumber(NSNumber(unsignedLongLong: number))
         XCTAssertTrue(format.containsString("312.5"))
     }
     
