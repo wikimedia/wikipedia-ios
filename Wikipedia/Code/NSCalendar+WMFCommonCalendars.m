d496394fa0b74d456108e724291006135768ce3a
joewalsh <joewalsh@wikimedia.org>
2016-10-13 09:21:20 -0400

    use system provided `isDateInToday:`

--- Wikipedia/Code/NSCalendar+WMFCommonCalendars.m
+++ Wikipedia/Code/NSCalendar+WMFCommonCalendars.m
@@ -24,27 +24,16 @@
 - (NSInteger)daysFromDate:(NSDate *)fromDate toDate:(NSDate *)toDate {
     NSDateComponents *fromDateComponents = [self components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:fromDate];
     NSDateComponents *toDateComponents = [self components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:toDate];
-    
+
     return [self components:NSCalendarUnitDay fromDateComponents:fromDateComponents toDateComponents:toDateComponents options:NSCalendarMatchStrictly].day;
 }
 
 @end
 
-
 @implementation NSDate (WMFComparisons)
 
-- (BOOL)wmf_isEqualToUTCDateIgnorningTime:(NSDate*)dateToCompare {
-    NSCalendar *calendar = [NSCalendar wmf_utcGregorianCalendar];
-    NSCalendarUnit componentFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay;
-    NSDateComponents* components1 = [calendar components:componentFlags fromDate:self];
-    NSDateComponents* components2 = [calendar components:componentFlags fromDate:dateToCompare];
-    return ((components1.year == components2.year) &&
-            (components1.month == components2.month) &&
-            (components1.day == components2.day));
-}
-
 - (BOOL)wmf_isTodayUTC {
-    return [self wmf_isEqualToUTCDateIgnorningTime:[NSDate date]];
+    return [[NSCalendar wmf_utcGregorianCalendar] isDateInToday:self];
 }
 
 @end
