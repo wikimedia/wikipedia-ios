5f5d3e9dc0d098a976fd6f308bba8882e6563db0
joewalsh <joewalsh@wikimedia.org>
2016-10-13 13:04:32 -0400

    use an unserialized operation manager for view count requests

--- Wikipedia/Code/WMFFeedContentFetcher.m
+++ Wikipedia/Code/WMFFeedContentFetcher.m
@@ -7,6 +7,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WMFFeedContentFetcher ()
 @property (nonatomic, strong) AFHTTPSessionManager *operationManager;
+@property (nonatomic, strong) AFHTTPSessionManager *unserializedOperationManager;
 @end
 
 @implementation WMFFeedContentFetcher
@@ -17,6 +18,9 @@ NS_ASSUME_NONNULL_BEGIN
         AFHTTPSessionManager *manager = [AFHTTPSessionManager wmf_createDefaultManager];
         manager.responseSerializer = [WMFMantleJSONResponseSerializer serializerForInstancesOf:[WMFFeedDayResponse class] fromKeypath:nil];
         self.operationManager = manager;
+
+        AFHTTPSessionManager *unserializedOperationManager = [AFHTTPSessionManager wmf_createDefaultManager];
+        self.unserializedOperationManager = unserializedOperationManager;
     }
     return self;
 }
@@ -25,45 +29,44 @@ NS_ASSUME_NONNULL_BEGIN
     return [[self.operationManager operationQueue] operationCount] > 0;
 }
 
-
-- (void)fetchFeedContentForURL:(NSURL *)siteURL date:(NSDate *)date failure:(WMFErrorHandler)failure success:(void (^) (WMFFeedDayResponse* feedDay))success{
+- (void)fetchFeedContentForURL:(NSURL *)siteURL date:(NSDate *)date failure:(WMFErrorHandler)failure success:(void (^)(WMFFeedDayResponse *feedDay))success {
     NSParameterAssert(siteURL);
     NSParameterAssert(date);
-    if(siteURL == nil || date  == nil){
+    if (siteURL == nil || date == nil) {
         NSError *error = [NSError wmf_errorWithType:WMFErrorTypeInvalidRequestParameters
                                            userInfo:nil];
         failure(error);
         return;
     }
-    
-    NSString* datePath = [[NSDateFormatter wmf_yearMonthDayPathDateFormatter] stringFromDate:date];
-    
+
+    NSString *datePath = [[NSDateFormatter wmf_yearMonthDayPathDateFormatter] stringFromDate:date];
+
     NSString *path = [NSString stringWithFormat:@"/api/rest_v1/feed/featured/%@", datePath];
-    
-    NSURL* url = [siteURL wmf_URLWithPath:path isMobile:NO];
+
+    NSURL *url = [siteURL wmf_URLWithPath:path isMobile:NO];
 
     [self.operationManager GET:[url absoluteString]
-                    parameters:nil
-                      progress:NULL
-                       success:^(NSURLSessionDataTask *operation, WMFFeedDayResponse *responseObject) {
-                           if (![responseObject isKindOfClass:[WMFFeedDayResponse class]]) {
-                               failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
-                                                         userInfo:nil]);
-                        
-                           }else{
-                               success(responseObject);
-                           }
-                       }
-                       failure:^(NSURLSessionDataTask *operation, NSError *error) {
-                           failure(error);
-                       }];
+        parameters:nil
+        progress:NULL
+        success:^(NSURLSessionDataTask *operation, WMFFeedDayResponse *responseObject) {
+            if (![responseObject isKindOfClass:[WMFFeedDayResponse class]]) {
+                failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
+                                          userInfo:nil]);
+
+            } else {
+                success(responseObject);
+            }
+        }
+        failure:^(NSURLSessionDataTask *operation, NSError *error) {
+            failure(error);
+        }];
 }
 
 - (void)fetchPageviewsForURL:(NSURL *)titleURL startDate:(NSDate *)startDate endDate:(NSDate *)endDate failure:(WMFErrorHandler)failure success:(WMFArrayOfNumbersHandler)success {
     NSString *title = [titleURL.wmf_titleWithUnderScores wmf_UTF8StringWithPercentEscapes];
     NSString *language = titleURL.wmf_language;
     NSString *domain = titleURL.wmf_domain;
-    
+
     NSParameterAssert(title);
     NSParameterAssert(language);
     NSParameterAssert(domain);
@@ -75,10 +78,10 @@ NS_ASSUME_NONNULL_BEGIN
         failure(error);
         return;
     }
-    
+
     NSString *startDateString = [[NSDateFormatter wmf_englishUTCNonDelimitedYearMonthDayFormatter] stringFromDate:startDate];
     NSString *endDateString = [[NSDateFormatter wmf_englishUTCNonDelimitedYearMonthDayFormatter] stringFromDate:endDate];
-    
+
     if (startDateString == nil || endDateString == nil) {
         DDLogError(@"Failed to format pageviews date URL component for dates: %@ %@", startDate, endDate);
         NSError *error = [NSError wmf_errorWithType:WMFErrorTypeInvalidRequestParameters
@@ -86,35 +89,35 @@ NS_ASSUME_NONNULL_BEGIN
         failure(error);
         return;
     }
-    
+
     NSString *path = [NSString stringWithFormat:@"/metrics/pageviews/per-article/%@.%@/all-access/user/%@/daily/%@/%@",
-                      language, domain, title, startDateString, endDateString];
-    
+                                                language, domain, title, startDateString, endDateString];
+
     NSString *requestURLString = [WMFWikimediaRestAPIURLStringWithVersion(1) stringByAppendingString:path];
-    
-    [self.operationManager GET:requestURLString
-                    parameters:nil
-                      progress:NULL
-                       success:^(NSURLSessionDataTask *operation, NSDictionary *responseObject) {
-                           NSArray *items = responseObject[@"items"];
-                           if (![items isKindOfClass:[NSArray class]]) {
-                               failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
-                                                         userInfo:nil]);
-                               return;
-                           }
-                           
-                           NSMutableArray *results = [NSMutableArray arrayWithCapacity:[items count]];
-                           for (id item in items) {
-                               if ([item isKindOfClass:[NSDictionary class]] && [item[@"views"] isKindOfClass:[NSNumber class]]) {
-                                   [results addObject:item[@"views"]];
-                               }
-                           }
-                           
-                           success([results copy]);
-                       }
-                       failure:^(NSURLSessionDataTask *operation, NSError *error) {
-                           failure(error);
-                       }];
+
+    [self.unserializedOperationManager GET:requestURLString
+        parameters:nil
+        progress:NULL
+        success:^(NSURLSessionDataTask *operation, NSDictionary *responseObject) {
+            NSArray *items = responseObject[@"items"];
+            if (![items isKindOfClass:[NSArray class]]) {
+                failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
+                                          userInfo:nil]);
+                return;
+            }
+
+            NSMutableArray *results = [NSMutableArray arrayWithCapacity:[items count]];
+            for (id item in items) {
+                if ([item isKindOfClass:[NSDictionary class]] && [item[@"views"] isKindOfClass:[NSNumber class]]) {
+                    [results addObject:item[@"views"]];
+                }
+            }
+
+            success([results copy]);
+        }
+        failure:^(NSURLSessionDataTask *operation, NSError *error) {
+            failure(error);
+        }];
 }
 
 @end
