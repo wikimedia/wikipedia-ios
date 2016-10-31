bf97683ec0424f73f8c42264bd82309bd9ec98fe
joewalsh <joewalsh@wikimedia.org>
2016-10-31 09:58:21 -0400

    add super awake from nib call

--- Wikipedia/Code/WMFArticleFooterViewHeader.swift
+++ Wikipedia/Code/WMFArticleFooterViewHeader.swift
@@ -4,6 +4,7 @@ public class WMFArticleFooterViewHeader: UIView {
     @IBOutlet public var headerLabel: UILabel!
     
     public override func awakeFromNib() {
+        super.awakeFromNib()
         headerLabel.accessibilityTraits = UIAccessibilityTraitHeader
     }
     
