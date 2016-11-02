055edffc6e18f82805ac4ea20991e13424684748
Corey Floyd <cfloyd@wikimedia.org>
2016-11-02 14:04:12 -0400

    Make sure long headers are prose and short headers are upper case

--- Wikipedia/Code/WMFTableHeaderLabelView.m
+++ Wikipedia/Code/WMFTableHeaderLabelView.m
@@ -11,21 +11,27 @@
 
 @implementation WMFTableHeaderLabelView
 
-- (void)awakeFromNib{
+- (void)awakeFromNib {
     [super awakeFromNib];
     self.textLabel.textColor = [UIColor wmf_nearbyDescriptionColor];
 }
 
-- (void)setText:(NSString *)text{
-    self.textLabel.text = text;
+- (void)setText:(NSString *)text {
+    //If its short, display as table view header text
+    //If its long, make it read as normal prose
+    if (text.length < 50) {
+        self.textLabel.text = [text uppercaseString];
+    } else {
+        self.textLabel.text = text;
+    }
 }
 
-- (NSString*)text{
+- (NSString *)text {
     return self.textLabel.text;
 }
 
-- (CGFloat)heightWithExpectedWidth:(CGFloat)width{
-    self.textLabel.preferredMaxLayoutWidth = width-self.leadingConstraint.constant-self.trailingConstraint.constant;
+- (CGFloat)heightWithExpectedWidth:(CGFloat)width {
+    self.textLabel.preferredMaxLayoutWidth = width - self.leadingConstraint.constant - self.trailingConstraint.constant;
     return [self systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
 }
 
