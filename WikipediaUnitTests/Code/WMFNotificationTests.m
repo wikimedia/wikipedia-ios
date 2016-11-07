b1dcc48525fcd5284b7fddd85de9b434bf7bf763
joewalsh <joewalsh@wikimedia.org>
2016-11-07 11:57:28 -0500

    add test for notification when haven't notified in awhile

--- WikipediaUnitTests/Code/WMFNotificationTests.m
+++ WikipediaUnitTests/Code/WMFNotificationTests.m
@@ -6,7 +6,6 @@
 @property (nonnull, nonatomic, strong) WMFArticlePreviewDataStore *previewStore;
 @property (nonnull, nonatomic, strong) WMFContentGroupDataStore *contentStore;
 
-
 @end
 
 @implementation WMFNotificationTests
@@ -18,4 +17,30 @@
     self.contentStore = [[WMFContentGroupDataStore alloc] initWithDatabase:[YapDatabase sharedInstance]];
     self.feedContentSource = [[WMFFeedContentSource alloc] initWithSiteURL:siteURL contentGroupDataStore:self.contentStore articlePreviewDataStore:self.previewStore userDataStore:[SessionSingleton sharedInstance].dataStore notificationsController:[WMFNotificationsController sharedNotificationsController]];
 }
+
+- (void)testNotifiesWhenMostRecentDateIsMoreThanThreeDaysAgo {
+    NSCalendar *calendar = [NSCalendar wmf_gregorianCalendar];
+    NSDate *now = [NSDate date];
+    NSDate *daysAgo = [calendar dateByAddingUnit:NSCalendarUnitDay value:-4 toDate:now options:NSCalendarMatchStrictly];
+    NSUserDefaults *defaults = [NSUserDefaults wmf_userDefaults];
+    [defaults wmf_setInTheNewsNotificationsEnabled:YES];
+    [defaults wmf_setMostRecentInTheNewsNotificationDate:daysAgo];
+    XCTAssertTrue([calendar daysFromDate:[defaults wmf_mostRecentInTheNewsNotificationDate] toDate:now] >= 3);
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for content to load"];
+
+    [self.feedContentSource loadNewContentForce:YES
+                                     completion:^{
+                                         NSDate *notificationDate = [defaults wmf_mostRecentInTheNewsNotificationDate];
+                                         XCTAssertTrue([calendar isDateInToday:notificationDate] || [calendar daysFromDate:now toDate:notificationDate] <= 1);
+                                         [expectation fulfill];
+                                     }];
+
+    [self waitForExpectationsWithTimeout:10
+                                 handler:^(NSError *_Nullable error) {
+                                     if (error) {
+                                         XCTFail();
+                                     }
+                                 }];
+}
+
 @end
