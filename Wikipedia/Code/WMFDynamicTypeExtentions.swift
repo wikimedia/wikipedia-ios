aef5294afe4dc63d8eafaa26d2a62e1e139e6f7e
montehurd <mhurd@wikimedia.org>
2016-11-16 21:27:00 -0600

    Convenience method for recursively configuring subviews for dynamic type.

---
+++ Wikipedia/Code/WMFDynamicTypeExtentions.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+extension UIButton {
+    private func wmf_configureForDynamicType(){
+        if #available(iOS 10.0, *) {
+            self.titleLabel!.adjustsFontForContentSizeCategory = true
+            self.titleLabel!.textAlignment = .Center
+            self.titleLabel!.numberOfLines = 1
+            self.titleLabel!.clipsToBounds = false
+            self.titleLabel!.lineBreakMode = .ByWordWrapping
+            self.titleLabel!.setContentCompressionResistancePriority(UILayoutPriorityFittingSizeLevel, forAxis: .Horizontal)
+            self.setContentCompressionResistancePriority(UILayoutPriorityFittingSizeLevel, forAxis: .Horizontal)
+        }
+    }
+}
+
+extension UILabel {
+    private func wmf_configureForDynamicType(){
+        if #available(iOS 10.0, *) {
+            self.adjustsFontForContentSizeCategory = true
+        }
+    }
+}
+
+extension UIView {
+    func wmf_configureSubviewsForDynamicType() {
+        if #available(iOS 10.0, *) {
+            if self.isKindOfClass(UIButton) {
+                (self as! UIButton).wmf_configureForDynamicType()
+            }else if self.isKindOfClass(UILabel) {
+                (self as! UILabel).wmf_configureForDynamicType()
+            }
+            for subview in self.subviews {
+                subview.wmf_configureSubviewsForDynamicType()
+            }
+        }
+    }
+}
