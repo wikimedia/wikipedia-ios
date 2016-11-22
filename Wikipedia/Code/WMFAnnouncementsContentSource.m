65c37a98ee474c56944301b5fe0f0a1b0cd86b08
joewalsh <joewalsh@wikimedia.org>
2016-11-22 11:17:35 -0500

    fix missing completion calls

--- Wikipedia/Code/WMFAnnouncementsContentSource.m
+++ Wikipedia/Code/WMFAnnouncementsContentSource.m
@@ -35,54 +35,65 @@
     return _fetcher;
 }
 
-- (void)loadNewContentForce:(BOOL)force completion:(nullable dispatch_block_t)completion{
-    
-    [self.fetcher fetchAnnouncementsForURL:self.siteURL force:force failure:^(NSError * _Nonnull error) {
-        [self updateVisibilityOfAnnouncements];
-        if(completion){
-            completion();
-        }
-    } success:^(NSArray<WMFAnnouncement *> *announcements) {
-        [self saveAnnouncements:announcements completion:^{
+- (void)loadNewContentForce:(BOOL)force completion:(nullable dispatch_block_t)completion {
+
+    [self.fetcher fetchAnnouncementsForURL:self.siteURL
+        force:force
+        failure:^(NSError *_Nonnull error) {
             [self updateVisibilityOfAnnouncements];
+            if (completion) {
+                completion();
+            }
+        }
+        success:^(NSArray<WMFAnnouncement *> *announcements) {
+            [self saveAnnouncements:announcements
+                         completion:^{
+                             [self updateVisibilityOfAnnouncements];
+                             if (completion) {
+                                 completion();
+                             }
+                         }];
         }];
-    }];
-    
 }
 
-- (void)removeAllContent{
+- (void)removeAllContent {
     [self.contentStore removeAllContentGroupsOfKind:WMFContentGroupKindAnnouncement];
 }
 
+- (void)saveAnnouncements:(NSArray<WMFAnnouncement *> *)announcements completion:(nullable dispatch_block_t)completion {
 
-- (void)saveAnnouncements:(NSArray<WMFAnnouncement *> *)announcements completion:(nullable dispatch_block_t)completion{
-    
-    [announcements enumerateObjectsUsingBlock:^(WMFAnnouncement * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-         NSURL* URL = [WMFContentGroup announcementURLForSiteURL:self.siteURL identifier:obj.identifier];
-        WMFContentGroup* group = [self.contentStore fetchOrCreateGroupForURL:URL ofKind:WMFContentGroupKindAnnouncement forDate:[NSDate date] withSiteURL:self.siteURL associatedContent:@[obj] customizationBlock:^(WMFContentGroup * _Nonnull group) {
-            
-        }];
+    [announcements enumerateObjectsUsingBlock:^(WMFAnnouncement *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        NSURL *URL = [WMFContentGroup announcementURLForSiteURL:self.siteURL identifier:obj.identifier];
+        WMFContentGroup *group = [self.contentStore fetchOrCreateGroupForURL:URL
+                                                                      ofKind:WMFContentGroupKindAnnouncement
+                                                                     forDate:[NSDate date]
+                                                                 withSiteURL:self.siteURL
+                                                           associatedContent:@[obj]
+                                                          customizationBlock:^(WMFContentGroup *_Nonnull group){
+
+                                                          }];
         //Make these visible immediately for previous users
-        if([[NSUserDefaults wmf_userDefaults] wmf_appResignActiveDate] != nil){
+        if ([[NSUserDefaults wmf_userDefaults] wmf_appResignActiveDate] != nil) {
             [group updateVisibility];
         }
     }];
+
+    if (completion) {
+        completion();
+    }
 }
 
-
-- (void)updateVisibilityOfAnnouncements{
+- (void)updateVisibilityOfAnnouncements {
     //Only make these visible for previous users of the app
     //Meaning a new install will only see these after they close the app and reopen
-    if([[NSUserDefaults wmf_userDefaults] wmf_appResignActiveDate] == nil){
+    if ([[NSUserDefaults wmf_userDefaults] wmf_appResignActiveDate] == nil) {
         return;
     }
 
-
-    [self.contentStore enumerateContentGroupsOfKind:WMFContentGroupKindAnnouncement withBlock:^(WMFContentGroup * _Nonnull group, BOOL * _Nonnull stop) {
-        [group updateVisibility];
-    }];
+    [self.contentStore enumerateContentGroupsOfKind:WMFContentGroupKindAnnouncement
+                                          withBlock:^(WMFContentGroup *_Nonnull group, BOOL *_Nonnull stop) {
+                                              [group updateVisibility];
+                                          }];
 }
-
-
 
 @end
