4164fa26afd762974c3dd22dc6bbed26044f35d1
joewalsh <joewalsh@wikimedia.org>
2016-11-28 15:31:57 -0500

    use temporary yapdb

--- WikipediaUnitTests/Code/WMFNotificationTests.m
+++ WikipediaUnitTests/Code/WMFNotificationTests.m
@@ -1,6 +1,7 @@
 #import <XCTest/XCTest.h>
 #import <Nocilla/LSNocilla.h>
 #import "MWKDataStore+TemporaryDataStore.h"
+#import "WMFRandomFileUtilities.h"
 
 @interface WMFNotificationTests : XCTestCase
 
@@ -19,9 +20,10 @@
 - (void)setUp {
     [super setUp];
     NSURL *siteURL = [NSURL URLWithString:@"https://en.wikipedia.org"];
-    self.previewStore = [[WMFArticlePreviewDataStore alloc] initWithDatabase:[YapDatabase sharedInstance]];
-    self.contentStore = [[WMFContentGroupDataStore alloc] initWithDatabase:[YapDatabase sharedInstance]];
     MWKDataStore *dataStore = [MWKDataStore temporaryDataStore];
+    YapDatabase *db = [YapDatabase wmf_databaseWithDefaultConfigurationAtPath:WMFRandomTemporaryPath()];
+    self.previewStore = [[WMFArticlePreviewDataStore alloc] initWithDatabase:db];
+    self.contentStore = [[WMFContentGroupDataStore alloc] initWithDatabase:db];
     self.feedContentSource = [[WMFFeedContentSource alloc] initWithSiteURL:siteURL contentGroupDataStore:self.contentStore articlePreviewDataStore:self.previewStore userDataStore:dataStore notificationsController:[WMFNotificationsController sharedNotificationsController]];
     self.feedContentSource.notificationSchedulingEnabled = YES;
 
