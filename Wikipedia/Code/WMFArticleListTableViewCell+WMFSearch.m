49ec8789d9491c5fe78174de9f8406071bf72dfb
joewalsh <joewalsh@wikimedia.org>
2016-12-06 08:12:00 -0500

    nil check highlightText - fix crash

--- Wikipedia/Code/WMFArticleListTableViewCell+WMFSearch.m
+++ Wikipedia/Code/WMFArticleListTableViewCell+WMFSearch.m
@@ -8,23 +8,23 @@
 
 - (void)wmf_setTitleText:(NSString *)text highlightingText:(NSString *)highlightText {
     NSMutableAttributedString *attributedTitle =
-    [[NSMutableAttributedString alloc] initWithString:text
-                                           attributes:@{
-                                                        NSFontAttributeName: [[self class] titleLabelFont]
-                                                        }];
-    
-    NSRange highlightRange = [text rangeOfString:highlightText options:NSCaseInsensitiveSearch];
-    if (!WMFRangeIsNotFoundOrEmpty(highlightRange)) {
-        [attributedTitle addAttribute:NSBackgroundColorAttributeName
-                                value:[UIColor colorWithRed:1.0 green:0.8 blue:0.2 alpha:0.3]
-                                range:highlightRange];
+        [[NSMutableAttributedString alloc] initWithString:text
+                                               attributes:@{
+                                                   NSFontAttributeName: [[self class] titleLabelFont]
+                                               }];
+
+    if (highlightText) {
+        NSRange highlightRange = [text rangeOfString:highlightText options:NSCaseInsensitiveSearch];
+        if (!WMFRangeIsNotFoundOrEmpty(highlightRange)) {
+            [attributedTitle addAttribute:NSBackgroundColorAttributeName
+                                    value:[UIColor colorWithRed:1.0 green:0.8 blue:0.2 alpha:0.3]
+                                    range:highlightRange];
+        }
     }
-    
     self.titleLabel.attributedText = attributedTitle;
 }
 
 @end
-
 
 @implementation WMFArticleListCollectionViewCell (WMFSearch)
 
@@ -34,18 +34,18 @@
 
 - (void)wmf_setTitleText:(NSString *)text highlightingText:(NSString *)highlightText {
     NSMutableAttributedString *attributedTitle =
-    [[NSMutableAttributedString alloc] initWithString:text
-                                           attributes:@{
-                                                        NSFontAttributeName: [[self class] titleLabelFont]
-                                                        }];
-    
+        [[NSMutableAttributedString alloc] initWithString:text
+                                               attributes:@{
+                                                   NSFontAttributeName: [[self class] titleLabelFont]
+                                               }];
+
     NSRange highlightRange = [text rangeOfString:highlightText options:NSCaseInsensitiveSearch];
     if (!WMFRangeIsNotFoundOrEmpty(highlightRange)) {
         [attributedTitle addAttribute:NSBackgroundColorAttributeName
                                 value:[UIColor colorWithRed:1.0 green:0.8 blue:0.2 alpha:0.3]
                                 range:highlightRange];
     }
-    
+
     self.titleLabel.attributedText = attributedTitle;
 }
 
