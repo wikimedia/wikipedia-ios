14f6a6e930ef89d7f31e5d042d2326a513b9d8a8
montehurd <mhurd@wikimedia.org>
2016-12-08 13:58:58 -0800

    Remove unused category methods.

--- Wikipedia/Code/WMFArticleListTableViewCell+WMFSearch.m
+++ Wikipedia/Code/WMFArticleListTableViewCell+WMFSearch.m
@@ -25,28 +25,3 @@
 }
 
 @end
-
-@implementation WMFArticleListCollectionViewCell (WMFSearch)
-
-+ (UIFont *)titleLabelFont {
-    return [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-}
-
-- (void)wmf_setTitleText:(NSString *)text highlightingText:(NSString *)highlightText {
-    NSMutableAttributedString *attributedTitle =
-        [[NSMutableAttributedString alloc] initWithString:text
-                                               attributes:@{
-                                                   NSFontAttributeName: [[self class] titleLabelFont]
-                                               }];
-
-    NSRange highlightRange = [text rangeOfString:highlightText options:NSCaseInsensitiveSearch];
-    if (!WMFRangeIsNotFoundOrEmpty(highlightRange)) {
-        [attributedTitle addAttribute:NSBackgroundColorAttributeName
-                                value:[UIColor colorWithRed:1.0 green:0.8 blue:0.2 alpha:0.3]
-                                range:highlightRange];
-    }
-
-    self.titleLabel.attributedText = attributedTitle;
-}
-
-@end
