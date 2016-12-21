e25c97716fd63a7a90de5d1909719eb34e77393a
joewalsh <joewalsh@wikimedia.org>
2016-12-21 13:21:39 -0500

    run when missing previous hash

--- scripts/twn.rb
+++ scripts/twn.rb
@@ -16,16 +16,11 @@ hash_file = "~/PR_#{pr_branch.upcase}_CURRENT_HASH"
 previous_hash = `cat #{hash_file}`.strip
 `echo "#{current_hash}" > #{hash_file}`
 
-if !previous_hash || previous_hash == ''
-  puts "missing previous hash"
-  abort
-end
-
 if previous_hash == current_hash
   puts "no changes"
   abort
 end
 
-puts "#{pr_branch} went from #{previous_hash} to #{current_hash}, opening PR"
+puts "#{pr_branch} went from #{previous_hash} to #{current_hash}, opening pr"
   
 puts `curl -i -d '{"title":"#{title}","head":"#{pr_branch}","base":"#{base_branch}"}' -H "Authorization: token#{ENV['GITHUB_TWN_ACCESS_TOKEN']} " -H "Content-Type: application/json; charset=utf-8" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/wikimedia/wikipedia-ios/pulls`
