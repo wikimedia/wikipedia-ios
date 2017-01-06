e948d366404ffddc7a5f45bcaccf8661663024dc
joewalsh <joewalsh@wikimedia.org>
2017-01-06 10:51:54 -0500

    add NearbyViewController

---
+++ Wikipedia/Code/NearbyViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import MapKit
+
+class NearbyViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+    }
+
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+    }
+}
+
