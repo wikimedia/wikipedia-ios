06b62cb814b1fed8eae47c451a59ff0afb6761f8
joewalsh <joewalsh@wikimedia.org>
2017-01-23 16:22:01 -0500

    add search bar and redo search button

--- Wikipedia/Code/PlacesViewController.swift
+++ Wikipedia/Code/PlacesViewController.swift
@@ -1,13 +1,23 @@
 import UIKit
 import MapKit
 
-class PlacesViewController: UIViewController, MKMapViewDelegate {
+class PlacesViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var redoSearchButton: UIButton!
+    let nearbyFetcher = WMFLocationSearchFetcher()
     @IBOutlet weak var mapView: MKMapView!
-    
+    var searchBar: UISearchBar!
+
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        //Override UINavigationBar.appearance settings from WMFStyleManager
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = nil
+        
         mapView.setUserTrackingMode(.follow, animated: true)
+        searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 32))
+        navigationItem.titleView = searchBar
+        searchBar.delegate = self
     }
 
     func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
@@ -21,5 +31,11 @@ class PlacesViewController: UIViewController, MKMapViewDelegate {
     func mapViewDidStopLocatingUser(_ mapView: MKMapView) {
         
     }
+    
+    @IBAction func redoSearch(_ sender: Any) {
+        
+    }
+    
+    
 }
 
