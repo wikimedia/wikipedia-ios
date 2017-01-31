627cab309446578758c3e432a20f393f5a8807bb
montehurd <mhurd@wikimedia.org>
2017-01-30 18:41:40 -0800

    Remove unused files.

--- Wikipedia/Code/WMFURLSessionDataTaskTypes.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-public typealias WMFURLSessionDataTaskSuccessHandler = ((URLSessionDataTask, Any) -> Void)?
-public typealias WMFURLSessionDataTaskFailureHandler = ((URLSessionDataTask?, Error) -> Void)?
