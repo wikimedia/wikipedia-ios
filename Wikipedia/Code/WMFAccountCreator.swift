f3e306f3d52260308340911034b1665a7f48369b
montehurd <mhurd@wikimedia.org>
2017-02-01 15:33:53 -0800

    New account creator.

---
+++ Wikipedia/Code/WMFAccountCreator.swift
@@ -0,0 +1,87 @@
+
+public enum WMFAccountCreatorError: LocalizedError {
+    case cannotExtractStatus
+    case statusNotPass(String?)
+    case needsCaptcha
+    public var errorDescription: String? {
+        switch self {
+        case .cannotExtractStatus:
+            return "Could not extract status"
+        case .statusNotPass(let message?):
+            return message
+        case .needsCaptcha:
+            return "Needs captcha"
+        default:
+            return "Unable to create account: Reason unknown"
+        }
+    }
+}
+
+public typealias WMFAccountCreatorResultBlock = (WMFAccountCreatorResult) -> Void
+
+public class WMFAccountCreatorResult: NSObject {
+    var status: String
+    var username: String
+    var message: String?
+    init(status: String, username: String, message: String?) {
+        self.status = status
+        self.username = username
+        self.message = message
+    }
+}
+
+public class WMFAccountCreator: NSObject {
+    private let manager = AFHTTPSessionManager.wmf_createDefault()
+    public func isFetching() -> Bool {
+        return manager!.operationQueue.operationCount > 0
+    }
+    
+    public func createAccount(username: String, password: String, email: String?, captchaID: String?, captchaWord: String?, token: String, siteURL: URL, completion: @escaping WMFAccountCreatorResultBlock, failure: @escaping WMFErrorHandler){
+        let manager = AFHTTPSessionManager(baseURL: siteURL)
+        manager.responseSerializer = WMFApiJsonResponseSerializer.init();
+        
+        var parameters = [
+            "action": "createaccount",
+            "username": username,
+            "password": password,
+            "retype": password,
+            "createreturnurl": "https://www.wikipedia.org",
+            "email": email,
+            "createtoken": token,
+            "format": "json"
+        ]
+        if let captchaID = captchaID {
+            parameters["captchaId"] = captchaID
+        }
+        if let captchaWord = captchaWord {
+            parameters["captchaWord"] = captchaWord
+        }
+        
+        _ = manager.wmf_apiPOSTWithParameters(parameters, success: {
+            (_, response: Any?) in
+         
+            guard
+                let response = response as? [String : AnyObject],
+                let createaccount = response["createaccount"] as? [String : AnyObject],
+                let status = createaccount["status"] as? String
+                else {
+                    failure(WMFAccountCreatorError.cannotExtractStatus)
+                    return
+            }
+            let message = createaccount["message"] as? String ?? nil
+            guard status == "PASS" else {
+                if message == "Incorrect or missing CAPTCHA." {
+                    failure(WMFAccountCreatorError.needsCaptcha)
+                }else{
+                    failure(WMFAccountCreatorError.statusNotPass(message))
+                }
+                return
+            }
+            let normalizedUsername = createaccount["username"] as? String ?? username
+            completion(WMFAccountCreatorResult.init(status: status, username: normalizedUsername, message: message))
+        }, failure: {
+            (_, error: Error) in
+            failure(error)
+        })
+    }
+}
