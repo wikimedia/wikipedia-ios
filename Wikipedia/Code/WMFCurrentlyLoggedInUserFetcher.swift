918ad5622f95e9ec57343daa72c3f98f8400bed0
montehurd <mhurd@wikimedia.org>
2017-02-06 19:37:33 -0800

    New vc & storyboard for 2 factor login. New currently logged in user fetcher.
    Because 2FA accounts *always* require 2FA token on login attempts
    we can't just always try to log in with saved credentials on app start.
    ( We'd have to bug the user for 2FA token each time. )
    Instead ask the API via "action=query&meta=userinfo" to see if a user
    is logged in.

---
+++ Wikipedia/Code/WMFCurrentlyLoggedInUserFetcher.swift
@@ -0,0 +1,81 @@
+
+@objc public enum WMFCurrentlyLoggedInUserFetcherErrorType: Int {
+    case cannotExtractUserInfo
+    case userIsAnonymous
+}
+
+// A CustomNSError's localized description survives @objc bridging
+// TODO: once WMFAuthenticationManager is converted to Swift we can go
+// back to using "public enum WMFCurrentlyLoggedInUserFetcherError: LocalizedError".
+public class WMFCurrentlyLoggedInUserFetcherError: CustomNSError {
+    let type: WMFCurrentlyLoggedInUserFetcherErrorType
+    let localizedDescription: String?
+    public required init(type:WMFCurrentlyLoggedInUserFetcherErrorType, localizedDescription: String?) {
+        self.type = type
+        self.localizedDescription = localizedDescription
+    }
+    public static var errorDomain: String {
+        return String(describing:self)
+    }
+    public var errorCode: Int {
+        return type.rawValue
+    }
+    public var errorUserInfo: [String : Any] {
+        var userInfo = [String: Any]()
+        if let localizedDescription = self.localizedDescription {
+            userInfo[NSLocalizedDescriptionKey] = localizedDescription
+        }
+        return userInfo
+    }
+}
+
+public typealias WMFCurrentlyLoggedInUserBlock = (WMFCurrentlyLoggedInUser) -> Void
+
+@objc public class WMFCurrentlyLoggedInUser: NSObject {
+    var userID: Int
+    var name: String
+    init(userID: Int, name: String) {
+        self.userID = userID
+        self.name = name
+    }
+}
+
+public class WMFCurrentlyLoggedInUserFetcher: NSObject {
+    private let manager = AFHTTPSessionManager.wmf_createDefault()
+    public func isFetching() -> Bool {
+        return manager!.operationQueue.operationCount > 0
+    }
+    
+    public func fetch(siteURL: URL, completion: @escaping WMFCurrentlyLoggedInUserBlock, failure: @escaping WMFErrorHandler){
+        let manager = AFHTTPSessionManager(baseURL: siteURL)
+        manager.responseSerializer = WMFApiJsonResponseSerializer.init();
+        
+        let parameters = [
+            "action": "query",
+            "meta": "userinfo",
+            "format": "json"
+        ]
+        
+        _ = manager.wmf_apiPOSTWithParameters(parameters, success: {
+            (_, response: Any?) in
+            guard
+                let response = response as? [String : AnyObject],
+                let query = response["query"] as? [String : AnyObject],
+                let userinfo = query["userinfo"] as? [String : AnyObject],
+                let userID = userinfo["id"] as? Int,
+                let userName = userinfo["name"] as? String
+                else {
+                    failure(WMFCurrentlyLoggedInUserFetcherError.init(type:.cannotExtractUserInfo, localizedDescription: "Could not extract user info"))
+                    return
+            }
+            guard (userinfo["anon"] == nil) else {
+                failure(WMFCurrentlyLoggedInUserFetcherError.init(type:.userIsAnonymous, localizedDescription: "User is anonymous"))
+                return
+            }
+            completion(WMFCurrentlyLoggedInUser.init(userID: userID, name: userName))
+        }, failure: {
+            (_, error: Error) in
+            failure(error)
+        })
+    }
+}
---
+++ Wikipedia/Code/WMFTwoFactorPasswordViewController.swift
@@ -0,0 +1,100 @@
+
+import UIKit
+
+class WMFTwoFactorPasswordViewController: UIViewController {
+    
+    @IBOutlet fileprivate var titleLabel: UILabel!
+    @IBOutlet fileprivate var subTitleLabel: UILabel!
+    @IBOutlet fileprivate var oathTokenField: UITextField!
+    @IBOutlet fileprivate var oathTokenUnderlineHeight: NSLayoutConstraint!
+    
+    fileprivate var doneButton: UIBarButtonItem!
+    
+    public var funnel: LoginFunnel?
+    
+    public var userName:String?
+    public var password:String?
+    
+    func doneButtonPushed(_ tap: UITapGestureRecognizer) {
+        save()
+    }
+    
+    func textFieldDidChange(_ sender: UITextField) {
+        enableProgressiveButton((oathTokenField.text!.characters.count > 0))
+    }
+    
+    func enableProgressiveButton(_ highlight: Bool) {
+        doneButton.isEnabled = highlight
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        enableProgressiveButton(false)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        oathTokenField.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        enableProgressiveButton(false)
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == self.oathTokenField) {
+            save()
+        }
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named:"close"), style: .plain, target:self, action:#selector(self.didTapClose(_:)))
+        
+        doneButton = UIBarButtonItem(title: localizedStringForKeyFallingBackOnEnglish("main-menu-account-login"), style: .plain, target: self, action: #selector(self.doneButtonPushed(_:)))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        oathTokenField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        
+        oathTokenUnderlineHeight.constant = 1.0 / UIScreen.main.scale
+        
+        titleLabel.text = localizedStringForKeyFallingBackOnEnglish("two-factor-login-title")
+        subTitleLabel.text = localizedStringForKeyFallingBackOnEnglish("two-factor-login-instructions")
+        oathTokenField.placeholder = localizedStringForKeyFallingBackOnEnglish("two-factor-login-token-placeholder-text")
+    }
+    
+    func didTapClose(_ tap: UITapGestureRecognizer) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func save() {
+        enableProgressiveButton(false)
+        WMFAlertManager.sharedInstance.dismissAlert()
+        
+        guard
+            let userName = userName,
+            let password = password
+        else {
+            return
+        }
+
+        WMFAuthenticationManager.sharedInstance()
+            .login(withUsername: userName,
+                   password: password,
+                   retypePassword: nil,
+                   oathToken: oathTokenField.text!,
+                   success: {
+                    let loggedInMessage = localizedStringForKeyFallingBackOnEnglish("main-menu-account-title-logged-in").replacingOccurrences(of: "$1", with: userName)
+                    WMFAlertManager.sharedInstance.showSuccessAlert(loggedInMessage, sticky: false, dismissPreviousAlerts: true, tapCallBack: nil)
+                    self.dismiss(animated: true, completion: nil)
+                    self.funnel?.logSuccess()
+            }, failure: { (error: Error) in
+                self.enableProgressiveButton(true)
+                WMFAlertManager.sharedInstance.showErrorAlert(error as NSError, sticky: true, dismissPreviousAlerts: true, tapCallBack: nil)
+                self.funnel?.logError(error.localizedDescription)
+                self.oathTokenField.text = nil
+            })
+    }
+}
