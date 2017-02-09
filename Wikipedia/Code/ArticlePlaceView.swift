5e72f9cfe120e6d08f8dd8615b6e126d11253570
joewalsh <joewalsh@wikimedia.org>
2017-02-09 09:16:05 -0500

    white background so images with transparency display correctly

--- Wikipedia/Code/ArticlePlaceView.swift
+++ Wikipedia/Code/ArticlePlaceView.swift
@@ -81,13 +81,17 @@ class ArticlePlaceView: MKAnnotationView {
                 dotView.backgroundColor = UIColor.wmf_green()
                 let article = articlePlace.articles[0]
                 if let thumbnailURL = article.thumbnailURL {
+                    selectedImageView.backgroundColor = UIColor.white
                     imageView.backgroundColor = UIColor.white
                     imageView.wmf_setImage(with: thumbnailURL, detectFaces: true, onGPU: true, failure: { (error) in
                         self.imageView.backgroundColor = UIColor.wmf_green()
                         self.selectedImageView.backgroundColor = UIColor.wmf_green()
+                        self.selectedImageView.image = nil
+                        self.imageView.image = nil
                     }, success: {
                         self.selectedImageView.wmf_setImage(with: thumbnailURL, detectFaces: true, onGPU: true, failure: { (error) in
-                            
+                            self.selectedImageView.backgroundColor = UIColor.wmf_green()
+                            self.selectedImageView.image = nil
                         }, success: {
                             
                         })
