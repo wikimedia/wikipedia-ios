ada848171640eb330ab74fac712e24bd1ea7f889
joewalsh <joewalsh@wikimedia.org>
2017-02-09 06:31:37 -0500

    use localized OK

--- Wikipedia/Code/UIViewController+WMFAlerts.swift
+++ Wikipedia/Code/UIViewController+WMFAlerts.swift
@@ -4,13 +4,13 @@ extension UIViewController {
     
     func wmf_showAlertWithError(_ error: NSError) {
         let alert = UIAlertController(title: error.localizedDescription, message: error.localizedRecoverySuggestion, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style:.default, handler: nil))
+        alert.addAction(UIAlertAction(title: localizedStringForKeyFallingBackOnEnglish("button-ok"), style:.default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
     
     func wmf_showAlertWithMessage(_ message: String) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style:.default, handler: nil))
+        alert.addAction(UIAlertAction(title: localizedStringForKeyFallingBackOnEnglish("button-ok"), style:.default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
     
