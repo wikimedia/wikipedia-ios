7efdb2ad84436c676bc9ef36148ed90d71221a7c
Andrew Mroczkowski <andy.mroczkowski@gmail.com>
2017-02-21 13:49:55 -0500

    Checking in face bounds are less than 25% of the image

--- Wikipedia/Code/UIImageView+WMFImageFetchingInternal.m
+++ Wikipedia/Code/UIImageView+WMFImageFetchingInternal.m
@@ -168,7 +168,8 @@ static const char *const WMFImageControllerAssociationKey = "WMFImageController"
              success:(WMFSuccessHandler)success {
     NSAssert([NSThread isMainThread], @"Interaction with a UIImageView should only happen on the main thread");
     CGRect faceBounds = [faceBoundsValue CGRectValue];
-    if (!CGRectIsEmpty(faceBounds)) {
+    CGFloat faceArea = faceBounds.size.width * faceBounds.size.height;
+    if (!CGRectIsEmpty(faceBounds) && (faceArea >= 0.25)) {
         [self wmf_cropContentsByVerticallyCenteringFrame:[image wmf_denormalizeRect:faceBounds]
                                      insideBoundsOfImage:image];
     } else {
