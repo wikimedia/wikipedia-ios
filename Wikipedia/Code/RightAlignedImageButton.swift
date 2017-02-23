56f7df687ad053d6c4426bceb8276d81f1b8355f
joewalsh <joewalsh@wikimedia.org>
2017-02-23 18:56:04 -0500

    update RightAlignedImageButton to work with RTL

--- Wikipedia/Code/RightAlignedImageButton.swift
+++ Wikipedia/Code/RightAlignedImageButton.swift
@@ -1,17 +1,40 @@
 import UIKit
 
 class RightAlignedImageButton: UIButton {
+    
+    let imageSpacing: CGFloat = 5
+    
     override func titleRect(forContentRect contentRect: CGRect) -> CGRect {
         var titleRect = super.titleRect(forContentRect: contentRect)
         let imageRect = super.imageRect(forContentRect: contentRect)
-        titleRect.origin.x = imageRect.origin.x
+        switch effectiveUserInterfaceLayoutDirection {
+        case .rightToLeft:
+            titleRect.origin.x = imageRect.maxX - titleRect.size.width - imageSpacing
+        default:
+            titleRect.origin.x = imageRect.minX
+        }
         return titleRect
     }
     
     override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
         var imageRect = super.imageRect(forContentRect: contentRect)
         let titleRect = super.titleRect(forContentRect: contentRect)
-        imageRect.origin.x = titleRect.origin.x + titleRect.size.width
+        switch effectiveUserInterfaceLayoutDirection {
+        case .rightToLeft:
+            imageRect.origin.x = titleRect.minX
+        default:
+            imageRect.origin.x = titleRect.maxX - imageRect.size.width + imageSpacing
+        }
         return imageRect
+    }
+    
+    override var effectiveUserInterfaceLayoutDirection: UIUserInterfaceLayoutDirection {
+        get {
+            if #available(iOS 10.0, *) {
+                return super.effectiveUserInterfaceLayoutDirection
+            } else {
+                return UIView.userInterfaceLayoutDirection(for: semanticContentAttribute)
+            }
+        }
     }
 }
