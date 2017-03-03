1869c00c9e74e3f72520b9d70914734ad3b281fa
montehurd <mhurd@wikimedia.org>
2017-03-03 14:47:17 -0800

    Revert "Stop using sd web image for fetching captcha image - don't want these cached."
    This reverts commit e24e660db5f94dd2b41cc15e1d410b0baea1d347.

--- WMF Framework/WMFCaptchaViewController.swift
+++ WMF Framework/WMFCaptchaViewController.swift
@@ -110,18 +110,7 @@ class WMFCaptchaViewController: UIViewController, UITextFieldDelegate {
             assert(false, "Unable to determine fullCaptchaImageURL")
             return
         }
-        URLSession.shared.dataTask(with: fullCaptchaImageURL, completionHandler: { (data, _, error) -> Void in
-            guard
-                error == nil,
-                let data = data,
-                let image = UIImage(data: data)
-            else {
-                return
-            }
-            DispatchQueue.main.async{
-                self.captchaImageView.image = image
-            }
-        }).resume()
+        captchaImageView.sd_setImage(with: fullCaptchaImageURL)
     }
     
     public func captchaTextFieldBecomeFirstResponder() {
