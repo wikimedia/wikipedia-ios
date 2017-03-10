5f5865f3b71405ca060565366f88daf65edc62d8
joewalsh <joewalsh@wikimedia.org>
2017-03-10 08:47:17 -0500

    add missing imports

--- Wikipedia/Code/ArticlePopoverViewController.swift
+++ Wikipedia/Code/ArticlePopoverViewController.swift
@@ -1,6 +1,5 @@
 import UIKit
 
-
 protocol ArticlePopoverViewControllerDelegate: NSObjectProtocol {
     func articlePopoverViewController(articlePopoverViewController: ArticlePopoverViewController, didSelectAction: WMFArticleAction)
 }
