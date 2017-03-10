cc46400d9bd6938ac8afaa6967f52f52b23127ac
montehurd <mhurd@wikimedia.org>
2017-03-09 20:07:48 -0800

    Clean up WMFAuthLinkLabel a bit.
    Broke fonts out to read only computed properties.
    
    Made explicit method for combining.

--- Wikipedia/Code/WMFAuthLinkLabel.swift
+++ Wikipedia/Code/WMFAuthLinkLabel.swift
@@ -19,23 +19,35 @@ class WMFAuthLinkLabel: UILabel {
     /// Some auth labels display a string from two localized strings, each styled differently.
     public var strings: WMFAuthLinkLabelStrings?
 
+    var boldSubheadlineFont: UIFont {
+        get {
+            return UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.systemBold, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
+        }
+    }
+
+    var subheadlineFont: UIFont {
+        get {
+            return UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.system, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
+        }
+    }
+
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
-        
-        let boldSubheadlineFont = UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.systemBold, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
-
         guard let strings = strings else {
-            // Not combining two strings.
             font = boldSubheadlineFont
             return
         }
-
-        // Combine and style 'dollarSignString' and 'substitutionString'.
-        // https://github.com/wikimedia/wikipedia-ios/pull/1216#discussion_r104224511
-        let subheadlineFont = UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.system, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
+        attributedText = combineAndStyleStrings(strings)
+    }
+    
+    fileprivate func combineAndStyleStrings(_ strings: WMFAuthLinkLabelStrings) -> NSAttributedString {
+        // Combine and style 'dollarSignString' and 'substitutionString': https://github.com/wikimedia/wikipedia-ios/pull/1216#discussion_r104224511
+        
         let dollarSignStringAttributes: [String:Any] = [NSForegroundColorAttributeName : UIColor.black, NSFontAttributeName : subheadlineFont]
         let substitutionStringAttributes: [String:Any] = [NSForegroundColorAttributeName : UIColor.wmf_blueTint(), NSFontAttributeName : boldSubheadlineFont]
+        
         assert(strings.dollarSignString.contains("$1"), "Expected dollar sign substitution placeholder not found")
-        attributedText = strings.dollarSignString.attributedString(attributes: dollarSignStringAttributes, substitutionStrings: [strings.substitutionString], substitutionAttributes: [substitutionStringAttributes])
+        
+        return strings.dollarSignString.attributedString(attributes: dollarSignStringAttributes, substitutionStrings: [strings.substitutionString], substitutionAttributes: [substitutionStringAttributes])
     }
 }
