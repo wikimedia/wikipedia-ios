1897543373b50e3bcef4173d624dee3c0da7bb1c
montehurd <mhurd@wikimedia.org>
2017-03-09 19:41:21 -0800

    Let WMFAuthLinkLabel handle compound auth label strings.
    Once I hook this up we can delete UILabel+WMFAuth.swift, which didn't handle using custom WMF dynamic type fonts.

--- Wikipedia/Code/WMFAuthLinkLabel.swift
+++ Wikipedia/Code/WMFAuthLinkLabel.swift
@@ -1,4 +1,12 @@
 
+struct WMFAuthLinkLabelStrings {
+    /// String containing "$1" substring.
+    var dollarSignString: String
+    
+    /// String which will replace "$1" in "dollarSignString".
+    var substitutionString: String
+}
+
 class WMFAuthLinkLabel: UILabel {
     override open func awakeFromNib() {
         super.awakeFromNib()
@@ -7,8 +15,27 @@ class WMFAuthLinkLabel: UILabel {
         lineBreakMode = .byWordWrapping
         textAlignment = .natural
     }
+
+    /// Some auth labels display a string from two localized strings, each styled differently.
+    public var strings: WMFAuthLinkLabelStrings?
+
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
-        font = UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.systemBold, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)
+        
+        let boldSubheadlineFont = UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.systemBold, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
+
+        guard let strings = strings else {
+            // Not combining two strings.
+            font = boldSubheadlineFont
+            return
+        }
+
+        // Combine and style 'dollarSignString' and 'substitutionString'.
+        // https://github.com/wikimedia/wikipedia-ios/pull/1216#discussion_r104224511
+        let subheadlineFont = UIFont.wmf_preferredFontForFontFamily(WMFFontFamily.system, withTextStyle: .subheadline, compatibleWithTraitCollection: self.traitCollection)!
+        let dollarSignStringAttributes: [String:Any] = [NSForegroundColorAttributeName : UIColor.black, NSFontAttributeName : subheadlineFont]
+        let substitutionStringAttributes: [String:Any] = [NSForegroundColorAttributeName : UIColor.wmf_blueTint(), NSFontAttributeName : boldSubheadlineFont]
+        assert(strings.dollarSignString.contains("$1"), "Expected dollar sign substitution placeholder not found")
+        attributedText = strings.dollarSignString.attributedString(attributes: dollarSignStringAttributes, substitutionStrings: [strings.substitutionString], substitutionAttributes: [substitutionStringAttributes])
     }
 }
