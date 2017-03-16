18c8c5c724a00b418ed3419f2eacebdde569fca0
joewalsh <joewalsh@wikimedia.org>
2017-03-16 13:26:19 -0400

    apparently we do still need this hack

--- Wikipedia/Code/PlacesViewController.swift
+++ Wikipedia/Code/PlacesViewController.swift
@@ -69,7 +69,7 @@ class PlacesViewController: UIViewController, MKMapViewDelegate, UISearchBarDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redoSearchButton.setTitle(localizedStringForKeyFallingBackOnEnglish("places-search-this-area"), for: .normal)
+        redoSearchButton.setTitle("    " + localizedStringForKeyFallingBackOnEnglish("places-search-this-area" + "    "), for: .normal)
         
         // Setup map view
         mapView.mapType = .standard
