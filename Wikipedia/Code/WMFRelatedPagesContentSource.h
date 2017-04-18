0ac4fa2fbc99c5873b576d6e54bd5d208c641aeb
joewalsh <joewalsh@wikimedia.org>
2017-04-18 14:12:30 -0400

    add ability to block addition of new content from related page content source

--- Wikipedia/Code/WMFRelatedPagesContentSource.h
+++ Wikipedia/Code/WMFRelatedPagesContentSource.h
@@ -6,6 +6,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WMFRelatedPagesContentSource : NSObject <WMFContentSource, WMFDateBasedContentSource>
 
+- (void)loadContentForDate:(NSDate *)date inManagedObjectContext:(NSManagedObjectContext *)moc force:(BOOL)force addNewContent:(BOOL)shouldAddNewContent completion:(nullable dispatch_block_t)completion;
+
 @end
 
 NS_ASSUME_NONNULL_END
