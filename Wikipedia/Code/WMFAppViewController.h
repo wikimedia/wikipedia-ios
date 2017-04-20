3c6624da2197258d22091b5517154ded1234e322
joewalsh <joewalsh@wikimedia.org>
2017-04-20 08:43:07 -0400

    Revert "fix background fetch - it shouldn't rely on the app view controller being instantiated"
    This reverts commit 13c1c22af3b804a4b509026701e02bea8ade6b44.

--- Wikipedia/Code/WMFAppViewController.h
+++ Wikipedia/Code/WMFAppViewController.h
@@ -14,6 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (BOOL)processUserActivity:(NSUserActivity *)activity completion:(dispatch_block_t)done;
 
+- (void)performBackgroundFetchWithCompletion:(void (^)(UIBackgroundFetchResult))completion;
+
 @end
 
 NS_ASSUME_NONNULL_END
