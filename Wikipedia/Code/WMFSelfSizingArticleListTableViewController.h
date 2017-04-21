bb9dae538bc7b359647111aad6c1b2d0a6fa23cd
montehurd <mhurd@wikimedia.org>
2017-04-21 14:30:13 -0700

    Remove unused objects.

--- Wikipedia/Code/WMFSelfSizingArticleListTableViewController.h
+++
@@ -1,9 +0,0 @@
-#import "WMFArticleListTableViewController.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface WMFSelfSizingArticleListTableViewController : WMFArticleListTableViewController
-
-@end
-
-NS_ASSUME_NONNULL_END
