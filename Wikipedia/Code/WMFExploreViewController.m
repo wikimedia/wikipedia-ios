7492484b2cca30e35f91795f35f851a2467b7f5f
joewalsh <joewalsh@wikimedia.org>
2017-04-25 18:06:18 -0400

    fix crash on move from & to same index path

--- Wikipedia/Code/WMFExploreViewController.m
+++ Wikipedia/Code/WMFExploreViewController.m
@@ -1625,8 +1625,12 @@ const NSInteger WMFExploreFeedMaximumNumberOfDays = 30;
                         }
                     } break;
                     case NSFetchedResultsChangeMove:
-                        [self.collectionView deleteSections:[NSIndexSet indexSetWithIndex:change.fromIndexPath.row]];
-                        [self.collectionView insertSections:[NSIndexSet indexSetWithIndex:change.toIndexPath.row]];
+                        if (change.fromIndexPath.row == change.toIndexPath.row) {
+                            [self.collectionView reloadSections:change.toIndexPath.row];
+                        } else {
+                            [self.collectionView deleteSections:[NSIndexSet indexSetWithIndex:change.fromIndexPath.row]];
+                            [self.collectionView insertSections:[NSIndexSet indexSetWithIndex:change.toIndexPath.row]];
+                        }
                         break;
                 }
             }
