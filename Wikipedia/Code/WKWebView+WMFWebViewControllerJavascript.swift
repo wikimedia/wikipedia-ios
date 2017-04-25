0fdf35664ccc1dbe459df3fa42667ce938ce7507
montehurd <mhurd@wikimedia.org>
2017-04-24 18:59:22 -0700

    Refactor legal footer for clarity and inject container id.

--- Wikipedia/Code/WKWebView+WMFWebViewControllerJavascript.swift
+++ Wikipedia/Code/WKWebView+WMFWebViewControllerJavascript.swift
@@ -115,7 +115,7 @@ extension WKWebView {
         "function(){" +
             "window.webkit.messageHandlers.footerLegalLicenseLinkClicked.postMessage('linkClicked');" +
         "}"
-        evaluateJavaScript("window.wmf.footerLegal.add( '\(licenseString)', '\(licenseSubstitutionString)', \(licenseLinkClickHandler) );", completionHandler: nil)
+        evaluateJavaScript("window.wmf.footerLegal.add( 'footer_legal_container', '\(licenseString)', '\(licenseSubstitutionString)', \(licenseLinkClickHandler) );", completionHandler: nil)
     }
 
     public func wmf_addFooterReadMoreForArticle(_ article: MWKArticle){
@@ -132,12 +132,12 @@ extension WKWebView {
 
         let saveButtonTapHandler =
         "function(title){" +
-        "  window.webkit.messageHandlers.footerReadMoreSaveClicked.postMessage({'title': title})" +
+            "window.webkit.messageHandlers.footerReadMoreSaveClicked.postMessage({'title': title})" +
         "}"
         
         let titlesShownHandler =
         "function(titles){" +
-        "  window.webkit.messageHandlers.footerReadMoreTitlesShown.postMessage(titles)" +
+            "window.webkit.messageHandlers.footerReadMoreTitlesShown.postMessage(titles)" +
         "}";
         
         evaluateJavaScript("window.wmf.footerReadMore.add( '\(proxyURL)', '\(title)', '\(headerString)', '\(saveForLaterString)', '\(savedForLaterString)', \(saveButtonTapHandler), \(titlesShownHandler) );", completionHandler: nil)
--- www/js/transforms/footerLegal.js
+++ www/js/transforms/footerLegal.js
@@ -1,31 +1,23 @@
 
-function add(licenseString, licenseSubstitutionString, licenceLinkClickHandler) {
-  var container = document.getElementById('footer_legal_container');
-
-  var contents = document.createElement('div');
-  contents.className = 'footer_legal_contents';
-
+function add(containerID, licenseString, licenseSubstitutionString, licenceLinkClickHandler) {
+  var container = document.getElementById(containerID);
   var licenseStringHalves = licenseString.split('$1');
 
-  var licenseEl = document.createElement('span');
-  licenseEl.className = 'footer_legal_licence';
+  container.innerHTML = 
+  "<div class='footer_legal_contents'>" +
+    "<span class='footer_legal_licence'>" +
+      licenseStringHalves[0] +
+      "<a class='footer_legal_licence_link'>" +
+        licenseSubstitutionString +
+      "</a>" +
+      licenseStringHalves[1] +
+    "</span>" +
+  "</div>";
 
-  var firstHalf = document.createTextNode(licenseStringHalves[0]);
-  licenseEl.appendChild(firstHalf);
-
-  var licenseLinkEl = document.createElement('span');
-  licenseLinkEl.className = 'footer_legal_licence_link';
-  licenseLinkEl.innerText = licenseSubstitutionString;
-  licenseLinkEl.addEventListener('click', function(){
-    licenceLinkClickHandler();
-  }, false);
-  licenseEl.appendChild(licenseLinkEl);
-
-  var secondHalf = document.createTextNode(licenseStringHalves[1]);
-  licenseEl.appendChild(secondHalf);
-
-  contents.appendChild(licenseEl);
-  container.appendChild(contents);
+  container.querySelector('.footer_legal_licence_link')
+           .addEventListener('click', function(){
+             licenceLinkClickHandler();
+           }, false);
 }
 
 exports.add = add;
