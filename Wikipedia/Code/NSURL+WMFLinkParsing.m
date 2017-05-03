627ddf0fb9f99abb753a7846e9ac132b473a9b52
joewalsh <joewalsh@wikimedia.org>
2017-05-03 16:37:03 -0400

    remove unnecessary assertion

--- Wikipedia/Code/NSURL+WMFLinkParsing.m
+++ Wikipedia/Code/NSURL+WMFLinkParsing.m
@@ -226,7 +226,8 @@ NSString *const WMFAPIPath = @"/w/api.php";
 #if WMF_USE_BETA_CLUSTER
             || [lowercaseHost isEqualToString:WMFDefaultSiteMainDomain] || [lowercaseHost hasSuffix:mainWikiResourceSuffix]
 #endif
-            || [lowercaseHost isEqualToString:WMFDefaultSiteDomain] || [lowercaseHost hasSuffix:wikiResourceSuffix] ||[lowercaseHost isEqualToString:WMFMediaWikiDomain] || [lowercaseHost hasSuffix:WMFMediaWikiDomain]) && [self.path wmf_isWikiResource];
+            || [lowercaseHost isEqualToString:WMFDefaultSiteDomain] || [lowercaseHost hasSuffix:wikiResourceSuffix] || [lowercaseHost isEqualToString:WMFMediaWikiDomain] || [lowercaseHost hasSuffix:WMFMediaWikiDomain]) &&
+           [self.path wmf_isWikiResource];
 }
 
 - (BOOL)wmf_isWikiCitation {
@@ -294,7 +295,6 @@ NSString *const WMFAPIPath = @"/w/api.php";
 }
 
 - (NSURL *)wmf_articleDatabaseKeyURL {
-    NSParameterAssert(self.wmf_title);
     NSURLComponents *components = [NSURLComponents componentsWithURL:self resolvingAgainstBaseURL:NO];
     components.host = [NSURLComponents wmf_hostWithDomain:self.wmf_domain language:self.wmf_language isMobile:NO];
     components.fragment = nil;
