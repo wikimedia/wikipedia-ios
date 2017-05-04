aa7ccfedbc216f80b345bb6be4cb629256f0c4fc
joewalsh <joewalsh@wikimedia.org>
2017-05-04 09:00:23 -0400

    cache language bundles

--- WMF Framework/WMFLocalization.m
+++ WMF Framework/WMFLocalization.m
@@ -6,7 +6,30 @@
 + (NSBundle *)wmf_localizationBundle {
     return [NSBundle wmf];
 }
+
++ (nonnull NSMutableDictionary *)wmf_languageBundles {
+    static dispatch_once_t onceToken;
+    static NSMutableDictionary *wmf_languageBundles;
+    dispatch_once(&onceToken, ^{
+        wmf_languageBundles = [NSMutableDictionary new];
+    });
+    return wmf_languageBundles;
 }
+
+
+- (nullable NSBundle *)wmf_languageBundleForLanguage:(nonnull NSString *)language {
+    NSMutableDictionary *bundles = [NSBundle wmf_languageBundles];
+    NSString *path = [self pathForResource:language ofType:@"lproj"];
+    NSBundle *bundle = bundles[path];
+    if (!bundle) {
+        bundle = [NSBundle bundleWithPath:path];
+        if (bundle) {
+            bundles[path] = bundle;
+        }
+    }
+    return bundle;
+}
+
 @end
 
 NSString *WMFLocalizedStringWithDefaultValue(NSString *key, NSURL * _Nullable url, NSBundle * _Nullable bundle, NSString *value, NSString *comment) {
@@ -17,8 +40,8 @@ NSString *WMFLocalizedStringWithDefaultValue(NSString *key, NSURL * _Nullable ur
     if (language == nil) {
        return NSLocalizedStringWithDefaultValue(key, nil, bundle, value, comment);
     }
-    NSString *path = [bundle pathForResource:language ofType:@"lproj"];
-    NSBundle *languageBundle = [NSBundle bundleWithPath:path];
+
+    NSBundle *languageBundle = [bundle wmf_languageBundleForLanguage:language];
     NSString *translation = nil;
     if (languageBundle) {
         translation = [languageBundle localizedStringForKey:key value:@"" table:nil];
