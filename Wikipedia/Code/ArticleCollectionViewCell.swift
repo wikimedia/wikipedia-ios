634eda81cd0a25cd3b04ac07512dc27d1a2ee00b
joewalsh <joewalsh@wikimedia.org>
2017-05-10 17:00:41 -0400

    featured article cards

--- Wikipedia/Code/ArticleCollectionViewCell.swift
+++ Wikipedia/Code/ArticleCollectionViewCell.swift
@@ -3,15 +3,17 @@ import UIKit
 @objc(WMFArticleCollectionViewCell) public class ArticleCollectionViewCell: WMFExploreCollectionViewCell {
     
     open var estimatedHeight: CGFloat {
+        assert(false, "Subclassers must implement estimatedHeight")
         return 0
     }
     
     open var imageWidth: Int {
+        assert(false, "Subclassers must implement imageWidth")
         return 0
     }
     
     open class var nibName: String {
-        assert(false, "Subclassers should implement nibName")
+        assert(false, "Subclassers must implement nibName")
         return "ArticleCollectionViewCell"
     }
     
@@ -19,12 +21,16 @@ import UIKit
         return UINib(nibName: nibName, bundle: nil)
     }
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var textContainerView: UIView?
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var extractLabel: UILabel?
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageContainerView: UIView?
+
     @IBOutlet weak var saveButton: SaveButton?
+    @IBOutlet weak var saveButtonContainerView: UIView?
     
     public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
@@ -40,9 +46,42 @@ import UIKit
         }
     }
     
+    public final var isSaveButtonHidden = false {
+        didSet {
+            saveButton?.isHidden = isSaveButtonHidden
+            saveButtonContainerView?.isHidden = isSaveButtonHidden
+        }
+    }
+    
     public override func prepareForReuse() {
         super.prepareForReuse()
         imageView.wmf_reset()
         saveButton?.saveButtonState = .longSave
+    }
+}
+
+extension ArticleCollectionViewCell {
+    public func configure(article: WMFArticle, contentGroup: WMFContentGroup, layoutOnly: Bool) {
+        if let imageURL = article.imageURL(forWidth: self.imageWidth) {
+            isImageViewHidden = false
+            if !layoutOnly {
+                imageView.wmf_setImage(with: imageURL, detectFaces: true, onGPU: true, failure: { (error) in }, success: {  })
+            }
+        } else {
+            isImageViewHidden = true
+        }
+        
+        titleLabel.text = article.displayTitle
+        if contentGroup.displayType() == WMFFeedDisplayType.pageWithPreview {
+            textContainerView?.backgroundColor = UIColor.white
+            descriptionLabel.text = article.wikidataDescription
+            extractLabel?.text = article.snippet
+            isSaveButtonHidden = false
+        } else {
+            descriptionLabel.text = article.wikidataDescriptionOrSnippet
+            textContainerView?.backgroundColor = UIColor.wmf_lightGrayCellBackground
+            extractLabel?.text = nil
+            isSaveButtonHidden = true
+        }
     }
 }
