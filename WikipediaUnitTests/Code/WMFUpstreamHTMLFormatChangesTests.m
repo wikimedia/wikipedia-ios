1ff29d96d3ac3fb197615eb55c5305fbc471793d
montehurd <mhurd@wikimedia.org>
2017-05-12 17:09:48 -0700

    Small cleanups to new test.

--- WikipediaUnitTests/Code/WMFUpstreamHTMLFormatChangesTests.m
+++ WikipediaUnitTests/Code/WMFUpstreamHTMLFormatChangesTests.m
@@ -86,28 +86,27 @@
 
 - (void)testFormatOfMobileviewSectionHTMLHasNotChangedOnBeta {
     NSArray *sectionsFromFixtureFile = [self loadJSON:@"obama-beta-cluster-revision-349208"][@"mobileview"][@"sections"];
-    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for article fetch"];
     NSURL *betaClusterURL = [NSURL URLWithString:@"https://en.m.wikipedia.beta.wmflabs.org/w/api.php?action=mobileview&format=json&page=Barack_Obama&sections=all&prop=text%7Csections%7Crevision&revision=349208"];
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:betaClusterURL
-                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                                                NSArray *sectionsFromBetaCluster = json[@"mobileview"][@"sections"];
-                                                
-                                                XCTAssertEqualObjects(@(sectionsFromBetaCluster.count), @(sectionsFromFixtureFile.count), @"Section count mismatch! See test for details.");
-                                                
-                                                for (NSInteger i = 0; i < sectionsFromBetaCluster.count; i++) {
-                                                    NSDictionary *sectionFromFixtureFile = sectionsFromFixtureFile[i];
-                                                    NSDictionary *sectionFromBetaCluster = sectionsFromBetaCluster[i];
-                                                    NSString *sectionTextFromFixture = sectionFromFixtureFile[@"text"];
-                                                    NSString *sectionTextFromBetaCluster = sectionFromBetaCluster[@"text"];
-                                                    XCTAssertTrue([sectionTextFromFixture isEqualToString:sectionTextFromBetaCluster], @"Something staged on the beta cluster appears to have changed something about the structure of the section HTML delivered by mobileview. (Background: We had a really bad change make it to production which introduced an opening DIV tag *without* a matching closing DIV tag to the first section HMTL for every mobileview request: https://phabricator.wikimedia.org/T165115). This test which just failed is an early warning sign that something has been staged to the beta cluster which may be similarly problematic if it is deployed to production. Don't ignore this test if it fails! This test makes a mobileview request to the beta cluster for a specific revision of an article and compares the fetched section html with that from a fixture file containing JSON from the same request. A failing test indicates mobileview output has diverged from what it output at the time we created the fixture file. If the diff (between 'sectionTextFromFixture' and 'sectionTextFromBetaCluster') is examined and the change is acceptable you'll need to regenerate the fixture file using the same url the test uses to make the api request. (No need to change the url or the fixture file name - can keep testing against this old revision.) If the diff is examined and the change is problematic find the upsteam ticket which introduced the change and raise a flag before the change is deployed to production! \n\n\n 'sectionTextFromFixture' was:\n\n%@ \n\n\n 'sectionTextFromBetaCluster' was:\n\n%@ \n\n\n", sectionTextFromFixture, sectionTextFromBetaCluster);
-                                                }
-                                                
-                                                [expectation fulfill];
-                                                
-                                            }];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for article fetch"];
+    NSURLSessionDataTask *dataTask =
+    [[NSURLSession sharedSession] dataTaskWithURL:betaClusterURL
+                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                    NSArray *sectionsFromBetaCluster = json[@"mobileview"][@"sections"];
+                                    
+                                    XCTAssertEqualObjects(@(sectionsFromBetaCluster.count), @(sectionsFromFixtureFile.count), @"Section count mismatch! See test for details.");
+                                    
+                                    for (NSInteger i = 0; i < sectionsFromBetaCluster.count; i++) {
+                                        NSDictionary *sectionFromFixtureFile = sectionsFromFixtureFile[i];
+                                        NSDictionary *sectionFromBetaCluster = sectionsFromBetaCluster[i];
+                                        NSString *sectionTextFromFixture = sectionFromFixtureFile[@"text"];
+                                        NSString *sectionTextFromBetaCluster = sectionFromBetaCluster[@"text"];
+                                        XCTAssertTrue([sectionTextFromFixture isEqualToString:sectionTextFromBetaCluster], @"Something staged on the beta cluster appears to have changed something about the structure of the section HTML delivered by mobileview. (Background: We had a really bad change make it to production which introduced an opening DIV tag *without* a matching closing DIV tag to the first section HMTL for every mobileview request: https://phabricator.wikimedia.org/T165115). This test which just failed is an early warning sign that something has been staged to the beta cluster which may be similarly problematic if it is deployed to production. Don't ignore this test if it fails! This test makes a mobileview request to the beta cluster for a specific revision of an article and compares the fetched section html with that from a fixture file containing JSON from the same request. A failing test indicates mobileview output has diverged from what it output at the time we created the fixture file. If the diff (between 'sectionTextFromFixture' and 'sectionTextFromBetaCluster') is examined and the change is acceptable you'll need to regenerate the fixture file using the same url the test uses to make the api request. (No need to change the url or the fixture file name - can keep testing against this old revision.) If the diff is examined and the change is problematic find the upsteam ticket which introduced the change and raise a flag before the change is deployed to production! \n\n\n 'sectionTextFromFixture' was:\n\n%@ \n\n\n 'sectionTextFromBetaCluster' was:\n\n%@ \n\n\n", sectionTextFromFixture, sectionTextFromBetaCluster);
+                                    }
+                                    
+                                    [expectation fulfill];
+                                    
+                                }];
     [dataTask resume];
     
     [self waitForExpectationsWithTimeout:120.0
