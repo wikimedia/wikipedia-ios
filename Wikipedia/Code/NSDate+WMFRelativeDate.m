c2885f0e9acb263c570b6000ae032c0acb8a2942
joewalsh <joewalsh@wikimedia.org>
2017-05-24 08:57:01 -0400

    Revert "fix format tokens"
    This reverts commit b8b22716231eda683eeaaf25ffe7293d22453f82.

--- Wikipedia/Code/NSDate+WMFRelativeDate.m
+++ Wikipedia/Code/NSDate+WMFRelativeDate.m
@@ -13,17 +13,13 @@
     if (minutes < 2.0) {
         return WMFLocalizedStringWithDefaultValue(@"timestamp-just-now", nil, nil, @"just now", @"Human-readable approximate timestamp for events in the last couple of minutes.\n{{Identical|Just now}}");
     } else if (hours < 2.0) {
-        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-minutes", nil, nil, @"%1$d minutes ago", @"Human-readable approximate timestamp for events in the last couple hours, expressed as minutes"), (int)round(minutes)];
+        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-minutes", nil, nil, @"%d minutes ago", @"Human-readable approximate timestamp for events in the last couple hours, expressed as minutes"), (int)round(minutes)];
     } else if (days < 2.0) {
-        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-hours", nil, nil, @"%1$d hours ago", @"Human-readable approximate timestamp for events in the last couple days, expressed as hours"), (int)round(hours)];
+        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-hours", nil, nil, @"%d hours ago", @"Human-readable approximate timestamp for events in the last couple days, expressed as hours"), (int)round(hours)];
     } else if (months < 2.0) {
-        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-days", nil, nil, @"%1$d days ago", @"Human-readable approximate timestamp for events in the last couple months, expressed as days"), (int)round(days)];
+        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-days", nil, nil, @"%d days ago", @"Human-readable approximate timestamp for events in the last couple months, expressed as days"), (int)round(days)];
     } else if (months < 24.0) {
-        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-months", nil, nil, @"%1$d months ago", @"Human-readable approximate timestamp for events in the last couple years, expressed as months"), (int)round(months)];
-    } else {
-        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-years", nil, nil, @"%1$d years ago", @"Human-readable approximate timestamp for events in the distant past, expressed as years"), (int)round(years)];
-    }
-}
+        return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-months", nil, nil, @"%d months ago", @"Human-readable approximate timestamp for events in the last couple years, expressed as months"), (int)round(months)];
     } else {
         return [NSString stringWithFormat:WMFLocalizedStringWithDefaultValue(@"timestamp-years", nil, nil, @"%d years ago", @"Human-readable approximate timestamp for events in the distant past, expressed as years"), (int)round(years)];
     }
