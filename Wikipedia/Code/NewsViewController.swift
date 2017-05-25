4b614eddbce97b67feab11fe2ada8d1b227078d0
joewalsh <joewalsh@wikimedia.org>
2017-05-25 12:16:03 -0400

    only override methods that provide custom behavior

--- Wikipedia/Code/NewsViewController.swift
+++ Wikipedia/Code/NewsViewController.swift
@@ -100,18 +100,10 @@ class NewsViewController: ColumnarCollectionViewController {
 }
 
 extension NewsViewController { // WMFColumnarCollectionViewLayoutDelegate {
-    override func collectionView(_ collectionView: UICollectionView, prefersWiderColumnForSectionAt index: UInt) -> Bool {
-        return index % 2 == 0
-    }
-
     override func collectionView(_ collectionView: UICollectionView, estimatedHeightForHeaderInSection section: Int, forColumnWidth columnWidth: CGFloat) -> CGFloat {
         return headerTitle(for: section) == nil ? 0 : 50
     }
     
-    override func collectionView(_ collectionView: UICollectionView, estimatedHeightForFooterInSection section: Int, forColumnWidth columnWidth: CGFloat) -> CGFloat {
-        return 0
-    }
-
     override func collectionView(_ collectionView: UICollectionView, estimatedHeightForItemAt indexPath: IndexPath, forColumnWidth columnWidth: CGFloat) -> WMFLayoutEstimate {
         return WMFLayoutEstimate(precalculated: false, height: 350)
     }
