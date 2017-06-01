a98e19a0542d0015c04d0f34265d8dc80720f43c
joewalsh <joewalsh@wikimedia.org>
2017-06-01 06:32:41 -0400

    use availableIdentifiers

--- scripts/localization.swift
+++ scripts/localization.swift
@@ -318,7 +318,10 @@ func exportLocalizationsFromSourceCode(_ path: String) {
     }
 }
 
-
+let locales = Set<String>(Locale.availableIdentifiers)
+func localeIsAvailable(_ locale: String) -> Bool {
+    return locales.contains(locale)
+}
 
 func importLocalizationsFromTWN(_ path: String) {
     let enPath = "\(path)/Wikipedia/iOS Native Localizations/en.lproj/Localizable.strings"
@@ -335,14 +338,13 @@ func importLocalizationsFromTWN(_ path: String) {
     }
     
     let fm = FileManager.default
-    
     do {
         let keysByLanguage = ["pl": ["one", "few"], "sr": ["one", "few", "many"]]
-        let languagesToSkip: Set<String> = ["qqq", "azb", "be-tarask", "bgn", "cnh", "gom-latn", "ku-latn", "nah", "olo", "wuu", "xmf"]
+        
         let defaultKeys = ["one"]
         let contents = try fm.contentsOfDirectory(atPath: "\(path)/Wikipedia/Localizations")
         for filename in contents {
-            guard let locale = filename.components(separatedBy: ".").first?.lowercased(), !languagesToSkip.contains(locale) else {
+            guard let locale = filename.components(separatedBy: ".").first?.lowercased(), localeIsAvailable(locale) else {
                 continue
             }
             guard let twnStrings = NSDictionary(contentsOfFile: "\(path)/Wikipedia/Localizations/\(locale).lproj/Localizable.strings") else {
