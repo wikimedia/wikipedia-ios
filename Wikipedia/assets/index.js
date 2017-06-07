158e6d35cd92861e5c33dcdff04225d4e61902f3
montehurd <mhurd@wikimedia.org>
2017-06-07 13:19:21 -0700

    Enum for click types and consolidate type determination to single method.

--- Wikipedia/assets/index.js
+++ Wikipedia/assets/index.js
@@ -19,19 +19,42 @@ var refs = require('./refs')
 var utilities = require('./utilities')
 var tableCollapser = require('wikimedia-page-library').CollapseTable
 
+const ClickTypeEnum = {
+  unknown: 0,
+  link: 1,
+  image: 2,
+  reference: 3
+}
+
+function clickTypeForTarget(hrefTarget, event){
+  if (!hrefTarget) {
+    return ClickTypeEnum.unknown
+  }
+  var href = hrefTarget.getAttribute( 'href' )
+  if (href && refs.isCitation(href)) {
+    return ClickTypeEnum.reference
+  } else if (event.target.tagName === 'IMG' && event.target.getAttribute( 'data-image-gallery' ) === 'true') {
+    return ClickTypeEnum.image
+  } else if (href) {
+    return ClickTypeEnum.link
+  }
+  return ClickTypeEnum.unknown
+}
+
 /**
  * Attempts to send message which corresponds to `hrefTarget`, based on various attributes.
  * @return `true` if a message was sent, otherwise `false`.
  */
 function maybeSendMessageForTarget(hrefTarget, event){
-  if (!hrefTarget) {
-    return false
-  }
-
   var href = hrefTarget.getAttribute( 'href' )
-  if (href && refs.isCitation(href)) {
-    refs.sendNearbyReferences( hrefTarget )    
-  } else if (event.target.tagName === 'IMG' && event.target.getAttribute( 'data-image-gallery' ) === 'true') {
+  switch(clickTypeForTarget(hrefTarget, event)) {
+  case ClickTypeEnum.link:
+    if(href[0] === '#'){
+      tableCollapser.expandCollapsedTableIfItContainsElement(document.getElementById(href.substring(1)))
+    }
+    window.webkit.messageHandlers.linkClicked.postMessage({ 'href': href })
+    break
+  case ClickTypeEnum.image:
     window.webkit.messageHandlers.imageClicked.postMessage({
       'src': event.target.getAttribute('src'),
       'width': event.target.naturalWidth,   // Image should be fetched by time it is tapped, so naturalWidth and height should be available.
@@ -39,12 +62,11 @@ function maybeSendMessageForTarget(hrefTarget, event){
       'data-file-width': event.target.getAttribute('data-file-width'),
       'data-file-height': event.target.getAttribute('data-file-height')
     })
-  } else if (href) {
-    if(href[0] === '#'){
-      tableCollapser.expandCollapsedTableIfItContainsElement(document.getElementById(href.substring(1)))
-    }
-    window.webkit.messageHandlers.linkClicked.postMessage({ 'href': href })
-  } else {
+    break
+  case ClickTypeEnum.reference:
+    refs.sendNearbyReferences( hrefTarget )
+    break
+  default:
     return false
   }
   return true
