65e1938d8b652367fd7c66a4d16a5eb71caf963f
montehurd <mhurd@wikimedia.org>
2017-06-07 12:48:31 -0700

    DRY up some common bits with reference and link click handling.

--- Wikipedia/assets/index.js
+++ Wikipedia/assets/index.js
@@ -30,16 +30,7 @@ function maybeSendMessageForTarget(hrefTarget, event){
 
   var href = hrefTarget.getAttribute( 'href' )
   if (href && refs.isCitation(href)) {
-      // Handle reference links with a popup view instead of scrolling about!
-    refs.sendNearbyReferences( hrefTarget )
-  } else if (href && href[0] === '#') {
-
-    tableCollapser.expandCollapsedTableIfItContainsElement(document.getElementById(href.substring(1)))
-
-      // If it is a link to an anchor in the current page, use existing link handling
-      // so top floating native header height can be taken into account by the regular
-      // fragment handling logic.
-    window.webkit.messageHandlers.linkClicked.postMessage({ 'href': href })
+    refs.sendNearbyReferences( hrefTarget )    
   } else if (event.target.tagName === 'IMG' && event.target.getAttribute( 'data-image-gallery' ) === 'true') {
     window.webkit.messageHandlers.imageClicked.postMessage({
       'src': event.target.getAttribute('src'),
@@ -49,6 +40,9 @@ function maybeSendMessageForTarget(hrefTarget, event){
       'data-file-height': event.target.getAttribute('data-file-height')
     })
   } else if (href) {
+    if(href[0] === '#'){
+      tableCollapser.expandCollapsedTableIfItContainsElement(document.getElementById(href.substring(1)))
+    }
     window.webkit.messageHandlers.linkClicked.postMessage({ 'href': href })
   } else {
     return false
@@ -59,8 +53,8 @@ function maybeSendMessageForTarget(hrefTarget, event){
 document.addEventListener('click', function (event) {
   event.preventDefault()
   /*
-  there are certain elements which don't have an <a> ancestor, so if we fail to find it,
-  specify the event's target instead
+  There are certain elements which don't have an <a> ancestor, so if we fail to find it,
+  specify the event's target instead.
   */
   var target = utilities.findClosest(event.target, 'A') || event.target
   maybeSendMessageForTarget(target, event)
