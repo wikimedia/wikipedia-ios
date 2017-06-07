3e7aed879427d3c3f879e498899adfaafd21a480
joewalsh <joewalsh@wikimedia.org>
2017-06-07 11:21:35 -0400

    add translations

--- Wikipedia/Code/WMFTitledExploreSectionFooter.swift
+++ Wikipedia/Code/WMFTitledExploreSectionFooter.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 class WMFTitledExploreSectionFooter: WMFExploreCollectionReusableView {
-	@IBOutlet weak var enableLocationButton: UIButton!
-	
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var enableLocationButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         wmf_configureSubviewsForDynamicType()
     }
