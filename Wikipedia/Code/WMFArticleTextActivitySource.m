e3a863e3fed361a4b5d41fba63f2313732114aeb
joewalsh <joewalsh@wikimedia.org>
2017-06-14 16:00:24 -0400

    Revert "fix nullability error"
    This reverts commit 865234c6ac6fc2ba1e008e0553b0b876c4c2724f.

--- Wikipedia/Code/WMFArticleTextActivitySource.m
+++ Wikipedia/Code/WMFArticleTextActivitySource.m
@@ -25,7 +25,7 @@ NS_ASSUME_NONNULL_BEGIN
     return [NSString string];
 }
 
-- (nullable id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(nullable UIActivityType)activityType {
+- (nullable id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(UIActivityType)activityType {
     if ([activityType isEqualToString:UIActivityTypeCopyToPasteboard] || [activityType isEqualToString:UIActivityTypeAirDrop]) {
         if (self.shareText.length > 0) {
             return self.shareText;
