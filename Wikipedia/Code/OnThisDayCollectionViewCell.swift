0f9ba2ee86d17327268f465a62fe71c4c7011ec5
montehurd <mhurd@wikimedia.org>
2017-06-16 17:28:43 -0700

    First hack at `years ago` string.

--- Wikipedia/Code/OnThisDayCollectionViewCell.swift
+++ Wikipedia/Code/OnThisDayCollectionViewCell.swift
@@ -6,7 +6,20 @@ class OnThisDayCollectionViewCell: SideScrollingCollectionViewCell {
     @objc(configureWithOnThisDayEvent:dataStore:layoutOnly:)
     func configure(with onThisDayEvent: WMFFeedOnThisDayEvent, dataStore: MWKDataStore, layoutOnly: Bool) {
         let previews = onThisDayEvent.articlePreviews ?? []
-        storyHTML = onThisDayEvent.text
+        
+        let eventYear = (onThisDayEvent.year!).intValue
+        
+        let currentYear = Calendar.current.component(.year, from: Date())
+        
+//TODO:
+// - format `eventYear` so negative `BC` years use lang appropriate `BC` string instead of negative dash
+// - use proper number formatter on `yearsSinceEvent` so number formatting is localized, has commas etc.
+// - fix endpoint off-by-one bug on BC years (it's not accounting for there being no year zero)
+// - add guarding to this method 
+
+        let yearsSinceEvent = currentYear - eventYear
+
+        storyHTML = "\(eventYear)\n\(String.localizedStringWithFormat(WMFLocalizedDateFormatStrings.yearsAgo(), yearsSinceEvent))\n\(onThisDayEvent.text!)"
         
         articles = previews.map { (articlePreview) -> CellArticle in
             let articleLanguage = (articlePreview.articleURL as NSURL?)?.wmf_language
