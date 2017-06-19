f3941cfd7b7c48bf5f1b28254017a428bf2137dc
joewalsh <joewalsh@wikimedia.org>
2017-06-19 08:41:18 -0400

    Add debug description to WMFChange

--- Wikipedia/Code/WMFChange.m
+++ Wikipedia/Code/WMFChange.m
@@ -2,12 +2,33 @@
 
 @implementation WMFChange
 
+- (NSString *)debugDescription {
+    switch (self.type) {
+        case NSFetchedResultsChangeInsert:
+            return @"insert";
+        case NSFetchedResultsChangeDelete:
+            return @"delete";
+        case NSFetchedResultsChangeMove:
+            return @"move";
+        default:
+            return @"update";
+    }
+}
+
 @end
 
 @implementation WMFSectionChange
 
+- (NSString *)debugDescription {
+    return [NSString stringWithFormat:@"%@ section %li", [super debugDescription], self.sectionIndex];
+}
+
 @end
 
 @implementation WMFObjectChange
+
+- (NSString *)debugDescription {
+    return [NSString stringWithFormat:@"%@ object %@ to %@", [super debugDescription], self.fromIndexPath, self.toIndexPath];
+}
 
 @end
