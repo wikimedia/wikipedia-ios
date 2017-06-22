eaeb30f31e19a7348eb032ae23e924e7a23791fe
joewalsh <joewalsh@wikimedia.org>
2017-06-22 11:41:46 -0400

    fix missing import

--- Wikipedia/Code/WMFFeedNotificationCell.m
+++ Wikipedia/Code/WMFFeedNotificationCell.m
@@ -1,6 +1,7 @@
 #import "WMFFeedNotificationCell.h"
 #import "WMFLeadingImageTrailingTextButton.h"
 @import WMF.WMFLocalization;
+@import WMF.Swift;
 
 @implementation WMFFeedNotificationCell
 
@@ -14,17 +15,17 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    
+
     NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:WMFLocalizedStringWithDefaultValue(@"feed-news-notification-text", nil, nil, @"You can now receive notifications about Wikipedia articles trending in the news.", @"Text shown to users to notify them that it is now possible to get notifications for articles related to trending news")];
-    
+
     [attributedText addAttribute:NSForegroundColorAttributeName value:[UIColor blackColor] range:NSMakeRange(0, attributedText.length)];
-    
+
     NSMutableParagraphStyle *p = [[NSMutableParagraphStyle alloc] init];
     [p setLineBreakMode:NSLineBreakByWordWrapping];
     p.lineSpacing = 5;
-    
+
     [attributedText addAttribute:NSParagraphStyleAttributeName value:p range:NSMakeRange(0, attributedText.length)];
-    
+
     self.textLabel.attributedText = attributedText;
     self.textLabel.numberOfLines = 0;
     [self.enableNotificationsButton configureAsNotifyTrendingButton];
