3897b29c184570a6814d58850fa53cfbde50e731
montehurd <mhurd@wikimedia.org>
2017-06-26 21:46:44 -0700

    Add private.

--- Wikipedia/Code/OnThisDayTimelineView.swift
+++ Wikipedia/Code/OnThisDayTimelineView.swift
@@ -53,7 +53,7 @@ class OnThisDayTimelineView: UIView {
         return shape
     }()
 
-    lazy var outerDotShapeLayer: CAShapeLayer = {
+    private lazy var outerDotShapeLayer: CAShapeLayer = {
         let shape = CAShapeLayer()
         shape.fillColor = UIColor.white.cgColor
         shape.strokeColor = UIColor.wmf_blue.cgColor
@@ -62,7 +62,7 @@ class OnThisDayTimelineView: UIView {
         return shape
     }()
 
-    lazy var innerDotShapeLayer: CAShapeLayer = {
+    private lazy var innerDotShapeLayer: CAShapeLayer = {
         let shape = CAShapeLayer()
         shape.fillColor = UIColor.wmf_blue.cgColor
         shape.strokeColor = UIColor.wmf_blue.cgColor
