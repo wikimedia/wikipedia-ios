b38908bc5ffc229d73d21832d72cec1c84541d06
nambatee <nambatee@users.noreply.github.com>
2017-07-11 18:30:57 -0400

    add more inits

--- Wikipedia/Code/ShareActivityController.swift
+++ Wikipedia/Code/ShareActivityController.swift
@@ -1,9 +1,7 @@
 @objc(WMFShareActivityController)
 class ShareActivityController: UIActivityViewController {
     
-    fileprivate let tracker = PiwikTracker.sharedInstance()
-    
-    init(with url: URL, userDataStore: MWKDataStore) {
+    init(url: URL, userDataStore: MWKDataStore) {
         let article = userDataStore.fetchArticle(with: url)
         var items = [Any]()
         let text: String
@@ -13,18 +11,62 @@ class ShareActivityController: UIActivityViewController {
             items.append(text)
         }
         
-        if NSURL.wmf_desktopURL(for: url) != nil {
-            var components = URLComponents.init(url: url, resolvingAgainstBaseURL: false)
-            
-            let queryItem = URLQueryItem.init(name: "wprov", value: "sfsi1")
-            components?.queryItems = [queryItem]
-            
-            if let componentsURL = components?.url {
-                items.append(componentsURL)
-            }
-        }
+        items.append(url)
         
         if let mapItem = article?.mapItem {
+            items.append(mapItem)
+        }
+        
+        super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
+        
+    }
+    
+    init(url: URL, userDataStore: MWKDataStore, context: AnalyticsContextProviding) {
+        let article = userDataStore.fetchArticle(with: url)
+        var items = [Any]()
+        let text: String
+        
+        if let article = article {
+            if let title = article.displayTitle {
+                text = "\"\(title)\" on @Wikipedia"
+                items.append(text)
+            }
+            let tracker = PiwikTracker.sharedInstance()
+            tracker?.wmf_logActionShare(inContext: context, contentType: article)
+        }
+        
+        items.append(url)
+        
+        
+        if let mapItem = article?.mapItem {
+            items.append(mapItem)
+        }
+        
+        super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
+        
+    }
+    
+    init(article: WMFArticle, context: AnalyticsContextProviding) {
+        //        guard let url = article.url else {
+        //            return
+        //        }
+        
+        let tracker = PiwikTracker.sharedInstance()
+        tracker?.wmf_logActionShare(inContext: context, contentType: article)
+        
+        var items = [Any]()
+        let text: String
+        
+        if let title = article.displayTitle {
+            text = "\"\(title)\" on @Wikipedia"
+            items.append(text)
+        }
+        
+        if let url = article.url {
+            items.append(url)
+        }
+        
+        if let mapItem = article.mapItem {
             items.append(mapItem)
         }
         
