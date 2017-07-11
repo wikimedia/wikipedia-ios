3ca83bd307cc525359e57e60b89f00a31559f228
joewalsh <joewalsh@users.noreply.github.com>
2017-07-11 11:12:50 -0400

    ensure header and footer themes are applied

--- Wikipedia/Code/UITableView+Theme.swift
+++ Wikipedia/Code/UITableView+Theme.swift
@@ -4,25 +4,32 @@ import WMF
 // Only use this for SSTableViews where the header and footer aren't easily accessible or updatable with a call to [UITableView reloadData]
 extension UITableView {
     public func wmf_applyThemeToHeadersAndFooters(_ theme: Theme) {
-        wmf_recursivelyApply(theme: theme, to: self, isInHeaderOrFooter: false)
-
+        wmf_recursivelyApply(theme: theme, isInHeaderOrFooter: false)
     }
-    
-    fileprivate func wmf_recursivelyApply(theme: Theme, to view: UIView, isInHeaderOrFooter: Bool) {
-        for subview in view.subviews {
+}
+
+extension UITableViewHeaderFooterView {
+    @objc(wmf_applyTheme:)
+    public func wmf_apply(theme: Theme) {
+        wmf_recursivelyApply(theme: theme, isInHeaderOrFooter: true)
+    }
+}
+
+extension UIView {
+    fileprivate func wmf_recursivelyApply(theme: Theme, isInHeaderOrFooter: Bool) {
+        for subview in subviews {
             if let headerOrFooter = subview as? UITableViewHeaderFooterView {
                 headerOrFooter.contentView.backgroundColor = theme.colors.baseBackground
-                wmf_recursivelyApply(theme: theme, to: headerOrFooter, isInHeaderOrFooter: true)
+                headerOrFooter.wmf_recursivelyApply(theme: theme, isInHeaderOrFooter: true)
             } else if isInHeaderOrFooter, let label = subview as? UILabel {
                 label.textColor = theme.colors.secondaryText
             } else if let _ = subview as? UITableViewCell {
                 continue
             } else {
-                wmf_recursivelyApply(theme: theme, to: subview, isInHeaderOrFooter: isInHeaderOrFooter)
+                subview.wmf_recursivelyApply(theme: theme, isInHeaderOrFooter: isInHeaderOrFooter)
             }
         }
     }
-    
 }
 
 
