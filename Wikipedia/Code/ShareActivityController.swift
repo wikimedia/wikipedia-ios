7f8255cc1f3dae8122aafd922f25e434c4457d75
nambatee <nambatee@users.noreply.github.com>
2017-07-11 21:00:17 -0400

    remove unused init

--- Wikipedia/Code/ShareActivityController.swift
+++ Wikipedia/Code/ShareActivityController.swift
@@ -1,26 +1,6 @@
 @objc(WMFShareActivityController)
 class ShareActivityController: UIActivityViewController {
     
-    init(url: URL, userDataStore: MWKDataStore) {
-        let article = userDataStore.fetchArticle(with: url)
-        var items = [Any]()
-        let text: String
-        
-        if let article = article, let title = article.displayTitle {
-            text = "\"\(title)\" on @Wikipedia"
-            items.append(text)
-        }
-        
-        items.append(url)
-        
-        if let mapItem = article?.mapItem {
-            items.append(mapItem)
-        }
-        
-        super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
-        
-    }
-    
     init(url: URL, userDataStore: MWKDataStore, context: AnalyticsContextProviding) {
         let article = userDataStore.fetchArticle(with: url)
         var items = [Any]()
@@ -37,20 +17,14 @@ class ShareActivityController: UIActivityViewController {
         
         items.append(url)
         
-        
         if let mapItem = article?.mapItem {
             items.append(mapItem)
         }
         
         super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
-        
     }
     
     init(article: WMFArticle, context: AnalyticsContextProviding) {
-        //        guard let url = article.url else {
-        //            return
-        //        }
-        
         let tracker = PiwikTracker.sharedInstance()
         tracker?.wmf_logActionShare(inContext: context, contentType: article)
         
@@ -71,7 +45,52 @@ class ShareActivityController: UIActivityViewController {
         }
         
         super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
+    }
+    
+    init(article: MWKArticle, textActivitySource: WMFArticleTextActivitySource) {
+        var items = [Any]()
         
+        items.append(textActivitySource)
+        
+        if let url = article.url {
+            items.append(url)
+        }
+        
+        if let mapItem = article.mapItem {
+            items.append(mapItem)
+        }
+        
+        super.init(activityItems: items, applicationActivities: [TUSafariActivity(), WMFOpenInMapsActivity(), WMFGetDirectionsInMapsActivity()])
+    }
+    
+    init(article: MWKArticle, image: UIImage?, title: String) {
+        var param: String
+        
+        var items = [Any]()
+        
+        items.append(title)
+        
+        //TODO: Is wprov param needed?
+        if image != nil {
+            param = "wprov=sfii1"
+            items.append(image)
+        }
+        
+        param = "wprov=sfti1"
+        
+        if article.url != nil, let url = URL(string: "\(article.url.absoluteString)?\(param)") {
+            items.append(url)
+        }
+        
+        super.init(activityItems: items, applicationActivities: [])
+    }
+    
+    init(imageInfo: MWKImageInfo, imageDownload: ImageDownload) {
+        var items = [Any]()
+        
+        items.append(contentsOf: [WMFImageTextActivitySource(info: imageInfo),WMFImageURLActivitySource(info: imageInfo), imageDownload.image])
+        
+        super.init(activityItems: items, applicationActivities: [])
     }
     
 }
