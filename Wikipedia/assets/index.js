649321f214bc47965a922f787f1c81910aca6625
montehurd <mhurd@wikimedia.org>
2017-07-11 18:40:12 -0700

    Rebuild bundled JS.

--- Wikipedia/assets/index.js
+++ Wikipedia/assets/index.js
@@ -551,30 +551,61 @@ function add(licenseString, licenseSubstitutionString, containerID, licenceLinkC
 exports.add = add
 },{}],10:[function(require,module,exports){
 
+function pageIssuesStringsArray() {
+  const tables = document.querySelectorAll( 'div#content_block_0 table.ambox:not(.ambox-multiple_issues):not(.ambox-notice)' )
+  // Get the tables into a fragment so we can remove some elements without triggering a layout
+  var fragment = document.createDocumentFragment()
+  for (var i = 0; i < tables.length; i++) {
+    fragment.appendChild(tables[i].cloneNode(true))
+  }
+  // Remove some element so their text doesn't appear when we use "innerText"
+  Array.from(fragment.querySelectorAll( '.hide-when-compact, .collapsed' )).forEach(el => el.remove())
+  // Get the innerText
+  return Array.from(fragment.querySelectorAll( 'td[class$=mbox-text]' )).map(el => el.innerText)
+}
+
+function disambiguationTitlesArray() {
+  return Array.from(document.querySelectorAll('div#content_block_0 div.hatnote a[href]:not([href=""]):not([redlink="1"])')).map(el => el.href)
+}
+
 // var thisType = IconTypeEnum.languages;
 // var iconClass = IconTypeEnum.properties[thisType].iconClass;
 // iconClass is 'footer_menu_icon_languages'
-var IconTypeEnum = {
+var ItemTypeEnum = {
   languages: 1,
   lastEdited: 2,
   pageIssues: 3,
   disambiguation: 4,
   coordinate: 5,
-  properties: {
-    1: {iconClass: 'footer_menu_icon_languages'},
-    2: {iconClass: 'footer_menu_icon_last_edited'},
-    3: {iconClass: 'footer_menu_icon_page_issues'},
-    4: {iconClass: 'footer_menu_icon_disambiguation'},
-    5: {iconClass: 'footer_menu_icon_coordinate'}
+  iconClass: {
+    1: 'footer_menu_icon_languages',
+    2: 'footer_menu_icon_last_edited',
+    3: 'footer_menu_icon_page_issues',
+    4: 'footer_menu_icon_disambiguation',
+    5: 'footer_menu_icon_coordinate'
+  },
+  payloadExtractor: {
+    1: null,
+    2: null,
+    3: pageIssuesStringsArray,
+    4: disambiguationTitlesArray,
+    5: null
   }
 }
 
 class WMFMenuItem {
-  constructor(title, subtitle, iconType, clickHandler) {
+  constructor(title, subtitle, itemType, clickHandler) {
     this.title = title
     this.subtitle = subtitle
-    this.iconType = iconType
+    this.itemType = itemType
     this.clickHandler = clickHandler
+    this.payload = []
+  }
+  iconClass(){
+    return ItemTypeEnum.iconClass[this.itemType]
+  }
+  payloadExtractor(){
+    return ItemTypeEnum.payloadExtractor[this.itemType]
   }
 }
 
@@ -585,7 +616,7 @@ class WMFMenuItemFragment {
 
     var containerAnchor = document.createElement('a')
     containerAnchor.addEventListener('click', function(){
-      wmfMenuItem.clickHandler()
+      wmfMenuItem.clickHandler(wmfMenuItem.payload)
     }, false)
 
     item.appendChild(containerAnchor)
@@ -605,8 +636,8 @@ class WMFMenuItemFragment {
       containerAnchor.appendChild(subtitle)
     }
 
-    if(wmfMenuItem.iconType){
-      var iconClass = IconTypeEnum.properties[wmfMenuItem.iconType].iconClass
+    var iconClass = wmfMenuItem.iconClass()
+    if(iconClass){
       item.classList.add(iconClass)
     }
 
@@ -614,10 +645,23 @@ class WMFMenuItemFragment {
   }
 }
 
-function addItem(title, subtitle, iconType, containerID, clickHandler) {
-  const itemModel = new WMFMenuItem(title, subtitle, iconType, clickHandler)
-  const itemFragment = new WMFMenuItemFragment(itemModel)
-  document.getElementById(containerID).appendChild(itemFragment)
+function maybeAddItem(title, subtitle, itemType, containerID, clickHandler) {
+  const item = new WMFMenuItem(title, subtitle, itemType, clickHandler)
+
+  // Items are not added if they have a payload extractor which fails to extract anything.
+  if (item.payloadExtractor() !== null){
+    item.payload = item.payloadExtractor()()
+    if(item.payload.length === 0){
+      return
+    }
+  }
+
+  addItem(item, containerID)
+}
+
+function addItem(wmfMenuItem, containerID) {
+  const fragment = new WMFMenuItemFragment(wmfMenuItem)
+  document.getElementById(containerID).appendChild(fragment)
 }
 
 function setHeading(headingString, headingID) {
@@ -626,9 +670,9 @@ function setHeading(headingString, headingID) {
   headingElement.title = headingString
 }
 
-exports.IconTypeEnum = IconTypeEnum
+exports.ItemTypeEnum = ItemTypeEnum
 exports.setHeading = setHeading
-exports.addItem = addItem
+exports.maybeAddItem = maybeAddItem
 },{}],11:[function(require,module,exports){
 
 var _saveButtonClickHandler = null
