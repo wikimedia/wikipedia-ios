2888452408442529b0ddcfcd17d6693d81a1ab42
joewalsh <joewalsh@users.noreply.github.com>
2017-07-13 15:34:34 -0400

    restore custom delete button to onboarding

---
+++ Wikipedia/Code/WMFCustomDeleteButtonTableViewCell.h
@@ -0,0 +1,12 @@
+@import UIKit.UITableViewCell;
+@import WMF.Swift;
+
+/**
+ * Table cell with a custom minus button achieved by overlaying the default minus button with a custom minus button.
+ */
+
+@interface WMFCustomDeleteButtonTableViewCell : UITableViewCell <WMFThemeable>
+
+@property (strong, nonatomic, readonly) UIButton *deleteButton;
+
+@end
