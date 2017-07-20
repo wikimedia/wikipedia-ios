b93e864dcb7f1d17a91c2d84b4b650f208be023b
joewalsh <joewalsh@users.noreply.github.com>
2017-07-20 15:55:53 -0400

    show appearance settings

--- Wikipedia/Code/AppearanceSettingsViewController.swift
+++ Wikipedia/Code/AppearanceSettingsViewController.swift
@@ -36,6 +36,7 @@ open class AppearanceSettingsViewController: UIViewController, UITableViewDataSo
     
     override open func viewDidLoad() {
         super.viewDidLoad()
+        title = WMFLocalizedString("appearance-settings-title", value: "Reading themes", comment: "Title of the Appearance view in Settings.")
         tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0);
         tableView.register(WMFSettingsTableViewCell.wmf_classNib(), forCellReuseIdentifier: WMFSettingsTableViewCell.identifier())
         tableView.delegate = self
