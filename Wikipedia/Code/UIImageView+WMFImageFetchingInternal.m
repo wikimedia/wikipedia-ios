94455764ebb19b5c56623527e94b3120a1c7f315
joewalsh <joewalsh@users.noreply.github.com>
2017-07-25 17:29:26 -0400

    revert the image cross dissolve fix

--- Wikipedia/Code/UIImageView+WMFImageFetchingInternal.m
+++ Wikipedia/Code/UIImageView+WMFImageFetchingInternal.m
@@ -185,24 +185,25 @@ static const char *const WMFImageControllerAssociationKey = "WMFImageController"
         animatedImageView.animatedImage = animatedImage;
         success();
     } else {
-        dispatch_block_t animations = ^{
+//  for now, keep the unanimated behavior, ignore the animated parameter
+//        dispatch_block_t animations = ^{
             if (image) {
                 self.backgroundColor = [UIColor whiteColor];
             }
             self.image = image;
-        };
-        if (animated) {
-            [UIView transitionWithView:self
-                              duration:[CATransaction animationDuration]
-                               options:UIViewAnimationOptionTransitionCrossDissolve
-                            animations:animations
-                            completion:^(BOOL finished) {
-                                success();
-                            }];
-        } else {
-            animations();
+//        };
+//        if (animated) {
+//            [UIView transitionWithView:self
+//                              duration:[CATransaction animationDuration]
+//                               options:UIViewAnimationOptionTransitionCrossDissolve
+//                            animations:animations
+//                            completion:^(BOOL finished) {
+//                                success();
+//                            }];
+//        } else {
+//            animations();
             success();
-        }
+//        }
     }
 }
 
