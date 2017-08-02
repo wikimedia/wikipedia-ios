055ba6d191afb08a032c380082fd3a814123c24b
nambatee <nambatee@users.noreply.github.com>
2017-08-02 16:56:52 -0400

    change font size

--- Wikipedia/Code/TextSizeChangeExampleViewController.swift
+++ Wikipedia/Code/TextSizeChangeExampleViewController.swift
@@ -9,10 +9,8 @@ class TextSizeChangeExampleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         apply(theme: self.theme)
-        //localize
         textSizeChangeExampleLabel.text = "Drag the slider above to change the article text sizing. Utilize your system text size to resize other text areas in the app."
         
-        //TODO: update notification name
         NotificationCenter.default.addObserver(self, selector: #selector(self.textSizeChanged(notification:)), name: NSNotification.Name(rawValue: FontSizeSliderViewController.WMFArticleFontSizeUpdatedNotification), object: nil)
     }
     
@@ -22,9 +20,11 @@ class TextSizeChangeExampleViewController: UIViewController {
     }
     
     func textSizeChanged(notification: Notification) {
+        print("font size before change: \(textSizeChangeExampleLabel.font.pointSize)")
         if let multiplier = notification.userInfo?[FontSizeSliderViewController.WMFArticleFontSizeMultiplierKey] as? NSNumber {
-            //Font size too big
-            textSizeChangeExampleLabel.font = textSizeChangeExampleLabel.font.withSize(CGFloat(multiplier.intValue))
+            textSizeChangeExampleLabel.font = textSizeChangeExampleLabel.font.withSize(CGFloat(multiplier.doubleValue*0.17))
+            print("intValue: \(multiplier.intValue)")
+            print("font size after change: \(textSizeChangeExampleLabel.font.pointSize)")
         }
     }
     
