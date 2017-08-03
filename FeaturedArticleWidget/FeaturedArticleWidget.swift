67d1119a1d010971351e0068a7993a36e42367a7
joewalsh <joewalsh@users.noreply.github.com>
2017-08-03 13:12:01 -0400

    add widgetPerformUpdate

--- FeaturedArticleWidget/FeaturedArticleWidget.swift
+++ FeaturedArticleWidget/FeaturedArticleWidget.swift
@@ -6,18 +6,42 @@ class FeaturedArticleWidget: UIViewController, NCWidgetProviding {
     let collapsedArticleView = ArticleRightAlignedImageCollectionViewCell()
     let expandedArticleView = ArticleFullWidthImageCollectionViewCell()
 
-    var isExpanded = false
-    var maximumSize = CGSize.zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.translatesAutoresizingMaskIntoConstraints = false
+
         collapsedArticleView.frame = view.bounds
         view.addSubview(collapsedArticleView)
 
         expandedArticleView.frame = view.bounds
         view.addSubview(expandedArticleView)
+    }
+    
+    var isEmptyViewHidden = true
+    
+    func widgetPerformUpdate(completionHandler: @escaping (NCUpdateResult) -> Void) {
+        guard let session = SessionSingleton.sharedInstance(),
+            let userStore = session.dataStore,
+            let siteURL = MWKLanguageLinkController.sharedInstance().appLanguage?.siteURL(),
+            let featuredContentGroup = userStore.viewContext.group(of: .featuredArticle, for: Date(), siteURL: siteURL),
+            let articleURL = featuredContentGroup.content?.first as? URL,
+            let article = userStore.fetchArticle(with: articleURL) else {
+                isEmptyViewHidden = false
+                completionHandler(.failed)
+                return
+        }
         
+        isEmptyViewHidden = true
+        expandedArticleView.configure(article: article, displayType: featuredContentGroup.displayTypeForItem(at: 0), index: 0, count: 1, theme: Theme.dark, layoutOnly: false)
+        updateView()
+        completionHandler(.newData)
+    }
+    
+    func updateView() {
+        var maximumSize = CGSize(width: view.bounds.size.width, height: UIViewNoIntrinsicMetric)
+        var isExpanded = false
         if let context = extensionContext {
             if #available(iOSApplicationExtension 10.0, *) {
                 context.widgetLargestAvailableDisplayMode = .expanded
@@ -28,43 +52,19 @@ class FeaturedArticleWidget: UIViewController, NCWidgetProviding {
                 maximumSize = UIScreen.main.bounds.size
             }
         }
-        updateView()
+        updateView(maximumSize: maximumSize, isExpanded: isExpanded)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        view.addConstraints([expandedArticleView.topAnchor.constraint(equalTo: view.topAnchor), expandedArticleView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-    }
-    
-    var isEmptyViewHidden = true
-    
-    func updateView() {
-        guard let session = SessionSingleton.sharedInstance(),
-            let userStore = session.dataStore,
-            let siteURL = MWKLanguageLinkController.sharedInstance().appLanguage?.siteURL(),
-            let featuredContentGroup = userStore.viewContext.group(of: .featuredArticle, for: Date(), siteURL: siteURL),
-            let articleURL = featuredContentGroup.content?.first as? URL,
-            let article = userStore.fetchArticle(with: articleURL) else {
-            isEmptyViewHidden = true
-            return
-        }
-        
+    func updateView(maximumSize: CGSize, isExpanded: Bool) {
         //collapsedArticleView.configure(article: article, displayType: .page, index: 0, count: 1, theme: Theme.dark, layoutOnly: false)
-        
-        
-        expandedArticleView.configure(article: article, displayType: featuredContentGroup.displayTypeForItem(at: 0), index: 0, count: 1, theme: Theme.dark, layoutOnly: false)
-        preferredContentSize = expandedArticleView.sizeThatFits(CGSize(width: view.bounds.size.width, height: UIViewNoIntrinsicMetric), apply: true)
-        //expandedArticleView.frame = CGRect(origin: .zero, size: preferredContentSize)
+        preferredContentSize = expandedArticleView.sizeThatFits(maximumSize, apply: true)
+        expandedArticleView.frame = CGRect(origin: .zero, size:preferredContentSize)
+        view.layoutIfNeeded()
     }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-    }
-    
     
     @available(iOSApplicationExtension 10.0, *)
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
-        
+        updateView(maximumSize: maxSize, isExpanded: activeDisplayMode == .expanded)
     }
     
 }
