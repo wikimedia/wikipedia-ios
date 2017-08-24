1fc18b2230a9e6a23ddf8fe03e60e2ad09765bfb
joewalsh <joewalsh@users.noreply.github.com>
2017-08-24 11:24:32 -0400

    Revert "fix slow type check"
    This reverts commit fc2b2fe6486af2e9264e7fcc60e1511687010bf6.

--- Wikipedia/Code/QuadKey.swift
+++ Wikipedia/Code/QuadKey.swift
@@ -139,9 +139,7 @@ public extension QuadKeyPart {
     }
     
     public static func max(atPrecision precision: QuadKeyPrecision) -> QuadKeyPart {
-        let minusOne: QuadKey = QuadKey(precision) - QuadKey(1)
-        let part: QuadKey = QuadKey(1) << minusOne
-        return QuadKeyPart(part)
+        return QuadKeyPart(QuadKey(1) << QuadKey(precision) - 1)
     }
     
     public var latitude: QuadKeyDegrees {
