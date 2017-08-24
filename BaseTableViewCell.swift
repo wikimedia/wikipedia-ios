895e336fe1e9f6c8e59329ac3d6fed20e46cb3a7
nambatee <nambatee@users.noreply.github.com>
2017-08-24 11:36:28 -0400

    Add BaseTableViewCell, that will serve as a SSBaseTableCell replacement

---
+++ BaseTableViewCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@objc(WMFBaseTableViewCell)
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
