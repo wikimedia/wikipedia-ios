ef8404f3f9c4d0e38d099947f04ec0c14af8cbae
nambatee <nambatee@users.noreply.github.com>
2017-08-24 20:29:29 -0400

    Remove unused methods from BaseTableViewCell

--- BaseTableViewCell.swift
+++ BaseTableViewCell.swift
@@ -2,20 +2,7 @@ import UIKit
 
 @objc(WMFBaseTableViewCell)
 class BaseTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     static func identifier() -> String {
         return String(describing: self)
     }
-    
 }
