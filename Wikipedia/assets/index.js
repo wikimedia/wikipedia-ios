d4af51e69adb69ecbf7003c7bb69a7588e9a6fdc
montehurd <mhurd@wikimedia.org>
2017-09-01 16:16:35 -0700

    Update js syntax in widenImages.js

--- Wikipedia/assets/index.js
+++ Wikipedia/assets/index.js
@@ -599,13 +599,11 @@ exports.moveFirstGoodParagraphAfterElement = moveFirstGoodParagraphAfterElement
 
 const maybeWidenImage = require('wikimedia-page-library').WidenImage.maybeWidenImage
 
-const isGalleryImage = function(image) {
-  // 'data-image-gallery' is added to 'gallery worthy' img tags before html is sent to WKWebView.
-  // WidenImage's maybeWidenImage code will do further checks before it widens an image.
-  return image.getAttribute('data-image-gallery') === 'true'
-}
+// 'data-image-gallery' is added to 'gallery worthy' img tags before html is sent to WKWebView.
+// WidenImage's maybeWidenImage code will do further checks before it widens an image.
+const isGalleryImage = image => image.getAttribute('data-image-gallery') === 'true'
 
-function widenImages(content) {
+const widenImages = content => {
   Array.from(content.querySelectorAll('img'))
     .filter(isGalleryImage)
     .forEach(maybeWidenImage)
