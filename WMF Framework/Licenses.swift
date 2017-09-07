addf511ec85f88b8411d0610972226c2e041485c
joewalsh <joewalsh@users.noreply.github.com>
2017-09-07 10:13:19 -0400

    add public

--- WMF Framework/Licenses.swift
+++ WMF Framework/Licenses.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 @objc(WMFLicenses)
-class Licenses: NSObject {
+public class Licenses: NSObject {
     @objc static let localizedCCBYSA3Title = WMFLocalizedString("cc-by-sa-3.0", value: "CC BY-SA 3.0", comment: "Name of the CC BY-SA 3.0 license - https://creativecommons.org/licenses/by-sa/3.0/")
     @objc static let localizedGDFLTitle = WMFLocalizedString("gdfl", value: "GDFL", comment: "Name of the GDFL license - https://www.gnu.org/licenses/fdl.html")
     @objc static let localizedSaveTermsTitle = WMFLocalizedString("wikitext-upload-save-terms-name", value: "Terms of Use", comment: "This message is used in the message [[Wikimedia:Wikipedia-ios-wikitext-upload-save-terms-and-license]].")
