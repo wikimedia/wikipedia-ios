66322f3ae5b0b638032d7ab4bc6b887e054dc10a
joewalsh <joewalsh@users.noreply.github.com>
2017-09-07 13:48:41 -0400

    update share strings

--- Wikipedia/Code/ShareAFactActivityTextItemProvider.swift
+++ Wikipedia/Code/ShareAFactActivityTextItemProvider.swift
@@ -21,6 +21,8 @@ class ShareAFactActivityTextItemProvider: UIActivityItemProvider {
             fallthrough
         case .postToTwitter:
             return socialRepresentation
+        case .mail:
+            return emailRepresentation
         default:
              return defaultRepresentation
         }
@@ -30,8 +32,13 @@ class ShareAFactActivityTextItemProvider: UIActivityItemProvider {
         return articleTitle
     }
     
+    var emailRepresentation: String {
+        let format = WMFLocalizedString("share-email-format", value: "“%1$@”\n\nfrom “%2$@”\n\n%3$@", comment: "Share string format for email. %1$@ is replaced with the selected text, %2$@ is replaced with the article title, %3$@ is replaced with the articleURL.")
+        return String.localizedStringWithFormat(format, text, articleTitle, articleURL.absoluteString)
+    }
+    
     var defaultRepresentation: String {
-        let format = WMFLocalizedString("share-default-format", value: "“%1$@”\n\nfrom “%2$@”\n\n%3$@", comment: "Share string format for email and copied text. %1$@ is replaced with the selected text, %2$@ is replaced with the article title, %3$@ is replaced with the articleURL.")
+        let format = WMFLocalizedString("share-default-format", value: "“%1$@” from “%2$@”: %3$@", comment: "Default share format string. %1$@ is replaced with the selected text, %2$@ is replaced with the article title, %3$@ is replaced with the articleURL.")
         return String.localizedStringWithFormat(format, text, articleTitle, articleURL.absoluteString)
     }
     
@@ -41,7 +48,7 @@ class ShareAFactActivityTextItemProvider: UIActivityItemProvider {
     }
     
     var socialRepresentation: String {
-        let format = WMFLocalizedString("share-social-format", value: "“%1$@” via @Wikipedia %2$@", comment: "Share string format for social platforms. %1$@ is replaced with the article title, %2$@ is replaced with the article URL.")
+        let format = WMFLocalizedString("share-social-format", value: "“%1$@” via @Wikipedia: %2$@", comment: "Share string format for social platforms. %1$@ is replaced with the article title, %2$@ is replaced with the article URL.")
         return String.localizedStringWithFormat(format, articleTitle, articleURL.absoluteString)
     }
 
