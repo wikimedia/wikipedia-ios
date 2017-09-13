23938ac1079efbbbde73f327145aaabb5544496a
joewalsh <joewalsh@users.noreply.github.com>
2017-09-13 10:21:16 -0400

    letterbox for landscape images

--- Wikipedia/Code/ShareAFactViewController.swift
+++ Wikipedia/Code/ShareAFactViewController.swift
@@ -3,25 +3,26 @@ import UIKit
 class ShareAFactViewController: UIViewController {
 
     @IBOutlet weak var articleTitleLabel: UILabel!
-//    @IBOutlet weak var onWikiLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var separatorView: UIView!
     @IBOutlet weak var textLabel: UILabel!
-//    @IBOutlet weak var articleLicenseView: LicenseView!
-//    @IBOutlet weak var imageLicenseView: LicenseView!
+    @IBOutlet weak var articleLicenseView: LicenseView!
+    @IBOutlet weak var imageLicenseView: LicenseView!
     @IBOutlet weak var imageGradientView: WMFGradientView!
 
     @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var imageViewWidthConstraint: NSLayoutConstraint!
     
+    @IBOutlet var imageViewLetterboxConstraints: [NSLayoutConstraint]!
+    
     override func viewDidLoad() {
         let theme = Theme.standard //always use the standard theme for now
         view.backgroundColor = theme.colors.paperBackground
         articleTitleLabel.textColor = theme.colors.primaryText
-//        onWikiLabel.textColor = theme.colors.secondaryText
         separatorView.backgroundColor = theme.colors.border
         textLabel.textColor = theme.colors.primaryText
-//        licenseView.tintColor = theme.colors.secondaryText
+        articleLicenseView.tintColor = theme.colors.secondaryText
+        imageLicenseView.tintColor = .white
         imageGradientView.setStart(.clear, end: UIColor(white: 0, alpha: 0.4))
     }
     
@@ -32,7 +33,23 @@ class ShareAFactViewController: UIViewController {
         isImageViewHidden = image == nil
         textLabel.text = text
         articleTitleLabel.text = articleTitle
-//        licenseView.licenseCodes = ["cc", "by", "sa"]
+        imageLicenseView.licenseCodes = ["cc", "by", "sa"]
+        articleLicenseView.licenseCodes = ["cc", "by", "sa"]
+        
+        guard let image = image else {
+            return
+        }
+        
+        guard image.size.width > image.size.height else {
+            return
+        }
+        
+        let aspect = image.size.height / image.size.width
+        let height = floor(imageViewWidthConstraint.constant * aspect)
+        let remainder = round(0.5 * (view.bounds.size.height - height))
+        for letterboxConstraint in imageViewLetterboxConstraints {
+            letterboxConstraint.constant = remainder
+        }
     }
     
     var isImageViewHidden: Bool = false {
