57aa9f8944d5cea7df9eb87d6d007feffc9a163e
montehurd <mhurd@wikimedia.org>
2017-09-19 15:40:28 -0700

    Wire Welcome intro page to use globe image and not use background.

--- Wikipedia/Code/WMFWelcomeAnimationViewControllers.swift
+++ Wikipedia/Code/WMFWelcomeAnimationViewControllers.swift
@@ -2,10 +2,11 @@ class WMFWelcomeAnimationViewController: UIViewController {
     var welcomePageType:WMFWelcomePageType = .intro
     fileprivate var hasAlreadyAnimated = false
     
-    open lazy var animationView = WMFWelcomeAnimationView()
+    open lazy var animationView: WMFWelcomeAnimationView? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let animationView = animationView else {return}
         animationView.translatesAutoresizingMaskIntoConstraints = false
         view.wmf_addSubviewWithConstraintsToEdges(animationView)
     }
@@ -13,12 +14,14 @@ class WMFWelcomeAnimationViewController: UIViewController {
     override func didMove(toParentViewController parent: UIViewController?) {
         super.didMove(toParentViewController: parent)
         view.superview?.layoutIfNeeded() // Fix for: http://stackoverflow.com/a/39614714
+        guard let animationView = animationView else {return}
         animationView.addAnimationElementsScaledToCurrentFrameSize()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if (!hasAlreadyAnimated) {
+            guard let animationView = animationView else {return}
             animationView.beginAnimations()
         }
         hasAlreadyAnimated = true
@@ -26,7 +29,7 @@ class WMFWelcomeAnimationViewController: UIViewController {
 }
 
 class WMFWelcomeAnimationForgroundViewController: WMFWelcomeAnimationViewController {
-    override lazy var animationView: WMFWelcomeAnimationView = {
+    override lazy var animationView: WMFWelcomeAnimationView? = {
         switch welcomePageType {
         case .intro:
             return WMFWelcomeIntroductionAnimationView()
@@ -41,19 +44,28 @@ class WMFWelcomeAnimationForgroundViewController: WMFWelcomeAnimationViewControl
     
     override func didMove(toParentViewController parent: UIViewController?) {
         super.didMove(toParentViewController: parent)
-        animationView.backgroundColor = UIColor(0xdee6f6)
-        animationView.layer.cornerRadius = animationView.frame.size.width / 2.0
-        animationView.layer.masksToBounds = true
+        guard let animationView = animationView else {return}
+        switch welcomePageType {
+        case .exploration:
+            fallthrough
+        case .languages:
+            fallthrough
+        case .analytics:
+            animationView.backgroundColor = UIColor(0xdee6f6)
+            animationView.layer.cornerRadius = animationView.frame.size.width / 2.0
+            animationView.layer.masksToBounds = true
+        default:
+            // animationView.backgroundColor = UIColor(0xdddddd)
+            break
+        }
     }
 }
 
 class WMFWelcomeAnimationBackgroundViewController: WMFWelcomeAnimationViewController {
-    override lazy var animationView: WMFWelcomeAnimationView = {
+    override lazy var animationView: WMFWelcomeAnimationView? = {
         switch welcomePageType {
         case .intro:
-            let view = WMFWelcomeIntroductionAnimationBackgroundView()
-            view.backgroundColor = .red
-            return view
+            return nil
         case .exploration:
             let view = WMFWelcomeExplorationAnimationBackgroundView()
             view.backgroundColor = .green
