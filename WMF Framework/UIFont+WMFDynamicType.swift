fe53bf74690341d6ed510aa78b4acadd78ce8af3
montehurd <mhurd@wikimedia.org>
2017-09-18 20:24:36 -0700

    Updates to WMF dynamic type for Welcome screens.

--- WMF Framework/UIFont+WMFDynamicType.swift
+++ WMF Framework/UIFont+WMFDynamicType.swift
@@ -117,16 +117,16 @@ let fontSizeTable: [WMFFontFamily:[UIFontTextStyle:[UIContentSizeCategory:CGFloa
                 .extraSmall: 12
             ],
             UIFontTextStyle.footnote: [
-                .accessibilityExtraExtraExtraLarge: 19,
-                .accessibilityExtraExtraLarge: 19,
-                .accessibilityExtraLarge: 19,
-                .accessibilityLarge: 19,
-                .accessibilityMedium: 19,
-                .extraExtraExtraLarge: 19,
-                .extraExtraLarge: 17,
-                .extraLarge: 15,
-                .large: 13,
-                .medium: 12,
+                .accessibilityExtraExtraExtraLarge: 20,
+                .accessibilityExtraExtraLarge: 20,
+                .accessibilityExtraLarge: 20,
+                .accessibilityLarge: 20,
+                .accessibilityMedium: 20,
+                .extraExtraExtraLarge: 20,
+                .extraExtraLarge: 18,
+                .extraLarge: 16,
+                .large: 14,
+                .medium: 13,
                 .small: 12,
                 .extraSmall: 12
             ],
@@ -159,6 +159,20 @@ let fontSizeTable: [WMFFontFamily:[UIFontTextStyle:[UIContentSizeCategory:CGFloa
                 .medium: 14,
                 .small: 13,
                 .extraSmall: 12
+            ],
+            UIFontTextStyle.headline: [ // Welcome screens headline
+                .accessibilityExtraExtraExtraLarge: 39,
+                .accessibilityExtraExtraLarge: 39,
+                .accessibilityExtraLarge: 39,
+                .accessibilityLarge: 39,
+                .accessibilityMedium: 39,
+                .extraExtraExtraLarge: 39,
+                .extraExtraLarge: 38,
+                .extraLarge: 37,
+                .large: 36,
+                .medium: 35,
+                .small: 34,
+                .extraSmall: 33
             ]
         ]
     ]
