9b76778fe0aa0984f393c16757a304fbd0cafd27
montehurd <mhurd@wikimedia.org>
2017-09-20 11:55:54 -0700

    Update Welcome langs and analytics background views to use new obj.
    Is copy of exploration background view at them moment. Will update each to use own icons next.

--- Wikipedia/Code/WMFWelcomeLanguagesAnimationBackgroundView.swift
+++ Wikipedia/Code/WMFWelcomeLanguagesAnimationBackgroundView.swift
@@ -1,166 +1,17 @@
 import Foundation
 
-open class WMFWelcomeLanguagesAnimationBackgroundView : WMFWelcomeAnimationView {
-
-    lazy var bubbleLeftImgView: UIImageView = {
-        let imgView = UIImageView(frame: bounds)
-        imgView.image = UIImage(named: "ftux-left-bubble")
-        imgView.contentMode = UIViewContentMode.scaleAspectFit
-        imgView.layer.zPosition = 102
-        imgView.layer.opacity = 0
-        imgView.layer.transform = wmf_scaleZeroAndLowerLeftTransform
-        return imgView
-    }()
-    
-    lazy var bubbleRightImgView: UIImageView = {
-        let imgView = UIImageView(frame: bounds)
-        imgView.image = UIImage(named: "ftux-right-bubble")
-        imgView.contentMode = UIViewContentMode.scaleAspectFit
-        imgView.layer.zPosition = 101
-        imgView.layer.opacity = 0
-        imgView.layer.transform = wmf_scaleZeroAndLowerRightTransform
-        return imgView
-    }()
-    
-    lazy var dashedCircle: WelcomeCircleShapeLayer = {
-        return WelcomeCircleShapeLayer(
-            unitRadius: 0.31,
-            unitOrigin: CGPoint(x: 0.508, y: 0.518),
-            referenceSize: frame.size,
-            isDashed: true,
-            transform: wmf_scaleZeroTransform,
-            opacity:0.0
-        )
-    }()
-    
-    lazy var solidCircle: WelcomeCircleShapeLayer = {
-        return WelcomeCircleShapeLayer(
-            unitRadius: 0.31,
-            unitOrigin: CGPoint(x: 0.39, y: 0.5),
-            referenceSize: frame.size,
-            isDashed: false,
-            transform: wmf_scaleZeroTransform,
-            opacity:0.0
-        )
-    }()
-    
-    lazy var plus1: WelcomePlusShapeLayer = {
-        return WelcomePlusShapeLayer(
-            unitOrigin: CGPoint(x: 0.825, y: 0.225),
-            unitWidth: 0.05,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var plus2: WelcomePlusShapeLayer = {
-        return WelcomePlusShapeLayer(
-            unitOrigin: CGPoint(x: 0.755, y: 0.17),
-            unitWidth: 0.05,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var plus3: WelcomePlusShapeLayer = {
-        return WelcomePlusShapeLayer(
-            unitOrigin: CGPoint(x: 0.112, y: 0.353),
-            unitWidth: 0.05,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line1: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.845, y: 0.865),
-            unitWidth: 0.135,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line2: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.255, y: 0.162),
-            unitWidth: 0.135,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line3: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.205, y: 0.127),
-            unitWidth: 0.135,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    override open func addAnimationElementsScaledToCurrentFrameSize(){
-        removeExistingSubviewsAndSublayers()
+open class WMFWelcomeLanguagesAnimationBackgroundView : WMFWelcomeAnimationBackgroundView {
+    override open lazy var imageModels:[ImageModel]? = [
+        ImageModel.init(name: "ftux-background-globe", unitSize: CGSize(width: 0.071875, height: 0.071875), unitDestination:CGPoint(x: 0.265625, y: -0.32621), delay: 0.8, duration: 1.3, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-map-dot", unitSize: CGSize(width: 0.0625, height: 0.071875), unitDestination:CGPoint(x: 0.2015625, y: 0.286585), delay: 1.0, duration: 1.4, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-calendar", unitSize: CGSize(width: 0.0625, height: 0.071875), unitDestination:CGPoint(x: -0.3140625, y: -0.417682), delay: 1.2, duration: 1.5, initialOpacity: 0.0),
         
-        addSubview(bubbleLeftImgView)
-        addSubview(bubbleRightImgView)
-
-        [
-            solidCircle,
-            dashedCircle,
-            plus1,
-            plus2,
-            plus3,
-            line1,
-            line2,
-            line3
-            ].forEach(layer.addSublayer)
-    }
-
-    override open func beginAnimations() {
-        CATransaction.begin()
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: 0.2015625, y: -0.49085), delay: 1.1, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: 0.284375, y: 0.10670), delay: 0.5, duration: 1.3, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: -0.303125, y: 0.051829), delay: 0.9, duration: 1.5, initialOpacity: 0.0),
         
-        bubbleLeftImgView.layer.wmf_animateToOpacity(1.0,
-            transform: CATransform3DIdentity,
-            delay: 0.1,
-            duration: 1.0
-        )
-        
-        bubbleRightImgView.layer.wmf_animateToOpacity(1.0,
-            transform: CATransform3DIdentity,
-            delay: 0.3,
-            duration: 1.0
-        )
-        
-        solidCircle.wmf_animateToOpacity(0.04,
-            transform: CATransform3DIdentity,
-            delay: 0.3,
-            duration: 1.0
-        )
-        
-        let animate = { (layer: CALayer) in
-            layer.wmf_animateToOpacity(0.15,
-                transform: CATransform3DIdentity,
-                delay: 0.3,
-                duration: 1.0
-            )
-        }
-        
-        [
-            dashedCircle,
-            plus1,
-            plus2,
-            plus3,
-            line1,
-            line2,
-            line3
-            ].forEach(animate)
-        
-        CATransaction.commit()
-    }
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: 0.3359375, y: -0.100609), delay: 1.1, duration: 1.4, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: -0.275, y: 0.34756), delay: 0.6, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: -0.3984375, y: -0.155487), delay: 0.8, duration: 1.2, initialOpacity: 0.0)
+    ]
 }
