51e03b3a0e28824fd1348e4143d25860e36d33b1
montehurd <mhurd@wikimedia.org>
2017-09-20 13:34:24 -0700

    Update Welcome analytics background animations.

--- Wikipedia/Code/WMFWelcomeAnalyticsAnimationBackgroundView.swift
+++ Wikipedia/Code/WMFWelcomeAnalyticsAnimationBackgroundView.swift
@@ -2,16 +2,16 @@ import Foundation
 
 open class WMFWelcomeAnalyticsAnimationBackgroundView : WMFWelcomeAnimationBackgroundView {
     override open lazy var imageModels:[ImageModel]? = [
-        ImageModel.init(name: "ftux-background-globe", unitSize: CGSize(width: 0.071875, height: 0.071875), unitDestination:CGPoint(x: 0.265625, y: -0.32621), delay: 0.8, duration: 1.3, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-map-dot", unitSize: CGSize(width: 0.0625, height: 0.071875), unitDestination:CGPoint(x: 0.2015625, y: 0.286585), delay: 1.0, duration: 1.4, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-calendar", unitSize: CGSize(width: 0.0625, height: 0.071875), unitDestination:CGPoint(x: -0.3140625, y: -0.417682), delay: 1.2, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-chart", unitSize: CGSize(width: 0.05, height: 0.04375), unitDestination:CGPoint(x: 0.26875, y: 0.329268), delay: 0.8, duration: 1.3, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-shield-star", unitSize: CGSize(width: 0.05, height: 0.0625), unitDestination:CGPoint(x: -0.3234375, y: -0.41158), delay: 1.0, duration: 1.4, initialOpacity: 0.0),
         
-        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: 0.2015625, y: -0.49085), delay: 1.1, duration: 1.5, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: 0.284375, y: 0.10670), delay: 0.5, duration: 1.3, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.060975), unitDestination:CGPoint(x: -0.303125, y: 0.051829), delay: 0.9, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.03125), unitDestination:CGPoint(x: 0.23125, y: -0.466463), delay: 1.1, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.03125), unitDestination:CGPoint(x: 0.2984375, y: 0.076219), delay: 0.5, duration: 1.3, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-plus", unitSize: CGSize(width: 0.03125, height: 0.03125), unitDestination:CGPoint(x: -0.309375, y: 0.070121), delay: 0.9, duration: 1.5, initialOpacity: 0.0),
         
-        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: 0.3359375, y: -0.100609), delay: 1.1, duration: 1.4, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: -0.275, y: 0.34756), delay: 0.6, duration: 1.5, initialOpacity: 0.0),
-        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.079268), unitDestination:CGPoint(x: -0.3984375, y: -0.155487), delay: 0.8, duration: 1.2, initialOpacity: 0.0)
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.040625), unitDestination:CGPoint(x: 0.3359375, y: -0.161585), delay: 0.6, duration: 1.5, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.040625), unitDestination:CGPoint(x: -0.3984375, y: -0.125), delay: 0.8, duration: 1.2, initialOpacity: 0.0),
+        ImageModel.init(name: "ftux-background-circle", unitSize: CGSize(width: 0.040625, height: 0.040625), unitDestination:CGPoint(x: -0.2765625, y: 0.31707), delay: 0.8, duration: 1.2, initialOpacity: 0.0)
     ]
 }
+
