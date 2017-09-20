0952170f30f694f02d8966a75f6054528caf7bd7
montehurd <mhurd@wikimedia.org>
2017-09-20 14:01:16 -0700

    Use new assets on Welcome analytics foreground animation.

--- Wikipedia/Code/WMFWelcomeAnalyticsAnimationView.swift
+++ Wikipedia/Code/WMFWelcomeAnalyticsAnimationView.swift
@@ -2,86 +2,30 @@ import Foundation
 
 open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
 
-    lazy var fileImgView: UIImageView = {
+    lazy var bubbleLeftImgView: UIImageView = {
         let imgView = UIImageView(frame: bounds)
-        imgView.image = UIImage(named: "ftux-file")
+        imgView.image = UIImage(named: "ftux-analytics-phone")
         imgView.contentMode = UIViewContentMode.scaleAspectFit
-        imgView.layer.zPosition = 101
+        imgView.layer.zPosition = 102
         imgView.layer.opacity = 0
-        imgView.layer.transform = wmf_leftTransform
+        imgView.layer.transform = wmf_scaleZeroAndLowerLeftTransform
         return imgView
     }()
     
-    var squashedHeightBarTransform: CATransform3D{
-        return CATransform3DMakeScale(1, 0, 1)
-    }
-    
-    var fullHeightBarTransform: CATransform3D{
-        return CATransform3DMakeScale(1, -1, 1) // -1 to flip Y so bars grow from bottom up.
-    }
-
-    var barOddColor: CGColor{
-        return UIColor(0x4A90E2).cgColor
-    }
-    
-    var barEvenColor: CGColor{
-        return UIColor(0x2A4B8D).cgColor
-    }
-
-    lazy var bar1: WelcomeBarShapeLayer = {
-        let bar = WelcomeBarShapeLayer(
-            unitRect: CGRect(x: 0.313, y: 0.64, width: 0.039, height: 0.18),
-            referenceSize: frame.size,
-            transform: squashedHeightBarTransform
-        )
-        bar.fillColor = barOddColor
-        return bar
-    }()
-    
-    lazy var bar2: WelcomeBarShapeLayer = {
-        let bar = WelcomeBarShapeLayer(
-            unitRect: CGRect(x: 0.383, y: 0.64, width: 0.039, height: 0.23),
-            referenceSize: frame.size,
-            transform: squashedHeightBarTransform
-        )
-        bar.fillColor = barEvenColor
-        return bar
-    }()
-    
-    lazy var bar3: WelcomeBarShapeLayer = {
-        let bar = WelcomeBarShapeLayer(
-            unitRect: CGRect(x: 0.453, y: 0.64, width: 0.039, height: 0.06),
-            referenceSize:frame.size,
-            transform: squashedHeightBarTransform
-        )
-        bar.fillColor = barOddColor
-        return bar
-    }()
-    
-    lazy var bar4: WelcomeBarShapeLayer = {
-        let bar = WelcomeBarShapeLayer(
-            unitRect: CGRect(x: 0.523, y: 0.64, width: 0.039, height: 0.12),
-            referenceSize: frame.size,
-            transform: squashedHeightBarTransform
-        )
-        bar.fillColor = barEvenColor
-        return bar
-    }()
-    
-    lazy var bar5: WelcomeBarShapeLayer = {
-        let bar = WelcomeBarShapeLayer(
-            unitRect: CGRect(x: 0.593, y: 0.64, width: 0.039, height: 0.15),
-            referenceSize: frame.size,
-            transform: squashedHeightBarTransform
-        )
-        bar.fillColor = barOddColor
-        return bar
+    lazy var bubbleRightImgView: UIImageView = {
+        let imgView = UIImageView(frame: bounds)
+        imgView.image = UIImage(named: "ftux-analytics-chart")
+        imgView.contentMode = UIViewContentMode.scaleAspectFit
+        imgView.layer.zPosition = 101
+        imgView.layer.opacity = 0
+        imgView.layer.transform = wmf_scaleZeroAndLowerRightTransform
+        return imgView
     }()
     
     lazy var dashedCircle: WelcomeCircleShapeLayer = {
         return WelcomeCircleShapeLayer(
-            unitRadius: 0.258,
-            unitOrigin: CGPoint(x: 0.61, y: 0.44),
+            unitRadius: 0.31,
+            unitOrigin: CGPoint(x: 0.508, y: 0.518),
             referenceSize: frame.size,
             isDashed: true,
             transform: wmf_scaleZeroTransform,
@@ -91,8 +35,8 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
     
     lazy var solidCircle: WelcomeCircleShapeLayer = {
         return WelcomeCircleShapeLayer(
-            unitRadius: 0.235,
-            unitOrigin: CGPoint(x: 0.654, y: 0.41),
+            unitRadius: 0.31,
+            unitOrigin: CGPoint(x: 0.39, y: 0.5),
             referenceSize: frame.size,
             isDashed: false,
             transform: wmf_scaleZeroTransform,
@@ -102,7 +46,7 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
     
     lazy var plus1: WelcomePlusShapeLayer = {
         return WelcomePlusShapeLayer(
-            unitOrigin: CGPoint(x: 0.9, y: 0.222),
+            unitOrigin: CGPoint(x: 0.825, y: 0.225),
             unitWidth: 0.05,
             referenceSize: frame.size,
             transform: wmf_scaleZeroTransform,
@@ -112,7 +56,17 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
     
     lazy var plus2: WelcomePlusShapeLayer = {
         return WelcomePlusShapeLayer(
-            unitOrigin: CGPoint(x: 0.832, y: 0.167),
+            unitOrigin: CGPoint(x: 0.755, y: 0.17),
+            unitWidth: 0.05,
+            referenceSize: frame.size,
+            transform: wmf_scaleZeroTransform,
+            opacity: 0.0
+        )
+    }()
+    
+    lazy var plus3: WelcomePlusShapeLayer = {
+        return WelcomePlusShapeLayer(
+            unitOrigin: CGPoint(x: 0.112, y: 0.353),
             unitWidth: 0.05,
             referenceSize: frame.size,
             transform: wmf_scaleZeroTransform,
@@ -122,58 +76,28 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
     
     lazy var line1: WelcomeLineShapeLayer = {
         return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.82, y: 0.778),
-            unitWidth: 0.125,
+            unitOrigin: CGPoint(x: 0.845, y: 0.865),
+            unitWidth: 0.135,
             referenceSize: frame.size,
-            transform: wmf_scaleZeroAndRightTransform,
+            transform: wmf_scaleZeroAndLeftTransform,
             opacity: 0.0
         )
     }()
     
     lazy var line2: WelcomeLineShapeLayer = {
         return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.775, y: 0.736),
-            unitWidth: 0.127,
+            unitOrigin: CGPoint(x: 0.255, y: 0.162),
+            unitWidth: 0.135,
             referenceSize: frame.size,
-            transform: wmf_scaleZeroAndRightTransform,
+            transform: wmf_scaleZeroAndLeftTransform,
             opacity: 0.0
         )
     }()
     
     lazy var line3: WelcomeLineShapeLayer = {
         return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.233, y: 0.385),
-            unitWidth: 0.043,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line4: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.17, y: 0.385),
-            unitWidth: 0.015,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line5: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.11, y: 0.427),
-            unitWidth: 0.043,
-            referenceSize: frame.size,
-            transform: wmf_scaleZeroAndLeftTransform,
-            opacity: 0.0
-        )
-    }()
-    
-    lazy var line6: WelcomeLineShapeLayer = {
-        return WelcomeLineShapeLayer(
-            unitOrigin: CGPoint(x: 0.173, y: 0.427),
-            unitWidth: 0.015,
+            unitOrigin: CGPoint(x: 0.205, y: 0.127),
+            unitWidth: 0.135,
             referenceSize: frame.size,
             transform: wmf_scaleZeroAndLeftTransform,
             opacity: 0.0
@@ -182,69 +106,48 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
     
     override open func addAnimationElementsScaledToCurrentFrameSize(){
         removeExistingSubviewsAndSublayers()
-
-        addSubview(fileImgView)
         
-        [
-            bar1,
-            bar2,
-            bar3,
-            bar4,
-            bar5
-            ].forEach(fileImgView.layer.addSublayer)
+        addSubview(bubbleLeftImgView)
+        addSubview(bubbleRightImgView)
         
         [
             solidCircle,
             dashedCircle,
             plus1,
             plus2,
+            plus3,
             line1,
             line2,
-            line3,
-            line4,
-            line5,
-            line6
+            line3
             ].forEach(layer.addSublayer)
     }
     
     override open func beginAnimations() {
         CATransaction.begin()
         
-        fileImgView.layer.wmf_animateToOpacity(1.0,
-            transform: CATransform3DIdentity,
-            delay: 0.1,
-            duration: 1.0
+        bubbleLeftImgView.layer.wmf_animateToOpacity(1.0,
+                                                     transform: CATransform3DIdentity,
+                                                     delay: 0.1,
+                                                     duration: 1.0
+        )
+        
+        bubbleRightImgView.layer.wmf_animateToOpacity(1.0,
+                                                      transform: CATransform3DIdentity,
+                                                      delay: 0.3,
+                                                      duration: 1.0
         )
         
         solidCircle.wmf_animateToOpacity(0.04,
-            transform: CATransform3DIdentity,
-            delay: 0.3,
-            duration: 1.0
+                                         transform: CATransform3DIdentity,
+                                         delay: 0.3,
+                                         duration: 1.0
         )
-
-        var barIndex = 0
-        let animateBarGrowingUp = { (layer: CALayer) in
-            layer.wmf_animateToOpacity(1.0,
-                transform: self.fullHeightBarTransform,
-                delay: (0.3 + (Double(barIndex) * 0.1)),
-                duration: 0.3
-            )
-            barIndex+=1
-        }
-        
-        [
-            bar1,
-            bar2,
-            bar3,
-            bar4,
-            bar5
-            ].forEach(animateBarGrowingUp)
         
         let animate = { (layer: CALayer) in
             layer.wmf_animateToOpacity(0.15,
-                transform: CATransform3DIdentity,
-                delay: 0.3,
-                duration: 1.0
+                                       transform: CATransform3DIdentity,
+                                       delay: 0.3,
+                                       duration: 1.0
             )
         }
         
@@ -252,12 +155,10 @@ open class WMFWelcomeAnalyticsAnimationView : WMFWelcomeAnimationView {
             dashedCircle,
             plus1,
             plus2,
+            plus3,
             line1,
             line2,
-            line3,
-            line4,
-            line5,
-            line6
+            line3
             ].forEach(animate)
         
         CATransaction.commit()
