b66c89e48fea911448a8f84b68f2807a8a0a135f
joewalsh <joewalsh@users.noreply.github.com>
2017-09-25 12:40:10 -0400

    Fix share-a-fact truncation

--- Wikipedia/Code/ShareAFactViewController.swift
+++ Wikipedia/Code/ShareAFactViewController.swift
@@ -64,6 +64,7 @@ class ShareAFactViewController: UIViewController {
         let size = textLabel.sizeThatFits(CGSize(width: width, height: view.bounds.size.height))
         if size.height > 0.6 * view.bounds.size.height {
             textLabel.font = UIFont.systemFont(ofSize: 14)
+            textLabel.attributedText = nil
             textLabel.text = text
         }
         articleTitleLabel.text = articleTitle
