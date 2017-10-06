897503e23c573686095db8747dbfe2264ca6e764
Natalia Harateh <nambatee@users.noreply.github.com>
2017-10-06 10:10:15 -0400

    adjust insets

--- Wikipedia/Code/WMFArticleViewController.m
+++ Wikipedia/Code/WMFArticleViewController.m
@@ -175,7 +175,7 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
         self.hidesBottomBarWhenPushed = YES;
         self.edgesForExtendedLayout = UIRectEdgeAll;
         self.extendedLayoutIncludesOpaqueBars = YES;
-        self.automaticallyAdjustsScrollViewInsets = NO;
+        self.automaticallyAdjustsScrollViewInsets = YES;
         self.reachabilityManager = [AFNetworkReachabilityManager manager];
         [self.reachabilityManager startMonitoring];
         self.savingOpenArticleTitleEnabled = YES;
