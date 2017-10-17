3d38ce93cfdd0fa8c2b79c85c5066330e91ce02f
Natalia Harateh <nambatee@users.noreply.github.com>
2017-10-17 17:23:12 -0400

    Apply theme in the init method

--- Wikipedia/Code/WMFThemeableNavigationController.m
+++ Wikipedia/Code/WMFThemeableNavigationController.m
@@ -12,16 +12,9 @@
     self = [super initWithRootViewController:rootViewController];
     if (self) {
         self.theme = theme;
+        [self applyTheme:theme];
     }
     return self;
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    if (self.theme == nil) {
-        self.theme = [WMFTheme standard];
-    }
-    [self applyTheme:self.theme];
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
