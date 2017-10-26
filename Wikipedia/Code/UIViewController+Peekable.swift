5f412afb95dac2a76535d8140066385954b99c75
Natalia Harateh <nambatee@users.noreply.github.com>
2017-10-26 11:54:20 -0400

    Add a func to set up Peekable with self

--- Wikipedia/Code/UIViewController+Peekable.swift
+++ Wikipedia/Code/UIViewController+Peekable.swift
@@ -2,6 +2,14 @@ import UIKit
 
 extension UIViewController {
     // Adds a peekable view controller on top of another view controller. Since the view controller is the one we want to show when the user peeks through, we're saving time by loading it behind the peekable view controller.
+    @objc func wmf_setupPeekable(_ peekableViewController: UIViewController, with articleURL: URL) -> UIViewController {
+        self.addChildViewController(peekableViewController)
+        peekableViewController.view.frame = self.view.frame
+        self.view.wmf_addSubviewWithConstraintsToEdges(peekableViewController.view)
+        peekableViewController.didMove(toParentViewController: self)
+        return self
+    }
+    
     @objc func wmf_setupPeekable(_ peekableViewController: UIViewController, on viewController: UIViewController, with articleURL: URL) -> UIViewController {
         viewController.addChildViewController(peekableViewController)
         peekableViewController.view.frame = viewController.view.frame
@@ -10,7 +18,7 @@ extension UIViewController {
         return viewController
     }
     
-    @objc func wmf_removePeekable(_ peekableViewController: UIViewController, from viewController: UIViewController) {
+    @objc func wmf_removePeekable(_ peekableViewController: UIViewController) {
         peekableViewController.view.removeFromSuperview()
         peekableViewController.removeFromParentViewController()
     }
