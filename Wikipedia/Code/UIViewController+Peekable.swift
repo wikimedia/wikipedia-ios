fb711f7490801503a19e33a129ab72185357012f
Natalia Harateh <nambatee@users.noreply.github.com>
2017-10-26 15:01:44 -0400

    Remove unused method

--- Wikipedia/Code/UIViewController+Peekable.swift
+++ Wikipedia/Code/UIViewController+Peekable.swift
@@ -6,13 +6,12 @@ protocol Peekable {
 
 extension UIViewController {
     // Adds a peekable view controller on top of another view controller. Since the view controller is the one we want to show when the user peeks through, we're saving time by loading it behind the peekable view controller.
-    fileprivate func wmf_addPeekableChildViewController(_ peekableViewController: UIViewController) {
-        guard peekableViewController is Peekable else {
-            return
-        }
-        self.addChildViewController(peekableViewController)
-        self.view.wmf_addSubviewWithConstraintsToEdges(peekableViewController.view)
-        peekableViewController.didMove(toParentViewController: self)
+    @objc func wmf_addPeekableChildViewController(for articleURL: URL, dataStore: MWKDataStore, theme: Theme) {
+        let articlePeekPreviewViewController = ArticlePeekPreviewViewController(articleURL: articleURL, dataStore: dataStore, theme: theme)
+        
+        self.addChildViewController(articlePeekPreviewViewController)
+        self.view.wmf_addSubviewWithConstraintsToEdges(articlePeekPreviewViewController.view)
+        articlePeekPreviewViewController.didMove(toParentViewController: self)
     }
     
     @objc func wmf_removePeekableChildViewControllers() {
@@ -25,9 +24,5 @@ extension UIViewController {
             vc.removeFromParentViewController()
         }
     }
-    
-    @objc func wmf_addPeekableChildViewController(for articleURL: URL, dataStore: MWKDataStore, theme: Theme) {
-        let articlePeekPreviewViewController = ArticlePeekPreviewViewController(articleURL: articleURL, dataStore: dataStore, theme: theme)
-        self.wmf_addPeekableChildViewController(articlePeekPreviewViewController)
-    }
+
 }
