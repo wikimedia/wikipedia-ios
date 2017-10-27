299a4bd49f1282bfd2ff3e305fb8f68d2b327572
montehurd <mhurd@wikimedia.org>
2017-10-27 12:09:37 -0700

    Remove unneeded hack.

--- www/js/transforms/relocateFirstParagraph.js
+++ www/js/transforms/relocateFirstParagraph.js
@@ -21,11 +21,7 @@ function moveFirstGoodParagraphAfterElement(preceedingElementID, content ) {
   function isParagraphGood(p) {
     // Narrow down to first P which is direct child of content_block_0 DIV.
     // (Don't want to yank P from somewhere in the middle of a table!)
-    if  (p.parentNode == block_0 ||
-            /* HAX: the line below is a temporary fix for <div class="mw-mobilefrontend-leadsection"> temporarily
-               leaking into mobileview output - as soon as that div is removed the line below will no longer be needed. */
-            p.parentNode.className == 'mw-mobilefrontend-leadsection'
-            ){
+    if  (p.parentNode == block_0){
                 // Ensure the P being pulled up has at least a couple lines of text.
                 // Otherwise silly things like a empty P or P which only contains a
                 // BR tag will get pulled up (see articles on "Chemical Reaction" and
