311289ccc744de1b7dd312d6c060921c25a54341
Natalia Harateh <nambatee@users.noreply.github.com>
2017-10-30 11:47:00 -0400

    Add accessibility notifications to swipe actions

--- Wikipedia/Code/ArticleCollectionViewController.swift
+++ Wikipedia/Code/ArticleCollectionViewController.swift
@@ -188,15 +188,18 @@ extension ArticleCollectionViewController: ActionDelegate {
         switch action.type {
         case .delete:
             delete(at: indexPath)
+            UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, WMFLocalizedString("article-deleted-accessibility-notification", value: "Article deleted", comment: "Notification spoken after user deletes an article from the list."))
             return true
         case .save:
             if let articleURL = articleURL(at: indexPath) {
                 dataStore.savedPageList.addSavedPage(with: articleURL)
+                UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, WMFLocalizedString("article-saved-for-later-accessibility-notification", value: "Article saved for later", comment: "Notification spoken after user saves an article for later."))
                 return true
             }
         case .unsave:
             if let articleURL = articleURL(at: indexPath) {
                 dataStore.savedPageList.removeEntry(with: articleURL)
+                UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, WMFLocalizedString("article-unsaved-accessibility-notification", value: "Article unsaved", comment: "Notification spoken after user removes an article from Saved articles."))
                 return true
             }
         case .share:
