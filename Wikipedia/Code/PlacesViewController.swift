75b721dc9eaef011c41e4773fa49aa7d0f1ad280
Natalia Harateh <nambatee@users.noreply.github.com>
2017-11-08 15:20:48 -0500

    Fix wrong point calculation resulting in wrong peek controller

--- Wikipedia/Code/PlacesViewController.swift
+++ Wikipedia/Code/PlacesViewController.swift
@@ -2703,7 +2703,7 @@ extension PlacesViewController {
         guard viewMode == .list else {
             return nil
         }
-        let point = view.convert(location, to: listViewController.view)
+        let point = view.convert(location, to: listViewController.collectionView)
         return listViewController.previewingContext(previewingContext, viewControllerForLocation:point)
     }
     
