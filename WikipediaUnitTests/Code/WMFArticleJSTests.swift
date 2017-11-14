c86c0c0218c4962746b80739d55e208d9710e2c9
montehurd <mhurd@wikimedia.org>
2017-11-13 21:04:45 -0800

    Re-organize so cache warming doesn't effect 'measureMetrics' loop.

--- WikipediaUnitTests/Code/WMFArticleJSTests.swift
+++ WikipediaUnitTests/Code/WMFArticleJSTests.swift
@@ -4,34 +4,72 @@ import XCTest
 
 class WMFArticleJSTests: XCTestCase, WKScriptMessageHandler {
     
-    var session: SessionSingleton?
-    var webVC: WebViewController?
-    var obamaURL: URL?
-    var obamaArticle: MWKArticle?
+    lazy var session: SessionSingleton = SessionSingleton.init(dataStore: MWKDataStore.temporary())
+    lazy var dataStore = session.dataStore!
+    lazy var obamaURL: URL = NSURL.wmf_URL(withDomain: "wikipedia.org", language: "en", title: "Barack Obama", fragment: nil)!
+    lazy var obamaArticle = article(withMobileViewJSONFixture: "Obama", with: obamaURL, dataStore: dataStore)
 
     var firstSectionAppearedMessageReceivedExpectation: XCTestExpectation?
     var startTimeMessageReceivedExpectation: XCTestExpectation?
 
+    lazy var webVC: WebViewController = {
+        // WebViewController configured to emit two events when an article is loaded:
+        //    - 'startTime'
+        //    - 'testFirstSectionAppearance'
+        let vc = WebViewController.wmf_initialViewControllerFromClassStoryboard()!
+    
+        // Configure the WKUserContentController used by the web view controller - easy way to attach testing JS while
+        // keeping all existing JS in place.
+        vc.wkUserContentControllerTestingConfigurationBlock = { userContentController in
+            // Add self as 'testFirstSectionAppearance' script message handler.
+            // The 'userContentController:didReceiveMessage:' delegate method will receive these messages.
+            userContentController.add(self, name: self.testFirstSectionAppearanceMessageHandlerString)
+            
+            // This message will be sent as soon as the web view inflates the DOM of the index.html (before our
+            // sections are injected).
+            let startTimeJS = "window.webkit.messageHandlers.\(self.testFirstSectionAppearanceMessageHandlerString).postMessage('\(self.testFirstSectionAppearanceStartTimeMessageString)')"
+            userContentController.addUserScript(
+                WKUserScript.init(source: startTimeJS, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            )
+            
+            // This message will fire when the element with the 'soughtID' appears.
+            // Idea from: https://davidwalsh.name/detect-node-insertion
+            let soughtID = "section_heading_and_content_block_0"
+            let sectionAppearanceJS = """
+            var style = document.createElement('style')
+            style.type = 'text/css'
+            style.innerHTML = `
+                @keyframes soughtNodeInsertionAnimation {
+                    from { opacity: .99; }
+                    to { opacity: 1; }
+                }
+                #\(soughtID) {
+                    animation-duration: 0.001s;
+                    animation-name: soughtNodeInsertionAnimation;
+                }
+            `
+            document.getElementsByTagName('head')[0].appendChild(style)
+            document.addEventListener('animationstart', (event) => {
+                if (event.animationName === 'soughtNodeInsertionAnimation') {
+                    if(event.target.id === '\(soughtID)'){
+                        window.webkit.messageHandlers.\(self.testFirstSectionAppearanceMessageHandlerString).postMessage('\(self.testFirstSectionAppearedMessageString)')
+                    }
+                }
+            }, false)
+            """
+            userContentController.addUserScript(
+                WKUserScript.init(source: sectionAppearanceJS, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            )
+        }
+
+        UIApplication.shared.keyWindow?.rootViewController = vc
+
+        return vc
+    }()
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        session = SessionSingleton.init(dataStore: MWKDataStore.temporary())
-        webVC = WebViewController.wmf_initialViewControllerFromClassStoryboard()
-        
-        guard
-            let newObamaURL = NSURL.wmf_URL(withDomain: "wikipedia.org", language: "en", title: "Barack Obama", fragment: nil),
-            let dataStore = session?.dataStore
-        else {
-            assertionFailure("Expected Obama article and data store")
-            return
-        }
-        obamaURL = newObamaURL
-        obamaArticle = article(withMobileViewJSONFixture: "Obama", with: newObamaURL, dataStore: dataStore)
-        obamaArticle?.save()
-        
-        startTimeMessageReceivedExpectation = nil
-        firstSectionAppearedMessageReceivedExpectation = nil
     }
     
     override func tearDown() {
@@ -66,62 +104,24 @@ class WMFArticleJSTests: XCTestCase, WKScriptMessageHandler {
             return
         }
     }
-    
+
     func testFirstSectionAppearanceDelay() {
 
+        // Load the article once before kicking off the 'measureMetrics' pass. Ensures any caching has been warmed.
+        webVC.setArticle(obamaArticle, articleURL: obamaArticle.url)
+        startTimeMessageReceivedExpectation = expectation(description: "waiting for start time message")
+        firstSectionAppearedMessageReceivedExpectation = expectation(description: "waiting for first section appeared message")
+        wait(for:[startTimeMessageReceivedExpectation!, firstSectionAppearedMessageReceivedExpectation!], timeout: 100, enforceOrder: true)
+
         measureMetrics(XCTestCase.defaultPerformanceMetrics, automaticallyStartMeasuring: false, for: {
-            
             // Needed because 'measureMetrics' fires this block off ten times and the other expectations aren't scoped
             // to this block because they are fulfilled in a delegate callback.
             let safeToContinueExpectation = expectation(description: "waiting for previous measurement to finish")
             
             startTimeMessageReceivedExpectation = expectation(description: "waiting for start time message")
             firstSectionAppearedMessageReceivedExpectation = expectation(description: "waiting for first section appeared message")
-
-            // Configure the WKUserContentController used by the web view controller - easy way to attach testing JS while
-            // keeping all existing JS in place.
-            webVC?.wkUserContentControllerTestingConfigurationBlock = { userContentController in
-                // Add self as 'jsTesting' script message handler.
-                userContentController.add(self, name: self.testFirstSectionAppearanceMessageHandlerString)
-                
-                // This message will be sent as soon as the web view inflates the DOM of the index.html (before our
-                // sections are injected).
-                let startTimeJS = "window.webkit.messageHandlers.\(self.testFirstSectionAppearanceMessageHandlerString).postMessage('\(self.testFirstSectionAppearanceStartTimeMessageString)')"
-                userContentController.addUserScript(
-                    WKUserScript.init(source: startTimeJS, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-                )
-                
-                // See https://davidwalsh.name/detect-node-insertion for section appearance approach details.
-                let soughtID = "section_heading_and_content_block_0"
-                let sectionAppearanceJS = """
-                var style = document.createElement('style')
-                style.type = 'text/css'
-                style.innerHTML = `
-                    @keyframes soughtNodeInsertionAnimation {
-                        from { opacity: .99; }
-                        to { opacity: 1; }
-                    }
-                    #\(soughtID) {
-                        animation-duration: 0.001s;
-                        animation-name: soughtNodeInsertionAnimation;
-                    }
-                `
-                document.getElementsByTagName('head')[0].appendChild(style)
-                document.addEventListener('animationstart', (event) => {
-                    if (event.animationName === 'soughtNodeInsertionAnimation') {
-                        if(event.target.id === '\(soughtID)'){
-                            window.webkit.messageHandlers.\(self.testFirstSectionAppearanceMessageHandlerString).postMessage('\(self.testFirstSectionAppearedMessageString)')
-                        }
-                    }
-                }, false)
-                """
-                userContentController.addUserScript(
-                    WKUserScript.init(source: sectionAppearanceJS, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-                )
-            }
             
-            UIApplication.shared.keyWindow?.rootViewController = webVC
-            webVC?.setArticle(obamaArticle, articleURL: obamaURL!)
+            webVC.setArticle(obamaArticle, articleURL: obamaArticle.url)
             
             wait(for: [startTimeMessageReceivedExpectation!], timeout: 100)
             startMeasuring()
@@ -134,11 +134,6 @@ class WMFArticleJSTests: XCTestCase, WKScriptMessageHandler {
             safeToContinueExpectation.fulfill()
             
             wait(for: [safeToContinueExpectation], timeout: 100)
-            
-            // Reset everything for next measurement run.
-            UIApplication.shared.keyWindow?.rootViewController = nil
-            firstSectionAppearedMessageReceivedExpectation = nil
-            startTimeMessageReceivedExpectation = nil
         })
     }
 }
