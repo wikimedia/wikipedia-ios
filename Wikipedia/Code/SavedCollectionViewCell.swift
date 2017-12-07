a993d96c7d0462511a9ef51decd0aa8cf2dc48f9
Natalia Harateh <nambatee@users.noreply.github.com>
2017-12-06 22:56:58 -0500

    Don't override sizeThatFits for now.

--- Wikipedia/Code/SavedCollectionViewCell.swift
+++ Wikipedia/Code/SavedCollectionViewCell.swift
@@ -49,43 +49,4 @@ class SavedCollectionViewCell: ArticleRightAlignedImageCollectionViewCell {
         setNeedsLayout()
     }
     
-    override func sizeThatFits(_ size: CGSize, apply: Bool) -> CGSize {
-        let superSize = super.sizeThatFits(size, apply: apply)
-        let isRTL = articleSemanticContentAttribute == .forceRightToLeft
-        let minHeight = imageViewDimension + layoutMargins.top + layoutMargins.bottom
-        var widthMinusMargins = size.width - layoutMargins.left - layoutMargins.right
-        
-        if !isImageViewHidden {
-            widthMinusMargins = widthMinusMargins - layoutMargins.right - imageViewDimension
-        }
-        
-        var x = layoutMargins.left
-        if isRTL {
-            x = size.width - x - widthMinusMargins
-        }
-        
-        let origin = CGPoint(x: x, y: layoutMargins.top)
-        let height = max(origin.y, minHeight)
-        
-        if (apply && !isImageViewHidden) {
-            let imageViewY = floor(0.5*height - 0.5*imageViewDimension)
-            var x = layoutMargins.right
-            if !isRTL {
-                x = size.width - x - imageViewDimension
-            }
-            imageView.frame = CGRect(x: x, y: imageViewY, width: imageViewDimension, height: imageViewDimension)
-        }
-        
-        let separatorWidth: CGFloat = isImageViewHidden ? size.width : size.width - (imageViewDimension * 1.8)
-        if (apply && !bottomSeparator.isHidden) {
-            bottomSeparator.frame = CGRect(x: 0, y: height - singlePixelDimension, width: separatorWidth, height: singlePixelDimension)
-        }
-        
-        if (apply && !topSeparator.isHidden) {
-            topSeparator.frame = CGRect(x: 0, y: 0, width: separatorWidth, height: singlePixelDimension)
-        }
-        
-        return superSize
-    }
-    
 }
