453ff3b4b4aaae4b753d497539d1991ba87f477c
joewalsh <joewalsh@users.noreply.github.com>
2017-12-07 12:44:46 -0500

    add comment

--- Wikipedia/Code/WMFViewController.h
+++ Wikipedia/Code/WMFViewController.h
@@ -5,6 +5,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class WMFNavigationBar;
 
+/*!
+ @class        WMFViewController
+ @abstract     Top level view controller that handles themeing and the navigation bar.
+ @discussion   In Swift, use the equivalent Swift class ViewController. Due to the use of Swift classes Themeable and NavigationBar, it was easier to duplicate the implementation then try to have a single Obj-C base class.
+ */
 @interface WMFViewController : UIViewController <WMFThemeable>
 
 @property (nonatomic, strong) WMFTheme *theme;
