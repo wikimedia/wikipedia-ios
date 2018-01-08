87418bae25ced1522aa043e4d059a1e3c5872050
Natalia Harateh <nambatee@users.noreply.github.com>
2018-01-08 13:45:19 -0500

    remove unused configure method

--- Wikipedia/Code/ReadingListsCollectionViewCell.swift
+++ Wikipedia/Code/ReadingListsCollectionViewCell.swift
@@ -98,41 +98,6 @@ class ReadingListsCollectionViewCell: ArticleCollectionViewCell {
         return CGSize(width: size.width, height: height)
     }
     
-    func configure(article: WMFArticle, index: Int, count: Int, shouldAdjustMargins: Bool = true, shouldShowSeparators: Bool = false, theme: Theme, layoutOnly: Bool) {
-        titleLabel.text = article.displayTitle
-        descriptionLabel.text = article.capitalizedWikidataDescriptionOrSnippet
-        
-        let imageWidthToRequest = imageView.frame.size.width < 300 ? traitCollection.wmf_nearbyThumbnailWidth : traitCollection.wmf_leadImageWidth // 300 is used to distinguish between full-awidth images and thumbnails. Ultimately this (and other thumbnail requests) should be updated with code that checks all the available buckets for the width that best matches the size of the image view.
-        if let imageURL = article.imageURL(forWidth: imageWidthToRequest) {
-            isImageViewHidden = false
-            if !layoutOnly {
-                imageView.wmf_setImage(with: imageURL, detectFaces: true, onGPU: true, failure: { (error) in }, success: { })
-            }
-        } else {
-            isImageViewHidden = true
-        }
-        
-        let articleLanguage = article.url?.wmf_language
-        titleLabel.accessibilityLanguage = articleLanguage
-        descriptionLabel.accessibilityLanguage = articleLanguage
-        extractLabel?.accessibilityLanguage = articleLanguage
-        articleSemanticContentAttribute = MWLanguageInfo.semanticContentAttribute(forWMFLanguage: articleLanguage)
-        
-        if shouldShowSeparators {
-            configureSeparators(for: index)
-        } else {
-            bottomSeparator.isHidden = true
-        }
-        apply(theme: theme)
-        isSaveButtonHidden = true
-        extractLabel?.text = nil
-        imageViewDimension = 40
-        if (shouldAdjustMargins) {
-            adjustMargins(for: index, count: count)
-        }
-        setNeedsLayout()
-    }
-    
     func configure(readingList: ReadingList, index: Int, count: Int, shouldAdjustMargins: Bool = true, shouldShowSeparators: Bool = false, theme: Theme) {
         configure(with: readingList.name, description: readingList.readingListDescription, index: index, count: count, shouldShowSeparators: shouldShowSeparators, theme: theme)
     }
