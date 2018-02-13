43d2abc8a0a1d41be2c76aedb2d6b9181c40d95a
Natalia Harateh <nambatee@users.noreply.github.com>
2018-02-13 15:56:17 -0500

    use existing article-deleted-accessibility-notification key to preserve existing translations

--- WMF Framework/CommonStrings.swift
+++ WMF Framework/CommonStrings.swift
@@ -17,7 +17,7 @@ public class CommonStrings: NSObject {
      @objc public static let accessibilityUnsavedNotification = WMFLocalizedString("action-unsaved-accessibility-notification", value: "Article unsaved", comment: "Notification spoken after user removes an article from Saved articles.")
     
     @objc public static func articleDeletedNotification(articleCount: Int) -> String {
-        return String.localizedStringWithFormat(WMFLocalizedString("article-deleted-notification", value: "{{PLURAL:%1$d|artice|articles}} deleted", comment: "Notification spoken after user deletes an article from the list."), articleCount)
+        return String.localizedStringWithFormat(WMFLocalizedString("article-deleted-accessibility-notification", value: "{{PLURAL:%1$d|artice|articles}} deleted", comment: "Notification spoken after user deletes an article from the list."), articleCount)
     }
     
     @objc public static let shortSaveTitle = WMFLocalizedString("action-save", value: "Save", comment: "Title for the 'Save' action\n{{Identical|Save}}")
