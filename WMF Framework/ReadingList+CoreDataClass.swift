a420b7abedde60629a6575a7e57755021d4174c8
joewalsh <joewalsh@users.noreply.github.com>
2018-02-16 13:31:10 -0500

    ensure articles are updated after list entries change

--- WMF Framework/ReadingList+CoreDataClass.swift
+++ WMF Framework/ReadingList+CoreDataClass.swift
@@ -34,18 +34,26 @@ public class ReadingList: NSManagedObject {
     }
     
     public func updateArticlesAndEntries() {
-        guard let entries = entries else {
-            countOfEntries = 0
-            articles = []
-            return
+        let previousArticles = articles ?? []
+        let previousKeys = Set<String>(previousArticles.flatMap { $0.key })
+        let validEntries = (entries ?? []).filter { !$0.isDeletedLocally }
+        let validArticleKeys = Set<String>(validEntries.flatMap { $0.articleKey })
+        for article in previousArticles {
+            guard let key = article.key, validArticleKeys.contains(key) else {
+                removeFromArticles(article)
+                article.readingListsDidChange()
+                continue
+            }
         }
-        let validEntries = entries.filter { !$0.isDeletedLocally }
-        let validArticleKeys = validEntries.flatMap { $0.articleKey }
         if validArticleKeys.count > 0 {
+            let articleKeysToAdd = validArticleKeys.subtracting(previousKeys)
             do {
-                let validArticles = try managedObjectContext?.wmf_fetch(objectsForEntityName: "WMFArticle", withValues: validArticleKeys, forKey: "key") as? [WMFArticle] ?? []
+                let articlesToAdd = try managedObjectContext?.wmf_fetch(objectsForEntityName: "WMFArticle", withValues: Array(articleKeysToAdd), forKey: "key") as? [WMFArticle] ?? []
                 countOfEntries = Int64(validEntries.count)
-                articles = Set<WMFArticle>(validArticles)
+                for article in articlesToAdd {
+                    addToArticles(article)
+                    article.readingListsDidChange()
+                }
             } catch let error {
                 DDLogError("error updating list: \(error)")
             }
