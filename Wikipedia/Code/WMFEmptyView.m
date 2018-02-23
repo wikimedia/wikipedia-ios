fbedb5ca76c72448cf5e316c1b29d66a3d0191a4
montehurd <mhurd@wikimedia.org>
2018-02-22 18:16:55 -0800

    New history empty view image asset.

--- Wikipedia/Code/WMFEmptyView.m
+++ Wikipedia/Code/WMFEmptyView.m
@@ -97,7 +97,7 @@
 
 + (instancetype)noHistoryEmptyView {
     WMFEmptyView *view = [[self class] emptyView];
-    view.imageView.image = [UIImage imageNamed:@"recent-blank"];
+    view.imageView.image = [UIImage imageNamed:@"history-blank"];
     view.titleLabel.text = WMFLocalizedStringWithDefaultValue(@"empty-no-history-title", nil, nil, @"No history to show", @"Title of a blank screen shown when a user has no history");
     view.messageLabel.text = WMFLocalizedStringWithDefaultValue(@"empty-no-history-message", nil, nil, @"Keep track of what you've been reading here", @"Message of a blank screen shown when a user has no history");
 
