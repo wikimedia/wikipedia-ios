394d9f3b37435bcd9f141cbbc7d90e7f1c009694
Natalia Harateh <nambatee@users.noreply.github.com>
2018-02-27 09:44:30 -0500

    update localized string for ReadingListError.listExistsWithTheSameName and remove associated value delete ReadingListsController+Extensions

--- Wikipedia/Code/ReadingListsViewController.swift
+++ Wikipedia/Code/ReadingListsViewController.swift
@@ -417,3 +417,15 @@ extension ReadingListsViewController {
         return WMFCVLMetrics.singleColumnMetrics(withBoundsSize: size, readableWidth: readableWidth)
     }
 }
+
+extension ReadingListsController {
+    public func handle(_ error: Error) {
+        if let readingListsError = error as? ReadingListError {
+            WMFAlertManager.sharedInstance.showAlert(readingListsError.localizedDescription, sticky: true, dismissPreviousAlerts: true, tapCallBack: nil)
+        } else {
+            WMFAlertManager.sharedInstance.showErrorAlertWithMessage(
+                CommonStrings.unknownError, sticky: false, dismissPreviousAlerts: true, tapCallBack: nil)
+        }
+    }
+}
+
