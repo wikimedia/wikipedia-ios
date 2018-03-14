d61e99580c94945d94c777037065f93e6d565ad0
Natalia Harateh <nambatee@users.noreply.github.com>
2018-03-14 16:35:37 +0100

    show error alert, not alert

--- Wikipedia/Code/ReadingListsViewController.swift
+++ Wikipedia/Code/ReadingListsViewController.swift
@@ -160,7 +160,7 @@ class ReadingListsViewController: ColumnarCollectionViewController, EditableColl
     
     private func handleReadingListError(_ error: Error) {
         if let readingListsError = error as? ReadingListError {
-            WMFAlertManager.sharedInstance.showAlert(readingListsError.localizedDescription, sticky: true, dismissPreviousAlerts: true, tapCallBack: nil)
+            WMFAlertManager.sharedInstance.showErrorAlertWithMessage(readingListsError.localizedDescription, sticky: true, dismissPreviousAlerts: true, tapCallBack: nil)
         } else {
             WMFAlertManager.sharedInstance.showErrorAlertWithMessage(
                 CommonStrings.unknownError, sticky: false, dismissPreviousAlerts: true, tapCallBack: nil)
