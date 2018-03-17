213a1c23e81fbc2869be4423b6d7ddd52d12da6a
Natalia Harateh <nambatee@users.noreply.github.com>
2018-03-17 11:14:31 +0100

    call new setup

--- Wikipedia/Code/ReadingListDetailViewController.swift
+++ Wikipedia/Code/ReadingListDetailViewController.swift
@@ -69,7 +69,7 @@ class ReadingListDetailViewController: ColumnarCollectionViewController, Editabl
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        readingListDetailExtendedViewController.setup(title: readingList.name, description: readingList.readingListDescription, articleCount: readingList.countOfEntries, isDefault: readingList.isDefault, listLimit: dataStore.viewContext.wmf_readingListsConfigMaxListsPerUser, listLimitExceeded: readingList.isListLimitExceeded)
+        readingListDetailExtendedViewController.setup(for: readingList, listLimit: dataStore.viewContext.wmf_readingListsConfigMaxListsPerUser, entryLimit: dataStore.viewContext.wmf_readingListsConfigMaxEntriesPerList.intValue)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
