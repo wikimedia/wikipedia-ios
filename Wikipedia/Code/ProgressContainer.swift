3ca7cdab24a3175e3aa5ec66339e875110774928
montehurd <mhurd@wikimedia.org>
2018-03-22 16:57:09 -0700

    ProgressContainer for making progress accessible to nested controllers.
    Ideally we'd use dependency injection, but at the moment it's a bit hairy to relay the progress (and future ones?) from the app vc to the interested vc.

---
+++ Wikipedia/Code/ProgressContainer.swift
@@ -0,0 +1,6 @@
+
+// This class simply makes Progress objects more easily accessible to nested VCs. Dependency injection would probably be better, but the wiring was getting a bit crazy.
+@objcMembers class ProgressContainer: NSObject {
+    static let shared = ProgressContainer()
+    dynamic var articleFetcherProgress: Progress? = nil
+}
