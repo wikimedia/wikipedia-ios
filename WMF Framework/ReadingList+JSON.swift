6d45b3bc525e22414371a489483ae0aedca9c5ea
Natalia Harateh <nambatee@users.noreply.github.com>
2018-03-23 20:49:47 +0100

    if remote reading list has the same name as our default list, rename it

--- WMF Framework/ReadingList+JSON.swift
+++ WMF Framework/ReadingList+JSON.swift
@@ -3,7 +3,7 @@ import Foundation
 extension ReadingList {
     func update(with remoteList: APIReadingList) {
         self.readingListID = NSNumber(value: remoteList.id)
-        self.name = remoteList.name
+        self.name = remoteList.name == CommonStrings.readingListsDefaultListTitle ? "\(remoteList.name)_[user created]" : remoteList.name
         self.readingListDescription = remoteList.description
         if let createdDate = DateFormatter.wmf_iso8601().date(from: remoteList.created) {
             self.createdDate = createdDate as NSDate
