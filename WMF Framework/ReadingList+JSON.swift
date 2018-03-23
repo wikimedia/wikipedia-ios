cf654222833f4b2d04adb1ca755e5f9baeee500b
Natalia Harateh <nambatee@users.noreply.github.com>
2018-03-23 22:22:38 +0100

    split condition

--- WMF Framework/ReadingList+JSON.swift
+++ WMF Framework/ReadingList+JSON.swift
@@ -3,7 +3,11 @@ import Foundation
 extension ReadingList {
     func update(with remoteList: APIReadingList) {
         self.readingListID = NSNumber(value: remoteList.id)
-        self.name = remoteList.name == CommonStrings.readingListsDefaultListTitle ? "\(remoteList.name)_[user created]" : remoteList.name
+        if remoteList.name == CommonStrings.readingListsDefaultListTitle {
+            self.name = "\(remoteList.name)_[user created]"
+        } else {
+            self.name = remoteList.name
+        }
         self.readingListDescription = remoteList.description
         if let createdDate = DateFormatter.wmf_iso8601().date(from: remoteList.created) {
             self.createdDate = createdDate as NSDate
