2a29ff38bcbbf523d72be8243cb1172d6ce81e79
Natalia Harateh <nambatee@users.noreply.github.com>
2018-04-06 11:33:44 -0400

    post notification only if reading lists weren't previously torn down

--- WMF Framework/ReadingListsSyncOperation.swift
+++ WMF Framework/ReadingListsSyncOperation.swift
@@ -167,8 +167,8 @@ internal class ReadingListsSyncOperation: ReadingListsOperation {
                         self.finish()
                     }
                 } else {
-                    if let apiError = updateError as? APIReadingListError, apiError == .notSetup {
-                        readingListsController.postReadingListsSyncWasEnabledNotification(false)
+                    if let apiError = updateError as? APIReadingListError, apiError == .notSetup, apiController.lastRequestType != .teardown {
+                        readingListsController.postReadingListsServerDidConfirmSyncIsEnabledForAccountNotification(false)
                     }
                     try executeLocalOnlySync(on: moc)
                     try moc.save()
