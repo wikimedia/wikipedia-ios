e1d3525ebd8b70901434fd606e8f405e8527834e
Natalia Harateh <nambatee@users.noreply.github.com>
2018-05-04 14:22:34 -0400

    use httpAdditionalHeaders to pass appInstallID if needed

--- Wikipedia/Code/Session.swift
+++ Wikipedia/Code/Session.swift
@@ -32,6 +32,13 @@ import Foundation
     }
 
     @objc public static let shared = Session()
+    
+    public override init() {
+        guard SessionSingleton.sharedInstance().shouldSendUsageReports, let appInstallID = UserDefaults.wmf_userDefaults().wmf_appInstallID else {
+            return
+        }
+        session.configuration.httpAdditionalHeaders = ["X-WMF-UUID": appInstallID]
+    }
 
     fileprivate let session = URLSession.shared
     
@@ -85,7 +92,6 @@ import Foundation
         request.httpMethod = method.stringValue
         request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
         request.setValue(WikipediaAppUtils.versionedUserAgent(), forHTTPHeaderField: "User-Agent")
-        addAppInstallIDIfNeeded(&request)
         if let parameters = bodyParameters {
             if bodyEncoding == .json {
                 do {
@@ -225,7 +231,6 @@ import Foundation
         var request = URLRequest(url: summaryURL)
         //The accept profile is case sensitive https://gerrit.wikimedia.org/r/#/c/356429/
         request.setValue("application/json; charset=utf-8; profile=\"https://www.mediawiki.org/wiki/Specs/Summary/1.1.2\"", forHTTPHeaderField: "Accept")
-        addAppInstallIDIfNeeded(&request)
         return jsonDictionaryTask(with: request, completionHandler: completionHandler)
     }
     
@@ -264,12 +269,5 @@ import Foundation
         taskGroup.waitInBackgroundAndNotify(on: queue) {
             completion(summaryResponses)
         }
-    }
-    
-    private func addAppInstallIDIfNeeded(_ request: inout URLRequest) {
-        guard SessionSingleton.sharedInstance().shouldSendUsageReports, let appInstallID = UserDefaults.wmf_userDefaults().wmf_appInstallID else {
-            return
-        }
-        request.addValue(appInstallID, forHTTPHeaderField: "X-WMF-UUID")
     }
 }
