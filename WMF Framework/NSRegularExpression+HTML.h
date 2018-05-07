efc5a01928759408ccbdcafba917849ba005d026
joewalsh <joewalsh@users.noreply.github.com>
2018-05-07 15:48:56 -0400

    add HTML entity regex

--- WMF Framework/NSRegularExpression+HTML.h
+++ WMF Framework/NSRegularExpression+HTML.h
@@ -7,6 +7,9 @@ NS_ASSUME_NONNULL_BEGIN
 // Matches HTML tags
 + (NSRegularExpression *)wmf_HTMLTagRegularExpression;
 
+// Matches HTML entities &amp; &nbsp; etc
++ (NSRegularExpression *)wmf_HTMLEntityRegularExpression;
+
 // Matches the provided text between > and < or the start and end of the string
 + (nullable NSRegularExpression *)wmf_HTMLRegularExpressionMatchingText:(NSString *)string;
 
--- WMF Framework/NSRegularExpression+HTML.m
+++ WMF Framework/NSRegularExpression+HTML.m
@@ -14,6 +14,19 @@
     return tagRegex;
 }
 
+
++ (NSRegularExpression *)wmf_HTMLEntityRegularExpression {
+    static NSRegularExpression *tagRegex;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *pattern = @"&[^\\s]+;";
+        tagRegex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                             options:NSRegularExpressionCaseInsensitive
+                                                               error:nil];
+    });
+    return tagRegex;
+}
+
 + (NSRegularExpression *)wmf_HTMLRegularExpressionMatchingText:(NSString *)string {
     NSString *pattern = [NSString stringWithFormat:@"(?:(?:^|>)[^>]*)(%@)(?:[^<]*(?:<|$))", string];
     return [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:nil];
