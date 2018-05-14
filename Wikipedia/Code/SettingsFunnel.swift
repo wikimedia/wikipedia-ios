ca5937e7ab18d1122ee2cb8130325d6e47591e90
Natalia Harateh <nambatee@users.noreply.github.com>
2018-05-14 12:26:20 -0400

    add SettingsFunnel

---
+++ Wikipedia/Code/SettingsFunnel.swift
@@ -0,0 +1,52 @@
+class SettingsFunnel: EventLoggingFunnel {
+    
+    private lazy var sessionID: String = {
+        return singleUseUUID()
+    }()
+    
+    private enum Action: String {
+        case impression
+        case sync
+        case unsync
+    }
+    
+    override init() {
+        super.init(schema: "MobileWikiAppiOSSettingAction", version: 17990226)
+    }
+    
+    private func event(category: EventLoggingCategory, label: EventLoggingLabel?, action: Action) -> Dictionary<String, Any> {
+        guard category != .undefined else {
+            assertionFailure("category cannot be undefined")
+            return [:]
+        }
+        let appInstallID = wmf_appInstallID()
+        let category = category.rawValue
+        let action = action.rawValue
+        let isAnon = !WMFAuthenticationManager.sharedInstance.isLoggedIn
+        let timestamp = String(describing: NSDate())
+        let primaryLanguage = MWKLanguageLinkController.sharedInstance().appLanguage?.languageCode ?? "en"
+        
+        var event: [String: Any] = ["app_install_id": appInstallID, "category": category, "action": action, "primary_language": primaryLanguage, "is_anon": isAnon, "event_dt": timestamp, "session_id": sessionID]
+        if let label = label {
+            event["label"] = label.rawValue
+        }
+        
+        return event
+    }
+    
+    public func logSyncEnabledInSettings() {
+        log(event(category: .setting, label: .syncArticle, action: .sync))
+    }
+    
+    public func logSyncDisabledInSettings() {
+        log(event(category: .setting, label: .syncArticle, action: .unsync))
+    }
+    
+    public func logEnableSyncPopoverImpression() {
+        log(event(category: .enableSyncPopover, label: nil, action: .impression))
+    }
+    
+    public func logEnableSyncPopoverSyncEnabled() {
+        log(event(category: .enableSyncPopover, label: nil, action: .sync))
+    }
+}
