eed45e9f7a266550a2eb85274c01eed0d5beafa7
Natalia Harateh <nambatee@users.noreply.github.com>
2018-05-23 19:12:31 -0400

    update ProtectedEditAttemptFunnel

--- Wikipedia/Code/ProtectedEditAttemptFunnel.m
+++ Wikipedia/Code/ProtectedEditAttemptFunnel.m
@@ -1,15 +1,24 @@
 #import "ProtectedEditAttemptFunnel.h"
 
+static NSString *const kAppInstallIdKey = @"appInstallID";
+static NSString *const kTimestampKey = @"ts";
+
 @implementation ProtectedEditAttemptFunnel
 
 - (id)init {
     // https://meta.wikimedia.org/wiki/Schema:MobileWikiAppProtectedEditAttempt
     self = [super initWithSchema:@"MobileWikiAppProtectedEditAttempt"
-                         version:8682497];
+                         version:17836991];
     if (self) {
         self.requiresAppInstallID = NO;
     }
     return self;
+}
+- (NSDictionary *)preprocessData:(NSDictionary *)eventData {
+    NSMutableDictionary *dict = [eventData mutableCopy];
+    dict[kAppInstallIdKey] = [self wmf_appInstallID];
+    dict[kTimestampKey] = [self timestamp];
+    return dict;
 }
 
 - (void)logProtectionStatus:(NSString *)protectionStatus {
