976d7f04207fc83f38532190d7fee5e56b145cfe
Natalia Harateh <nambatee@users.noreply.github.com>
2018-05-23 19:05:12 -0400

    update CreateAccountFunnel

--- Wikipedia/Code/CreateAccountFunnel.m
+++ Wikipedia/Code/CreateAccountFunnel.m
@@ -1,11 +1,15 @@
 #import "CreateAccountFunnel.h"
 
+static NSString *const kAppInstallIdKey = @"appInstallID";
+static NSString *const kTimestampKey = @"ts";
+static NSString *const kIsAnonKey = @"is_anon";
+
 @implementation CreateAccountFunnel
 
 - (id)init {
     // https://meta.wikimedia.org/wiki/Schema:MobileWikiAppCreateAccount
     self = [self initWithSchema:@"MobileWikiAppCreateAccount"
-                        version:8240702];
+                        version:17836914];
     if (self) {
         self.createAccountSessionToken = [self singleUseUUID];
         self.requiresAppInstallID = NO;
@@ -16,6 +20,9 @@
 - (NSDictionary *)preprocessData:(NSDictionary *)eventData {
     NSMutableDictionary *dict = [eventData mutableCopy];
     dict[@"createAccountSessionToken"] = self.createAccountSessionToken;
+    dict[kAppInstallIdKey] = [self wmf_appInstallID];
+    dict[kTimestampKey] = [self timestamp];
+    dict[kIsAnonKey] = [self isAnon];
     return [NSDictionary dictionaryWithDictionary:dict];
 }
 
