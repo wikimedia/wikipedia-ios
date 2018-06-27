4648d078c3d19fe751f953afc5b6cb90921adeb0
joewalsh <joewalsh@users.noreply.github.com>
2018-06-27 13:56:58 -0400

    SearchViewController

---
+++ Wikipedia/Code/SearchViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SearchViewController: ColumnarCollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
