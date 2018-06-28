2bb5193d0c01974268f9b33cf6cbb67dcf8bbd48
Natalia Harateh <nambatee@users.noreply.github.com>
2018-06-27 23:35:27 -0400

    add an extension to check if content group kind is global

--- Wikipedia/Code/WMFContentGroupKind+IsInFeed.swift
+++ Wikipedia/Code/WMFContentGroupKind+IsInFeed.swift
@@ -2,4 +2,17 @@ extension WMFContentGroupKind {
     var isInFeed: Bool {
         return !SessionSingleton.sharedInstance().dataStore.feedContentController.languageCodes(for: self).isEmpty
     }
+
+    var isGlobal: Bool {
+        switch self {
+        case .relatedPages:
+            fallthrough
+        case .continueReading:
+            fallthrough
+        case .pictureOfTheDay:
+            return true
+        default:
+            return false
+        }
+    }
 }
