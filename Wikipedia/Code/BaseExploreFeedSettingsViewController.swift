f91856c7cccadcf4642cf9983d72bc8991e8a0bc
Natalia Harateh <nambatee@users.noreply.github.com>
2018-06-28 00:01:19 -0400

    set table view reload animation to none

--- Wikipedia/Code/BaseExploreFeedSettingsViewController.swift
+++ Wikipedia/Code/BaseExploreFeedSettingsViewController.swift
@@ -216,7 +216,7 @@ extension BaseExploreFeedSettingsViewController {
             guard self.shouldReload else {
                 return
             }
-            self.tableView.reloadRows(at: self.indexPathsForCellsThatNeedReloading, with: .automatic)
+            self.tableView.reloadRows(at: self.indexPathsForCellsThatNeedReloading, with: .none)
         }
     }
 }
