d3c7cc99193849c75aa5b3e9769a3382d1160b04
Natalia Harateh <nambatee@users.noreply.github.com>
2018-07-02 17:06:46 -0400

    add a configure method to the coordinator

--- WMF Framework/ExploreFeedPreferencesUpdateCoordinator.swift
+++ WMF Framework/ExploreFeedPreferencesUpdateCoordinator.swift
@@ -1,7 +1,17 @@
 @objc public class ExploreFeedPreferencesUpdateCoordinator: NSObject {
     private let feedContentController: WMFExploreFeedContentController
+    private var oldExploreFeedPreferences = Dictionary<String, Any>()
+    private var newExploreFeedPreferences = Dictionary<String, Any>()
+    private var willTurnOnContentGroupOrLanguage = false
 
     @objc public init(feedContentController: WMFExploreFeedContentController) {
         self.feedContentController = feedContentController
     }
+
+    @objc public func configure(oldExploreFeedPreferences: Dictionary<String, Any>, newExploreFeedPreferences: Dictionary<String, Any>, willTurnOnContentGroupOrLanguage: Bool) {
+        self.oldExploreFeedPreferences = oldExploreFeedPreferences
+        self.newExploreFeedPreferences = newExploreFeedPreferences
+        self.willTurnOnContentGroupOrLanguage = willTurnOnContentGroupOrLanguage
+    }
+
 }
