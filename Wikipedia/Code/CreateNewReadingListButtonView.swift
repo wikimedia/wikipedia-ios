4dd2142f197304a4c41bcb2bb631699ec2cac6a4
Natalia Harateh <nambatee@users.noreply.github.com>
2018-07-10 08:34:27 -0400

    add a class for CreateNewReadingListButtonView

--- Wikipedia/Code/CreateNewReadingListButtonView.swift
+++ Wikipedia/Code/CreateNewReadingListButtonView.swift
@@ -1,22 +1,24 @@
-public class CreateNewReadingListButtonView: SetupView {
-    public let button = AlignedImageButton(type: .system)
-    public override func setup() {
-        super.setup()
-        translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(CommonStrings.createNewListTitle, for: .normal)
-        button.titleLabel?.font = UIFont.wmf_font(.body, compatibleWithTraitCollection: traitCollection)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(button)
-        let centerX = centerXAnchor.constraint(equalTo: button.centerXAnchor)
-        let centerY = centerYAnchor.constraint(equalTo: button.centerYAnchor)
-        let heightConstraint = heightAnchor.constraint(equalToConstant: 50)
-        NSLayoutConstraint.activate([centerX, centerY, heightConstraint])
+public class CreateNewReadingListButtonView: UIView {
+    @IBOutlet private weak var button: AlignedImageButton!
+    @IBOutlet private weak var separator: UIView!
+
+    public var title: String? {
+        didSet {
+            button.setTitle(title, for: .normal)
+            button.titleLabel?.font = UIFont.wmf_font(.body, compatibleWithTraitCollection: traitCollection)
+        }
     }
+
+    public func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControlEvents) {
+        button.addTarget(target, action: action, for: controlEvents)
+    }
+
 }
 
 extension CreateNewReadingListButtonView: Themeable {
     public func apply(theme: Theme) {
-        backgroundColor = theme.colors.paperBackground
+        backgroundColor = theme.colors.chromeBackground
         button.tintColor = theme.colors.link
+        separator.backgroundColor = theme.colors.border
     }
 }
