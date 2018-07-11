02e9a770aeb4508ed62430f5a41b8ea42fdb0df0
Natalia Harateh <nambatee@users.noreply.github.com>
2018-07-11 14:36:36 -0400

    set alertController.popoverPresentationController

--- Wikipedia/Code/PlacesViewController.swift
+++ Wikipedia/Code/PlacesViewController.swift
@@ -142,7 +142,7 @@ class PlacesViewController: PreviewingViewController, UISearchBarDelegate, Artic
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: WMFLocalizedString("places-filter-button-title", value: "Filter", comment: "Title for button that allows users to filter places"), style: .plain, target: self, action: #selector(filterButtonPressed))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: WMFLocalizedString("places-filter-button-title", value: "Filter", comment: "Title for button that allows users to filter places"), style: .plain, target: self, action: #selector(filterButtonPressed(_:)))
         navigationBar.addUnderNavigationBarView(searchBarContainerView)
         navigationBar.displayType = .largeTitle
         navigationBar.delegate = self
@@ -355,7 +355,7 @@ class PlacesViewController: PreviewingViewController, UISearchBarDelegate, Artic
 
     // MARKL - Filtering
 
-    @objc private func filterButtonPressed() {
+    @objc private func filterButtonPressed(_ sender: UIBarButtonItem) {
         let alertController = UIAlertController(title: WMFLocalizedString("places-filter-articles-action-sheet-title", value: "Filter articles", comment: "Title for action sheet that allows users to filter Places articles"), message: nil, preferredStyle: .actionSheet)
         let topReadArticlesAction = UIAlertAction(title: WMFLocalizedString("places-filter-top-read-articles", value: "Top read articles", comment: "Title for action that allows users to filter top read articles"), style: .default) { (_) in
             self.currentSearchFilter = .top
@@ -366,6 +366,7 @@ class PlacesViewController: PreviewingViewController, UISearchBarDelegate, Artic
         alertController.addAction(topReadArticlesAction)
         alertController.addAction(savedArticlesAction)
         alertController.addAction(UIAlertAction(title: CommonStrings.cancelActionTitle, style: .cancel))
+        alertController.popoverPresentationController?.barButtonItem = sender
         present(alertController, animated: true)
     }
     
