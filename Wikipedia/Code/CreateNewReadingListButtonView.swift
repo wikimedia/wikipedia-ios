8db88976d4f2d39bd98d4e006ae1d96e06b8f1b0
joewalsh <joewalsh@users.noreply.github.com>
2018-07-13 16:48:16 -0400

    add plus icon to add to reading list button

--- Wikipedia/Code/CreateNewReadingListButtonView.swift
+++ Wikipedia/Code/CreateNewReadingListButtonView.swift
@@ -2,11 +2,21 @@ public class CreateNewReadingListButtonView: UIView {
     @IBOutlet private weak var button: AlignedImageButton!
     @IBOutlet private weak var separator: UIView!
 
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        button.setImage(#imageLiteral(resourceName: "plus"), for: .normal)
+        button.horizontalSpacing = 7
+    }
+    
     public var title: String? {
         didSet {
             button.setTitle(title, for: .normal)
-            button.titleLabel?.font = UIFont.wmf_font(.body, compatibleWithTraitCollection: traitCollection)
         }
+    }
+    
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        button.titleLabel?.font = UIFont.wmf_font(.body, compatibleWithTraitCollection: traitCollection)
     }
 
     public func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControlEvents) {
