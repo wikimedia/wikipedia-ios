694a82bb31c42f975bb96c4b72d20c838b19bf27
joewalsh <joewalsh@users.noreply.github.com>
2018-07-17 07:57:23 -0400

    invalidate cached heights for reloaded item

--- Wikipedia/Code/ExploreViewController.swift
+++ Wikipedia/Code/ExploreViewController.swift
@@ -306,11 +306,6 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
         layoutCache.reset()
         super.contentSizeCategoryDidChange(notification)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        layoutCache.reset()
-    }
     
     // MARK - UICollectionViewDataSource
     
@@ -419,10 +414,15 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
     }
     
     // MARK: - ColumnarCollectionViewLayoutDelegate
+    
+    private func cacheUserInfoForItem(at indexPath: IndexPath) -> String {
+        let group = fetchedResultsController.object(at: indexPath)
+        return "evc-cell-\(group.key ?? "")"
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, estimatedHeightForItemAt indexPath: IndexPath, forColumnWidth columnWidth: CGFloat) -> ColumnarCollectionViewLayoutHeightEstimate {
         let identifier = ExploreCardCollectionViewCell.identifier
-        let group = fetchedResultsController.object(at: indexPath)
-        let userInfo = "evc-cell-\(group.key ?? "")"
+        let userInfo = cacheUserInfoForItem(at: indexPath)
         if let cachedHeight = layoutCache.cachedHeightForCellWithIdentifier(identifier, columnWidth: columnWidth, userInfo: userInfo) {
             return ColumnarCollectionViewLayoutHeightEstimate(precalculated: true, height: cachedHeight)
         }
@@ -493,6 +493,9 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
     }
     
     func collectionViewUpdater<T>(_ updater: CollectionViewUpdater<T>, updateItemAtIndexPath indexPath: IndexPath, in collectionView: UICollectionView) where T : NSFetchRequestResult {
+        let identifier = ExploreCardCollectionViewCell.identifier
+        let userInfo = cacheUserInfoForItem(at: indexPath)
+        layoutCache.removeCachedHeightsForCellWithIdentifier(identifier, userInfo: userInfo)
         collectionView.collectionViewLayout.invalidateLayout()
         needsReloadVisibleCells = true
     }
