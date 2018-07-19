0f881dc9bc558dc91f6f28506d94308b5f5fe38e
montehurd <mhurd@wikimedia.org>
2018-07-18 19:32:05 -0700

    add logging properties to WMFContentGroupKind extension

--- Wikipedia/Code/WMFContentGroupKind+FeedCustomization.swift
+++ Wikipedia/Code/WMFContentGroupKind+FeedCustomization.swift
@@ -18,7 +18,51 @@ extension WMFContentGroupKind {
         return feedContentController.languageCodes(for: self)
     }
 
+    private var offLanguageCodes: Set<String> {
+        let preferredLangCodes = MWKLanguageLinkController.sharedInstance().preferredLanguages.map{$0.languageCode}
+        return Set(preferredLangCodes).subtracting(languageCodes)
+    }
+    
     private var feedContentController: WMFExploreFeedContentController {
         return SessionSingleton.sharedInstance().dataStore.feedContentController
+    }
+    
+    // codes define by: https://meta.wikimedia.org/wiki/Schema:MobileWikiAppiOSUserHistory
+    var loggingCode: String {
+        switch self {
+        case .featuredArticle:
+            return "fa"
+        case .topRead:
+            return "tr"
+        case .onThisDay:
+            return "od"
+        case .news:
+            return "ns"
+        case .relatedPages:
+            return "rp"
+        case .continueReading:
+            return "cr"
+        case .location:
+            return "pl"
+        case .random:
+            return "rd"
+        case .pictureOfTheDay:
+            return "pd"
+        default:
+            assertionFailure("Expected logging code not found")
+            return ""
+        }
+    }
+    
+    // "on" / "off" define by: https://meta.wikimedia.org/wiki/Schema:MobileWikiAppiOSUserHistory
+    var loggingLanguageInfo: [String: [String]] {
+        var info = [String: [String]]()
+        if languageCodes.count > 0 {
+            info["on"] = Array(languageCodes)
+        }
+        if offLanguageCodes.count > 0 {
+            info["off"] = Array(offLanguageCodes)
+        }
+        return info
     }
 }
