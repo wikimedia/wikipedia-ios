55b945b346aa6dd4bc9b08ebb131efb1a3fbb736
montehurd <mhurd@wikimedia.org>
2018-07-23 14:48:27 -0700

    revert moving content group kind extensions to WMF framework
    the new extension that i added actually belongs in the user history funnel since the strings it uses are defined by the user history schema itself
    
    also makes userHistorySchemaCode optional so we have easy way to check if we should log info for given content group kind

--- Wikipedia/Code/UserHistoryFunnel.swift
+++ Wikipedia/Code/UserHistoryFunnel.swift
@@ -103,3 +103,48 @@
         // DDLogDebug("Attempted to log starting User History snapshot")
     }
 }
+
+private extension WMFContentGroupKind {
+    var offLanguageCodes: Set<String> {
+        let preferredLangCodes = MWKLanguageLinkController.sharedInstance().preferredLanguages.map{$0.languageCode}
+        return Set(preferredLangCodes).subtracting(languageCodes)
+    }
+    
+    // codes define by: https://meta.wikimedia.org/wiki/Schema:MobileWikiAppiOSUserHistory
+    var userHistorySchemaCode: String? {
+        switch self {
+        case .featuredArticle:
+            return "fa"
+        case .topRead:
+            return "tr"
+        case .onThisDay:
+            return "od"
+        case .news:
+            return "ns"
+        case .relatedPages:
+            return "rp"
+        case .continueReading:
+            return "cr"
+        case .location:
+            return "pl"
+        case .random:
+            return "rd"
+        case .pictureOfTheDay:
+            return "pd"
+        default:
+            return nil
+        }
+    }
+    
+    // "on" / "off" define by: https://meta.wikimedia.org/wiki/Schema:MobileWikiAppiOSUserHistory
+    var userHistorySchemaLanguageInfo: [String: [String]] {
+        var info = [String: [String]]()
+        if languageCodes.count > 0 {
+            info["on"] = Array(languageCodes)
+        }
+        if offLanguageCodes.count > 0 {
+            info["off"] = Array(offLanguageCodes)
+        }
+        return info
+    }
+}
