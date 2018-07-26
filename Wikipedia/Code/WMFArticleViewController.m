be28e270c7515ffd142abcc7342980de36dea9f7
Natalia Harateh <nambatee@users.noreply.github.com>
2018-07-26 13:57:47 -0400

    if article title's longer than 15 chars, set "Back" as navigationItem's title

--- Wikipedia/Code/WMFArticleViewController.m
+++ Wikipedia/Code/WMFArticleViewController.m
@@ -1217,7 +1217,11 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
 
 - (void)articleDidLoad {
     dispatch_async(dispatch_get_main_queue(), ^{
-        self.navigationItem.title = self.article.displaytitle.wmf_stringByRemovingHTML;
+        NSString *navigationTitle = self.article.displaytitle.wmf_stringByRemovingHTML;
+        if ([navigationTitle length] > 15) {
+            navigationTitle = [WMFCommonStrings backTitle];
+        }
+        self.navigationItem.title = navigationTitle;
         dispatch_block_t completion = self.articleLoadCompletion;
         if (completion) {
             completion();
