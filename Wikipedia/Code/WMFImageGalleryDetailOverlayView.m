1bff6e920a4fc2c1bde01274cc8aa8ec16da7ddc
montehurd <mhurd@wikimedia.org>
2018-07-26 17:10:14 -0700

    revert using `wmf_isPrimarilyRTL` for gallery descriptions

--- Wikipedia/Code/WMFImageGalleryDetailOverlayView.m
+++ Wikipedia/Code/WMFImageGalleryDetailOverlayView.m
@@ -79,7 +79,6 @@
 
 - (void)setImageDescription:(NSString *)imageDescription {
     self.imageDescriptionLabel.text = imageDescription;
-    self.imageDescriptionLabel.textAlignment = [imageDescription wmf_isPrimarilyRTL] ? NSTextAlignmentRight : NSTextAlignmentLeft;
 }
 
 - (void)setLicense:(MWKLicense *)license owner:(NSString *)owner {
