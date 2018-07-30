7851c9332f2ac0cefa54071ed817d7345e80dc87
montehurd <mhurd@wikimedia.org>
2018-07-30 15:36:48 -0700

    migrate AFHTTPSessionManager+WMFGet extension bits to AFHTTPSessionManager+WMFAPI
    - simplified things
    - re-named methods which use zero url to be explicit about doing so

--- Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
+++ Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
@@ -1,9 +1,26 @@
-
 extension AFHTTPSessionManager {
-    @objc public func wmf_apiPOSTWithParameters(_ parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
-        return self.wmf_apiPOSTWithURLString(WMFAPIPath, parameters: parameters, success: success)
+    @objc @discardableResult public func wmf_apiPOSTWithParameters(_ parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+        return self.post(WMFAPIPath, parameters: parameters, progress: nil, success:success, failure:failure)
     }
-    @objc public func wmf_apiPOSTWithURLString(_ URLString: String, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
-        return self.post(URLString, parameters: parameters, progress: nil, success:success, failure:failure)
+    @objc @discardableResult public func wmf_apiZeroSafePOSTWithURL(_ url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+        guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
+            assertionFailure("Expected URL not found")
+            return nil
+        }
+        return self.post(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+    }
+    @objc @discardableResult public func wmf_zeroSafeGETWithURL(_ url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+        guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
+            assertionFailure("Expected URL not found")
+            return nil
+        }
+        return self.get(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+    }
+}
+
+private extension URL {
+    func wmf_zeroRatedAPIURL() -> URL? {
+        // If Zero rated use mobile domain, else use desktop domain.
+        return SessionSingleton.sharedInstance().zeroConfigurationManager.isZeroRated ? NSURL.wmf_mobileAPIURL(for: self) : NSURL.wmf_desktopAPIURL(for: self)
     }
 }
