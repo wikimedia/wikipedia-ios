61d859de4d088f515bd7e659f55c858d924691e5
montehurd <mhurd@wikimedia.org>
2018-07-30 13:55:47 -0700

    simplify `wmf_GETAndRetryWithURL` since it doesn't actually retry. rename it too

--- Wikipedia/Code/AFHTTPSessionManager+WMFDesktopRetry.h
+++ Wikipedia/Code/AFHTTPSessionManager+WMFDesktopRetry.h
@@ -3,20 +3,19 @@
 @interface AFHTTPSessionManager (WMFDesktopRetry)
 
 /**
- *  Send a @c GET request to the mobile endpoint (see -[NSURL wmf_mobileAPIURL])
- *  for the given url, falling back to desktop endpoint (-[NSURL wmf_desktopAPIURL])
- *  if the mobile endpoint fails.
+ *  Sends a @c GET request to the mobile endpoint (see -[NSURL wmf_mobileAPIURL])
+ *  if Wikipedia Zero is active or to the desktop endpoint (-[NSURL wmf_desktopAPIURL]) if not.
  *
- *  @param URL The URL to derive the endpoints from. First mobile, then desktop.
+ *  @param URL The URL to derive the endpoints from.
  *  @param parameters       The parameters for the request
  *  @param success          The success block
  *  @param failure          The failure block
  *
  *  @return The operation which represents the state of the request.
  */
-- (NSURLSessionDataTask *)wmf_GETAndRetryWithURL:(NSURL *)URL
-                                      parameters:(id)parameters
-                                         success:(void (^)(NSURLSessionDataTask *operation, id responseObject))success
-                                         failure:(void (^)(NSURLSessionDataTask *operation, NSError *error))failure;
+- (NSURLSessionDataTask *)wmf_GETWithURL:(NSURL *)URL
+                              parameters:(id)parameters
+                                 success:(void (^)(NSURLSessionDataTask *operation, id responseObject))success
+                                 failure:(void (^)(NSURLSessionDataTask *operation, NSError *error))failure;
 
 @end
