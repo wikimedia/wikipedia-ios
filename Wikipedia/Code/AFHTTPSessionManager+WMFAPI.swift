e4db938bd1da92aea86df52c94f3b9195e9d937c
montehurd <mhurd@wikimedia.org>
2018-07-30 13:29:55 -0700

    add session manager extension for post with url string

--- Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
+++ Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
@@ -1,6 +1,9 @@
 
 extension AFHTTPSessionManager {
-    public func wmf_apiPOSTWithParameters(_ parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
-        return self.post(WMFAPIPath, parameters: parameters, progress: nil, success:success, failure:failure)
+    @objc public func wmf_apiPOSTWithParameters(_ parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+        return self.wmf_apiPOSTWithURLString(WMFAPIPath, parameters: parameters, success: success)
+    }
+    @objc public func wmf_apiPOSTWithURLString(_ URLString: String, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+        return self.post(URLString, parameters: parameters, progress: nil, success:success, failure:failure)
     }
 }
