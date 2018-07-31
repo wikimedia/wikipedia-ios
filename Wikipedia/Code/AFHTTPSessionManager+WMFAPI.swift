89899a790126ee6dd5af6d74a94e66e78cda20e7
montehurd <mhurd@wikimedia.org>
2018-07-31 14:05:55 -0700

    remove unneeded `self`

--- Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
+++ Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
@@ -1,20 +1,20 @@
 extension AFHTTPSessionManager {
     @objc @discardableResult public func wmf_apiPOSTWithParameters(_ parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
-        return self.post(WMFAPIPath, parameters: parameters, progress: nil, success:success, failure:failure)
+        return post(WMFAPIPath, parameters: parameters, progress: nil, success:success, failure:failure)
     }
     @objc @discardableResult public func wmf_apiZeroSafePOSTWithURL(_ url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
         guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
             assertionFailure("Expected URL not found")
             return nil
         }
-        return self.post(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+        return post(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
     }
     @objc @discardableResult public func wmf_zeroSafeGETWithURL(_ url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
         guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
             assertionFailure("Expected URL not found")
             return nil
         }
-        return self.get(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+        return get(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
     }
 }
 
