7f7b94f89bad34d98ddd5ad47d184cbb13c0f60c
montehurd <mhurd@wikimedia.org>
2018-07-31 14:38:51 -0700

    typealias to cut down on repetition

--- Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
+++ Wikipedia/Code/AFHTTPSessionManager+WMFAPI.swift
@@ -1,23 +1,26 @@
+public typealias SessionManagerSuccess = ((URLSessionDataTask, Any?) -> Swift.Void)?
+public typealias SessionManagerFailure = ((URLSessionDataTask?, Error) -> Swift.Void)?
+
 extension AFHTTPSessionManager {
     @objc (wmf_apiPOSTWithParameters:success:failure:)
-    @discardableResult public func wmf_apiPOST(with parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
-        return post(WMFAPIPath, parameters: parameters, progress: nil, success:success, failure:failure)
+    @discardableResult public func wmf_apiPOST(with parameters: Any?, success: SessionManagerSuccess, failure: SessionManagerFailure = nil) -> URLSessionDataTask? {
+        return post(WMFAPIPath, parameters: parameters, progress: nil, success: success, failure: failure)
     }
     @objc (wmf_apiZeroSafePOSTWithURL:parameters:success:failure:)
-    @discardableResult public func wmf_apiZeroSafePOST(with url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+    @discardableResult public func wmf_apiZeroSafePOST(with url: URL, parameters: Any?, success: SessionManagerSuccess, failure: SessionManagerFailure = nil) -> URLSessionDataTask? {
         guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
             assertionFailure("Expected URL not found")
             return nil
         }
-        return post(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+        return post(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success: success, failure: failure)
     }
     @objc (wmf_apiZeroSafeGETWithURL:parameters:success:failure:)
-    @discardableResult public func wmf_apiZeroSafeGET(with url: URL, parameters: Any?, success: ((URLSessionDataTask, Any?) -> Swift.Void)?, failure: ((URLSessionDataTask?, Error) -> Swift.Void)? = nil) -> URLSessionDataTask? {
+    @discardableResult public func wmf_apiZeroSafeGET(with url: URL, parameters: Any?, success: SessionManagerSuccess, failure: SessionManagerFailure = nil) -> URLSessionDataTask? {
         guard let zeroRatedAPIURL = url.wmf_zeroRatedAPIURL() else {
             assertionFailure("Expected URL not found")
             return nil
         }
-        return get(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success:success, failure:failure)
+        return get(zeroRatedAPIURL.absoluteString, parameters: parameters, progress: nil, success: success, failure: failure)
     }
 }
 
