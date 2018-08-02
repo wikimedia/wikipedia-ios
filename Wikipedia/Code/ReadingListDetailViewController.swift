3611bddd5b568beeb33f5ff7d2082b619398d201
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-02 08:39:56 -0400

    return without assertion for cases when cell was deleted via swipe

--- Wikipedia/Code/ReadingListDetailViewController.swift
+++ Wikipedia/Code/ReadingListDetailViewController.swift
@@ -522,7 +522,6 @@ extension ReadingListDetailViewController {
         cell.isBatchEditing = editController.isBatchEditing
         
         guard let entry = entry(at: indexPath), let articleKey = entry.articleKey else {
-            assertionFailure("Coudn't get a reading list entry or an article key to configure the cell")
             return
         }
         
