66ee3dc3d272fb9935f865112402d574e83ab769
montehurd <mhurd@wikimedia.org>
2018-08-10 14:34:51 -0700

    fix missing notification screen title
    https://phabricator.wikimedia.org/T195877

--- Wikipedia/Code/NotificationSettingsViewController.swift
+++ Wikipedia/Code/NotificationSettingsViewController.swift
@@ -34,6 +34,7 @@ class NotificationSettingsViewController: UIViewController, UITableViewDataSourc
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = CommonStrings.notifications
         tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0);
         tableView.register(WMFSettingsTableViewCell.wmf_classNib(), forCellReuseIdentifier: WMFSettingsTableViewCell.identifier)
         tableView.delegate = self
