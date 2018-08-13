9d6ec2b06db5437e61f89490213ec229de31e555
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-13 17:02:01 -0400

    make settings vc inherit from wmf vc

--- Wikipedia/Code/WMFSettingsViewController.h
+++ Wikipedia/Code/WMFSettingsViewController.h
@@ -3,7 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WMFSettingsViewController : UIViewController <WMFThemeable>
+@interface WMFSettingsViewController : WMFViewController <WMFThemeable>
 
 + (instancetype)settingsViewControllerWithDataStore:(MWKDataStore *)store;
 
