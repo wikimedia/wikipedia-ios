71c01495ecb90839a1a3a40635296d9caf43acb4
montehurd <mhurd@wikimedia.org>
2018-08-28 02:36:48 -0700

    auto layout safe multi-line button
    plays nice in stackviews

---
+++ Wikipedia/Code/AutoLayoutSafeMultiLineButton.swift
@@ -0,0 +1,49 @@
+
+// See multi-line button thread: https://stackoverflow.com/q/23845982/135557
+@IBDesignable
+class AutoLayoutSafeMultiLineButton: UIButton {
+    func multiLineSafeSetup () {
+        titleLabel?.numberOfLines = 0
+        titleLabel?.lineBreakMode = .byWordWrapping
+        setContentHuggingPriority(.defaultLow + 1, for: .vertical)
+        setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        multiLineSafeSetup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        multiLineSafeSetup()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        guard let titleLabel = titleLabel else {
+            return super.intrinsicContentSize
+        }
+        return sizeByAddingInsets(to: titleLabel.intrinsicContentSize)
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let titleLabel = titleLabel else {
+            return super.sizeThatFits(size)
+        }
+        return sizeByAddingInsets(to: titleLabel.sizeThatFits(size))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let titleLabel = titleLabel {
+            titleLabel.preferredMaxLayoutWidth = titleLabel.frame.width
+        }
+    }
+    
+    private func sizeByAddingInsets(to size: CGSize) -> CGSize {
+        return CGSize.init(
+            width: size.width + titleEdgeInsets.left + titleEdgeInsets.right,
+            height: size.height + titleEdgeInsets.top + titleEdgeInsets.bottom
+        )
+    }
+}
