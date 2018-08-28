2fee64628434cff743a59f5daab2bd157c02b857
montehurd <mhurd@wikimedia.org>
2018-08-28 16:26:39 -0700

    use setup button class

--- Wikipedia/Code/AutoLayoutSafeMultiLineButton.swift
+++ Wikipedia/Code/AutoLayoutSafeMultiLineButton.swift
@@ -1,25 +1,16 @@
 
 // See multi-line button thread: https://stackoverflow.com/q/23845982/135557
 @IBDesignable
-class AutoLayoutSafeMultiLineButton: UIButton {
-    func multiLineSafeSetup () {
+class AutoLayoutSafeMultiLineButton: SetupButton {
+    override func setup() {
+        super.setup()
         titleLabel?.numberOfLines = 0
         titleLabel?.lineBreakMode = .byWordWrapping
         titleLabel?.adjustsFontSizeToFitWidth = false
         setContentHuggingPriority(.defaultLow + 1, for: .vertical)
         setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        multiLineSafeSetup()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        multiLineSafeSetup()
-    }
-    
+
     override var intrinsicContentSize: CGSize {
         guard let titleLabel = titleLabel else {
             return super.intrinsicContentSize
