e1515958e84b94c2a138b1392fe866a8e1c0e2e3
joewalsh <joewalsh@users.noreply.github.com>
2018-08-28 19:32:04 -0400

    use wmf_sizeThatFits

--- Wikipedia/Code/AutoLayoutSafeMultiLineButton.swift
+++ Wikipedia/Code/AutoLayoutSafeMultiLineButton.swift
@@ -1,50 +1,11 @@
-
-// See multi-line button thread: https://stackoverflow.com/q/23845982/135557
 @IBDesignable
 class AutoLayoutSafeMultiLineButton: SetupButton {
     override func setup() {
         super.setup()
         titleLabel?.numberOfLines = 0
-        titleLabel?.lineBreakMode = .byWordWrapping
-        titleLabel?.adjustsFontSizeToFitWidth = false
-        setContentHuggingPriority(.defaultLow + 1, for: .vertical)
-        setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
     }
 
     override var intrinsicContentSize: CGSize {
-        guard let titleLabel = titleLabel else {
-            return super.intrinsicContentSize
-        }
-        return sizeByAddingInsets(to: titleLabel.intrinsicContentSize)
-    }
-    
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-        guard let titleLabel = titleLabel else {
-            return super.sizeThatFits(size)
-        }
-        return sizeByAddingInsets(to: titleLabel.sizeThatFits(size))
-    }
-    
-    private var isSafeToLayoutSubviews = true // Recursion guard for setting `titleLabel.preferredMaxLayoutWidth` within `layoutSubviews()`.
-    override func layoutSubviews() {
-        guard isSafeToLayoutSubviews else {
-            isSafeToLayoutSubviews = true
-            return
-        }
-        guard let titleLabel = titleLabel else {
-            super.layoutSubviews()
-            return
-        }
-        isSafeToLayoutSubviews = false
-        titleLabel.preferredMaxLayoutWidth = titleLabel.frame.width
-        isSafeToLayoutSubviews = true
-        super.layoutSubviews()
-    }
-    
-    private func sizeByAddingInsets(to size: CGSize) -> CGSize {
-        return CGSize(
-            width: size.width + titleEdgeInsets.left + titleEdgeInsets.right,
-            height: size.height + titleEdgeInsets.top + titleEdgeInsets.bottom
-        )
+        return wmf_sizeThatFits(CGSize(width: bounds.size.width, height: UIViewNoIntrinsicMetric))
     }
 }
