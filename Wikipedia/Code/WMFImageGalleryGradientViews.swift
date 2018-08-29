5354856e6612dda8678324fa0d59d0c7a16ea185
montehurd <mhurd@wikimedia.org>
2018-08-29 13:14:04 -0700

    remove top gradient view class

--- Wikipedia/Code/WMFImageGalleryGradientViews.swift
+++ Wikipedia/Code/WMFImageGalleryGradientViews.swift
@@ -1,20 +1,4 @@
 
-@objcMembers class WMFImageGalleryTopGradientView: SetupGradientView {
-    public static let sharedStretchableImage = {
-        return WMFImageGalleryTopGradientView.init(frame: CGRect(x: 0, y: 0, width: 25, height: 44)).wmf_stretchableSnapshotImageWithAlphaChannel()
-    }()
-    override public func setup(gradientLayer: CAGradientLayer) {
-        backgroundColor = .clear
-        gradientLayer.locations = [1.0, 0.0]
-        gradientLayer.colors = [
-            UIColor(white: 0, alpha: 0.35).cgColor,
-            UIColor.clear.cgColor
-        ]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
-    }
-}
-
 @objcMembers class WMFImageGalleryBottomGradientView: SetupGradientView {
     override public func setup(gradientLayer: CAGradientLayer) {
         gradientLayer.locations = [0.0, 0.5, 1.0]
