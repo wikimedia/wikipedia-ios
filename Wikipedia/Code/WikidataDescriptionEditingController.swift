27a9c3fd1765a9782281d1a503a4dac4e285cc6c
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-30 14:49:28 -0400

    add a method to check if given lang is blacklisted

--- Wikipedia/Code/WikidataDescriptionEditingController.swift
+++ Wikipedia/Code/WikidataDescriptionEditingController.swift
@@ -4,4 +4,12 @@
     @objc public func setBlacklistedLanguages(_ blacklistedLanguagesFromRemoteConfig: Array<String>) {
         blacklistedLanguages = Set(blacklistedLanguagesFromRemoteConfig)
     }
+
+    public func isBlacklisted(_ languageCode: String) -> Bool {
+        guard !blacklistedLanguages.isEmpty else {
+            return false
+        }
+        return blacklistedLanguages.contains(languageCode)
+    }
+}
 }
