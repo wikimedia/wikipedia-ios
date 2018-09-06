fcfe2cfed7e2b516051c57cdd7453a64512c459e
Joe Walsh <joewalsh@users.noreply.github.com>
2018-09-06 12:41:32 -0400

    fix build errors for tests

--- WikipediaUnitTests/Code/UIView+VisualTestSizingUtils.m
+++ WikipediaUnitTests/Code/UIView+VisualTestSizingUtils.m
@@ -3,7 +3,7 @@
 @implementation UIView (VisualTestSizingUtils)
 
 - (CGRect)wmf_sizeThatFitsWidth:(CGFloat)width {
-    CGSize sizeThatFitsWidth = [self systemLayoutSizeFittingSize:CGSizeMake(width, UIView.noIntrinsicMetric)
+    CGSize sizeThatFitsWidth = [self systemLayoutSizeFittingSize:CGSizeMake(width, UIViewNoIntrinsicMetric)
                                    withHorizontalFittingPriority:UILayoutPriorityRequired
                                          verticalFittingPriority:UILayoutPriorityFittingSizeLevel];
     return (CGRect){
