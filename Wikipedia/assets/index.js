fa32274fb57a6d7290718edfaa4e638a56532295
montehurd <mhurd@wikimedia.org>
2018-09-06 16:53:14 -0700

    Revert "make js section loading accept lead image url parameter"
    This reverts commit 82b553bcbec6ea7c8cf54d9abafdcc78f68aca7d.

--- Wikipedia/assets/index.js
+++ Wikipedia/assets/index.js
@@ -615,17 +615,6 @@ const fragmentForSection = section => {
   return fragment
 }
 
-const fragmentForLeadImageURL = leadImageURL => {
-  const fragment = lazyDocument.createDocumentFragment()
-  enrichFragment(fragment)
-  const container = fragment.createElement('div')
-  const img = fragment.createElement('img')
-  img.src = leadImageURL
-  container.appendChild(img)
-  fragment.appendChild(container)
-  return fragment
-}
-
 const applyTransformationsToFragment = (fragment, article, isLead) => {
   requirements.redLinks.hideRedLinks(fragment)
 
@@ -715,11 +704,10 @@ const scrollToSection = hash => {
   }
 }
 
-const fetchTransformAndAppendSectionsToDocument = (article, articleSectionsURL, articleLeadImageURL, hash, successCallback) => {
+const fetchTransformAndAppendSectionsToDocument = (article, articleSectionsURL, hash, successCallback) => {
   performEarlyNonSectionTransforms(article)
   const mainContentDiv = liveDocument.querySelector('div.content')
-
-  const articleFetchPromise = fetch(articleSectionsURL)
+  fetch(articleSectionsURL)
     .then(processResponseStatus)
     .then(extractResponseJSON)
     .then(extractSectionsJSON)
@@ -735,25 +723,7 @@ const fetchTransformAndAppendSectionsToDocument = (article, articleSectionsURL, 
         successCallback()
       }, nonLeadDelay)
     })
-    .catch(error => console.log(`Article fetch promise was rejected with error: ${error}`))
-
-  fetch(articleLeadImageURL)
-    .then(processResponseStatus)
-    .catch(error => {
-      console.log(`Lead image URL fetch promise was rejected with error: ${error}`)
-      return {} // Resolve promise so chain continues
-    })
-    .then(response => response.text())
-    .catch(error => {
-      console.log(`Lead image URL response text extraction promise was rejected with error: ${error}`)
-      return null  // Resolve promise so chain continues
-    })
-    .then(leadImageURL => {
-      if (leadImageURL) {
-        mainContentDiv.appendChild(fragmentForLeadImageURL(leadImageURL))
-      }
-      return articleFetchPromise
-    })
+    .catch(error => console.log(`Promise was rejected with error: ${error}`))
 }
 
 // Object containing the following localized strings key/value pairs: 'tableInfoboxTitle', 'tableOtherTitle', 'tableFooterTitle'
