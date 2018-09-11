53b7aad82ad0258c6a45aea956747839926274d1
montehurd <mhurd@wikimedia.org>
2018-09-10 19:08:12 -0700

    specify obj-c signature for color with hex and alpha

--- WMF Framework/Theme.swift
+++ WMF Framework/Theme.swift
@@ -2,7 +2,8 @@ import Foundation
 import SystemConfiguration
 
 public extension UIColor {
-    @objc public convenience init(_ hex: Int, alpha: CGFloat) {
+    @objc(initWithHexInteger:alpha:)
+    public convenience init(_ hex: Int, alpha: CGFloat) {
         let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
         let g = CGFloat((hex & 0xFF00) >> 8) / 255.0
         let b = CGFloat(hex & 0xFF) / 255.0
