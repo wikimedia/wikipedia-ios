13ce275eca3d2c78a6a66a629da7206ab5379752
montehurd <mhurd@wikimedia.org>
2018-09-13 18:41:10 -0700

    set titleEdgeInsets by default to 10 all around on AutoLayoutSafeMultiLineButton

--- Wikipedia/Code/WMFAuthButton.swift
+++ Wikipedia/Code/WMFAuthButton.swift
@@ -14,6 +14,7 @@ class WMFAuthButton: AutoLayoutSafeMultiLineButton, Themeable {
     override open func awakeFromNib() {
         super.awakeFromNib()
         layer.cornerRadius = 5
+        titleEdgeInsets = UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 10)
         apply(theme: self.theme)
     }
     
