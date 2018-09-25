97d30778bd1578dd49ecba3b0296dc770015e89e
Natalia Harateh <nambatee@users.noreply.github.com>
2018-09-25 11:39:52 -0400

    add RemoteNotificationsOperation

---
+++ WMF Framework/Remote Notifications/Operations/RemoteNotificationsOperation.swift
@@ -0,0 +1,16 @@
+class RemoteNotificationsOperation: AsyncOperation {
+    let apiController: RemoteNotificationsAPIController
+    let modelController: RemoteNotificationsModelController
+    let managedObjectContext: NSManagedObjectContext
+
+    init(with apiController: RemoteNotificationsAPIController, modelController: RemoteNotificationsModelController) {
+        self.apiController = apiController
+        self.modelController = modelController
+        self.managedObjectContext = modelController.managedObjectContext
+        super.init()
+    }
+
+    override var shouldExecute: Bool {
+        return WMFAuthenticationManager.sharedInstance.isLoggedIn
+    }
+}
