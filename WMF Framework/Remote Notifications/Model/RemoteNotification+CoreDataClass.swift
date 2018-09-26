81757afa1e66647adf58d61f87d57cadef72c9e7
Natalia Harateh <nambatee@users.noreply.github.com>
2018-09-25 20:26:28 -0400

    add a Category enum on RemoteNotification

--- WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
+++ WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
@@ -4,4 +4,9 @@ import CoreData
 @objc(RemoteNotification)
 public class RemoteNotification: NSManagedObject {
 
+    enum Category: String {
+        case editReverted = "reverted"
+        case unknown
+    }
+
 }
