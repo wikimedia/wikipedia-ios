0ca4db36fec03dd661b5b730d5731da0cb966dc3
Natalia Harateh <nambatee@users.noreply.github.com>
2018-09-25 20:26:55 -0400

    add a computed category property on RemoteNotification

--- WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
+++ WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
@@ -9,4 +9,11 @@ public class RemoteNotification: NSManagedObject {
         case unknown
     }
 
+    private var category: Category {
+        guard let categoryString = categoryString else {
+            return .unknown
+        }
+        return Category(rawValue: categoryString) ?? .unknown
+    }
+
 }
