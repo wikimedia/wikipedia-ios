8167dabbb1e26a945c21a8beb65ef970154bc26c
Natalia Harateh <nambatee@users.noreply.github.com>
2018-09-27 09:16:16 -0400

    rename Category on RemoteNotification to RemoteNotificationCategory, make it visible in @objc

--- WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
+++ WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
@@ -1,28 +1,28 @@
 import Foundation
 import CoreData
 
+@objc public enum RemoteNotificationCategory: Int {
+    case editReverted
+    case unknown
+
+    init(stringValue: String) {
+        switch stringValue {
+        case "reverted":
+            self = .editReverted
+        default:
+            self = .unknown
+        }
+    }
+}
+
 @objc(RemoteNotification)
 public class RemoteNotification: NSManagedObject {
 
-    @objc public enum Category: Int {
-        case editReverted
-        case unknown
-
-        init(stringValue: String) {
-            switch stringValue {
-            case "reverted":
-                self = .editReverted
-            default:
-                self = .unknown
-            }
-        }
-    }
-
-    public var category: Category {
+    public var category: RemoteNotificationCategory {
         guard let categoryString = categoryString else {
             return .unknown
         }
-        return Category(stringValue: categoryString)
+        return RemoteNotificationCategory(stringValue: categoryString)
     }
 
 }
