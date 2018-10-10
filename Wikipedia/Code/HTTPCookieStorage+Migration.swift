975f03102d258437753d0599ac0a8acb394ba699
joewalsh <joewalsh@users.noreply.github.com>
2018-10-10 09:40:32 -0400

    copy cookies

---
+++ Wikipedia/Code/HTTPCookieStorage+Migration.swift
@@ -0,0 +1,24 @@
+extension HTTPCookieStorage {
+    public func cookiesWithNamePrefix(_ prefix: String, for domain: String) -> [HTTPCookie] {
+        let standardizedPrefix = prefix.lowercased().precomposedStringWithCanonicalMapping
+        let standardizedDomain = domain.lowercased().precomposedStringWithCanonicalMapping
+        return (cookies ?? []).filter({ (cookie) -> Bool in
+            return cookie.name.lowercased().precomposedStringWithCanonicalMapping.hasPrefix(standardizedPrefix) && cookie.domain.lowercased().precomposedStringWithCanonicalMapping.hasSuffix(standardizedDomain)
+        })
+    }
+    
+    public func copyCookiesWithNamePrefix(_ prefix: String, for domain: String, to toDomains: [String]) {
+        let cookies = cookiesWithNamePrefix(prefix, for: domain)
+        for toDomain in toDomains {
+            for cookie in cookies {
+                var properties = cookie.properties ?? [:]
+                properties[.domain] = toDomain
+                guard let copiedCookie = HTTPCookie(properties: properties) else {
+                    continue
+                }
+                setCookie(copiedCookie)
+            }
+        }
+    }
+    
+}
