8c5a0b497de49613d747035fd50a3aaa80e29aa2
Joe Walsh <joewalsh@users.noreply.github.com>
2018-10-12 17:13:00 -0400

    check for wikidataId and descriptionSource

--- Wikipedia/Code/WikidataDescriptionEditingController.swift
+++ Wikipedia/Code/WikidataDescriptionEditingController.swift
@@ -80,6 +80,6 @@ enum WikidataPublishingError: LocalizedError {
 
 public extension MWKArticle {
     @objc var isWikidataDescriptionEditable: Bool {
-        return descriptionSource != .local
+        return wikidataId != nil && descriptionSource != .local
     }
 }
