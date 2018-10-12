3eae88a1d440ce9e790323a614fab7828a08c0d7
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-12 12:02:45 -0400

    don't start on main

--- WMF Framework/Remote Notifications/Operations/RemoteNotificationsFetchOperation.swift
+++ WMF Framework/Remote Notifications/Operations/RemoteNotificationsFetchOperation.swift
@@ -1,38 +1,36 @@
 class RemoteNotificationsFetchOperation: RemoteNotificationsOperation {
     override func execute() {
-        DispatchQueue.main.async {
-            self.managedObjectContext.perform {
-                self.apiController.getAllUnreadNotifications(from: ["wikidata", "en"]) { fetchedNotifications, error in
-                    if let error = error {
-                        self.finish(with: error)
-                    } else {
-                        self.modelController.getAllNotifications { savedNotifications in
-                            guard let savedNotifications = savedNotifications, let fetchedNotifications = fetchedNotifications else {
-                                assertionFailure()
-                                self.finish()
-                                return
-                            }
-                            if fetchedNotifications.isEmpty && savedNotifications.isEmpty {
-                                self.finish()
+        self.managedObjectContext.perform {
+            self.apiController.getAllUnreadNotifications(from: ["wikidata", "en"]) { fetchedNotifications, error in
+                if let error = error {
+                    self.finish(with: error)
+                } else {
+                    self.modelController.getAllNotifications { savedNotifications in
+                        guard let savedNotifications = savedNotifications, let fetchedNotifications = fetchedNotifications else {
+                            assertionFailure()
+                            self.finish()
+                            return
+                        }
+                        if fetchedNotifications.isEmpty && savedNotifications.isEmpty {
+                            self.finish()
+                        } else {
+                            if savedNotifications.isEmpty {
+                                do {
+                                    try self.modelController.createNewNotifications(from: fetchedNotifications) {
+                                        self.finish()
+                                    }
+                                } catch let error {
+                                    assertionFailure()
+                                    self.finish(with: error)
+                                }
                             } else {
-                                if savedNotifications.isEmpty {
-                                    do {
-                                        try self.modelController.createNewNotifications(from: fetchedNotifications) {
-                                            self.finish()
-                                        }
-                                    } catch let error {
-                                        assertionFailure()
-                                        self.finish(with: error)
+                                do {
+                                    try self.modelController.updateNotifications(savedNotifications, with: fetchedNotifications) {
+                                        self.finish()
                                     }
-                                } else {
-                                    do {
-                                        try self.modelController.updateNotifications(savedNotifications, with: fetchedNotifications) {
-                                            self.finish()
-                                        }
-                                    } catch let error {
-                                        assertionFailure()
-                                        self.finish(with: error)
-                                    }
+                                } catch let error {
+                                    assertionFailure()
+                                    self.finish(with: error)
                                 }
                             }
                         }
--- WMF Framework/Remote Notifications/Operations/RemoteNotificationsMarkAsReadOperation.swift
+++ WMF Framework/Remote Notifications/Operations/RemoteNotificationsMarkAsReadOperation.swift
@@ -1,18 +1,16 @@
 class RemoteNotificationsMarkAsReadOperation: RemoteNotificationsOperation {
     override func execute() {
-        DispatchQueue.main.async {
-            self.managedObjectContext.perform {
-                self.modelController.getReadNotifications { readNotifications in
-                    guard let readNotifications = readNotifications, !readNotifications.isEmpty else {
+        self.managedObjectContext.perform {
+            self.modelController.getReadNotifications { readNotifications in
+                guard let readNotifications = readNotifications, !readNotifications.isEmpty else {
+                    self.finish()
+                    return
+                }
+                self.apiController.markAsRead(readNotifications) { error in
+                    if let error = error {
+                        self.finish(with: error)
+                    } else {
                         self.finish()
-                        return
-                    }
-                    self.apiController.markAsRead(readNotifications) { error in
-                        if let error = error {
-                            self.finish(with: error)
-                        } else {
-                            self.finish()
-                        }
                     }
                 }
             }
