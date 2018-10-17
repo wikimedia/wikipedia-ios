8d52daaf33ff30a952383ef41062d6c98d6374b1
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-17 13:58:03 -0400

    remove value from defaults

--- Wikipedia/Code/NSUserDefaults+WMFExtensions.swift
+++ Wikipedia/Code/NSUserDefaults+WMFExtensions.swift
@@ -44,7 +44,6 @@ let WMFSearchLanguageKey = "WMFSearchLanguageKey"
 @objc public extension UserDefaults {
     @objc(WMFUserDefaultsKey) public class Key: NSObject {
         @objc static let defaultTabType = "WMFDefaultTabTypeKey"
-        @objc static let didMakeAuthorizedTitleDescriptionEdit = "WMFDidMakeAuthorizedTitleDescriptionEdit"
     }
 
     @objc public static let wmf: UserDefaults = {
@@ -466,14 +465,5 @@ let WMFSearchLanguageKey = "WMFSearchLanguageKey"
     
     @objc public func wmf_setDidShowTitleDescriptionEditingIntro(_ didShow: Bool) {
         self.set(didShow, forKey: WMFDidShowTitleDescriptionEditingIntro)
-    }
-
-    @objc public var didMakeAuthorizedTitleDescriptionEdit: Bool {
-        get {
-            return bool(forKey: UserDefaults.Key.didMakeAuthorizedTitleDescriptionEdit)
-        }
-        set {
-            set(newValue, forKey: UserDefaults.Key.didMakeAuthorizedTitleDescriptionEdit)
-        }
     }
 }
