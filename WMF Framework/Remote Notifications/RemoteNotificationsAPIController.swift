396fd88eea17b2f8d6558c5f3500736b6c91397f
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-19 12:39:45 -0400

    make Array extension to split array into chunks public

--- WMF Framework/Remote Notifications/RemoteNotificationsAPIController.swift
+++ WMF Framework/Remote Notifications/RemoteNotificationsAPIController.swift
@@ -258,11 +258,3 @@ extension RemoteNotificationsAPIController {
         return session.hasValidCentralAuthCookies(for: .mediawiki)
     }
 }
-
-private extension Array {
-    func chunked(into size: Int) -> [[Element]] {
-        return stride(from: 0, to: count, by: size).map {
-            Array(self[$0 ..< Swift.min($0 + size, count)])
-        }
-    }
-}
