1ccb04afe439500dd059ead9338cec16f72155c0
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-30 09:59:15 -0400

    use a single completion

--- Wikipedia/Code/ReadingListsAlertController.swift
+++ Wikipedia/Code/ReadingListsAlertController.swift
@@ -41,26 +41,30 @@ public class ReadingListsAlertController: NSObject {
         presenter.present(alert(with: title, message: message, actions: [ReadingListsAlertActionType.cancel.action(), unsave]), animated: true)
     }
     
-    func showAlert(presenter: UIViewController, for articles: [WMFArticle], with actions: [UIAlertAction], completion: (() -> Void)? = nil, failure: () -> Bool) -> Bool {
+    func showAlert(presenter: UIViewController, for articles: [WMFArticle], with actions: [UIAlertAction], completion: ((Bool) -> Void)? = nil) {
         let articlesCount = articles.count
         guard articles.filter ({ $0.isOnlyInDefaultList }).count != articlesCount else {
-            return failure()
+            completion?(false)
+            return
         }
         let title = CommonStrings.unsaveArticleAndRemoveFromListsTitle(articleCount: articlesCount)
         let message = CommonStrings.unsaveArticleAndRemoveFromListsMessage(articleCount: articlesCount)
-        presenter.present(alert(with: title, message: message, actions: actions), animated: true, completion: completion)
-        return true
+        presenter.present(alert(with: title, message: message, actions: actions), animated: true) {
+            completion?(true)
+        }
     }
     
-    func showAlert(presenter: UIViewController, for readingLists: [ReadingList], with actions: [UIAlertAction], completion: (() -> Void)? = nil, failure: () -> Bool) -> Bool {
+    func showAlert(presenter: UIViewController, for readingLists: [ReadingList], with actions: [UIAlertAction], completion: ((Bool) -> Void)? = nil) {
         let readingListsCount = readingLists.count
         guard Int(readingLists.compactMap({ $0.countOfEntries }).reduce(0, +)) > 0 else {
-            return failure()
+            completion?(false)
+            return
         }
         let title = String.localizedStringWithFormat(WMFLocalizedString("reading-lists-delete-reading-list-alert-title", value: "Delete {{PLURAL:%1$d|list|lists}}?", comment: "Title of the alert shown before deleting selected reading lists. %1$d is replaced with number of lists to be deleted. %1$d will be replaced with the appropriate plural for the number of lists being deleted"), readingListsCount)
         let message =  String.localizedStringWithFormat(WMFLocalizedString("reading-lists-delete-reading-list-alert-message", value: "This action cannot be undone. Any articles saved only to {{PLURAL:%1$d|this list|these lists}} will be unsaved.", comment: "Title of the altert shown before deleting selected reading lists. %1$d will be replaced with the appropriate plural for the number of lists being deleted"), readingListsCount)
-        presenter.present(alert(with: title, message: message, actions: actions), animated: true, completion: completion)
-        return true
+        presenter.present(alert(with: title, message: message, actions: actions), animated: true) {
+            completion?(true)
+        }
     }
     
     private func alert(with title: String, message: String?, actions: [UIAlertAction]) -> UIAlertController {
