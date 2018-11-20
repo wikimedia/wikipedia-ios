8f5c5704f4ecef24815a7e37471ea95add52d8e2
Natalia Harateh <nambatee@users.noreply.github.com>
2018-11-20 07:58:40 -0500

    add top shadow

--- Wikipedia/Code/EditToolbarAccessoryView.swift
+++ Wikipedia/Code/EditToolbarAccessoryView.swift
@@ -4,5 +4,15 @@ import UIKit
 class EditToolbarAccessoryView: UIView {
     @IBOutlet weak var editToolbar: EditToolbar!
 
-    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addTopShadow(with: Theme.standard)
+    }
+
+    private func addTopShadow(with theme: Theme) {
+        layer.shadowOffset = CGSize(width: 0, height: -2)
+        layer.shadowRadius = 10
+        layer.shadowColor = theme.colors.shadow.cgColor
+        layer.shadowOpacity = 1.0
+    }
 }
