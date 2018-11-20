cfcc22662e0cbee953444a7b744cd83ad8675214
Natalia Harateh <nambatee@users.noreply.github.com>
2018-11-20 14:54:00 -0500

    format

--- Wikipedia/Code/WMFFeedContentFetcher.m
+++ Wikipedia/Code/WMFFeedContentFetcher.m
@@ -78,45 +78,47 @@ static const NSInteger WMFFeedContentFetcherMinimumMaxAge = 18000; // 5 minutes
     }
 
     NSURL *feedURL = [[self class] feedContentURLForSiteURL:siteURL onDate:date];
-    [self.session getJSONDictionaryFromURL:feedURL ignoreCache:NO completionHandler:^(NSDictionary<NSString *,id> * _Nullable jsonDictionary, NSHTTPURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (error) {
-            failure(error);
-            return;
-        }
-        
-        if (!force && response.statusCode == 304) {
-            failure([NSError wmf_errorWithType:WMFErrorTypeNoNewData userInfo:nil]);
-            return;
-        }
-        
-        NSError *mantleError = nil;
-        WMFFeedDayResponse *responseObject = [MTLJSONAdapter modelOfClass:[WMFFeedDayResponse class] fromJSONDictionary:jsonDictionary error:&mantleError];
-        if (mantleError) {
-            DDLogError(@"Error parsing feed day response: %@", mantleError);
-            failure(mantleError);
-            return;
-        }
-        
-        if (![responseObject isKindOfClass:[WMFFeedDayResponse class]]) {
-            failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
-                                      userInfo:nil]);
-            
-        } else {
-            NSDictionary *headers = [response allHeaderFields];
-            NSString *cacheControlHeader = headers[@"Cache-Control"];
-            NSInteger maxAge = WMFFeedContentFetcherMinimumMaxAge;
-            NSRegularExpression *regex = [WMFFeedContentFetcher cacheControlRegex];
-            if (regex && cacheControlHeader.length > 0) {
-                NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:cacheControlHeader options:0 range:NSMakeRange(0, [cacheControlHeader length])];
-                if (rangeOfFirstMatch.location != NSNotFound) {
-                    NSString *substringForFirstMatch = [cacheControlHeader substringWithRange:rangeOfFirstMatch];
-                    maxAge = MAX([substringForFirstMatch intValue], WMFFeedContentFetcherMinimumMaxAge);
-                }
-            }
-            responseObject.maxAge = maxAge;
-            success(responseObject);
-        }
-    }];
+    [self.session getJSONDictionaryFromURL:feedURL
+                               ignoreCache:NO
+                         completionHandler:^(NSDictionary<NSString *, id> *_Nullable jsonDictionary, NSHTTPURLResponse *_Nullable response, NSError *_Nullable error) {
+                             if (error) {
+                                 failure(error);
+                                 return;
+                             }
+
+                             if (!force && response.statusCode == 304) {
+                                 failure([NSError wmf_errorWithType:WMFErrorTypeNoNewData userInfo:nil]);
+                                 return;
+                             }
+
+                             NSError *mantleError = nil;
+                             WMFFeedDayResponse *responseObject = [MTLJSONAdapter modelOfClass:[WMFFeedDayResponse class] fromJSONDictionary:jsonDictionary error:&mantleError];
+                             if (mantleError) {
+                                 DDLogError(@"Error parsing feed day response: %@", mantleError);
+                                 failure(mantleError);
+                                 return;
+                             }
+
+                             if (![responseObject isKindOfClass:[WMFFeedDayResponse class]]) {
+                                 failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
+                                                           userInfo:nil]);
+
+                             } else {
+                                 NSDictionary *headers = [response allHeaderFields];
+                                 NSString *cacheControlHeader = headers[@"Cache-Control"];
+                                 NSInteger maxAge = WMFFeedContentFetcherMinimumMaxAge;
+                                 NSRegularExpression *regex = [WMFFeedContentFetcher cacheControlRegex];
+                                 if (regex && cacheControlHeader.length > 0) {
+                                     NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:cacheControlHeader options:0 range:NSMakeRange(0, [cacheControlHeader length])];
+                                     if (rangeOfFirstMatch.location != NSNotFound) {
+                                         NSString *substringForFirstMatch = [cacheControlHeader substringWithRange:rangeOfFirstMatch];
+                                         maxAge = MAX([substringForFirstMatch intValue], WMFFeedContentFetcherMinimumMaxAge);
+                                     }
+                                 }
+                                 responseObject.maxAge = maxAge;
+                                 success(responseObject);
+                             }
+                         }];
 }
 
 - (void)fetchPageviewsForURL:(NSURL *)titleURL startDate:(NSDate *)startDate endDate:(NSDate *)endDate failure:(WMFErrorHandler)failure success:(WMFPageViewsHandler)success {
@@ -144,49 +146,51 @@ static const NSInteger WMFFeedContentFetcherMinimumMaxAge = 18000; // 5 minutes
     if (startDateString == nil || endDateString == nil) {
         DDLogError(@"Failed to format pageviews date URL component for dates: %@ %@", startDate, endDate);
         NSError *error = [NSError wmf_errorWithType:WMFErrorTypeInvalidRequestParameters
-                                           userInfo:@{ WMFFailingRequestParametersUserInfoKey: @{@"start": startDate, @"end": endDate} }];
+                                           userInfo:@{WMFFailingRequestParametersUserInfoKey: @{@"start": startDate, @"end": endDate}}];
         failure(error);
         return;
     }
-    
+
     NSString *domainPathComponent = [NSString stringWithFormat:@"%@.%@", language, domain];
     NSArray<NSString *> *path = @[@"metrics", @"pageviews", @"per-article", domainPathComponent, @"all-access", @"user", title, @"daily", startDateString, endDateString];
     NSURLComponents *components = [WMFConfiguration.current mobileAppsServicesAPIURLComponentsForHost:titleURL.wmf_siteURL.host appendingPathComponents:path];
     NSCalendar *calendar = [NSCalendar wmf_utcGregorianCalendar];
-    [self.session getJSONDictionaryFromURL:components.URL ignoreCache:NO completionHandler:^(NSDictionary<NSString *,id> * _Nullable responseObject, NSHTTPURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (error) {
-            failure(error);
-            return;
-        }
-        dispatch_async(self.serialQueue, ^{
-            NSArray *items = responseObject[@"items"];
-            if (![items isKindOfClass:[NSArray class]]) {
-                failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
-                                          userInfo:nil]);
-                return;
-            }
+    [self.session getJSONDictionaryFromURL:components.URL
+                               ignoreCache:NO
+                         completionHandler:^(NSDictionary<NSString *, id> *_Nullable responseObject, NSHTTPURLResponse *_Nullable response, NSError *_Nullable error) {
+                             if (error) {
+                                 failure(error);
+                                 return;
+                             }
+                             dispatch_async(self.serialQueue, ^{
+                                 NSArray *items = responseObject[@"items"];
+                                 if (![items isKindOfClass:[NSArray class]]) {
+                                     failure([NSError wmf_errorWithType:WMFErrorTypeUnexpectedResponseType
+                                                               userInfo:nil]);
+                                     return;
+                                 }
 
-            NSMutableDictionary *results = [NSMutableDictionary dictionaryWithCapacity:[items count]];
-            for (id item in items) {
-                if ([item isKindOfClass:[NSDictionary class]] && [item[@"views"] isKindOfClass:[NSNumber class]] && [item[@"timestamp"] isKindOfClass:[NSString class]]) {
-                    NSDate *date = [[NSDateFormatter wmf_englishUTCNonDelimitedYearMonthDayHourFormatter] dateFromString:item[@"timestamp"]];
-                    results[date] = item[@"views"];
-                }
-            }
+                                 NSMutableDictionary *results = [NSMutableDictionary dictionaryWithCapacity:[items count]];
+                                 for (id item in items) {
+                                     if ([item isKindOfClass:[NSDictionary class]] && [item[@"views"] isKindOfClass:[NSNumber class]] && [item[@"timestamp"] isKindOfClass:[NSString class]]) {
+                                         NSDate *date = [[NSDateFormatter wmf_englishUTCNonDelimitedYearMonthDayHourFormatter] dateFromString:item[@"timestamp"]];
+                                         results[date] = item[@"views"];
+                                     }
+                                 }
 
-            NSDate *date = startDate;
+                                 NSDate *date = startDate;
 
-            while ([date compare:endDate] == NSOrderedAscending) {
-                if (results[date]) {
-                    break;
-                }
-                results[date] = @(0);
-                date = [calendar dateByAddingUnit:NSCalendarUnitDay value:1 toDate:date options:NSCalendarMatchStrictly];
-            }
+                                 while ([date compare:endDate] == NSOrderedAscending) {
+                                     if (results[date]) {
+                                         break;
+                                     }
+                                     results[date] = @(0);
+                                     date = [calendar dateByAddingUnit:NSCalendarUnitDay value:1 toDate:date options:NSCalendarMatchStrictly];
+                                 }
 
-            success([results copy]);
-        });
-    }];
+                                 success([results copy]);
+                             });
+                         }];
 }
 
 @end
