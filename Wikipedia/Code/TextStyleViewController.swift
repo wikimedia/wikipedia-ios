0a8a242f3bba5e6148b469a3b08ebe963316994e
Natalia Harateh <nambatee@users.noreply.github.com>
2018-11-29 12:40:33 -0500

    remove text style vc for now

--- Wikipedia/Code/TextStyleViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class TextStyleViewController: UIViewController {
-
-    static func loadFromNib() -> TextStyleViewController {
-        return TextStyleViewController(nibName: "TextStyleViewController", bundle: Bundle.main)
-    }
-
-}
