569027ca4f483119bf9facd2872ab23a95812344
Natalia Harateh <nambatee@users.noreply.github.com>
2018-11-30 13:15:39 -0500

    set checkmark when selected

--- Wikipedia/Code/TextStyleFormattingTableViewController.swift
+++ Wikipedia/Code/TextStyleFormattingTableViewController.swift
@@ -18,10 +18,30 @@ class TextStyleFormattingTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return configuredCell(cell, at: indexPath)
+    }
+
+    private func configuredCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
+        let isFirst = indexPath.item == 0
+        if isFirst {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         let style = styles[indexPath.item]
         cell.textLabel?.text = style.name
         cell.textLabel?.font = style.font
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+    }
+
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .none
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
