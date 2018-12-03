135c265a6291cb3ee79c6affbd02d37fe724d719
Natalia Harateh <nambatee@users.noreply.github.com>
2018-12-03 09:58:26 -0500

    add edit menu items

--- Wikipedia/Code/EditTextView.swift
+++ Wikipedia/Code/EditTextView.swift
@@ -10,6 +10,40 @@ class EditTextView: UITextView {
     @objc weak var dataSource: EditTextViewDataSource?
     @objc weak var inputViewControllerDelegate: TextFormattingTableViewControllerDelegate?
 
+    private lazy var editMenuItems: [UIMenuItem] = {
+        let addCitation = UIMenuItem(title: "+", action: #selector(addCitation(_:)))
+        let addLink = UIMenuItem(title: "🔗", action: #selector(addLink(_:)))
+        let addCurlyBrackets = UIMenuItem(title: "{}", action: #selector(addCurlyBrackets(_:)))
+        let makeBold = UIMenuItem(title: "𝗕", action: #selector(makeBold(_:)))
+        let makeItalic = UIMenuItem(title: "𝐼", action: #selector(makeItalic(_:)))
+        return [addCitation, addLink, addCurlyBrackets, makeBold, makeItalic]
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setEditMenuItems()
+    }
+
+    private func setEditMenuItems() {
+        guard let newMenuItems = existingMenuItemsCombinedWithEditMenuItems else {
+            return
+        }
+        UIMenuController.shared.menuItems = newMenuItems
+    }
+
+    private lazy var existingMenuItemsCombinedWithEditMenuItems: [UIMenuItem]? = {
+        guard let existingMenuItems = UIMenuController.shared.menuItems else {
+            return editMenuItems
+        }
+        let existing = NSMutableOrderedSet(array: existingMenuItems)
+        let new = NSOrderedSet(array: editMenuItems)
+        existing.union(new)
+        guard let newMenuItems = Array(existing) as? [UIMenuItem] else {
+            return nil
+        }
+        return newMenuItems
+    }()
+
     override var inputViewController: UIInputViewController? {
         guard
             let dataSource = dataSource,
@@ -19,5 +53,25 @@ class EditTextView: UITextView {
         let textFormattingInputViewController = TextFormattingInputViewController.wmf_viewControllerFromStoryboardNamed("TextFormatting")
         textFormattingInputViewController.delegate = inputViewControllerDelegate
         return textFormattingInputViewController
+    }
+
+    @objc private func makeBold(_ sender: UIMenuItem) {
+
+    }
+
+    @objc private func makeItalic(_ sender: UIMenuItem) {
+
+    }
+
+    @objc private func addCitation(_ sender: UIMenuItem) {
+
+    }
+
+    @objc private func addLink(_ sender: UIMenuItem) {
+
+    }
+
+    @objc private func addCurlyBrackets(_ sender: UIMenuItem) {
+
     }
 }
