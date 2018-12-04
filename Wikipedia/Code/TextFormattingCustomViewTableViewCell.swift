435e3c465167498475aa5d24e8cc92d577c6d13b
Natalia Harateh <nambatee@users.noreply.github.com>
2018-12-04 18:11:25 -0500

    remove unused call

--- Wikipedia/Code/TextFormattingCustomViewTableViewCell.swift
+++ Wikipedia/Code/TextFormattingCustomViewTableViewCell.swift
@@ -17,8 +17,6 @@ class TextFormattingCustomViewTableViewCell: TextFormattingTableViewCell {
             leadingConstraint
         ])
 
-        bringSubviewToFront(topSeparator)
-        
         self.customView = customView
     }
 
