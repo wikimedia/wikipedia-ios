6419f710b9f19c851d51895c6ad768166470a6d8
montehurd <mhurd@wikimedia.org>
2018-12-04 18:01:32 -0800

    stub out explicit methods for communicating button taps to JS land

--- Wikipedia/Code/SectionEditorWebView+ExecCommand.swift
+++ Wikipedia/Code/SectionEditorWebView+ExecCommand.swift
@@ -31,3 +31,70 @@ extension SectionEditorWebView {
         evaluateJavaScript("window.wmf.execCommand(window.wmf.ExecCommandType.\(string(for: type)));", completionHandler: completionHandler)
     }
 }
+
+
+
+/*
+TODO:
+- use separate methods instead of the exec command approach above
+*/
+
+extension SectionEditorWebView {
+    @objc func toggleBoldSelection() {
+        print("0")
+    }
+    @objc func toggleItalicSelection() {
+        print("1")
+    }
+    @objc func toggleReferenceSelection() {
+        print("2")
+    }
+    @objc func toggleTemplateSelection() {
+        print("3")
+    }
+    @objc func toggleAnchorSelection() {
+        print("4")
+    }
+    @objc func toggleIndentSelection() {
+        print("5")
+    }
+    @objc func toggleSignatureSelection() {
+        print("6")
+    }
+    @objc func toggleListSelection() {
+        print("7")
+    }
+    @objc func toggleHeadingSelection() {
+        print("8")
+    }
+    @objc func increaseIndentDepth() {
+        print("9")
+    }
+    @objc func decreaseIndentDepth() {
+        print("10")
+    }
+    
+    
+    @objc func undo() {
+        print("11")
+    }
+    @objc func redo() {
+        print("12")
+    }
+
+    
+    @objc func moveCursorDown() {
+        print("13")
+    }
+    @objc func moveCursorUp() {
+        print("14")
+    }
+    @objc func moveCursorLeft() {
+        print("15")
+    }
+    @objc func moveCursorRight() {
+        print("16")
+    }
+
+    
+}
