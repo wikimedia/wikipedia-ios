8ddcbd054dfd110666275cac6c9d5ff0829ece2a
Natalia Harateh <nambatee@users.noreply.github.com>
2018-12-05 22:18:21 -0500

    swizzle

--- Wikipedia/Code/SectionEditorWebView.swift
+++ Wikipedia/Code/SectionEditorWebView.swift
@@ -14,6 +14,7 @@ class SectionEditorWebView: WKWebView {
         super.init(frame: .zero, configuration: SectionEditorWebViewConfiguration())
         loadHTMLFromAssetsFile(codeMirrorIndexFileName, scrolledToFragment: nil)
         config.selectionChangedDelegate = self
+        scrollView.keyboardDismissMode = .interactive
     }
 
     required init?(coder: NSCoder) {
@@ -76,41 +77,58 @@ class SectionEditorWebView: WKWebView {
 
     // MARK: Accessory views
 
-    private let defaultEditToolbar = DefaultEditToolbarView.wmf_viewFromClassNib()!
-    private let contextualHighlightEditToolbar = ContextualHighlightEditToolbarView.wmf_viewFromClassNib()!
+    private lazy var defaultEditToolbarView: DefaultEditToolbarView = {
+        let view = DefaultEditToolbarView.wmf_viewFromClassNib()!
+        view.delegate = self
+        return view
+    }()
 
-    func gestCustomInputAccessoryView() -> UIView? {
-        return defaultEditToolbar
-    }
+    private lazy var contextualHighlightEditToolbarView: ContextualHighlightEditToolbarView = {
+        let view = ContextualHighlightEditToolbarView.wmf_viewFromClassNib()!
+        // view.delegate = self
+        return view
+    }()
 
-    override var inputView: UIView? {
-        return nil
-    }
+    // MARK: Input view
 
-    override var inputAccessoryViewController: UIInputViewController? {
-        let textFormattingInputViewController = TextFormattingInputViewController.wmf_viewControllerFromStoryboardNamed("TextFormatting")
-        //        textFormattingInputViewController.delegate = textFormattingDelegate
-        //        textFormattingInputViewController.inputViewType = preferredInputViewType
-        return nil
-    }
+    private var inputViewType: TextFormattingInputViewController.InputViewType?
 
     override var inputViewController: UIInputViewController? {
+        guard let inputViewType = inputViewType else {
+            return nil
+        }
         let textFormattingInputViewController = TextFormattingInputViewController.wmf_viewControllerFromStoryboardNamed("TextFormatting")
-//        textFormattingInputViewController.delegate = textFormattingDelegate
-//        textFormattingInputViewController.inputViewType = preferredInputViewType
-        return nil
-    }
-
-    func configureCustomInputAccessoryView() {
-        setCustomInputAccessoryView(defaultEditToolbar)
+        textFormattingInputViewController.delegate = self
+        textFormattingInputViewController.inputViewType = inputViewType
+        return textFormattingInputViewController
     }
 
     // MARK: Swizzling input accessory view
 
-    static var customInputAccessoryViewKey = 0
+    func configureInputAccessoryViews() {
+        _ = setInputAccessoryViews
+    }
 
-    private lazy var setCustomInputAccessoryView: (UIView) -> Void = { customInputAccessoryView in
-        objc_setAssociatedObject(self, &SectionEditorWebView.customInputAccessoryViewKey, customInputAccessoryView, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    private var previousInputAccessoryViewType: InputAccessoryViewType?
+    private var inputAccessoryViewType: InputAccessoryViewType? = .default {
+        didSet {
+            previousInputAccessoryViewType = oldValue
+        }
+    }
+
+    private struct InputAccessoryViewKey {
+        static var Default = "wmf_AssociatedDefaultEditToolbarView"
+        static var Highlight = "wmf_AssociatedHighlightEditToolbarView"
+    }
+
+    private enum InputAccessoryViewType {
+        case `default`
+        case highlight
+    }
+
+    private lazy var setInputAccessoryViews: Void = {
+        objc_setAssociatedObject(self, &InputAccessoryViewKey.Default, defaultEditToolbarView, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        objc_setAssociatedObject(self, &InputAccessoryViewKey.Highlight, contextualHighlightEditToolbarView, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
 
         guard let wkContent = self.scrollView.subviews.first(where:  { String(describing: type(of: $0)).hasPrefix("WKContent") }) else {
             assertionFailure("Couldn't find WKContent among scrollView's subviews")
@@ -128,26 +146,121 @@ class SectionEditorWebView: WKWebView {
             return
         }
 
-        guard let getter = class_getInstanceMethod(SectionEditorWebView.self, #selector(getter: SectionEditorWebView.customInputAccessoryView)) else {
-            assertionFailure("Couldn't get instance method")
+        let newSelector = #selector(getter: SectionEditorWebView.customInputAccessoryView)
+        guard let newMethod = class_getInstanceMethod(SectionEditorWebView.self, newSelector) else {
+            assertionFailure("Couldn't get instance method for \(newSelector)")
             return
         }
 
-        class_addMethod(newClass, #selector(getter: SectionEditorWebView.inputAccessoryView), method_getImplementation(getter), method_getTypeEncoding(getter))
+        let originalSelector = #selector(getter: SectionEditorWebView.inputAccessoryView)
+
+        class_addMethod(newClass, originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))
         objc_registerClassPair(newClass)
         object_setClass(wkContent, newClass)
+    }()
+
+    private func associateViewWithSelf(_ view: UIView, using key: inout Int) {
+        objc_setAssociatedObject(self, &key, view, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
 
     @objc private var customInputAccessoryView: UIView? {
-        var view: UIView? = self
-        while (view != nil) && !(view is WKWebView) {
-            view = view?.superview
+        var trueSelf: UIView? = self
+        while (trueSelf != nil) && !(trueSelf is WKWebView) {
+            trueSelf = trueSelf?.superview
         }
-        guard let webView = view else {
+        guard let webView = trueSelf as? SectionEditorWebView else {
             return nil
         }
-        let customInputAccessory = objc_getAssociatedObject(webView, &SectionEditorWebView.customInputAccessoryViewKey)
-        return customInputAccessory as? UIView
+
+        guard let preferredInputAccessoryView = preferredInputAccessoryView(associatedWith: webView) else {
+            return nil
+        }
+
+        preferredInputAccessoryView.apply(theme: Theme.standard)
+        return preferredInputAccessoryView
+    }
+
+    private func preferredInputAccessoryView(associatedWith webView: SectionEditorWebView) -> (UIView & Themeable)? {
+        guard let inputAccessoryViewType = webView.inputAccessoryViewType else {
+            return nil
+        }
+
+        let maybeView: Any?
+
+        switch inputAccessoryViewType {
+        case .default:
+            maybeView = objc_getAssociatedObject(webView, &InputAccessoryViewKey.Default)
+        case .highlight:
+            maybeView = objc_getAssociatedObject(webView, &InputAccessoryViewKey.Highlight)
+        }
+
+        guard let preferredInputAccessoryView = maybeView as? UIView & Themeable else {
+            assertionFailure("Couldn't get object associated with \(webView)")
+            return nil
+        }
+
+        return preferredInputAccessoryView
+    }
+
+    private func themeableView(associatedWith object: Any, key: inout String) -> (UIView & Themeable)? {
+        guard let view = objc_getAssociatedObject(object, key) as? (UIView & Themeable) else {
+            return nil
+        }
+        return view
+    }
+
+    // MARK: -
+
+    func setInputViewHidden(type: TextFormattingInputViewController.InputViewType? = nil, hidden: Bool) {
+        if hidden {
+            inputAccessoryViewType = previousInputAccessoryViewType
+        } else {
+            inputAccessoryViewType = nil
+        }
+
+        let animator = UIViewPropertyAnimator.init(duration: 0.3, curve: .easeInOut) {
+            self.resignFirstResponder()
+        }
+
+        animator.addCompletion { (_) in
+            self.becomeFirstResponder()
+        }
+
+        inputViewType = type
+
+        animator.startAnimation()
+    }
+}
+
+extension SectionEditorWebView: DefaultEditToolbarViewDelegate {
+    func defaultEditToolbarViewDidTapTextFormattingButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        setInputViewHidden(type: .textFormatting, hidden: false)
+    }
+
+    func defaultEditToolbarViewDidTapHeaderFormattingButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        setInputViewHidden(type: .textStyle, hidden: false)
+    }
+
+    func defaultEditToolbarViewDidTapAddCitationButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        //
+    }
+
+    func defaultEditToolbarViewDidTapAddLinkButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        //
+    }
+
+    func defaultEditToolbarViewDidTapUnorderedListButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        //
+    }
+
+    func defaultEditToolbarViewDidTapOrderedListButton(_ defaultEditToolbarView: DefaultEditToolbarView, button: UIButton) {
+        //
+    }
+}
+
+extension SectionEditorWebView: TextFormattingDelegate {
+    func textFormattingProvidingDidTapCloseButton(_ textFormattingProviding: TextFormattingProviding) {
+        setInputViewHidden(hidden: true)
     }
 }
 
@@ -199,6 +312,4 @@ extension SectionEditorWebView: SectionEditorWebViewSelectionChangedDelegate {
     func highlightRedoButton() {
         //
     }
-
-
 }
