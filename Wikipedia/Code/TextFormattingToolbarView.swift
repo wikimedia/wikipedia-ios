1196ebece672a022f3c0cbfbf6393e968c2cb355
Natalia Harateh <nambatee@users.noreply.github.com>
2018-12-06 16:59:52 -0500

    add action

--- Wikipedia/Code/TextFormattingToolbarView.swift
+++ Wikipedia/Code/TextFormattingToolbarView.swift
@@ -1,9 +1,13 @@
 protocol TextFormattingToolbarViewDelegate: class {
-
+    func textFormattingToolbarViewDidTapBoldButton(_ textFormattingToolbarView: TextFormattingToolbarView, button: UIButton)
 }
 
 class TextFormattingToolbarView: UIView {
     weak var delegate: TextFormattingToolbarViewDelegate?
+
+    @IBAction private func toggleBold(_ sender: UIButton) {
+        delegate?.textFormattingToolbarViewDidTapBoldButton(self, button: sender)
+    }
 }
 
 extension TextFormattingToolbarView: Themeable {
