b261edfe5ac20eed366ee0ed29a11b368ba7883a
montehurd <mhurd@wikimedia.org>
2018-12-19 03:50:33 -0800

    wire up grouped toolbar view to use button selection messages

--- Wikipedia/Code/TextFormattingGroupedToolbarView.swift
+++ Wikipedia/Code/TextFormattingGroupedToolbarView.swift
@@ -1,5 +1,56 @@
 class TextFormattingGroupedToolbarView: UIView, TextFormattingProviding {
     weak var delegate: TextFormattingDelegate?
+
+    @IBOutlet var allButtons: [TextFormattingButton]!
+    @IBOutlet weak var indentDecrease: TextFormattingButton!
+    @IBOutlet weak var indentIncrease: TextFormattingButton!
+    @IBOutlet weak var unorderedList: TextFormattingButton!
+    @IBOutlet weak var orderedList: TextFormattingButton!
+    @IBOutlet weak var xUp: TextFormattingButton!
+    @IBOutlet weak var xDown: TextFormattingButton!
+    @IBOutlet weak var underline: TextFormattingButton!
+    @IBOutlet weak var strikethrough: TextFormattingButton!
+
+    private func deselectAllButtons() {
+        allButtons.forEach() {
+            $0.isSelected = false
+        }
+    }
+    
+    private func selectButton(type: EditButtonType, ordered: Bool) {
+        switch (type) {
+        case .li:
+            if ordered {
+                orderedList.isSelected = true
+            } else {
+                unorderedList.isSelected = true
+            }
+        default:
+            print("button type not yet handled: \(type)")
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name.WMFSectionEditorSelectionChangedNotification, object: nil, queue: nil) { [weak self] notification in
+            self?.deselectAllButtons()
+            // if let message = notification.userInfo?[SectionEditorWebViewConfiguration.WMFSectionEditorSelectionChanged] as? SelectionChangedMessage {
+            //     print("selectionChangedMessage = \(message)")
+            // }
+        }
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name.WMFSectionEditorButtonHighlightNotification, object: nil, queue: nil) { [weak self] notification in
+            if let message = notification.userInfo?[SectionEditorWebViewConfiguration.WMFSectionEditorSelectionChangedSelectedButton] as? ButtonNeedsToBeSelectedMessage {
+                self?.selectButton(type: message.type, ordered: message.ordered)
+                // print("buttonNeedsToBeSelectedMessage = \(message)")
+            }
+        }
+        
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
 
 extension TextFormattingGroupedToolbarView: Themeable {
