69f907afee8cd7a2b068bd1fbfac25fa5ce7e351
montehurd <mhurd@wikimedia.org>
2018-12-19 03:23:28 -0800

    add reminder note

--- Wikipedia/Code/TextFormattingButton.swift
+++ Wikipedia/Code/TextFormattingButton.swift
@@ -3,6 +3,8 @@ class TextFormattingButton: UIButton {
     override var isSelected: Bool {
         didSet{
             self.tintColor = self.isSelected ? .black : .darkGray
+            // self.backgroundColor = self.isSelected ? .lightGray : .clear
+            // ^ once we get the updated assets from carolyn have a hangout to decide specifics for tint/bgcolor
         }
     }
 }
