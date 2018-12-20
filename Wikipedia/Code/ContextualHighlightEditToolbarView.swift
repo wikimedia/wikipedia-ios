e31fc9f773b9a1eefa6000158f7761272d506f15
montehurd <mhurd@wikimedia.org>
2018-12-19 17:20:13 -0800

    make contextual menu respond to button highlight messages

--- Wikipedia/Code/ContextualHighlightEditToolbarView.swift
+++ Wikipedia/Code/ContextualHighlightEditToolbarView.swift
@@ -6,6 +6,36 @@ protocol ContextualHighlightEditToolbarViewDelegate: class {
 class ContextualHighlightEditToolbarView: EditToolbarView {
     weak var delegate: ContextualHighlightEditToolbarViewDelegate?
 
+    @IBOutlet weak var boldButton: TextFormattingButton!
+    @IBOutlet weak var italicButton: TextFormattingButton!
+
+    private func selectButton(type: EditButtonType, ordered: Bool) {
+        switch (type) {
+        case .bold:
+            boldButton.isSelected = true
+        case .italic:
+            italicButton.isSelected = true
+        default:
+            break
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name.WMFSectionEditorButtonHighlightNotification, object: nil, queue: nil) { [weak self] notification in
+            if let message = notification.userInfo?[SectionEditorWebViewConfiguration.WMFSectionEditorSelectionChangedSelectedButton] as? ButtonNeedsToBeSelectedMessage {
+                self?.selectButton(type: message.type, ordered: message.ordered)
+                // print("buttonNeedsToBeSelectedMessage = \(message)")
+            }
+        }
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
     @IBAction private func formatHeader(_ sender: UIButton) {
         delegate?.contextualHighlightEditToolbarViewDidTapHeaderFormattingButton(self, button: sender)
     }
