a9d05482089902f73d7647b98d48d2e183a1290c
montehurd <mhurd@wikimedia.org>
2018-12-21 11:56:50 -0800

    increase icon touch targets

--- Wikipedia/Code/TextFormattingButton.swift
+++ Wikipedia/Code/TextFormattingButton.swift
@@ -7,4 +7,12 @@ class TextFormattingButton: UIButton {
             // ^ once we get the updated assets from carolyn have a hangout to decide specifics for tint/bgcolor
         }
     }
+    
+    override open var intrinsicContentSize: CGSize {
+        get {
+            // Workaround for increasing touch targets
+            let superSize = super.intrinsicContentSize
+            return CGSize(width: max(superSize.width, 35), height: superSize.height)
+        }
+    }
 }
