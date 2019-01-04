ca952a56354f21dbbb0a6e12c5aa1c60f1307667
Natalia Harateh <nambatee@users.noreply.github.com>
2019-01-04 15:30:28 +0100

    set isEnabled in xib rely on parent when responding to selection changes

--- Wikipedia/Code/TextFormattingGroupedToolbarView.swift
+++ Wikipedia/Code/TextFormattingGroupedToolbarView.swift
@@ -1,18 +1,4 @@
-class TextFormattingGroupedToolbarView: UIView, TextFormattingProviding {
-    weak var delegate: TextFormattingDelegate?
-
-    @IBOutlet var buttons: [TextFormattingButton]!
-    @IBOutlet weak var indentDecrease: TextFormattingButton!
-    @IBOutlet weak var indentIncrease: TextFormattingButton!
-    @IBOutlet weak var unorderedList: TextFormattingButton!
-    @IBOutlet weak var orderedList: TextFormattingButton!
-    @IBOutlet weak var xUp: TextFormattingButton!
-    @IBOutlet weak var xDown: TextFormattingButton!
-    @IBOutlet weak var underline: TextFormattingButton!
-    @IBOutlet weak var strikethrough: TextFormattingButton!
-    @IBOutlet weak var stackView: UIStackView!
-
-    
+class TextFormattingGroupedToolbarView: TextFormattingToolbarView {
     @IBAction private func increaseIndent(sender: UIButton) {
         delegate?.textFormattingProvidingDidTapIncreaseIndent()
     }
@@ -36,63 +22,5 @@ class TextFormattingGroupedToolbarView: UIView, TextFormattingProviding {
     }
     @IBAction private func toggleStrikethrough(sender: UIButton) {
         delegate?.textFormattingProvidingDidTapStrikethrough()
-    }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        unorderedList.isEnabled = false
-        orderedList.isEnabled = false
-        indentDecrease.isEnabled = false
-        indentIncrease.isEnabled = false
-        stackView.isLayoutMarginsRelativeArrangement = true
-    }
-    
-    private func deselectAllButtons() {
-        buttons.forEach() {
-            $0.isSelected = false
-        }
-    }
-    
-    private func selectButton(type: EditButtonType, ordered: Bool) {
-        switch (type) {
-        case .superscript:
-            xUp.isSelected = true
-        case .subscript:
-            xDown.isSelected = true
-        case .underline:
-            underline.isSelected = true
-        case .strikethrough:
-            strikethrough.isSelected = true
-        default:
-            break
-        }
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
-        NotificationCenter.default.addObserver(forName: Notification.Name.WMFSectionEditorSelectionChangedNotification, object: nil, queue: nil) { [weak self] notification in
-            self?.deselectAllButtons()
-            // if let message = notification.userInfo?[SectionEditorWebViewConfiguration.WMFSectionEditorSelectionChanged] as? SelectionChangedMessage {
-            //     print("selectionChangedMessage = \(message)")
-            // }
-        }
-        
-        NotificationCenter.default.addObserver(forName: Notification.Name.WMFSectionEditorButtonHighlightNotification, object: nil, queue: nil) { [weak self] notification in
-            if let message = notification.userInfo?[SectionEditorWebViewConfiguration.WMFSectionEditorSelectionChangedSelectedButton] as? ButtonNeedsToBeSelectedMessage {
-                self?.selectButton(type: message.type, ordered: message.ordered)
-                // print("buttonNeedsToBeSelectedMessage = \(message)")
-            }
-        }
-        
-    }
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-}
-
-extension TextFormattingGroupedToolbarView: Themeable {
-    func apply(theme: Theme) {
-
     }
 }
