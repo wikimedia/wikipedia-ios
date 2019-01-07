eae9cd6195f060c46c34449ce1ae15305c073464
joewalsh <joewalsh@users.noreply.github.com>
2019-01-07 17:38:50 -0500

    weak self

--- Wikipedia/Code/SectionEditorViewController.swift
+++ Wikipedia/Code/SectionEditorViewController.swift
@@ -180,15 +180,15 @@ class SectionEditorViewController: UIViewController {
         guard isCodemirrorReady, let wikitext = wikitext else {
             return
         }
-        self.setWikitextToWebView(wikitext) { (error) in
+        setWikitextToWebView(wikitext) { [weak self] (error) in
             if let error = error {
                 assertionFailure(error.localizedDescription)
             } else {
                 DispatchQueue.main.async {
-                    self.webViewCoverView?.removeFromSuperview()
-                    self.webView.focus()
+                    self?.webViewCoverView?.removeFromSuperview()
+                    self?.webView.focus()
                     // TODO: Remove
-                    self.progressButton.isEnabled = true
+                    self?.progressButton.isEnabled = true
                 }
             }
         }
