83cb56f05df2c44d75b002f4a8021b4361770027
joewalsh <joewalsh@users.noreply.github.com>
2019-01-23 10:55:47 -0500

    remove unused code

--- Wikipedia/Code/WMFArticlePreviewFetcher.m
+++ Wikipedia/Code/WMFArticlePreviewFetcher.m
@@ -5,20 +5,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#pragma mark - Internal Class Declarations
-
-@interface WMFArticlePreviewRequestParameters : NSObject
-
-@property (nonatomic, strong) NSArray<NSURL *> *articleURLs;
-@property (nonatomic, assign) NSUInteger extractLength;
-@property (nonatomic, assign) NSUInteger thumbnailWidth;
-
-@end
-
-@interface WMFArticlePreviewRequestSerializer : WMFBaseRequestSerializer
-
-@end
-
 #pragma mark - Fetcher Implementation
 
 @implementation WMFArticlePreviewFetcher
@@ -36,69 +22,55 @@ NS_ASSUME_NONNULL_BEGIN
                                   thumbnailWidth:(NSUInteger)thumbnailWidth
                                       completion:(void (^)(NSArray<MWKSearchResult *> *results))completion
                                          failure:(void (^)(NSError *error))failure {
-    
+
     NSMutableDictionary *params =
-    [NSMutableDictionary wmf_titlePreviewRequestParametersWithExtractLength:extractLength
-                                                                 imageWidth:@(thumbnailWidth)];
+        [NSMutableDictionary wmf_titlePreviewRequestParametersWithExtractLength:extractLength
+                                                                     imageWidth:@(thumbnailWidth)];
     NSString *titles = WMFJoinedPropertyParameters([articleURLs wmf_map:^NSString *(NSURL *URL) {
         return URL.wmf_title;
     }]);
     [params setValuesForKeysWithDictionary:@{@"titles": titles, @"pilimit": @(articleURLs.count)}];
-    
+
     params[@"prop"] = [params[@"prop"] stringByAppendingString:@"|coordinates"];
-    
+
     if (extractLength > 0) {
         params[@"exlimit"] = @(articleURLs.count);
     }
- 
+
     @weakify(self);
     NSURLComponents *components = [self.configuration mediaWikiAPIURLComponentsForHost:siteURL.host withQueryParameters:params];
-    [self.session getJSONDictionaryFromURL:components.URL ignoreCache:NO completionHandler:^(NSDictionary<NSString *,id> * _Nullable result, NSHTTPURLResponse * _Nullable response, NSError * _Nullable error) {
-        @strongify(self);
-        if (!self) {
-            failure([NSError wmf_cancelledError]);
-            return;
-        }
-        if (error) {
-            failure(error);
-            return;
-        }
-        
-        NSError *jsonError = nil;
-        NSArray<MWKSearchResult *> *unsortedPreviews = [WMFLegacySerializer modelsOfClass:[MWKSearchResult class] fromArrayForKeyPath:@"query.pages" inJSONDictionary:result error:&jsonError];
-        if (jsonError) {
-            failure(jsonError);
-            return;
-        }
+    [self.session getJSONDictionaryFromURL:components.URL
+                               ignoreCache:NO
+                         completionHandler:^(NSDictionary<NSString *, id> *_Nullable result, NSHTTPURLResponse *_Nullable response, NSError *_Nullable error) {
+                             @strongify(self);
+                             if (!self) {
+                                 failure([NSError wmf_cancelledError]);
+                                 return;
+                             }
+                             if (error) {
+                                 failure(error);
+                                 return;
+                             }
 
-        NSArray *results = [articleURLs wmf_mapAndRejectNil:^(NSURL *articleURL) {
-            MWKSearchResult *matchingPreview = [unsortedPreviews wmf_match:^BOOL(MWKSearchResult *preview) {
-                return [preview.displayTitle isEqualToString:articleURL.wmf_title];
-            }];
-            if (!matchingPreview) {
-                DDLogWarn(@"Couldn't find requested preview for %@. Returned previews: %@", articleURL, unsortedPreviews);
-            }
-            return matchingPreview;
-        }];
-                                                     
-        completion(results);
-    }];
-}
+                             NSError *jsonError = nil;
+                             NSArray<MWKSearchResult *> *unsortedPreviews = [WMFLegacySerializer modelsOfClass:[MWKSearchResult class] fromArrayForKeyPath:@"query.pages" inJSONDictionary:result error:&jsonError];
+                             if (jsonError) {
+                                 failure(jsonError);
+                                 return;
+                             }
 
-@end
+                             NSArray *results = [articleURLs wmf_mapAndRejectNil:^(NSURL *articleURL) {
+                                 MWKSearchResult *matchingPreview = [unsortedPreviews wmf_match:^BOOL(MWKSearchResult *preview) {
+                                     return [preview.displayTitle isEqualToString:articleURL.wmf_title];
+                                 }];
+                                 if (!matchingPreview) {
+                                     DDLogWarn(@"Couldn't find requested preview for %@. Returned previews: %@", articleURL, unsortedPreviews);
+                                 }
+                                 return matchingPreview;
+                             }];
 
-#pragma mark - Internal Class Implementations
-
-@implementation WMFArticlePreviewRequestParameters
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _articleURLs = @[];
-        _extractLength = WMFNumberOfExtractCharacters;
-        _thumbnailWidth = [[UIScreen mainScreen] wmf_leadImageWidthForScale].unsignedIntegerValue;
-    }
-    return self;
+                             completion(results);
+                         }];
 }
 
 @end
