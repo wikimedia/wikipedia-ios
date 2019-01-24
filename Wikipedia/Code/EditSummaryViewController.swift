5b838d2d7e8f0402e2d90b92299cf55271389214
montehurd <mhurd@wikimedia.org>
2019-01-23 21:27:02 -0800

    update edit summary interface

--- Wikipedia/Code/EditSummaryViewController.swift
+++ Wikipedia/Code/EditSummaryViewController.swift
@@ -1,39 +1,59 @@
-import UIKit
-import WMF
 
-enum EditSummaryViewControllerConstants {
-    static let maximumSummaryLength = 255
+import UIKit
+
+@objc protocol EditSummaryViewDelegate: AnyObject {
+    func summaryChanged(newSummary: String?)
+    func learnMoreButtonTapped(sender: UIButton)
+    func cannedButtonTapped(type: EditSummaryViewCannedButtonType)
 }
 
-class EditSummaryViewController: UIViewController {
+@objc enum EditSummaryViewCannedButtonType: Int {
+    case typo, grammar, link
+}
+
+@objcMembers class EditSummaryViewController: UIViewController, Themeable {
+    static let maximumSummaryLength = 255
     
-    typealias C = EditSummaryViewControllerConstants
-    
-    @objc var didSaveSummary: ((String?) -> ())?
-    @objc var summaryText: String = ""
-    
-    fileprivate var theme: Theme = Theme.standard
-    @IBOutlet private weak var bottomLineHeightConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var summaryTextField: ThemeableTextField! {
+    public var theme: Theme? {
         didSet {
-            summaryTextField.placeholder = WMFLocalizedStringWithDefaultValue("edit-summary-field-placeholder-text", nil, nil, "Other ways you improved the article", "Placeholder text which appears initially in the free-form edit summary text box")
-            summaryTextField.returnKeyType = .done
-            summaryTextField.delegate = self
-            summaryTextField.textAlignment = .natural
-            summaryTextField.font = UIFont.systemFont(ofSize: 14.0)
+            guard let theme = theme else {
+                return
+            }
+            apply(theme: theme)
         }
     }
+
+    public weak var delegate: EditSummaryViewDelegate?
+    
+    @IBOutlet private weak var addSummaryLabel: UILabel!
+    @IBOutlet private weak var learnMoreButton: UIButton!
+    @IBOutlet private weak var summaryTextField: ThemeableTextField!
+    @IBOutlet private weak var fixedTypoButton: UIButton!
+    @IBOutlet private weak var fixedGrammarButton: UIButton!
+    @IBOutlet private weak var addedLinksButton: UIButton!
+    @IBOutlet private weak var dividerView: UIView!
+
+    private let placeholderText = WMFLocalizedStringWithDefaultValue("edit-summary-placeholder-text", nil, nil, "How did you improve the article?", "Placeholder text which appears initially in the free-form edit summary text box")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        fixedTypoButton.titleLabel?.numberOfLines = 1
+        fixedGrammarButton.titleLabel?.numberOfLines = 1
+        addedLinksButton.titleLabel?.numberOfLines = 1
+
+        fixedTypoButton.titleLabel?.setContentCompressionResistancePriority(.required, for: .horizontal)
+        fixedGrammarButton.titleLabel?.setContentCompressionResistancePriority(.required, for: .horizontal)
+        addedLinksButton.titleLabel?.setContentCompressionResistancePriority(.required, for: .horizontal)
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem.wmf_buttonType(.X, target: self, action: #selector(closeButtonPressed))
-        let localization = WMFLocalizedStringWithDefaultValue("button-done", nil, nil, "Done", "Button text for done button used in various places.\n{{Identical|Done}}")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: localization, style: .plain, target: self, action: #selector(save))
-        
-        bottomLineHeightConstraint.constant = 1.0 / UIScreen.main.scale
-        
-        apply(theme: theme)
+        addSummaryLabel.text = WMFLocalizedStringWithDefaultValue("edit-summary-add-summary-text", nil, nil, "Add an edit summary", "Text for add summary label")
+        learnMoreButton.setTitle(WMFLocalizedStringWithDefaultValue("edit-summary-learn-more-text", nil, nil, "Learn more", "Text for learn more button"), for: .normal)
+        summaryTextField.placeholder = placeholderText
+        summaryTextField.delegate = self
+        summaryTextField.addTarget(self, action: #selector(self.textFieldDidChange), for: .editingChanged)
+        fixedTypoButton.setTitle(WMFLocalizedStringWithDefaultValue("edit-summary-choice-fixed-typos", nil, nil, "Fixed typo", "Button text for quick 'fixed typos' edit summary selection"), for: .normal)
+        fixedGrammarButton.setTitle(WMFLocalizedStringWithDefaultValue("edit-summary-choice-fixed-grammar", nil, nil, "Fixed grammar", "Button text for quick 'improved grammar' edit summary selection"), for: .normal)
+        addedLinksButton.setTitle(WMFLocalizedStringWithDefaultValue("edit-summary-choice-linked-words", nil, nil, "Added links", "Button text for quick 'link addition' edit summary selection"), for: .normal)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,44 +61,52 @@ class EditSummaryViewController: UIViewController {
         summaryTextField.becomeFirstResponder()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        summaryTextField.text = summaryText
+    @IBAction private func learnMoreButtonTapped(sender: UIButton) {
+        delegate?.learnMoreButtonTapped(sender: sender)
     }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        summaryTextField.resignFirstResponder()
+
+    public func textFieldDidChange(textField: UITextField){
+        notifyDelegateOfSummaryChange()
     }
-    
-    @objc func closeButtonPressed() {
-        dismiss(animated: true, completion: nil)
+
+    private func notifyDelegateOfSummaryChange() {
+        delegate?.summaryChanged(newSummary: summaryTextField.text)
     }
-    
-    @objc func save() {
-        let trimmedSummary = summaryTextField.text?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        didSaveSummary?(trimmedSummary)
-        dismiss(animated: true, completion: nil)
+
+    @IBAction private func cannedSummaryButtonTapped(sender: UIButton) {
+        summaryTextField.text = sender.titleLabel?.text
+        notifyDelegateOfSummaryChange()
+        guard let buttonType = EditSummaryViewCannedButtonType(rawValue: sender.tag) else {
+            assertionFailure("Expected button type not found")
+            return
+        }
+        delegate?.cannedButtonTapped(type: buttonType)
+    }
+
+    public func apply(theme: Theme) {
+        view.backgroundColor = theme.colors.paperBackground
+        dividerView.backgroundColor = theme.colors.border
+        
+        addSummaryLabel.textColor = theme.colors.secondaryText
+        learnMoreButton.titleLabel?.textColor = theme.colors.link
+        fixedTypoButton.titleLabel?.textColor = theme.colors.link
+        fixedGrammarButton.titleLabel?.textColor = theme.colors.link
+        addedLinksButton.titleLabel?.textColor = theme.colors.link
+        summaryTextField.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor: theme.colors.tertiaryText])
+
+        fixedTypoButton.backgroundColor = theme.colors.border
+        fixedGrammarButton.backgroundColor = theme.colors.border
+        addedLinksButton.backgroundColor = theme.colors.border
     }
 }
 
 extension EditSummaryViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        save()
+        //save()
         return true
     }
-    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let newLength = (textField.text?.count ?? 0) + string.count - range.length
-        return newLength <= C.maximumSummaryLength
-    }
-}
-
-extension EditSummaryViewController: Themeable {
-    func apply(theme: Theme) {
-        self.theme = theme
-        guard viewIfLoaded != nil else { return }
-        view.backgroundColor = theme.colors.paperBackground
-        summaryTextField.apply(theme: theme)
+        return newLength <= EditSummaryViewController.maximumSummaryLength
     }
 }
