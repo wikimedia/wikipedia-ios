5880a32203cefb44b157caa6c154aa471d38ab66
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-12 10:09:02 -0500

    fix typo

--- Wikipedia/Code/WMFFeedArticlePreview+DescriptionOrSnippet.swift
+++ Wikipedia/Code/WMFFeedArticlePreview+DescriptionOrSnippet.swift
@@ -5,7 +5,7 @@ public extension WMFFeedArticlePreview {
             let articleLanguage = articleURL.wmf_language
             return wikidataDescription.wmf_stringByCapitalizingFirstCharacter(usingWikipediaLanguage: articleLanguage)
         }
-        if let snippet = snippet, !snippet.isEmpty0 {
+        if let snippet = snippet, !snippet.isEmpty {
             return String(snippet.prefix(128))
         }
         return nil
