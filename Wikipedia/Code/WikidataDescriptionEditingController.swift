2d134e971fab356a5ab345cffedef4eb749276ad
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-01 15:13:39 -0500

    Revert "notify when wikidata description edit is published"
    This reverts commit 61eb7065c89a387d228af2fe1b01d91d3d38dc66.

--- Wikipedia/Code/WikidataDescriptionEditingController.swift
+++ Wikipedia/Code/WikidataDescriptionEditingController.swift
@@ -30,7 +30,7 @@ enum WikidataPublishingError: LocalizedError {
 }
 
 @objc public final class WikidataDescriptionEditingController: Fetcher {
-    @objc static let didPublishNotification = NSNotification.Name(rawValue: "WikidataDescriptionEditingControllerDidPublishNotification")
+    static let DidMakeAuthorizedWikidataDescriptionEditNotification = NSNotification.Name(rawValue: "WMFDidMakeAuthorizedWikidataDescriptionEdit")
     /// Publish new wikidata description.
     ///
     /// - Parameters:
@@ -57,7 +57,7 @@ enum WikidataPublishingError: LocalizedError {
 
             if let authorized = authorized, authorized, result.error == nil {
                 DispatchQueue.main.async {
-                    NotificationCenter.default.post(name: WikidataDescriptionEditingController.didPublishNotification, object: authorized)
+                    NotificationCenter.default.post(name: WikidataDescriptionEditingController.DidMakeAuthorizedWikidataDescriptionEditNotification, object: nil)
                 }
             }
         }
