9c019882573d799654e8c9f744da4e9e000bdb38
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-01 12:57:28 -0500

    set new default

--- Wikipedia/Code/Panels.swift
+++ Wikipedia/Code/Panels.swift
@@ -371,11 +371,16 @@ extension UIViewController {
     }
 
     @objc func wmf_showDescriptionPublishedPanelViewController(theme: Theme) {
+        guard !UserDefaults.wmf.didShowDescriptionEditPublishedPanel else {
+            return
+        }
         let doneTapHandler: ScrollableEducationPanelButtonTapHandler = { sender in
             self.dismiss(animated: true, completion: nil)
         }
         let panelVC = DescriptionPublishedPanelViewController(showCloseButton: true, primaryButtonTapHandler: doneTapHandler, secondaryButtonTapHandler: nil, dismissHandler: nil, theme: theme)
-        present(panelVC, animated: true, completion: nil)
+        present(panelVC, animated: true) {
+            UserDefaults.wmf.didShowDescriptionEditPublishedPanel = true
+        }
     }
 
     @objc func wmf_showEditPublishedPanelViewController(theme: Theme) {
