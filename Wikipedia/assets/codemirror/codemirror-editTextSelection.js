1b6fdb90bd3b5903eede26acbee7ab7b0840ab8d
montehurd <mhurd@wikimedia.org>
2019-03-08 15:00:55 -0800

    set cursor insertion point after selection
    we don't want to highlight the range from start to finish because we're marking the range using 'markText'

--- Wikipedia/assets/codemirror/codemirror-editTextSelection.js
+++ Wikipedia/assets/codemirror/codemirror-editTextSelection.js
@@ -95,6 +95,8 @@ const getWikitextRangeToSelect = (wikitextBeforeSelection, wikitextSelection) =>
 }
 
 const scrollToAndHighlightRange = (range, codemirror) => {
+  codemirror.setCursor(range.to)
+
   let marker = null
   marker = codemirror.markText(range.from, range.to, {
     css: 'background-color: rgba(255, 204, 51, 0.4)', // Can use 'className' (vs 'css') if needed.
