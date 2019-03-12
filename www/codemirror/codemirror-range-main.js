a518e2f27be8fd3d19e26b2890b92d5c8a6148a6
montehurd <mhurd@wikimedia.org>
2019-03-12 13:36:30 -0700

    expose new range bits on a 'RangeHelper' window object

---
+++ www/codemirror/codemirror-range-main.js
@@ -0,0 +1,8 @@
+const RangeHelper = {}
+
+RangeHelper.rangeDebugging = require('./codemirror-range-debugging')
+RangeHelper.rangeDetermination = require('./codemirror-range-determination')
+RangeHelper.rangeObjects = require('./codemirror-range-objects')
+RangeHelper.rangeUtilities = require('./codemirror-range-utilities')
+
+window.RangeHelper = RangeHelper
