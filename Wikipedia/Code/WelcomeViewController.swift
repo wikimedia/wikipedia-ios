a4cc23a43a61666ccb5b9f076317ee3e6f1cdf68
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-14 18:48:39 -0400

    remove init

--- Wikipedia/Code/WelcomeViewController.swift
+++ Wikipedia/Code/WelcomeViewController.swift
@@ -9,10 +9,6 @@ class WelcomeViewController: UIViewController {
         addPageViewController(with: viewControllers)
     }
 
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
