58d3bbbfe68fec5fdd31ced64e2871254e9f5254
montehurd <mhurd@wikimedia.org>
2019-03-15 17:53:08 -0700

    method for getting markup items for ALL lines for a range

--- www/codemirror/codemirror-range-determination.js
+++ www/codemirror/codemirror-range-determination.js
@@ -1,6 +1,6 @@
-
 const tagMarkupItemsForLineTokens = require('./codemirror-range-determination-tag').tagMarkupItemsForLineTokens
 const nonTagMarkupItemsForLineTokens = require('./codemirror-range-determination-non-tag').nonTagMarkupItemsForLineTokens
+const ItemRange = require('./codemirror-range-objects').ItemRange
 
 const markupItemsForLineTokens = (lineTokens, line) => {
   const tagMarkupItems = tagMarkupItemsForLineTokens(lineTokens, line)
@@ -9,4 +9,17 @@ const markupItemsForLineTokens = (lineTokens, line) => {
   return markupItems
 }
 
+// Gets all markup items for ALL lines between range's start and end. 
+// May include markup items to left of range start or right range end.
+const markupItemsForItemRangeLines = (codeMirror, range) => {
+  let markupItems = []
+  for (let line = range.startLocation.line; line <= range.endLocation.line; line++) {
+    const tokens = codeMirror.getLineTokens(line, true)
+    const items = markupItemsForLineTokens(tokens, line)
+    markupItems.push(...items)
+  }
+  return markupItems
+}
+
 exports.markupItemsForLineTokens = markupItemsForLineTokens
+exports.markupItemsForItemRangeLines = markupItemsForItemRangeLines
