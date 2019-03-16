6411f0cb299bf835869abf4c7637e49192d64ab1
montehurd <mhurd@wikimedia.org>
2019-03-15 17:59:07 -0700

    method for moving or removing selection markup
    works off of range info only via MarkupItem interrogation - ie not string reg-ex based
    
    works with multi-line selections
    
    edge case not handled:
    - doesn't handle the case where a selection will be split and markup will be ADDED - this will be handled by a separate follow-up method

---
+++ www/codemirror/codemirror-range-clear-formatting.js
@@ -0,0 +1,134 @@
+const ItemRange = require('./codemirror-range-objects').ItemRange
+const ItemLocation = require('./codemirror-range-objects').ItemLocation
+
+const getItemRangeFromSelection = require('./codemirror-range-utilities').getItemRangeFromSelection
+const getMarkupItemsIntersectingSelection = require('./codemirror-range-utilities').getMarkupItemsIntersectingSelection
+const getButtonNamesFromMarkupItems = require('./codemirror-range-utilities').getButtonNamesFromMarkupItems
+const markupItemsForItemRangeLines = require('./codemirror-range-determination').markupItemsForItemRangeLines
+
+const canClearFormatting = (codeMirror) => {
+  let selectionRange = getItemRangeFromSelection(codeMirror)
+  if (selectionRange.isZeroLength()) {
+    return false
+  }
+
+  const markupItems = markupItemsForItemRangeLines(codeMirror, selectionRange)
+  const markupItemsIntersectingSelection = getMarkupItemsIntersectingSelection(codeMirror, markupItems, selectionRange)
+  const buttonNames = getButtonNamesFromMarkupItems(markupItemsIntersectingSelection)
+
+  if (buttonNames.includes('reference') || buttonNames.includes('template')) {
+    return false
+  }
+  
+  return relocateOrRemoveExistingMarkupForSelectionRange(codeMirror, true)
+}
+
+const relocateOrRemoveExistingMarkupForSelectionRange = (codeMirror, evaluateOnly = false) => {
+  let selectionRange = getItemRangeFromSelection(codeMirror)
+  const originalSelectionRange = selectionRange
+
+  const markupItems = markupItemsForItemRangeLines(codeMirror, selectionRange)
+  
+  selectionRange = getExpandedSelectionRange(codeMirror, markupItems, selectionRange)
+  if (!evaluateOnly) {
+    codeMirror.setSelection(selectionRange.startLocation, selectionRange.endLocation)
+  }
+
+  const markupItemsIntersectingSelection = getMarkupItemsIntersectingSelection(codeMirror, markupItems, selectionRange)
+
+  let markupRangesToMoveAfterSelection = []
+  let markupRangesToMoveBeforeSelection = []
+  let markupRangesToRemove = []
+  markupItemsIntersectingSelection.forEach(item => {
+    const startsInside = item.outerRange.startsInsideRange(selectionRange, true)
+    const endsInside = item.outerRange.endsInsideRange(selectionRange, true)
+    if (!(startsInside === endsInside)) { // XOR
+      if (startsInside) {
+        markupRangesToMoveAfterSelection.push(item.openingMarkupRange())
+      }
+      if (endsInside) {
+        markupRangesToMoveBeforeSelection.unshift(item.closingMarkupRange())
+      }
+    } else if (startsInside && endsInside) {
+      markupRangesToRemove.push(item.openingMarkupRange())
+      markupRangesToRemove.push(item.closingMarkupRange())
+    }
+  })
+  
+  const noMarkupToBeMovedToEitherSide = (markupRangesToMoveAfterSelection.length === 0 && markupRangesToMoveBeforeSelection.length === 0)
+  if (noMarkupToBeMovedToEitherSide) {
+    const openingMarkupRanges = markupItemsIntersectingSelection.map(item => item.openingMarkupRange())
+    const closingMarkupRanges = markupItemsIntersectingSelection.map(item => item.closingMarkupRange())
+    const allMarkupRanges = openingMarkupRanges.concat(closingMarkupRanges)
+    if (evaluateOnly) {
+      return allMarkupRanges.length > 0
+    }
+    removeTextFromRanges(codeMirror, allMarkupRanges)
+    return
+  }
+  if (evaluateOnly) {
+    return true
+  }
+
+  const accumulatedLeftMarkup = getTextFromRanges(codeMirror, markupRangesToMoveAfterSelection)
+  const accumulatedRightMarkup = getTextFromRanges(codeMirror, markupRangesToMoveBeforeSelection)
+
+  // Relocate any markup that needs to be moved after selection
+  codeMirror.replaceRange(accumulatedLeftMarkup, selectionRange.endLocation, null, '+')
+
+  // Remove any markup that needs to be blasted
+  const allMarkupRangesToRemove = markupRangesToMoveBeforeSelection.concat(markupRangesToMoveAfterSelection).concat(markupRangesToRemove)
+  removeTextFromRanges(codeMirror, allMarkupRangesToRemove)
+
+  // Relocate any markup that needs to be moved before selection
+  codeMirror.replaceRange(accumulatedRightMarkup, selectionRange.startLocation, null, '+')
+
+  // Adjust selection to account for adjustments made above.
+  codeMirror.setSelection(
+    selectionRange.startLocation.withOffset(0, accumulatedRightMarkup.length), 
+    selectionRange.endLocation.withOffset(0, -getTextFromRanges(codeMirror, markupRangesToMoveAfterSelection.concat(markupRangesToRemove)).length)
+  )
+}
+
+// Need to remove in reverse order of appearance to avoid invalidating yet-to-be-removed ranges.
+const removeTextFromRanges = (codeMirror, ranges) => {
+  const reverseSortedRanges = Array.from(ranges).sort((a, b) => {
+    return a.startLocation.lessThan(b.startLocation)
+  })
+  reverseSortedRanges.forEach(range => codeMirror.replaceRange('', range.startLocation, range.endLocation, '+'))
+}
+
+const getTextFromRanges = (codeMirror, ranges) => ranges.map(range => codeMirror.getRange(range.startLocation, range.endLocation)).join('')
+
+const getExpandedSelectionRange = (codeMirror, markupItems, selectionRange) => {
+  let newSelectionRange = selectionRange
+  
+  // If selectionRange starts inside a markup item's opening markup, the returned range start will be moved to start of opening markup.
+  const selectionStartsInOpeningMarkupOfItem = markupItems.find(item => selectionRange.startsInsideRange(item.openingMarkupRange(), true))
+  if (selectionStartsInOpeningMarkupOfItem) {
+    newSelectionRange.startLocation = selectionStartsInOpeningMarkupOfItem.openingMarkupRange().startLocation
+  } else {
+    // If selectionRange starts inside a markup item's closing markup, the returned range start will be moved to end of closing markup.
+    const selectionStartsInClosingMarkupOfItem = markupItems.find(item => selectionRange.startsInsideRange(item.closingMarkupRange(), true))
+    if (selectionStartsInClosingMarkupOfItem) {
+      newSelectionRange.startLocation = selectionStartsInClosingMarkupOfItem.closingMarkupRange().endLocation
+    }
+  }
+
+  // If selectionRange ends inside a markup item's closing markup, the returned range end will be moved to end of closing markup.
+  const selectionEndsInClosingMarkupOfItem = markupItems.find(item => selectionRange.endsInsideRange(item.closingMarkupRange(), true))
+  if (selectionEndsInClosingMarkupOfItem) {
+    newSelectionRange.endLocation = selectionEndsInClosingMarkupOfItem.closingMarkupRange().endLocation
+  } else {
+    // If selectionRange ends inside a markup item's opening markup, the returned range end will be moved to start of opening markup.
+    const selectionEndsInOpeningMarkupOfItem = markupItems.find(item => selectionRange.endsInsideRange(item.openingMarkupRange(), true))
+    if (selectionEndsInOpeningMarkupOfItem) {
+      newSelectionRange.endLocation = selectionEndsInOpeningMarkupOfItem.openingMarkupRange().startLocation
+    }
+  }
+  
+  return newSelectionRange
+}
+
+exports.relocateOrRemoveExistingMarkupForSelectionRange = relocateOrRemoveExistingMarkupForSelectionRange
+exports.canClearFormatting = canClearFormatting
--- www/codemirror/codemirror-range-main.js
+++ www/codemirror/codemirror-range-main.js
@@ -4,5 +4,6 @@ RangeHelper.rangeDebugging = require('./codemirror-range-debugging')
 RangeHelper.rangeDetermination = require('./codemirror-range-determination')
 RangeHelper.rangeObjects = require('./codemirror-range-objects')
 RangeHelper.rangeUtilities = require('./codemirror-range-utilities')
+RangeHelper.rangeClearFormatting = require('./codemirror-range-clear-formatting')
 
 window.RangeHelper = RangeHelper
