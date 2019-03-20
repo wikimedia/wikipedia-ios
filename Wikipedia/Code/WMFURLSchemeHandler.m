40f8e70c85366153638f8da129614e70ae888ea6
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-20 08:36:00 -0400

    Revert "check if task active before finishing"
    This reverts commit 2358a14dc0392b2d82d26adb57b1310888aafc47.

--- Wikipedia/Code/WMFURLSchemeHandler.m
+++ Wikipedia/Code/WMFURLSchemeHandler.m
@@ -63,6 +63,9 @@ static const NSInteger WMFCachedResponseCountLimit = 6;
     if (![self isTaskActive:task]) {
         return;
     }
+    dispatch_barrier_async(self.activeTaskQueue, ^{
+        [self.activeTasks removeObject:task];
+    });
     if (error) {
         [task didFailWithError:error];
     } else if (response) {
@@ -70,15 +73,10 @@ static const NSInteger WMFCachedResponseCountLimit = 6;
         if (data) {
             [task didReceiveData:data];
         }
-        if ([self isTaskActive:task]) {
-            [task didFinish];
-        }
+        [task didFinish];
     } else {
         [task didFailWithError:[WMFFetcher unexpectedResponseError]];
     }
-    dispatch_barrier_async(self.activeTaskQueue, ^{
-        [self.activeTasks removeObject:task];
-    });
 }
 
 - (void)finishTask:(id<WKURLSchemeTask>)task withProxiedResponse:(NSURLResponse *)proxiedResponse data:(nullable NSData *)data error:(NSError *)error {
