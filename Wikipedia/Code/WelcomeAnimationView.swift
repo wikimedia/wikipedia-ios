69eaa09f7fab7b99bec01113c34ead8282a7dec8
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-28 18:01:02 -0400

    expand WelcomeAnimationView

--- Wikipedia/Code/WelcomeAnimationView.swift
+++ Wikipedia/Code/WelcomeAnimationView.swift
@@ -1,68 +1,121 @@
 import UIKit
+import AVFoundation
+
+typealias WelcomeAnimatedImageView = WelcomeAnimationView.AnimatedImageView
 
 final class WelcomeAnimationView: UIView {
-    private var imageModels: [ImageModel]?
+    private let sizeReference: CGSize
+    private var animatedImageViews: [AnimatedImageView]?
+    private var propertyAnimator: UIViewPropertyAnimator?
 
-    struct ImageModel {
-        let name: String
-        let unitSize: CGSize
-        let unitDestination: CGPoint
-        let delay: Double
-        let duration: Double
-        let initialOpacity: Float
-    }
+    final class AnimatedImageView: UIImageView {
+        let origin: CGPoint
+        let destination: CGPoint?
+        var normalizedDestination: CGPoint?
+        let insertBelow: Bool
+        let sizeReference: CGSize
 
-    struct ImageViewAndModel {
-        let imageView: UIImageView
-        let model: ImageModel
-    }
-
-
-    private lazy var imageViewsAndModels: [ImageViewAndModel]? = {
-        guard let imageModels = imageModels else {
-            return nil
+        init(imageName: String, contentMode: UIView.ContentMode = .scaleAspectFit, origin: CGPoint = .zero, destination: CGPoint? = nil, insertBelow: Bool = true, initialAlpha: CGFloat = 0) {
+            self.origin = origin
+            self.destination = destination
+            self.insertBelow = insertBelow
+            let image = UIImage(named: imageName)!
+            sizeReference = image.size
+            super.init(image: image)
+            self.contentMode = contentMode
+            self.alpha = initialAlpha
         }
-        return imageModels.map { imageModel in
-            let imageView = UIImageView()
-            imageView.image = UIImage(named: imageModel.name)
-            imageView.contentMode = UIView.ContentMode.scaleAspectFit
-            imageView.layer.opacity = imageModel.initialOpacity
-            return ImageViewAndModel(imageView: imageView, model: imageModel)
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
         }
-    }()
-
-    open func animate() {
-
     }
 
-    init(staticImage: UIImage) {
+    func normalizedPoint(_ point: CGPoint, from aspectRatio: CGSize, to newRect: CGRect) -> CGPoint {
+        let rect = AVMakeRect(aspectRatio: aspectRatio, insideRect: newRect)
+        let scaledX = rect.width * (point.x / aspectRatio.width)
+        let scaledY = rect.height * (point.y / aspectRatio.height)
+        return CGPoint(x: rect.origin.x + scaledX, y: rect.origin.y + scaledY)
+    }
+
+    private var finishedAnimating = false
+
+    func animate() {
+        propertyAnimator?.startAnimation()
+        propertyAnimator?.addCompletion { position in
+            self.finishedAnimating = true
+        }
+    }
+
+    init(staticImageNamed staticImageName: String, contentMode: UIView.ContentMode = .scaleAspectFit, animatedImageViews: [AnimatedImageView]? = nil, propertyAnimator: UIViewPropertyAnimator? = nil) {
+        let staticImage = UIImage(named: staticImageName)!
+        sizeReference = staticImage.size
+        let staticImageView = UIImageView(image: staticImage)
+        self.animatedImageViews = animatedImageViews
+        self.propertyAnimator = propertyAnimator
         super.init(frame: .zero)
-        let imageView = UIImageView(image: staticImage)
-        imageView.contentMode = .scaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        wmf_addSubviewWithConstraintsToEdges(imageView)
+        staticImageView.contentMode = contentMode
+        wmf_addSubviewWithConstraintsToEdges(staticImageView)
+        animatedImageViews?.forEach { imageView in
+            if imageView.insertBelow {
+                let subview = subviews.last ?? staticImageView
+                insertSubview(imageView, belowSubview: subview)
+            } else {
+                addSubview(imageView)
+            }
+        }
     }
 
-    init(imageModels: [ImageModel]) {
-        self.imageModels = imageModels
-        super.init(frame: .zero)
+    override var bounds: CGRect {
+        didSet {
+            guard oldValue != bounds else {
+                return
+            }
+            updateLayout()
+        }
     }
 
-    private func addAnimationElementsScaledToCurrentFrameSize() {
-        guard let imageViewsAndModels = imageViewsAndModels else {
+    private func updateLayout() {
+        guard let animatedImageViews = animatedImageViews else {
             return
         }
-        imageViewsAndModels.forEach { imageViewAndModel in
-            addSubview(imageViewAndModel.imageView)
-            // Start all images in the center. imageModel unitDestination assumes center origin.
-            imageViewAndModel.imageView.center = CGPoint(x: bounds.midX, y: bounds.midY)
-            // Denormalize unitSize using a scalar (NOT a CGSize - so aspect ratio remains constant)
-            imageViewAndModel.imageView.frame.size = imageViewAndModel.model.unitSize.wmf_denormalizeUsingReference(frame.size.width)
+        for imageView in animatedImageViews {
+            imageView.frame.size = size(for: imageView)
+
+            let normalizedZero = normalizedPoint(.zero, from: imageView.sizeReference, to: imageView.bounds)
+            let normalizedOrigin = normalizedPoint(imageView.origin, from: sizeReference, to: bounds)
+
+            if finishedAnimating, let destination = imageView.destination {
+                let normalizedDestination = normalizedPoint(destination, from: sizeReference, to: bounds)
+                imageView.frame.origin = CGPoint(x: normalizedDestination.x - normalizedZero.x, y: normalizedDestination.y - normalizedZero.y)
+            } else {
+                imageView.frame.origin = CGPoint(x: normalizedOrigin.x - normalizedZero.x, y: normalizedOrigin.y - normalizedZero.y)
+            }
+
+            if let destination = imageView.destination {
+                let normalizedDestination = normalizedPoint(destination, from: sizeReference, to: bounds)
+                imageView.normalizedDestination = CGPoint(x: normalizedDestination.x - normalizedZero.x, y: normalizedDestination.y - normalizedZero.y)
+            }
         }
-        /*
-         isUserInteractionEnabled = true
-         addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGestureRecognizer(_:))))
-         */
+    }
+
+    private func size(for imageView: AnimatedImageView) -> CGSize {
+        let sizeThatFits = imageView.sizeThatFits(frame.size)
+
+        let widthRatio = sizeThatFits.width / sizeReference.width
+        let heightRatio = sizeThatFits.height / sizeReference.height
+        let maxRatio = max(widthRatio, heightRatio)
+
+        let width: CGFloat
+        let height: CGFloat
+        if sizeThatFits.width == sizeThatFits.height {
+            width = bounds.width * maxRatio
+            height = bounds.height * maxRatio
+        } else {
+            width = bounds.width * widthRatio
+            height = bounds.height * heightRatio
+        }
+        return CGSize(width: width, height: height)
     }
 
     required init?(coder aDecoder: NSCoder) {
