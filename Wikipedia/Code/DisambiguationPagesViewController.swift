b357b3fd3e0bb89664353964f2fd0a232ea4c373
Joe Walsh <joewalsh@users.noreply.github.com>
2019-03-29 18:05:31 -0400

    remove unused and unecessary code

--- Wikipedia/Code/DisambiguationPagesViewController.swift
+++ Wikipedia/Code/DisambiguationPagesViewController.swift
@@ -15,8 +15,13 @@ class DisambiguationPagesViewController: ArticleFetchedResultsViewController {
         super.init()
         self.dataStore = dataStore
         self.theme = theme
+        self.title = WMFLocalizedString("page-similar-titles", value: "Similar pages", comment: "Label for button that shows a list of similar titles (disambiguation) for the current page")
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func setupFetchedResultsController(with dataStore: MWKDataStore) {
         let request = WMFArticle.fetchRequest()
         request.predicate = NSPredicate(format: "key IN %@", articleURLs.compactMap { $0.wmf_articleDatabaseKey })
@@ -24,25 +29,11 @@ class DisambiguationPagesViewController: ArticleFetchedResultsViewController {
         fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: dataStore.viewContext, sectionNameKeyPath: nil, cacheName: nil)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) is not allowed")
-    }
-    
-    required init(articleURLs: [URL], dataStore: MWKDataStore, contentGroup: WMFContentGroup?, theme: Theme) {
-        fatalError("init(articleURLs:dataStore:contentGroup:theme:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = WMFLocalizedString("page-similar-titles", value: "Similar pages", comment: "Label for button that shows a list of similar titles (disambiguation) for the current page")
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetch()
     }
     
-    var didFetch = false
     func fetch() {
         self.dataStore.articleSummaryController.updateOrCreateArticleSummariesForArticles(withURLs: articleURLs) { (_) in // don't care, FRC will update
             
