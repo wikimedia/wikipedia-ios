189e30c4a1248490aaffa6b44626008632f2d576
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-09 17:19:10 -0400

    add kids

--- Wikipedia/Code/VerticallySplitViewController.swift
+++ Wikipedia/Code/VerticallySplitViewController.swift
@@ -4,15 +4,22 @@ class VerticallySplitViewController: UIViewController {
     @IBOutlet private weak var topView: UIView!
     @IBOutlet private weak var bottomView: UIView!
 
-    private let topViewController: UIViewController
-    private let bottomViewController: UIViewController
+    private let topViewController: UIViewController & Themeable
+    private let bottomViewController: UIViewController & Themeable
 
     private var theme = Theme.standard
 
-    init(topViewController: UIViewController, bottomViewController: UIViewController) {
+    init(topViewController: UIViewController & Themeable, bottomViewController: UIViewController & Themeable) {
         self.topViewController = topViewController
         self.bottomViewController = bottomViewController
         super.init(nibName: "VerticallySplitViewController", bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wmf_add(childController: topViewController, andConstrainToEdgesOfContainerView: topView)
+        wmf_add(childController: bottomViewController, andConstrainToEdgesOfContainerView: bottomView)
+        apply(theme: theme)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -26,5 +33,8 @@ extension VerticallySplitViewController: Themeable {
             self.theme = theme
             return
         }
+        topViewController.apply(theme: theme)
+        bottomViewController.apply(theme: theme)
+        view.backgroundColor = bottomViewController.view.backgroundColor
     }
 }
