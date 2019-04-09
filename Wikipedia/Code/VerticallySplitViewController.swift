e6b48f75ea7bceeccab05501f8f7c7c8af00c5d5
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-09 17:02:32 -0400

    make VerticallySplitViewController Themeable

--- Wikipedia/Code/VerticallySplitViewController.swift
+++ Wikipedia/Code/VerticallySplitViewController.swift
@@ -7,6 +7,8 @@ class VerticallySplitViewController: UIViewController {
     private let topViewController: UIViewController
     private let bottomViewController: UIViewController
 
+    private var theme = Theme.standard
+
     init(topViewController: UIViewController, bottomViewController: UIViewController) {
         self.topViewController = topViewController
         self.bottomViewController = bottomViewController
@@ -15,5 +17,14 @@ class VerticallySplitViewController: UIViewController {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension VerticallySplitViewController: Themeable {
+    func apply(theme: Theme) {
+        guard viewIfLoaded != nil else {
+            self.theme = theme
+            return
+        }
     }
 }
