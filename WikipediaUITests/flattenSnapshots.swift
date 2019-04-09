ba6a9871a71960a6d31141d7f19fc33f820c33c6
montehurd <mhurd@wikimedia.org>
2019-04-09 13:00:07 -0700

    clear out previous "flattened" snapshots

--- WikipediaUITests/flattenSnapshots.swift
+++ WikipediaUITests/flattenSnapshots.swift
@@ -18,7 +18,7 @@ extension FileManager {
     }
 
 	func listFiles(path: String, withSuffix suffix: String) -> [URL] {
-		return listFiles(path: currentDirectoryPath).filter({$0.absoluteString.hasSuffix(suffix)})
+		return listFiles(path: path).filter({$0.absoluteString.hasSuffix(suffix)})
 	}
 }
 
@@ -32,15 +32,31 @@ extension String {
     }
 }
 
+let snapshotsFlattenedPathString = "\(FileManager.default.currentDirectoryPath)/SnapshotsFlattened/"
+
+let removeFile = { (file: URL) in
+    do {
+        print(file)
+        try FileManager.default.removeItem(at: file)
+    }
+    catch let error as NSError {
+        print("Something went wrong: \(error)")
+    }
+}
+
+// Clear out pngs from "SnapshotsFlattened"
+let filesToRemove = FileManager.default.listFiles(path: snapshotsFlattenedPathString, withSuffix: ".png")
+filesToRemove.forEach(removeFile)
+
 let snapshotsPathString = NSURL.fileURL(withPath: "\(FileManager.default.currentDirectoryPath)/Snapshots").absoluteString
 
-// Move the file from its subdir to the "allImagesFlattened" dir in same dir as this script.
+// Copy the file from its subdir to the "SnapshotsFlattened" dir in same dir as this script.
 // Keeps subdir(s) as part of the file name - slashes and spaces are converted to underscores.
 let renameAndMoveFileToDirOfThisScript = { (file: URL) in
 	let filePathWithoutSnapshotsPathStringPrefix = file.absoluteString.deletingPrefix(snapshotsPathString)
 	let newFileNameWithoutSpacesOrSlashes = filePathWithoutSnapshotsPathStringPrefix.replacingOccurrences(of: "(%20)|[\\s/]+", with: "_", options: [.regularExpression])
 	do {
-	    try FileManager.default.copyItem(at: file, to: NSURL.fileURL(withPath: "\(FileManager.default.currentDirectoryPath)/SnapshotsFlattened/\(newFileNameWithoutSpacesOrSlashes)"))
+        try FileManager.default.copyItem(at: file, to: NSURL.fileURL(withPath: "\(snapshotsFlattenedPathString)/\(newFileNameWithoutSpacesOrSlashes)"))
 	}
 	catch let error as NSError {
 	    print("Something went wrong: \(error)")
