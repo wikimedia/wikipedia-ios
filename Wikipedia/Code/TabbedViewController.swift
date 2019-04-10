c9a96215f9a5061c9ef49097148e7afe49903923
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-10 13:53:56 -0400

    make TabbedViewController inherit from ViewController

--- Wikipedia/Code/TabbedViewController.swift
+++ Wikipedia/Code/TabbedViewController.swift
@@ -9,7 +9,46 @@ extension TabbedViewController: Themeable {
         self.theme = theme
         guard viewIfLoaded != nil else {
             return
+final class TabbedViewController: ViewController {
+    private let viewControllers: [UIViewController & Themeable]
+
+    private lazy var tabsView: TabsView = {
+        var underlineButtons = [UnderlineButton]()
+        for (index, viewController) in viewControllers.enumerated() {
+            let underlineButton = UnderlineButton()
+            underlineButton.setTitle(viewController.title, for: .normal)
+            underlineButton.sizeToFit()
+            underlineButton.tag = index
+            underlineButton.addTarget(self, action: #selector(didSelectViewController(_:)), for: .touchUpInside)
+            underlineButtons.append(underlineButton)
         }
+        return TabsView(buttons: underlineButtons)
+    }()
+
+    init(viewControllers: [UIViewController & Themeable]) {
+        self.viewControllers = viewControllers
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Hello"
+        navigationBar.addUnderNavigationBarView(tabsView)
+    }
+
+    @objc private func didSelectViewController(_ sender: UIButton) {
+        print("")
+    }
+
+    // MARK: Themeable
+
+    override func apply(theme: Theme) {
+        super.apply(theme: theme)
         view.backgroundColor = theme.colors.paperBackground
+        tabsView.apply(theme: theme)
     }
 }
