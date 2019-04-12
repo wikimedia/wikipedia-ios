acb51c6f259a22e1198c4fd5a90b9786f02cf2e8
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-12 13:59:16 -0400

    use flow layout

--- Wikipedia/Code/InsertMediaSearchResultsCollectionViewController.swift
+++ Wikipedia/Code/InsertMediaSearchResultsCollectionViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 
-class InsertMediaSearchResultsCollectionViewController: ColumnarCollectionViewController {
+class InsertMediaSearchResultsCollectionViewController: UICollectionViewController {
+    private let flowLayout: UICollectionViewFlowLayout
+    private var theme = Theme.standard
+    private let itemDimension: CGFloat = 100
+
     var results = [MWKSearchResult]() {
         didSet {
             assert(Thread.isMainThread)
@@ -8,10 +12,12 @@ class InsertMediaSearchResultsCollectionViewController: ColumnarCollectionViewCo
         }
     }
 
-    override init() {
-        super.init()
+    init() {
+        flowLayout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: flowLayout)
         title = CommonStrings.searchTitle
     }
+
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -19,26 +25,32 @@ class InsertMediaSearchResultsCollectionViewController: ColumnarCollectionViewCo
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        layoutManager.register(InsertMediaSearchResultCollectionViewCell.self, forCellWithReuseIdentifier: InsertMediaSearchResultCollectionViewCell.identifier, addPlaceholder: true)
-        
+        collectionView.register(InsertMediaSearchResultCollectionViewCell.self, forCellWithReuseIdentifier: InsertMediaSearchResultCollectionViewCell.identifier)
+        flowLayout.itemSize = CGSize(width: itemDimension, height: itemDimension)
+        flowLayout.minimumInteritemSpacing = 8
+        flowLayout.minimumLineSpacing = 8
+        // TODO: Make this VC a ViewController so that it can work with the nav bar
+        collectionView.contentInset = UIEdgeInsets(top: 120, left: 12, bottom: 0, right: 24)
     }
 
     func reload() {
         collectionView.reloadData()
-        updateEmptyState()
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return results.count
-    }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        // TODO: updateEmptyState()
     }
 
     private func configure(_ cell: InsertMediaSearchResultCollectionViewCell, at indexPath: IndexPath) {
         let result = results[indexPath.item]
-        cell.imageURL = result.thumbnailURL
+        cell.configure(imageURL: result.thumbnailURL, imageViewDimension: itemDimension, title: result.displayTitle)
+    }
+}
+
+extension InsertMediaSearchResultsCollectionViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -49,10 +61,18 @@ class InsertMediaSearchResultsCollectionViewController: ColumnarCollectionViewCo
         configure(searchResultCell, at: indexPath)
         return searchResultCell
     }
-
-
 }
 
 extension InsertMediaSearchResultsCollectionViewController: UISearchBarDelegate {
     
+}
+
+extension InsertMediaSearchResultsCollectionViewController: Themeable {
+    func apply(theme: Theme) {
+        self.theme = theme
+        guard viewIfLoaded != nil else {
+            return
+        }
+        
+    }
 }
