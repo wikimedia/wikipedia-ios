52acbb615fe55a2836e2f3dd90a8b8411828d9a9
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-14 12:45:46 -0400

    update cell

--- Wikipedia/Code/InsertMediaSearchResultCollectionViewCell.swift
+++ Wikipedia/Code/InsertMediaSearchResultCollectionViewCell.swift
@@ -2,10 +2,8 @@ import UIKit
 
 class InsertMediaSearchResultCollectionViewCell: CollectionViewCell {
     private let imageView = UIImageView()
-    private let titleLabel = UILabel()
-    private var spacing: CGFloat = 8
+    private let captionLabel = UILabel()
 
-    private var title: String?
     private var imageURL: URL?
     private var imageViewDimension: CGFloat = 0
 
@@ -13,26 +11,26 @@ class InsertMediaSearchResultCollectionViewCell: CollectionViewCell {
         super.setup()
         imageView.accessibilityIgnoresInvertColors = true
         imageView.contentMode = .scaleAspectFit
-        addSubview(imageView)
-        titleLabel.numberOfLines = 1
-        addSubview(titleLabel)
+        contentView.addSubview(imageView)
+        captionLabel.numberOfLines = 1
+        contentView.addSubview(captionLabel)
     }
 
     override func updateFonts(with traitCollection: UITraitCollection) {
         super.updateFonts(with: traitCollection)
-        titleLabel.font = UIFont.wmf_font(.subheadline, compatibleWithTraitCollection: traitCollection)
+        captionLabel.font = UIFont.wmf_font(.subheadline, compatibleWithTraitCollection: traitCollection)
     }
 
     override func reset() {
         super.reset()
         imageView.wmf_reset()
-        titleLabel.text = nil
+        captionLabel.text = nil
     }
 
     func configure(imageURL: URL?, imageViewDimension: CGFloat, title: String?) {
         self.imageURL = imageURL
         self.imageViewDimension = imageViewDimension
-        self.title = title
+        captionLabel.text = title
         setNeedsLayout()
     }
 
@@ -42,39 +40,42 @@ class InsertMediaSearchResultCollectionViewCell: CollectionViewCell {
         }
 
         let size = super.sizeThatFits(size, apply: apply)
-        let minHeight = imageViewDimension + layoutMargins.top + layoutMargins.bottom
-        let widthMinusMargins = layoutWidth(for: size)
 
         let isRTL = traitCollection.layoutDirection == .rightToLeft
         let x: CGFloat
         if isRTL {
-            x = size.width - widthMinusMargins
+            x = size.width
         } else {
-            x = layoutMargins.left
+            x = 0
         }
 
-        var origin = CGPoint(x: x, y: layoutMargins.top)
-        let height = max(origin.y, minHeight)
+        var origin = CGPoint(x: x, y: 0)
+        let height = max(origin.y, imageViewDimension)
 
         if (apply) {
-            let imageViewX = layoutMargins.left
-            let imageViewY = layoutMargins.top
-            imageView.frame = CGRect(x: imageViewX, y: imageViewY, width: imageViewDimension, height: imageViewDimension)
+            imageView.frame = CGRect(x: origin.x, y: origin.y, width: imageViewDimension, height: imageViewDimension)
             imageView.wmf_setImage(with: imageURL, detectFaces: true, onGPU: true, failure: { error in
                 self.imageView.image = UIImage(named: "media-wizard/placeholder")
             }, success: {})
-            origin.y += imageView.frame.layoutHeight(with: spacing)
+            origin.y += imageView.frame.height
         }
 
         let semanticContentAttribute: UISemanticContentAttribute = isRTL ? .forceRightToLeft : .forceLeftToRight
 
-        if titleLabel.wmf_hasAnyNonWhitespaceText {
-            let titleLabelFrame = titleLabel.wmf_preferredFrame(at: origin, maximumWidth: imageViewDimension, alignedBy: semanticContentAttribute, apply: apply)
-            origin.y += titleLabelFrame.height
+        if captionLabel.wmf_hasAnyNonWhitespaceText {
+            let captionLabelFrame = captionLabel.wmf_preferredFrame(at: origin, maximumWidth: imageViewDimension, alignedBy: semanticContentAttribute, apply: apply)
+            origin.y += captionLabelFrame.height
         }
 
-        titleLabel.isHidden = !titleLabel.wmf_hasAnyNonWhitespaceText
+        captionLabel.isHidden = !captionLabel.wmf_hasAnyNonWhitespaceText
 
         return CGSize(width: size.width, height: height)
+    }
+}
+
+extension InsertMediaSearchResultCollectionViewCell: Themeable {
+    func apply(theme: Theme) {
+        captionLabel.textColor = theme.colors.primaryText
+        labelBackgroundColor = theme.colors.paperBackground
     }
 }
