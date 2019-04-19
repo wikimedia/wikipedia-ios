8a91c4e2fe91bf300c3c7e828afe76c938435fe9
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-18 21:18:36 -0400

    set action

--- Wikipedia/Code/InsertMediaSelectedImageView.swift
+++ Wikipedia/Code/InsertMediaSelectedImageView.swift
@@ -70,6 +70,7 @@ class InsertMediaSelectedImageView: SetupView {
             self.searchResult = searchResult
             self.imageView.backgroundColor = self.backgroundColor
             self.imageInfoView.configure(with: searchResult, showImageDescription: false, showLicenseName: true, showMoreInformationButtonTitle: false, theme: theme)
+            self.imageInfoView.moreInformationAction = self.moreInformationAction
             self.adjustImageInfoContainerViewBottomConstraint()
             completion(nil)
         }
