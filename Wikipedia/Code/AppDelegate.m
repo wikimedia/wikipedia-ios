1510fc786c572c48f31120fbdcc1f0166725efd5
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-23 09:32:40 -0400

    handle nil date

--- Wikipedia/Code/AppDelegate.m
+++ Wikipedia/Code/AppDelegate.m
@@ -118,6 +118,9 @@ static NSTimeInterval const WMFBackgroundFetchInterval = 10800; // 3 Hours
 
 - (BOOL)shouldShowLastReadArticleOnResume {
     NSDate *resignActiveDate = [[NSUserDefaults wmf] wmf_appResignActiveDate];
+    if (!resignActiveDate) {
+        return NO;
+    }
     NSDate *becomeActiveDate = [[NSUserDefaults wmf] wmf_appBecomeActiveDate];
     NSDate *cutoffDate = [[NSCalendar wmf_utcGregorianCalendar] nextDateAfterDate:resignActiveDate matchingHour:5 minute:0 second:0 options:NSCalendarMatchStrictly];
     BOOL isBeforeCutoffDate = [becomeActiveDate compare:cutoffDate] == NSOrderedAscending;
