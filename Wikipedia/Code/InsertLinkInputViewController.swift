b0827a8fc1bd8d5edf0db2d548ee951d593ee0b6
Natalia Harateh <nambatee@users.noreply.github.com>
2019-05-08 11:56:17 -0400

    add InsertLinkInputViewController

---
+++ Wikipedia/Code/InsertLinkInputViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+fileprivate class View: UIView {
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: bounds.width, height: 400)
+    }
+}
+
+class InsertLinkInputViewController: UIInputViewController {
+    typealias Link = SectionEditorWebViewMessagingController.Link
+    var link: Link?
+
+    private lazy var searchViewController: SearchViewController = {
+        let searchViewController = SearchViewController()
+        searchViewController.areRecentSearchesEnabled = false
+        searchViewController.shouldBecomeFirstResponder = true
+        #warning("Inject dataStore")
+        searchViewController.dataStore = SessionSingleton.sharedInstance()?.dataStore
+        return searchViewController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let navigationController = UINavigationController(rootViewController: searchViewController)
+        navigationController.isNavigationBarHidden = true
+        wmf_add(childController: navigationController, andConstrainToEdgesOfContainerView: view)
+    }
+
+    override func loadView() {
+        let view = View()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.view = view
+    }
+}
