6438c00917c14d9f8d38845146c59f39dbbfafda
Natalia Harateh <nambatee@users.noreply.github.com>
2019-05-08 15:07:52 -0400

    add InputVC superclass

---
+++ Wikipedia/Code/SectionEditorInputViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SectionEditorInputViewController: UIInputViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
