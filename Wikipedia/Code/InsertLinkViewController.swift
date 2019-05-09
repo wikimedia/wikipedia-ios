ebec5e63fa4d66394d0f83490ceff12901b3baa6
Natalia Harateh <nambatee@users.noreply.github.com>
2019-05-09 16:36:05 -0400

    rename

---
+++ Wikipedia/Code/InsertLinkViewController.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+fileprivate class View: UIView {
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: bounds.width, height: 400)
+    }
+}
+
+class InsertLinkViewController: UIViewController {
+    private var theme = Theme.standard
+
+    typealias Link = SectionEditorWebViewMessagingController.Link
+    var link: Link? {
+        didSet {
+            guard let link = link else {
+                return
+            }
+            searchViewController.searchTerm = link.page
+            searchViewController.search()
+        }
+    }
+
+    // use a delegate if there's more than 1 method
+    var insertLink: ((String, String?) -> Void)?
+
+    private lazy var searchViewController: SearchViewController = {
+        let searchViewController = SearchViewController()
+        searchViewController.areRecentSearchesEnabled = false
+        searchViewController.shouldBecomeFirstResponder = true
+        #warning("Inject dataStore")
+        searchViewController.dataStore = SessionSingleton.sharedInstance()?.dataStore
+        searchViewController.shouldShowCancelButton = false
+        searchViewController.delegate = self
+        searchViewController.delegatesSelection = true
+        searchViewController.showLanguageBar = false
+        return searchViewController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let navigationController = UINavigationController(rootViewController: searchViewController)
+        navigationController.isNavigationBarHidden = true
+        wmf_add(childController: navigationController, andConstrainToEdgesOfContainerView: view)
+        addTopShadow()
+        apply(theme: theme)
+    }
+
+    private func addTopShadow() {
+        view.layer.shadowOffset = CGSize(width: 0, height: -2)
+        view.layer.shadowRadius = 10
+        view.layer.shadowOpacity = 1.0
+    }
+
+    override func loadView() {
+        let view = View()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.view = view
+    }
+}
+
+extension InsertLinkViewController: ArticleCollectionViewControllerDelegate {
+    func articleCollectionViewController(_ articleCollectionViewController: ArticleCollectionViewController, didSelectArticleWith articleURL: URL, at indexPath: IndexPath) {
+        guard let title = articleURL.wmf_title else {
+            return
+        }
+        insertLink?(title, nil)
+    }
+}
+
+extension InsertLinkViewController: Themeable {
+    func apply(theme: Theme) {
+        self.theme = theme
+        guard viewIfLoaded != nil else {
+            return
+        }
+        view.backgroundColor = theme.colors.inputAccessoryBackground
+        view.layer.shadowColor = theme.colors.shadow.cgColor
+    }
+}
