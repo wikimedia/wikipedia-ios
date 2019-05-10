0ef862a29810b8ee444e5e9ba4914ea043e7f4aa
Toni Sevener <tsevener@wikimedia.org>
2019-05-10 16:41:59 -0500

    talk page - temporarily forcing "test" language

--- Wikipedia/Code/ArticleCollectionViewController.swift
+++ Wikipedia/Code/ArticleCollectionViewController.swift
@@ -177,7 +177,7 @@ extension ArticleCollectionViewController {
         
         if let userTalkPageTitle = userTalkPageTitle(at: indexPath) {
             //todo: smart host & language code
-            let talkPageContainer = TalkPageContainerViewController(title: userTalkPageTitle, host: "en.wikipedia.org", languageCode: "en", titleIncludesPrefix: true, type: .user, dataStore: dataStore)
+            let talkPageContainer = TalkPageContainerViewController(title: userTalkPageTitle, host: "test.wikipedia.org", languageCode: "test", titleIncludesPrefix: true, type: .user, dataStore: dataStore)
             talkPageContainer.apply(theme: theme)
             wmf_push(talkPageContainer, animated: true)
             return
