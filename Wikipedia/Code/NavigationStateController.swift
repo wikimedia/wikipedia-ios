de00339390dbcb8e3c6cc5a02e60e0f0262b12ad
Natalia Harateh <nambatee@users.noreply.github.com>
2019-05-16 08:30:17 -0400

    add key

--- Wikipedia/Code/NavigationStateController.swift
+++ Wikipedia/Code/NavigationStateController.swift
@@ -1,5 +1,6 @@
 @objc(WMFNavigationStateController)
 final class NavigationStateController: NSObject {
+    private let key = "nav_state"
     private let dataStore: MWKDataStore
 
     init(dataStore: MWKDataStore) {
