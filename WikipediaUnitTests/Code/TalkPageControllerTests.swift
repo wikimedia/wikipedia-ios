059a99d013364320ea110d0a5e781b3d57819fef
Toni Sevener <tsevener@wikimedia.org>
2019-05-20 10:38:09 -0500

    fix tests for new wmflabs talk page endpoint

--- WikipediaUnitTests/Code/TalkPageControllerTests.swift
+++ WikipediaUnitTests/Code/TalkPageControllerTests.swift
@@ -12,7 +12,7 @@ fileprivate class MockTalkPageFetcher: TalkPageFetcher {
     override func fetchTalkPage(urlTitle: String, displayTitle: String, host: String, languageCode: String, revisionID: Int64, completion: @escaping (Result<NetworkTalkPage, Error>) -> Void) {
         
         fetchCalled = true
-        if let networkTalkPage = TalkPageTestHelpers.networkTalkPage(for: "https://\(MockTalkPageFetcher.domain)/api/rest_v1/page/talk/\(urlTitle)", revisionId: MockArticleRevisionFetcher.revisionId) {
+        if let networkTalkPage = TalkPageTestHelpers.networkTalkPage(for: "https://talk-pages.wmflabs.org/\(MockTalkPageFetcher.domain)/v1/page/talk/\(urlTitle)", revisionId: MockArticleRevisionFetcher.revisionId) {
             completion(.success(networkTalkPage))
         } else {
             XCTFail("Expected network talk page from helper")
