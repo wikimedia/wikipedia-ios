3561393d1684104d17b908ab8096fe7ea5a94992
Natalia Harateh <nambatee@users.noreply.github.com>
2019-05-23 10:56:09 -0400

    remove html from displayTitle before searching for media

--- Wikipedia/Code/SectionEditorViewController.swift
+++ Wikipedia/Code/SectionEditorViewController.swift
@@ -579,7 +579,7 @@ extension SectionEditorViewController: SectionEditorWebViewMessagingControllerSc
 
 extension SectionEditorViewController: SectionEditorInputViewsControllerDelegate {
     func sectionEditorInputViewsControllerDidTapMediaInsert(_ sectionEditorInputViewsController: SectionEditorInputViewsController) {
-        let insertMediaViewController = InsertMediaViewController(articleTitle: section?.article?.displaytitle, siteURL: section?.article?.url.wmf_site)
+        let insertMediaViewController = InsertMediaViewController(articleTitle: section?.article?.displaytitle?.wmf_stringByRemovingHTML(), siteURL: section?.article?.url.wmf_site)
         insertMediaViewController.delegate = self
         insertMediaViewController.apply(theme: theme)
         let navigationController = UINavigationController(rootViewController: insertMediaViewController)
