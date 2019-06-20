e65d841d21b174042c92a859e9b200986cc5a643
Natalia Harateh <nambatee@users.noreply.github.com>
2019-06-20 11:49:42 -0400

    update when view context saves

--- WMF Framework/WMFExploreFeedContentController.m
+++ WMF Framework/WMFExploreFeedContentController.m
@@ -72,7 +72,7 @@ NSString *const WMFNewExploreFeedPreferencesWereRejectedNotification = @"WMFNewE
 - (void)setDataStore:(MWKDataStore *)dataStore {
     _dataStore = dataStore;
     self.exploreFeedPreferencesUpdateCoordinator = [[ExploreFeedPreferencesUpdateCoordinator alloc] initWithFeedContentController:self];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateExploreFeedPreferencesFromDidSaveNotification:) name:WMFFeedImportContextDidSave object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateExploreFeedPreferencesFromDidSaveNotification:) name:WMFViewContextDidSave object:nil];
 }
 
 - (NSDictionary *)exploreFeedPreferences {
