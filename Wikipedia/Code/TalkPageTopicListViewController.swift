5128520b677966c5b2542a662bb614b3f336b302
Toni Sevener <tsevener@wikimedia.org>
2019-06-27 10:59:31 -0500

    talk page - workaround for inset bug when state is restored

--- Wikipedia/Code/TalkPageTopicListViewController.swift
+++ Wikipedia/Code/TalkPageTopicListViewController.swift
@@ -27,6 +27,8 @@ class TalkPageTopicListViewController: ColumnarCollectionViewController {
     private let talkPageSemanticContentAttribute: UISemanticContentAttribute
 
     private var completedActivityType: UIActivity.ActivityType?
+    
+    var fromNavigationStateRestoration: Bool = false
 
     required init(dataStore: MWKDataStore, talkPageTitle: String, talkPage: TalkPage, siteURL: URL, type: TalkPageType, talkPageSemanticContentAttribute: UISemanticContentAttribute) {
         self.dataStore = dataStore
@@ -57,6 +59,17 @@ class TalkPageTopicListViewController: ColumnarCollectionViewController {
         setupToolbar()
 
         NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if fromNavigationStateRestoration {
+            navigationBar.setNeedsLayout()
+            navigationBar.layoutIfNeeded()
+            updateScrollViewInsets()
+            fromNavigationStateRestoration = false
+        }
     }
 
     deinit {
