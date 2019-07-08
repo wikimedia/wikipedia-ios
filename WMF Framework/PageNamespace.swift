e9ca72c6700f7c3882ad7106335f4c43a12401e0
Natalia Harateh <nambatee@users.noreply.github.com>
2019-07-08 17:19:34 -0400

    add PageInfoFetcher

--- WMF Framework/PageNamespace.swift
+++ WMF Framework/PageNamespace.swift
@@ -1,5 +1,5 @@
 // https://en.wikipedia.org/wiki/Wikipedia:Namespace
-public enum PageNamespace: Int {
+@objc public enum PageNamespace: Int {
     case main
     case talk
     case user
@@ -20,13 +20,57 @@ extension PageNamespace {
 }
 
 extension WMFArticle {
-    var pageNamespace: PageNamespace? {
+    public var pageNamespace: PageNamespace? {
         return PageNamespace(namespaceValue: ns?.intValue)
     }
 }
 
 extension MWKSearchResult {
-    var pageNamespace: PageNamespace? {
+    public var pageNamespace: PageNamespace? {
         return PageNamespace(namespaceValue: titleNamespace?.intValue)
+    }
+}
+
+@objc(WMFPageInfoFetcher)
+public final class PageInfoFetcher: Fetcher {
+    private struct PageInfo: Decodable {
+        let query: Query?
+
+        struct Query: Decodable {
+            let pages: [Page]?
+
+            struct Page: Decodable {
+                let ns: Int?
+            }
+
+            enum CodingKeys: String, CodingKey {
+                case pages
+            }
+
+            init(from decoder: Decoder) throws {
+                let values = try decoder.container(keyedBy: CodingKeys.self)
+                let pages: Dictionary<String, Page> = try values.decode(Dictionary.self, forKey: .pages)
+                self.pages = Array(pages.values)
+            }
+        }
+    }
+
+    @objc public func fetchNamespaceValueForArticle(with url: URL, completion: @escaping (NSNumber?) -> Void) {
+        guard
+            let language = url.wmf_language,
+            let title = url.wmf_title,
+            let url = configuration.mediaWikiAPIURLForWikiLanguage(language, with: ["action": "query", "format": "json", "titles": title]).url
+        else {
+            completion(nil)
+            return
+        }
+
+        session.jsonDecodableTask(with: url) { (pageInfo: PageInfo?, response, error) in
+            if let ns = pageInfo?.query?.pages?.first?.ns {
+                completion(NSNumber(value: ns))
+            } else {
+                completion(nil)
+            }
+        }
     }
 }
