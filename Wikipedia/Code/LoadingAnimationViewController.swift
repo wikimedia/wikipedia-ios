d3469aa22733f61054540aac517a8b5e1e00093b
Toni Sevener <tsevener@wikimedia.org>
2019-09-04 13:15:37 -0500

    loading animation - accessibility tweaks

--- Wikipedia/Code/LoadingAnimationViewController.swift
+++ Wikipedia/Code/LoadingAnimationViewController.swift
@@ -21,29 +21,43 @@ class LoadingAnimationViewController: UIViewController {
         
         cancelButton.setTitle(CommonStrings.cancelActionTitle, for: .normal)
         loadingPageLabel.text = WMFLocalizedString("link-loading-title", value: "Loading page...", comment: "Title displayed in loading overlay after link is tapped.")
+        view.accessibilityViewIsModal = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         satelliteImageView.startRotating()
+        UIAccessibility.post(notification: UIAccessibility.Notification.screenChanged, argument: loadingPageLabel)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
         satelliteImageView.stopRotating()
+        UIAccessibility.post(notification: UIAccessibility.Notification.screenChanged, argument: nil)
     }
     
 
     @IBAction func tappedCancel(_ sender: UIButton) {
         cancelBlock?()
     }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        cancelButton.titleLabel?.font = UIFont.wmf_font(.semiboldTitle3, compatibleWithTraitCollection: traitCollection)
+        loadingPageLabel.font = UIFont.wmf_font(.boldHeadline, compatibleWithTraitCollection: traitCollection)
+    }
 }
 
 private extension UIView {
+    
+    var rotationKey: String {
+        return "rotation"
+    }
+    
     func startRotating(duration: Double = 1) {
-        let kAnimationKey = "rotation"
+        let kAnimationKey = rotationKey
         
         if layer.animation(forKey: kAnimationKey) == nil {
             let animate = CABasicAnimation(keyPath: "transform.rotation")
@@ -55,7 +69,7 @@ private extension UIView {
         }
     }
     func stopRotating() {
-        let kAnimationKey = "rotation"
+        let kAnimationKey = rotationKey
         
         if self.layer.animation(forKey: kAnimationKey) != nil {
             self.layer.removeAnimation(forKey: kAnimationKey)
