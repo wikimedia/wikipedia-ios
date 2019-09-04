87990454a1846e268e703df01ff6c416dc58c549
Joe Walsh <joewalsh@users.noreply.github.com>
2019-09-04 08:30:12 -0400

    dark launch screen

--- Wikipedia/Code/WMFThemeableNavigationController.m
+++ Wikipedia/Code/WMFThemeableNavigationController.m
@@ -59,9 +59,14 @@
         if (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad) {
             [_splashView setImage:[UIImage imageNamed:@"splashscreen-background"]];
         }
-        _splashView.backgroundColor = [UIColor whiteColor];
+        if (@available(iOS 13.0, *)) {
+            _splashView.backgroundColor = [UIColor systemBackgroundColor];
+        } else {
+            _splashView.backgroundColor = [UIColor whiteColor];
+
+        }
         [self.view wmf_addSubviewWithConstraintsToEdges:_splashView];
-        UIImage *wordmark = [UIImage imageNamed:@"wikipedia-wordmark"];
+        UIImage *wordmark = [UIImage imageNamed:@"splashscreen-wordmark"];
         UIImageView *wordmarkView = [[UIImageView alloc] initWithImage:wordmark];
         wordmarkView.translatesAutoresizingMaskIntoConstraints = NO;
         [_splashView addSubview:wordmarkView];
