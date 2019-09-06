93053ce9584c51f161653f950fcfc29eaa8c6ef4
Joe Walsh <joewalsh@users.noreply.github.com>
2019-09-06 12:43:26 -0400

    Revert "adjust search bar style"
    This reverts commit 2d8ecb50d4d11dc4b47f78957224bf5c08a74844.

--- WMF Framework/Theme.swift
+++ WMF Framework/Theme.swift
@@ -381,16 +381,7 @@ public class Theme: NSObject {
     }
     
     @objc public lazy var searchFieldBackgroundImage: UIImage? = {
-        let height: CGFloat
-        let cornerRadius: CGFloat
-        if #available(iOS 13, *) {
-            height = 30
-            cornerRadius = 7
-        } else {
-            height = 36
-            cornerRadius = 10
-        }
-        return Theme.roundedRectImage(with: colors.searchFieldBackground, cornerRadius: cornerRadius, height: height)
+        return Theme.roundedRectImage(with: colors.searchFieldBackground, cornerRadius: 10, height: 36)
     }()
 
     @objc public lazy var navigationBarTitleTextAttributes: [NSAttributedString.Key: Any] = {
