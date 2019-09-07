d9128bb3ceeda92011a96642189b20d8badb14f6
montehurd <mhurd@wikimedia.org>
2019-09-06 18:44:40 -0700

    fix issue when scrolling to some nested sections
    one more fix was needed for the repro steps found on:
    https://phabricator.wikimedia.org/T232141
    
    - scrolling code was acting on elements having fragment id, but 'current' section determination was looking at section wrapper elements. now scrolling code scrolls the wrapper, if found
    
    - logic determining how far to scroll and logic determining which section was 'current' needed slight tweaks - essentially a 1px error was possible which could cause previous TOC item to be selected even if you just tapped the item below it

--- www/js/elementLocation.js
+++ www/js/elementLocation.js
@@ -15,18 +15,33 @@ exports.getElementRect = element => {
   }
 }
 
-exports.getIndexOfFirstOnScreenElement = (elementPrefix, elementCount) => {
+exports.getIndexOfFirstOnScreenElement = (elementPrefix, elementCount, insetTop) => {
   for (let i = 0; i < elementCount; ++i) {
     const div = document.getElementById(elementPrefix + i)
     if (div === null) {
       continue
     }
     const rect = this.getElementRect(div)
-    if (rect.Y >= -1 || rect.Y + rect.Height >= 50) {
+    if (rect.Y > insetTop || rect.Y + rect.Height > insetTop) {
       return i
     }
   }
   return -1
+}
+
+exports.getElementToMakeFirstOnScreenElement = (fragment, parentElementPrefix) => {
+  var element = document.getElementById(fragment)
+  // needed so the same element which `getIndexOfFirstOnScreenElement` looks for is scrolled to top (if the case the fragment is for a TOC item)
+  if (element.parentElement.id && element.parentElement.id.startsWith(parentElementPrefix)) {
+    element = element.parentElement
+  }
+  return element
+}
+
+exports.makeElementFirstOnScreenElement = (element, insetTop) => {
+  element.scrollIntoView(true)
+  // `- 1` needed so when element is scrolled to the top it's far enough onscreen so `getIndexOfFirstOnScreenElement` will determine that it is currently the first onscreen element (if the fragment is for a TOC item)
+  window.scrollBy(0, -Math.max(0, insetTop - 1))
 }
 
 exports.getElementFromPoint = (x, y) => document.elementFromPoint(x - window.pageXOffset, y - window.pageYOffset)
