4c7b48b776d87bdc0b754cae55552ad04c4a33e9
Joe Walsh <joewalsh@users.noreply.github.com>
2019-09-12 11:14:38 -0400

    use built in scroll view animation and delegate callback

--- Wikipedia/Code/UIScrollView+WMFContentOffsetUtils.m
+++ Wikipedia/Code/UIScrollView+WMFContentOffsetUtils.m
@@ -15,33 +15,4 @@
     return UIEdgeInsetsInsetRect(CGRectOffset(self.frame, 0, self.contentOffset.y), self.contentInset);
 }
 
-- (void)wmf_safeSetContentOffset:(CGPoint)offset animated:(BOOL)animated completion:(void (^__nullable)(BOOL finished))completion {
-    if (!isnan(offset.x) && !isinf(offset.x) && !isnan(offset.y) && !isinf(offset.y)) {
-#if DEBUG
-        // log warning, but still scroll, if we get an out-of-bounds offset
-        if (self.contentSize.width < offset.x || self.contentSize.height < offset.y) {
-            DDLogDebug(@"Attempting to scroll to offset %@ which exceeds contentSize scroll view %@",
-                       NSStringFromCGPoint(offset), self);
-        }
-#endif
-        if (animated) {
-            [UIView animateWithDuration:0.3
-                             animations:^{
-                                 [self setContentOffset:offset animated:YES];
-                             }
-                             completion:completion];
-        } else {
-            [self setContentOffset:offset animated:NO];
-            if (completion) {
-                completion(YES);
-            }
-        }
-    } else {
-        DDLogError(@"Ignoring invalid offset %@ for scroll view %@", NSStringFromCGPoint(offset), self);
-        if (completion) {
-            completion(NO);
-        }
-    }
-}
-
 @end
