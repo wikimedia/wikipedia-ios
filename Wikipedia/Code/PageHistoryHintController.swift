09f14d24788abb4ae50990f91387bb8e1a9c43db
Natalia Harateh <nambatee@users.noreply.github.com>
2019-10-14 19:31:36 -0700

    update hint icon & colors

--- Wikipedia/Code/PageHistoryHintController.swift
+++ Wikipedia/Code/PageHistoryHintController.swift
@@ -12,7 +12,11 @@ class PageHistoryHintController: HintController {
     }
 
     public func hide(_ hide: Bool, presenter: HintPresentingViewController, theme: Theme) {
-        super.toggle(presenter: presenter, context: nil, theme: theme)
+        super.toggle(presenter: presenter, context: nil, theme: theme, setPrimaryColor: { (primaryColor: inout UIColor?) in
+            primaryColor = UIColor.wmf_red
+        }, setBackgroundColor: { (backgroundColor: inout UIColor?) in
+            backgroundColor = UIColor.wmf_lightRed
+        })
         setHintHidden(hide)
     }
 }
