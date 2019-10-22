3755d4580999129985ec9c0527430d03f6006ee4
Natalia Harateh <nambatee@users.noreply.github.com>
2019-10-22 14:09:45 -0400

    Remove old files

--- Wikipedia/Code/LegacyPageHistoryViewController.h
+++
@@ -1,9 +0,0 @@
-@import UIKit;
-@import WMF.Swift;
-
-@class MWKArticle;
-@interface LegacyPageHistoryViewController : UIViewController <WMFThemeable>
-
-@property (nonatomic, strong) MWKArticle *article;
-
-@end
