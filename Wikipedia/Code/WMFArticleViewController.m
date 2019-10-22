088647d0755e68696fdd09a60006f4abe30c9c7a
Joe Walsh <joewalsh@users.noreply.github.com>
2019-10-22 19:27:18 -0400

    contextMenuConfiguration

--- Wikipedia/Code/WMFArticleViewController.m
+++ Wikipedia/Code/WMFArticleViewController.m
@@ -1980,14 +1980,10 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
     //no-op
 }
 
-#pragma mark - Article link and image peeking via WKUIDelegate
+#pragma mark - Context menus
 
-- (BOOL)webView:(WKWebView *)webView shouldPreviewElement:(WKPreviewElementInfo *)elementInfo {
-    return elementInfo.linkURL && [elementInfo.linkURL wmf_isPeekable];
-}
-
-- (nullable UIViewController *)webView:(WKWebView *)webView previewingViewControllerForElement:(WKPreviewElementInfo *)elementInfo defaultActions:(NSArray<id<WKPreviewActionItem>> *)previewActions {
-    NSURLComponents *linkURLComponents = [[NSURLComponents alloc] initWithURL:elementInfo.linkURL resolvingAgainstBaseURL:NO];
+- (NSURL *)updatedLinkURLForPreviewingLinkURL:(NSURL *)linkURL {
+    NSURLComponents *linkURLComponents = [[NSURLComponents alloc] initWithURL:linkURL resolvingAgainstBaseURL:NO];
     NSString *eventLoggingLabel = linkURLComponents.wmf_eventLoggingLabel;
     DDLogDebug(@"Event logging label is %@", eventLoggingLabel);
     if (eventLoggingLabel) {
@@ -1996,21 +1992,47 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
         self.eventLoggingLabel = EventLoggingLabelOutLink;
     }
     NSURLComponents *updatedLinkURLComponents = linkURLComponents.wmf_componentsByRemovingInternalQueryParameters;
-    NSURL *updatedLinkURL = updatedLinkURLComponents.URL ?: elementInfo.linkURL;
+    return updatedLinkURLComponents.URL ?: linkURL;
+}
+
+- (void)webView:(WKWebView *)webView contextMenuConfigurationForElement:(WKContextMenuElementInfo *)elementInfo completionHandler:(void (^)(UIContextMenuConfiguration *_Nullable))completionHandler API_AVAILABLE(ios(13.0)) {
+    NSURL *updatedLinkURL = [self updatedLinkURLForPreviewingLinkURL:elementInfo.linkURL];
+    UIViewController *peekVC = [self peekViewControllerForURL:updatedLinkURL];
+    if (!peekVC) {
+        completionHandler(nil);
+    }
+    [self.webViewController hideFindInPageWithCompletion:nil];
+    UIContextMenuConfiguration *config = [UIContextMenuConfiguration configurationWithIdentifier:updatedLinkURL
+        previewProvider:^UIViewController *_Nullable {
+            return peekVC;
+        }
+        actionProvider:^UIMenu *_Nullable(NSArray<UIMenuElement *> *_Nonnull suggestedActions) {
+            return nil;
+        }];
+    completionHandler(config);
+}
+
+- (void)webView:(WKWebView *)webView contextMenuForElement:(WKContextMenuElementInfo *)elementInfo willCommitWithAnimator:(id<UIContextMenuInteractionCommitAnimating>)animator API_AVAILABLE(ios(13.0)) {
+}
+
+- (void)webView:(WKWebView *)webView contextMenuWillPresentForElement:(WKContextMenuElementInfo *)elementInfo API_AVAILABLE(ios(13.0)) {
+}
+
+- (void)webView:(WKWebView *)webView contextMenuDidEndForElement:(WKContextMenuElementInfo *)elementInf API_AVAILABLE(ios(13.0)) {
+}
+#pragma mark - Article link and image peeking via WKUIDelegate
+
+- (BOOL)webView:(WKWebView *)webView shouldPreviewElement:(WKPreviewElementInfo *)elementInfo {
+    return elementInfo.linkURL && [elementInfo.linkURL wmf_isPeekable];
+}
+
+- (nullable UIViewController *)webView:(WKWebView *)webView previewingViewControllerForElement:(WKPreviewElementInfo *)elementInfo defaultActions:(NSArray<id<WKPreviewActionItem>> *)previewActions {
+    NSURL *updatedLinkURL = [self updatedLinkURLForPreviewingLinkURL:elementInfo.linkURL];
     UIViewController *peekVC = [self peekViewControllerForURL:updatedLinkURL];
     if (peekVC) {
         [self.webViewController hideFindInPageWithCompletion:nil];
-
-        if ([peekVC isKindOfClass:[WMFArticleViewController class]]) {
-            ((WMFArticleViewController *)peekVC).articlePreviewingActionsDelegate = self;
-        }
-
-        if ([peekVC conformsToProtocol:@protocol(WMFThemeable)]) {
-            [(id<WMFThemeable>)peekVC applyTheme:self.theme];
-        }
-        return peekVC;
     }
-    return nil;
+    return peekVC;
 }
 
 - (void)webView:(WKWebView *)webView commitPreviewingViewController:(UIViewController *)previewingViewController {
@@ -2071,11 +2093,19 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
 }
 
 - (nullable UIViewController *)peekViewControllerForURL:(NSURL *)linkURL {
+    UIViewController *peekVC = nil;
     if ([self.peekableImageExtensions containsObject:[[linkURL pathExtension] lowercaseString]]) {
-        return [self viewControllerForImageFilePageURL:linkURL withTopBarHidden:YES];
+        peekVC = [self viewControllerForImageFilePageURL:linkURL withTopBarHidden:YES];
     } else {
-        return [self viewControllerForPreviewURL:linkURL];
+        peekVC = [self viewControllerForPreviewURL:linkURL];
     }
+    if ([peekVC isKindOfClass:[WMFArticleViewController class]]) {
+        ((WMFArticleViewController *)peekVC).articlePreviewingActionsDelegate = self;
+    }
+    if ([peekVC conformsToProtocol:@protocol(WMFThemeable)]) {
+        [(id<WMFThemeable>)peekVC applyTheme:self.theme];
+    }
+    return peekVC;
 }
 
 - (NSURL *)galleryURLFromImageFilePageURL:(NSURL *)imageFilePageURL {
