ec2ab4fcb6fc80f9d1a8d46cfff857559c2054fe
Natalia Harateh <nambatee@users.noreply.github.com>
2019-10-23 12:59:54 -0400

    fix common string

--- WMF Framework/CommonStrings.swift
+++ WMF Framework/CommonStrings.swift
@@ -101,7 +101,7 @@ public class CommonStrings: NSObject {
     @objc public static let undo = WMFLocalizedString("action-undo", value: "Undo", comment: "Title text and accessibility label for undo action on buttons or info sheets.")
     @objc public static let redo = WMFLocalizedString("action-redo", value: "Redo", comment: "Title text and accessibility label for redo action on buttons or info sheets.")
     @objc public static let findInPage = WMFLocalizedString("action-find-in-page", value: "Find in page", comment: "Title text and accessibility label for find in page action on buttons or info sheets.")
-    @objc public static let readingThemesControls = WMFLocalizedStringWithDefaultValue("article-toolbar-reading-themes-controls-toolbar-item", nil, nil, "Reading Themes Controls", "Accessibility label for the Reading Themes Controls article toolbar item");
+    @objc public static let readingThemesControls = WMFLocalizedString("article-toolbar-reading-themes-controls-toolbar-item", value: "Reading Themes Controls", comment: "Accessibility label for the Reading Themes Controls article toolbar item")
 
     public static let welcomePromiseTitle = WMFLocalizedString("description-welcome-promise-title", value:"By starting, I promise not to misuse this feature", comment:"Title text asking user to edit descriptions responsibly")
     @objc public static let gotItButtonTitle = WMFLocalizedString("welcome-explore-tell-me-more-done-button", value: "Got it", comment:"Text for button dismissing detailed explanation of new features")
