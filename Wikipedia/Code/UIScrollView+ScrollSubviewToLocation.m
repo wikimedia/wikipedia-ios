d21e660e6505b3b1deef3998f849fef7e5ac4655
montehurd <mhurd@wikimedia.org>
2019-11-27 16:46:24 -0800

    rename method

--- Wikipedia/Code/UIScrollView+ScrollSubviewToLocation.m
+++ Wikipedia/Code/UIScrollView+ScrollSubviewToLocation.m
@@ -2,11 +2,11 @@
 
 @implementation UIScrollView (ScrollSubviewToLocation)
 
-- (void)scrollSubViewToTop:(UIView *)subview animated:(BOOL)animated {
-    [self scrollSubViewToTop:subview offset:0 animated:animated];
+- (void)scrollSubviewToTop:(UIView *)subview animated:(BOOL)animated {
+    [self scrollSubviewToTop:subview offset:0 animated:animated];
 }
 
-- (void)scrollSubViewToTop:(UIView *)subview offset:(CGFloat)offset animated:(BOOL)animated {
+- (void)scrollSubviewToTop:(UIView *)subview offset:(CGFloat)offset animated:(BOOL)animated {
     // Find the scroll view's subview location in the scroll view's superview coordinates.
     CGPoint locationInSuperview = [subview convertPoint:CGPointZero toView:self.superview];
     // Determine how far the scroll view will need to be scrolled to move the subview's top
