37f1bab1c074fc46596c19182a476ac07cead77c
Toni Sevener <tsevener@wikimedia.org>
2020-01-22 08:39:59 -0600

    remove header

--- WMF Framework/PersistentCacheGroup+CoreDataProperties.swift
+++ WMF Framework/PersistentCacheGroup+CoreDataProperties.swift
@@ -1,11 +1,3 @@
-//
-//  PersistentCacheGroup+CoreDataProperties.swift
-//  WMF
-//
-//  Created by Toni Sevener on 1/21/20.
-//  Copyright © 2020 Wikimedia Foundation. All rights reserved.
-//
-//
 
 import Foundation
 import CoreData
