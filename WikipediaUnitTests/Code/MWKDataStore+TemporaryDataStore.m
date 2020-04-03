45346114b0d72f241ddc2bc7d7f2d050cf5e1758
Joe Walsh <joewalsh@users.noreply.github.com>
2020-04-03 09:35:44 -0400

    It's no longer necessary to perform these updates on a new library, fixes a crash when running tests

--- WikipediaUnitTests/Code/MWKDataStore+TemporaryDataStore.m
+++ WikipediaUnitTests/Code/MWKDataStore+TemporaryDataStore.m
@@ -8,7 +8,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)temporaryDataStore {
     MWKDataStore *dataStore = [[MWKDataStore alloc] initWithContainerURL:[NSURL fileURLWithPath:WMFRandomTemporaryPath()]];
-    [dataStore performUpdatesFromLibraryVersion:0 inManagedObjectContext:dataStore.viewContext];
     return dataStore;
 }
 
