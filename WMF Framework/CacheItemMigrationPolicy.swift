11b5df9ddb82077b11fc20a91dd4d522e5b46c91
Joe Walsh <joewalsh@users.noreply.github.com>
2020-04-09 07:13:47 -0400

    Revert "run response header save on background thread during cache item migration"
    This reverts commit 378c1fbeb4a7532f2f7813b93ef04b232f2f139c.

--- WMF Framework/CacheItemMigrationPolicy.swift
+++ WMF Framework/CacheItemMigrationPolicy.swift
@@ -27,25 +27,25 @@ class CacheItemMigrationPolicy: NSEntityMigrationPolicy {
             destinationItem.setValue(newVariant, forKey: "variant")
             destinationItem.setValue(date, forKey: "date")
             
+            //artifically create and save image response header
             var isDownloaded = false
             if let fileName = fetcher.uniqueFileNameForItemKey(key, variant: newVariant),
                 let headerFileName = fetcher.uniqueHeaderFileNameForItemKey(key, variant: newVariant) {
-                
                 let filePath = CacheFileWriterHelper.fileURL(for: fileName).path
-                isDownloaded = FileManager.default.fileExists(atPath: filePath)
                 
-                //artifically create and save image response header
-                //perform on another thread in case this takes a while
-                DispatchQueue.global(qos: .background).async {
-                    if let mimeType = FileManager.default.getValueForExtendedFileAttributeNamed(WMFExtendedFileAttributeNameMIMEType, forFileAtPath: filePath),
-                        let data = FileManager.default.contents(atPath: filePath) {
-                            //construct response header file
-                        let headerFields: [String: String] = [
-                                "Content-Type": mimeType,
-                                "Content-Length": String(data.count)
-                            ]
-                        CacheFileWriterHelper.saveResponseHeader(headerFields: headerFields, toNewFileName: headerFileName) { (result) in
-                            //no-op
+                if let mimeType = FileManager.default.getValueForExtendedFileAttributeNamed(WMFExtendedFileAttributeNameMIMEType, forFileAtPath: filePath),
+                    let data = FileManager.default.contents(atPath: filePath) {
+                        //construct response header file
+                    let headerFields: [String: String] = [
+                            "Content-Type": mimeType,
+                            "Content-Length": String(data.count)
+                        ]
+                    CacheFileWriterHelper.saveResponseHeader(headerFields: headerFields, toNewFileName: headerFileName) { (result) in
+                        switch result {
+                        case .success, .exists:
+                            isDownloaded = true
+                        case .failure:
+                            break
                         }
                     }
                 }
