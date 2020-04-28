9890f6cb6addc1000b265e95d1098b33559b5d27
Toni Sevener <tsevener@wikimedia.org>
2020-04-28 13:00:49 -0500

    add variant fallback test

--- WikipediaUnitTests/Code/ArticleCacheReadingTests.swift
+++ WikipediaUnitTests/Code/ArticleCacheReadingTests.swift
@@ -6,10 +6,11 @@ private class BasicCachingWebViewController: UIViewController, WKNavigationDeleg
     static let webProcessPool = WKProcessPool()
     
     let schemeHandler = SchemeHandler.shared
-    let articleURL = URL(string: "app://en.wikipedia.org/api/rest_v1/page/mobile-html/United_States")!
+    var articleURL = URL(string: "app://en.wikipedia.org/api/rest_v1/page/mobile-html/United_States")!
     let session = Session.shared
     let configuration = Configuration.current
     var didReceiveDataCallback: ((WKURLSchemeTask, Data) -> Void)?
+    var extraHeaders: [String: String] = [:]
     
     internal lazy var fetcher: ArticleFetcher = ArticleFetcher(session: session, configuration: configuration)
     
@@ -36,7 +37,11 @@ private class BasicCachingWebViewController: UIViewController, WKNavigationDeleg
     
     private func getRequest() -> URLRequest? {
         let acceptUTF8HTML = ["Accept": "text/html; charset=utf-8"]
-        return fetcher.urlRequest(from: articleURL, headers: acceptUTF8HTML)
+        
+        for (key, value) in acceptUTF8HTML {
+            extraHeaders[key] = value
+        }
+        return fetcher.urlRequest(from: articleURL, headers: extraHeaders)
     }
     
     private func setupWebView() {
@@ -46,19 +51,29 @@ private class BasicCachingWebViewController: UIViewController, WKNavigationDeleg
     }
 }
 
-class ArticleCachingTests: XCTestCase {
+class ArticleCacheReadingTests: XCTestCase {
     
     var imageData: Data?
     var basicHTMLData: Data?
     var basicCSSData: Data?
     var basicCachedHTMLData: Data?
     var basicCachedCSSData: Data?
+    var basicCachedZhansHTMLData: Data?
+    var basicCachedZhCSSData: Data?
+    var imageZh640Data: Data?
 
     override func setUp() {
         super.setUp()
         
         pullDataFromFixtures()
         setupTemporaryCacheController()
+    }
+    
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        URLCache.shared.removeAllCachedResponses()
     }
     
     private func setupTemporaryCacheController() {
@@ -75,7 +90,10 @@ class ArticleCachingTests: XCTestCase {
             let basicHTMLData = wmf_bundle().wmf_data(fromContentsOfFile:"basic", ofType:"html"),
             let basicCssData = wmf_bundle().wmf_data(fromContentsOfFile:"basic", ofType:"css"),
             let basicCachedHTMLData = wmf_bundle().wmf_data(fromContentsOfFile:"basicCached", ofType:"html"),
-            let basicCachedCssData = wmf_bundle().wmf_data(fromContentsOfFile:"basicCached", ofType:"css") else {
+            let basicCachedCssData = wmf_bundle().wmf_data(fromContentsOfFile:"basicCached", ofType:"css"),
+            let basicCachedZhansHTMLData = wmf_bundle().wmf_data(fromContentsOfFile:"basicZhans", ofType:"html"),
+            let basicCachedZhCSSData = wmf_bundle().wmf_data(fromContentsOfFile:"basicZh", ofType:"css"),
+            let imageZh640Data = wmf_bundle().wmf_data(fromContentsOfFile:"640px-Flag_of_the_United_States_(Pantone).svg", ofType:"png") else {
             assertionFailure("Error setting up fixtures.")
             return
         }
@@ -85,6 +103,9 @@ class ArticleCachingTests: XCTestCase {
         self.basicCSSData = basicCssData
         self.basicCachedCSSData = basicCachedCssData
         self.imageData = imageData
+        self.basicCachedZhansHTMLData = basicCachedZhansHTMLData
+        self.basicCachedZhCSSData = basicCachedZhCSSData
+        self.imageZh640Data = imageZh640Data
     }
     
     func stub200Responses() {
@@ -129,6 +150,18 @@ class ArticleCachingTests: XCTestCase {
         .andReturn(500)
         
         let _ = stubRequest("GET", "https://en.wikipedia.org/api/rest_v1/data/css/mobile/site" as NSString)
+        .andReturn(500)
+    }
+    
+    func stub500ZhResponses() {
+        
+        let _ = stubRequest("GET", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/960px-Flag_of_the_United_States_%28Pantone%29.svg.png" as NSString)
+        .andReturn(500)
+        
+        let _ = stubRequest("GET", "https://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD" as NSString)
+        .andReturn(500)
+        
+        let _ = stubRequest("GET", "https://zh.wikipedia.org/api/rest_v1/data/css/mobile/site" as NSString)
         .andReturn(500)
     }
     
@@ -291,7 +324,6 @@ class ArticleCachingTests: XCTestCase {
         
         //FLIP DEVICE TO AIRPLANE MODE BEFORE RUNNING THIS
         
-        //stub200Responses()
         writeCachedPiecesToCachingSystem()
         
         let basicVC = BasicCachingWebViewController()
@@ -336,6 +368,240 @@ class ArticleCachingTests: XCTestCase {
         wait(for: [htmlExpectation], timeout: 10)
         wait(for: [imageExpectation], timeout: 10)
         wait(for: [cssExpectation], timeout: 10)
+    }
+    
+    func testVariantFallbacksUponNetworkFailure() {
+        LSNocilla.sharedInstance().start()
+        stub500ZhResponses()
+        writeVariantPiecesToCachingSystem()
+        
+        let basicVC = BasicCachingWebViewController()
+        basicVC.extraHeaders = ["Accept-Language": "zh-hant"]
+        basicVC.articleURL = URL(string: "app://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD")!
+        
+        let htmlExpectation = expectation(description: "Waiting for html content to return")
+        let imageExpectation = expectation(description: "Waiting for image load to return")
+        let cssExpectation = expectation(description: "Waiting for css load to return")
+        
+        basicVC.didReceiveDataCallback = { urlSchemeTask, data in
+            
+            guard let urlString = urlSchemeTask.request.url?.absoluteString else {
+                XCTFail("Unable to determine urlString from scheme task")
+                return
+            }
+            
+            switch urlString {
+            case "app://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD":
+                
+                htmlExpectation.fulfill()
+                
+                let htmlString = String(decoding: data, as: UTF8.self)
+                let trimmedHTML = String(htmlString.filter { !"\n\t\r".contains($0) })
+                XCTAssertEqual(trimmedHTML, "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"//zh.wikipedia.org/api/rest_v1/data/css/mobile/site\"></head><body><p>美国 (美洲北部国家)</p><img src=\"//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/960px-Flag_of_the_United_States_%28Pantone%29.svg.png\"></body></html>")
+                
+            case "app://zh.wikipedia.org/api/rest_v1/data/css/mobile/site":
+                
+                cssExpectation.fulfill()
+                
+                let cssString = String(decoding: data, as: UTF8.self)
+                let trimmedCSS = String(cssString.filter { !"\n\t\r".contains($0) })
+                XCTAssertEqual(trimmedCSS, "body {background-color: blue;}", "Unexpected basic HTML content")
+            case "app://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/960px-Flag_of_the_United_States_%28Pantone%29.svg.png":
+                imageExpectation.fulfill()
+            default:
+                XCTFail("Unexpected scheme task callback")
+            }
+        }
+        
+        UIApplication.shared.keyWindow?.rootViewController = basicVC
+        let _ = basicVC.view
+        
+        wait(for: [htmlExpectation], timeout: 10)
+        wait(for: [imageExpectation], timeout: 100)
+        wait(for: [cssExpectation], timeout: 10)
+        
+        LSNocilla.sharedInstance().stop()
+    }
+    
+    func testVariantFallbacksUponConnectionFailure() {
+
+        //FLIP DEVICE TO AIRPLANE MODE BEFORE RUNNING THIS
+        
+        writeVariantPiecesToCachingSystem()
+        
+        let basicVC = BasicCachingWebViewController()
+        basicVC.extraHeaders = ["Accept-Language": "zh-hant"]
+        basicVC.articleURL = URL(string: "app://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD")!
+        
+        let htmlExpectation = expectation(description: "Waiting for html content to return")
+        let imageExpectation = expectation(description: "Waiting for image load to return")
+        let cssExpectation = expectation(description: "Waiting for css load to return")
+        
+        basicVC.didReceiveDataCallback = { urlSchemeTask, data in
+            
+            guard let urlString = urlSchemeTask.request.url?.absoluteString else {
+                XCTFail("Unable to determine urlString from scheme task")
+                return
+            }
+            
+            switch urlString {
+            case "app://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD":
+                
+                htmlExpectation.fulfill()
+                
+                let htmlString = String(decoding: data, as: UTF8.self)
+                let trimmedHTML = String(htmlString.filter { !"\n\t\r".contains($0) })
+                XCTAssertEqual(trimmedHTML, "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"//zh.wikipedia.org/api/rest_v1/data/css/mobile/site\"></head><body><p>美国 (美洲北部国家)</p><img src=\"//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/960px-Flag_of_the_United_States_%28Pantone%29.svg.png\"></body></html>")
+                
+            case "app://zh.wikipedia.org/api/rest_v1/data/css/mobile/site":
+                
+                cssExpectation.fulfill()
+                
+                let cssString = String(decoding: data, as: UTF8.self)
+                let trimmedCSS = String(cssString.filter { !"\n\t\r".contains($0) })
+                XCTAssertEqual(trimmedCSS, "body {background-color: blue;}", "Unexpected basic HTML content")
+            case "app://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/960px-Flag_of_the_United_States_%28Pantone%29.svg.png":
+                imageExpectation.fulfill()
+            default:
+                XCTFail("Unexpected scheme task callback")
+            }
+        }
+        
+        UIApplication.shared.keyWindow?.rootViewController = basicVC
+        let _ = basicVC.view
+        
+        wait(for: [htmlExpectation], timeout: 10)
+        wait(for: [imageExpectation], timeout: 100)
+        wait(for: [cssExpectation], timeout: 10)
+    }
+    
+    private func writeVariantPiecesToCachingSystem() {
+        guard let moc = CacheController.backgroundCacheContext else {
+            XCTFail("Unable to pull backgroundCacheContext")
+            return
+        }
+         
+        let mobileHTMLURLString = "https://zh.wikipedia.org/api/rest_v1/page/mobile-html/%E7%BE%8E%E5%9B%BD"
+        let cssString = "https://zh.wikipedia.org/api/rest_v1/data/css/mobile/site"
+        let imageURLString = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/640px-Flag_of_the_United_States_%28Pantone%29.svg.png"
+        
+        //save objects database
+        guard let cacheGroup = CacheDBWriterHelper.createCacheGroup(with: mobileHTMLURLString, in: moc),
+            let htmlCacheItem = CacheDBWriterHelper.createCacheItem(with: URL(string: mobileHTMLURLString)!, itemKey: mobileHTMLURLString, variant: "zh-hans", in: moc),
+            let imageCacheItem = CacheDBWriterHelper.createCacheItem(with: URL(string: imageURLString)!, itemKey: "upload.wikimedia.org__Flag_of_the_United_States_%28Pantone%29.svg", variant: "640", in: moc),
+            let cssCacheItem = CacheDBWriterHelper.createCacheItem(with: URL(string: cssString)!, itemKey: cssString, variant: nil, in: moc) else {
+            XCTFail("Unable to create Cache DB objects")
+            return
+        }
+        imageCacheItem.isDownloaded = true
+        cacheGroup.addToCacheItems(htmlCacheItem)
+        cacheGroup.addToCacheItems(imageCacheItem)
+        cacheGroup.addToCacheItems(cssCacheItem)
+        
+        CacheDBWriterHelper.save(moc: moc) { (result) in
+            
+            switch result {
+            case .success:
+                break
+            case .failure:
+                XCTFail("Failure saving Cache DB objects")
+            }
+        }
+        
+        //set up file names and content
+        let fileNameGenerator = PermanentlyPersistableURLCache()
+        guard let htmlURL = URL(string: mobileHTMLURLString),
+            let uniqueHTMLFileName = fileNameGenerator.uniqueFileNameForURL(htmlURL, type: .article),
+            let uniqueHTMLHeaderFileName = fileNameGenerator.uniqueHeaderFileNameForURL(htmlURL, type: .article) else {
+                XCTFail("Failure determining html file name")
+                return
+        }
+        
+        guard let cssURL = URL(string: cssString),
+            let uniqueCSSFileName = fileNameGenerator.uniqueFileNameForURL(cssURL, type: .article),
+            let uniqueCSSHeaderFileName = fileNameGenerator.uniqueHeaderFileNameForURL(cssURL, type: .article) else {
+                XCTFail("Failure determining html file name")
+                return
+        }
+        
+        guard let imageURL = URL(string: imageURLString),
+            let uniqueImageFileName = fileNameGenerator.uniqueFileNameForURL(imageURL, type: .image),
+        let uniqueImageHeaderFileName = fileNameGenerator.uniqueHeaderFileNameForURL(imageURL, type: .image) else {
+                XCTFail("Failure determining image file name")
+                return
+        }
+        
+        guard let htmlData = self.basicCachedZhansHTMLData,
+            let imageData = self.imageZh640Data,
+            let cssData = self.basicCachedZhCSSData else {
+                XCTFail("Failure pulling data from Fixtures")
+                return
+        }
+        
+        //save content in file system
+        CacheFileWriterHelper.saveData(data: htmlData, toNewFileWithKey: uniqueHTMLFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                XCTFail("Failure saving html data")
+            }
+        }
+        
+        CacheFileWriterHelper.saveData(data: imageData, toNewFileWithKey: uniqueImageFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                XCTFail("Failure saving html data")
+            }
+        }
+        
+        CacheFileWriterHelper.saveData(data: cssData, toNewFileWithKey: uniqueCSSFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                XCTFail("Failure saving html data")
+            }
+        }
+        
+        //save headers in file system
+        let htmlHeaders: [String: String] = ["Content-Type": "text/html; charset=utf-8;",
+                                             "Vary": "Accept-Language, Accept-Encoding"]
+        
+        CacheFileWriterHelper.saveResponseHeader(headerFields: htmlHeaders, toNewFileName: uniqueHTMLHeaderFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                break
+            }
+        }
+        
+        let imageHeaders: [String: String] = ["Content-Type": "image/jpeg",
+                                              "Content-Length": String((imageData as NSData).length)]
+        
+        CacheFileWriterHelper.saveResponseHeader(headerFields: imageHeaders, toNewFileName: uniqueImageHeaderFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                break
+            }
+        }
+        
+        let cssHeaders: [String: String] = ["Content-Type": "text/css; charset=utf-8;",
+                                              "Content-Length": String((cssData as NSData).length)]
+        
+        CacheFileWriterHelper.saveResponseHeader(headerFields: cssHeaders, toNewFileName: uniqueCSSHeaderFileName) { (result) in
+            switch result {
+            case .success, .exists:
+                break
+            case .failure:
+                break
+            }
+        }
     }
     
     private func writeCachedPiecesToCachingSystem() {
