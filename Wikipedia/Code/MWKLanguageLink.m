391cd13e716848cd61784d6c79024759bd91ae3e
Matt Cleinman <mcleinman@wikimedia.org>
2020-05-05 09:24:24 -0700

    fix language comparison error and add defensive check to bar

--- Wikipedia/Code/MWKLanguageLink.m
+++ Wikipedia/Code/MWKLanguageLink.m
@@ -37,7 +37,7 @@ WMF_SYNTHESIZE_IS_EQUAL(MWKLanguageLink, isEqualToLanguageLink:)
 }
 
 - (BOOL)isEqualToLanguageLink:(MWKLanguageLink *)rhs {
-    return WMF_RHS_PROP_EQUAL(languageCode, isEqualToString:) && WMF_RHS_PROP_EQUAL(pageTitleText, isEqualToString:) && WMF_RHS_PROP_EQUAL(name, isEqualToString:) && WMF_RHS_PROP_EQUAL(localizedName, isEqualToString:) && [self.namespaces isEqualToDictionary:rhs.namespaces];
+    return WMF_RHS_PROP_EQUAL(languageCode, isEqualToString:) && WMF_RHS_PROP_EQUAL(pageTitleText, isEqualToString:) && WMF_RHS_PROP_EQUAL(name, isEqualToString:) && WMF_RHS_PROP_EQUAL(localizedName, isEqualToString:) && WMF_RHS_PROP_EQUAL(localizedName, isEqualToString:);
 }
 
 - (NSComparisonResult)compare:(MWKLanguageLink *)other {
