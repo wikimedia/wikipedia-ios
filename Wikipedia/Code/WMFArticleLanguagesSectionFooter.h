093c5b389b6c3f8c2959b1208ffbbb6d082e2df8
Matt Cleinman <mcleinman@wikimedia.org>
2020-05-05 16:10:31 -0700

    Change to use delegate rather than first responder.

--- Wikipedia/Code/WMFArticleLanguagesSectionFooter.h
+++ Wikipedia/Code/WMFArticleLanguagesSectionFooter.h
@@ -1,7 +1,13 @@
 @import UIKit;
 @import WMF.Swift;
 
+@protocol WMFAddLanguageDelegate
+- (void)addLanguageButtonTapped;
+@end
+
+
 @interface WMFArticleLanguagesSectionFooter : UITableViewHeaderFooterView <WMFThemeable>
+@property (nonatomic, weak) id<WMFAddLanguageDelegate> delegate;
 
 - (void)setTitle:(NSString *)title;
 - (void)setButtonHidden:(BOOL)hidden;
