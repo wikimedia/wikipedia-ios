68b36a1bc190bf95789bb594d4aa19d360f0f4f0
Deepak Mantena <dmantena@wikimedia.org>
2020-05-27 14:04:14 -0700

    Convert `Date+ExtensionTests` to use spaces for indentation.

--- WikipediaUnitTests/Code/Date+ExtensionTests.swift
+++ WikipediaUnitTests/Code/Date+ExtensionTests.swift
@@ -3,30 +3,30 @@ import XCTest
 class Date_ExtensionTests: XCTestCase {
 
     func testBeforeAfter() throws {
-		let currentDate = Date()
-		let beforeDate = currentDate.addingTimeInterval(-100)
-		let afterDate = currentDate.addingTimeInterval(100)
+        let currentDate = Date()
+        let beforeDate = currentDate.addingTimeInterval(-100)
+        let afterDate = currentDate.addingTimeInterval(100)
 
-		// Date in past
-		XCTAssert(beforeDate.isBefore(afterDate))
-		XCTAssert(!beforeDate.isAfter(afterDate))
+        // Date in past
+        XCTAssert(beforeDate.isBefore(afterDate))
+        XCTAssert(!beforeDate.isAfter(afterDate))
 
-		XCTAssert(beforeDate.isBefore(afterDate, inclusive: true))
-		XCTAssert(!beforeDate.isAfter(afterDate, inclusive: true))
+        XCTAssert(beforeDate.isBefore(afterDate, inclusive: true))
+        XCTAssert(!beforeDate.isAfter(afterDate, inclusive: true))
 
-		// Date in future
-		XCTAssert(!afterDate.isBefore(beforeDate))
-		XCTAssert(afterDate.isAfter(beforeDate))
+        // Date in future
+        XCTAssert(!afterDate.isBefore(beforeDate))
+        XCTAssert(afterDate.isAfter(beforeDate))
 
-		XCTAssert(!afterDate.isBefore(beforeDate, inclusive: true))
-		XCTAssert(afterDate.isAfter(beforeDate, inclusive: true))
+        XCTAssert(!afterDate.isBefore(beforeDate, inclusive: true))
+        XCTAssert(afterDate.isAfter(beforeDate, inclusive: true))
 
-		// Inclusive cases
-		XCTAssert(currentDate.isBefore(currentDate, inclusive: true))
-		XCTAssert(currentDate.isAfter(currentDate, inclusive: true))
+        // Inclusive cases
+        XCTAssert(currentDate.isBefore(currentDate, inclusive: true))
+        XCTAssert(currentDate.isAfter(currentDate, inclusive: true))
 
-		XCTAssert(!currentDate.isAfter(currentDate))
-		XCTAssert(!currentDate.isBefore(currentDate))
-	}
+        XCTAssert(!currentDate.isAfter(currentDate))
+        XCTAssert(!currentDate.isBefore(currentDate))
+    }
 
 }
