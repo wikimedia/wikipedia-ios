b465f84e1fe4d4dbb278a0e1b73a96c2e37acd91
Toni Sevener <tsevener@wikimedia.org>
2020-08-12 14:29:29 -0500

    fix linker error

--- WMF Framework/LocationManagerFactory.swift
+++ WMF Framework/LocationManagerFactory.swift
@@ -1,7 +1,7 @@
 
 import Foundation
 
-@objc final class LocationManagerFactory: NSObject {
+@objc public final class LocationManagerFactory: NSObject {
     @objc static func coarseLocationManager() -> LocationManagerProtocol {
         return LocationManager(configuration: .coarse)
     }
