47c1864e192575e437b1bddd6735c9dc8d7af495
Joe Walsh <joewalsh@users.noreply.github.com>
2020-08-17 08:50:03 -0400

    Rewrite WikipediaLanguageUtility the Combine way

--- Command Line Tools/Update Languages/WikipediaLanguageUtilityAPI.swift
+++ Command Line Tools/Update Languages/WikipediaLanguageUtilityAPI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 /// **THIS IS NOT PART OF THE MAIN APP - IT'S A COMMAND LINE UTILITY**
 ///
@@ -13,72 +14,86 @@ class WikipediaLanguageUtilityAPI {
     //      "name": "Qaf\u00e1r af",
     //       "localname": "Afar"
     //     },
-    func getSites(completion: @escaping (Result<[Wikipedia], Error>) -> Void) {
+    func getSites() -> AnyPublisher<[Wikipedia], Error> {
         let sitematrixURL = URL(string: "https://meta.wikimedia.org/w/api.php?action=sitematrix&format=json&formatversion=2&origin=*")!
-        URLSession.shared.dataTask(with: sitematrixURL) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            do {
-                guard
-                    let data = data,
-                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
-                else {
-                     throw WikipediaLanguageUtilityAPIError.generic
-                }
+        return URLSession.shared
+            .dataTaskPublisher(for: sitematrixURL)
+            .tryMap { result -> [String: Any] in
                 /// See above as to why all of this is necessary instead of using codable
-                guard let sitematrix = jsonObject["sitematrix"] as? [String: Any] else {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: result.data, options: .allowFragments) as? [String: Any] else {
                     throw WikipediaLanguageUtilityAPIError.generic
                 }
-                var wikipedias =  sitematrix.compactMap { (kv) -> Wikipedia? in
-                    guard
-                        let result = kv.value as? [String: Any],
-                        let code = result["code"] as? String,
-                        let name = result["name"] as? String,
-                        let localname = result["localname"] as? String,
-                        let sites = result["site"] as? [[String: Any]]
-                        else {
-                            return nil
-                    }
-                    guard
-                        let wikipedia = sites.first(where: { (site) -> Bool in
-                            site["code"] as? String == "wiki"
-                        }),
-                        let sitename = wikipedia["sitename"] as? String
+                return jsonObject
+        }
+        .map { jsonObject -> [Wikipedia] in
+            /// See above as to why all of this is necessary instead of using codable
+            guard let sitematrix = jsonObject["sitematrix"] as? [String: Any] else {
+                return []
+            }
+            var wikipedias = sitematrix.compactMap { (kv) -> Wikipedia? in
+                guard
+                    let result = kv.value as? [String: Any],
+                    let code = result["code"] as? String,
+                    let name = result["name"] as? String,
+                    let localname = result["localname"] as? String,
+                    let sites = result["site"] as? [[String: Any]]
                     else {
                         return nil
-                    }
-                    return Wikipedia(languageCode: code, languageName: name, localName: localname, siteName: sitename)
                 }
-                // Add testwiki, it's not returned by the site matrix
-                wikipedias.append(Wikipedia(languageCode: "test", languageName: "Test", localName: "Test", siteName: "Test Wikipedia"))
-                completion(.success(wikipedias))
-            } catch let error {
-                completion(.failure(error))
+                guard
+                    let wikipedia = sites.first(where: { (site) -> Bool in
+                        site["code"] as? String == "wiki"
+                    }),
+                    let sitename = wikipedia["sitename"] as? String
+                    else {
+                        return nil
+                }
+                return Wikipedia(languageCode: code, languageName: name, localName: localname, siteName: sitename)
             }
-        }.resume()
+            // Add testwiki, it's not returned by the site matrix
+            wikipedias.append(Wikipedia(languageCode: "test", languageName: "Test", localName: "Test", siteName: "Test Wikipedia"))
+            return wikipedias
+        }.eraseToAnyPublisher()
     }
     
-    func getSiteInfo(with languageCode: String, completion: @escaping (Result<SiteInfo, Error>) -> Void) {
+    func getSiteInfo(with languageCode: String) -> AnyPublisher<SiteInfo, Error> {
         let siteInfoURL = URL(string: "https://\(languageCode).wikipedia.org/w/api.php?action=query&format=json&prop=&list=&meta=siteinfo&siprop=namespaces%7Cgeneral%7Cnamespacealiases&formatversion=2&origin=*")!
-        URLSession.shared.dataTask(with: siteInfoURL) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else {
-                completion(.failure(WikipediaLanguageUtilityAPIError.generic))
-                return
-            }
-            do {
-                let result = try JSONDecoder().decode(SiteInfo.self, from: data)
-                completion(.success(result))
-            } catch let error {
-                completion(.failure(error))
-            }
-        }.resume()
+        return URLSession.shared
+            .dataTaskPublisher(for: siteInfoURL)
+            .tryMap { (result) -> SiteInfo in
+                try JSONDecoder().decode(SiteInfo.self, from: result.data)
+        }.eraseToAnyPublisher()
     }
+    
+    func getCodeMirrorConfigJSON(for wikiLanguage: String) -> AnyPublisher<String, Error> {
+        let codeMirrorConfigURL = URL(string: "http://\(wikiLanguage).wikipedia.org/w/load.php?debug=false&lang=en&modules=ext.CodeMirror.data")!
+        return URLSession.shared.dataTaskPublisher(for: codeMirrorConfigURL)
+            .tryMap { (result) -> String in
+                guard
+                    let responseString = String(data: result.data, encoding: .utf8),
+                    let soughtSubstring = self.extractJSONString(from: responseString)
+                    else {
+                        throw WikipediaLanguageUtilityAPIError.generic
+                }
+                return soughtSubstring.replacingOccurrences(of: "!0", with: "true")
+        }.eraseToAnyPublisher()
+    }
+    
+    private let jsonExtractionRegex = try! NSRegularExpression(pattern: #"(?:mw\.config\.set\()(.*?)(?:\);\n*\}\);)"#, options: [.dotMatchesLineSeparators])
+    
+    private func extractJSONString(from responseString: String) -> String? {
+        let results = jsonExtractionRegex.matches(in: responseString, range: NSRange(responseString.startIndex..., in: responseString))
+        guard
+            results.count == 1,
+            let firstResult = results.first,
+            firstResult.numberOfRanges == 2,
+            let soughtCaptureGroupRange = Range(firstResult.range(at: 1), in: responseString)
+            else {
+                return nil
+        }
+        return String(responseString[soughtCaptureGroupRange])
+    }
+    
 }
 
 enum WikipediaLanguageUtilityAPIError: Error {
