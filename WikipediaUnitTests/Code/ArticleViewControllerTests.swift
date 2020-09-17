445d0ae3f7b083bb9ef42281117547bbc2dd5d64
Joe Walsh <joewalsh@users.noreply.github.com>
2020-09-17 07:03:18 -0400

    Break MockSchemeHandler retain cycle

--- WikipediaUnitTests/Code/ArticleViewControllerTests.swift
+++ WikipediaUnitTests/Code/ArticleViewControllerTests.swift
@@ -7,8 +7,8 @@ private class MockSchemeHandler: SchemeHandler {
     
     required init(scheme: String, session: Session) {
         super.init(scheme: scheme, session: session)
-        let didReceiveDataCallback: ((WKURLSchemeTask, Data) -> Void)? = { _, _ in
-            self.accessed = true
+        let didReceiveDataCallback: ((WKURLSchemeTask, Data) -> Void)? = { [weak self] _, _ in
+            self?.accessed = true
         }
         self.didReceiveDataCallback = didReceiveDataCallback
     }
