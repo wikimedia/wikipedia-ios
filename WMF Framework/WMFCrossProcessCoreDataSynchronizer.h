79b88838a2bcc08ceb4c3705fb8cfc1cd45ea13b
Joe Walsh <joewalsh@users.noreply.github.com>
2020-09-18 14:27:09 -0400

    Add replacement notes

--- WMF Framework/WMFCrossProcessCoreDataSynchronizer.h
+++ WMF Framework/WMFCrossProcessCoreDataSynchronizer.h
@@ -3,6 +3,10 @@
 @class NSManagedObjectContext;
 
 NS_ASSUME_NONNULL_BEGIN
+
+/// Keeps a shared persistent store in sync across different processes
+/// Likely should be replaced with Persistent History Tracking introduced in iOS 13: https://developer.apple.com/videos/play/wwdc2017/210/
+
 @interface WMFCrossProcessCoreDataSynchronizer : NSObject
 
 - (instancetype)initWithIdentifier:(NSString *)identifier storageDirectory:(NSURL *)directoryURL NS_DESIGNATED_INITIALIZER;
