f15950e4d9fdda83860bfc1cd0061bfc1819e037
James Dempsey <dempsey@tapas-software.net>
2020-12-09 11:03:30 -0800

    Refactor NSLocale/Locale extensions - Make internally used constants fileprivate - Drop wmf_ prefix from fileprivate constants - Migrate two Obj-C NSLocale tests to LocaleTests.swift - Remove WMFLocaleTests.m - Move extensions used by Swift-only code from NSLocale to Locale - Update call sites to use migrated methods   - Session.swift   - TableOfContentsHeader.swift

--- WikipediaUnitTests/Code/LocaleTests.swift
+++ WikipediaUnitTests/Code/LocaleTests.swift
@@ -16,4 +16,22 @@ class LocaleTests: XCTestCase {
         let srVariant = Locale.preferredWikipediaLanguageVariant(for: srURL, preferredLanguages: preferredLanguages)
         XCTAssertEqual("sr-ec", srVariant)
     }
+    
+    func testLocaleHeader() {
+        let languages = Locale.uniqueWikipediaLanguages(with: ["en-US", "zh-Hans-US", "zh-Hant-US", "zh-Hant-TW", "en-GB"])
+        let expectedResult = ["en-us", "zh-hans", "zh-hant", "zh-tw", "en-gb"]
+        XCTAssertEqual(languages, expectedResult)
+        
+        let header = Locale.acceptLanguageHeaderForLanguageCodes(languages)
+        XCTAssertEqual(header, "en-us, zh-hans;q=0.8, zh-hant;q=0.6, zh-tw;q=0.4, en-gb;q=0.2")
+    }
+    
+    func testZHHeaders() {
+        let languages = Locale.uniqueWikipediaLanguages(with: ["zh-Hans-CN", "zh-Hans-SG", "zh-Hant-MO", "zh-Hant-TW", "zh-Hant-HK"])
+        let expectedResult = ["zh-cn", "zh-sg", "zh-mo", "zh-tw", "zh-hk"]
+        XCTAssertEqual(languages, expectedResult)
+        
+        let header = Locale.acceptLanguageHeaderForLanguageCodes(languages)
+        XCTAssertEqual(header, "zh-cn, zh-sg;q=0.8, zh-mo;q=0.6, zh-tw;q=0.4, zh-hk;q=0.2")
+    }
 }
