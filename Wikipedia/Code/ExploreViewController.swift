99946ef2f2c68ec4856bda94679c97bff5add63d
Deepak Mantena <dmantena@wikimedia.org>
2020-12-11 13:07:32 -0800

    Fix indentation.

--- Wikipedia/Code/ExploreViewController.swift
+++ Wikipedia/Code/ExploreViewController.swift
@@ -230,13 +230,13 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
         searchBar.searchBarStyle = .minimal
         searchBar.placeholder =  WMFLocalizedString("search-field-placeholder-text", value: "Search Wikipedia", comment: "Search field placeholder text")
 
-		// Disable Scribble on this placeholder text field
-		if UIDevice.current.userInterfaceIdiom == .pad, #available(iOS 14.0, *) {
-			let existingInteractions = searchBar.searchTextField.interactions
-			existingInteractions.forEach { searchBar.searchTextField.removeInteraction($0) }
-			let scribbleIgnoringInteraction = UIScribbleInteraction(delegate: scribbleIgnoringDelegate)
-			searchBar.searchTextField.addInteraction(scribbleIgnoringInteraction)
-		}
+        // Disable Scribble on this placeholder text field
+        if UIDevice.current.userInterfaceIdiom == .pad, #available(iOS 14.0, *) {
+            let existingInteractions = searchBar.searchTextField.interactions
+            existingInteractions.forEach { searchBar.searchTextField.removeInteraction($0) }
+            let scribbleIgnoringInteraction = UIScribbleInteraction(delegate: scribbleIgnoringDelegate)
+            searchBar.searchTextField.addInteraction(scribbleIgnoringInteraction)
+        }
 
 		return searchBar
     }()
