e6082f4d027e8991de21732f05483e19c5752b48
James Dempsey <dempsey@tapas-software.net>
2021-02-02 07:47:54 -0800

    Accept clang formatter change

--- Wikipedia/Code/MWKTitleLanguageController.m
+++ Wikipedia/Code/MWKTitleLanguageController.m
@@ -37,19 +37,19 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)fetchLanguagesWithSuccess:(dispatch_block_t)success
                           failure:(void (^__nullable)(NSError *__nonnull))failure {
     [self.fetcher fetchLanguageLinksForArticleURL:self.articleURL
-                                          success:^(NSArray *languageLinks) {
-                                              dispatch_async(dispatch_get_main_queue(), ^{
-                                                  self.availableLanguages = languageLinks;
-                                                  if (success) {
-                                                      success();
-                                                  }
-                                              });
-                                          }
-                                          failure:^(NSError * _Nonnull error) {
-                                              dispatch_async(dispatch_get_main_queue(), ^{
-                                                  failure(error);
-                                              });
-                                          }];
+        success:^(NSArray *languageLinks) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.availableLanguages = languageLinks;
+                if (success) {
+                    success();
+                }
+            });
+        }
+        failure:^(NSError *_Nonnull error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                failure(error);
+            });
+        }];
 }
 
 - (void)setAvailableLanguages:(NSArray *)availableLanguages {
