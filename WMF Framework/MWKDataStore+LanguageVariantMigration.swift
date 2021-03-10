e5e7353ecb383e421109137e3b763d2c4e54d1e2
James Dempsey <dempsey@tapas-software.net>
2021-03-10 12:16:42 -0800

    Add MWKDataStore extension in new file for migration - Add MWKDataStore+LanguageVariantMigration.swift file to project - Add stub migrateToLangaugeVariants() method

---
+++ WMF Framework/MWKDataStore+LanguageVariantMigration.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension MWKDataStore {
+    @objc(migrateToLanguageVariantsForLanguageCodes:inManagedObjectContext:)
+    public func migrateToLanguageVariants(for languageCodes: [String], in moc: NSManagedObjectContext) {
+
+    }
+}
