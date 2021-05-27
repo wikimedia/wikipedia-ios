3728b4c77ac323b3ae6bb76dc67920254a7b7b15
James Dempsey <dempsey@tapas-software.net>
2021-05-27 10:15:25 -0700

    Add stub PermanentCacheCore class to project

---
+++ WMF Framework/PermanentCacheCore.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/** The PermanentCacheCore is the lowest layer of the permanent cache.
+ *  It serves as a fallback for standard URLCache behavior for PermanentlyPersistableURLCache.
+ *  The article and image cache controller subsystems both sit on top of PermanentCacheCore.
+ */
+class PermanentCacheCore {
+    
+}
