ac9d9b6f02d482402297cd89266b781052dad9cb
Deepak Mantena <dmantena@wikimedia.org>
2021-07-14 13:39:03 -0700

    Add interactive pop gesture functionality to Settings navigation stack.

--- Wikipedia/Code/WMFAppViewController.m
+++ Wikipedia/Code/WMFAppViewController.m
@@ -1978,6 +1978,7 @@ static NSString *const WMFDidShowOnboarding = @"DidShowOnboarding5.3";
         [self applyTheme:self.theme toNavigationControllers:@[navController]];
         _settingsNavigationController = navController;
         _settingsNavigationController.modalPresentationStyle = UIModalPresentationOverFullScreen;
+        _settingsNavigationController.interactivePopGestureRecognizer.delegate = nil;
     }
 
     if (_settingsNavigationController.viewControllers.firstObject != self.settingsViewController) {
