f07bfa81dffe354784a5d2404cc588ccae861757
Toni Sevener <tsevener@wikimedia.org>
2021-10-01 16:24:05 -0500

    Persist completed importing flags so import operations are once per install

--- WMF Framework/Remote Notifications/Operations/RemoteNotificationsImportOperation.swift
+++ WMF Framework/Remote Notifications/Operations/RemoteNotificationsImportOperation.swift
@@ -1,4 +1,16 @@
+import CocoaLumberjackSwift
+
 class RemoteNotificationsImportOperation: RemoteNotificationsOperation {
+    
+    private enum LibraryKey: String {
+        case completedImportFlags = "RemoteNotificationsCompletedImportFlags"
+        case continueIdentifer = "RemoteNotificationsContinueIdentifier"
+        
+        func fullKeyForProject(_ project: RemoteNotificationsProject) -> String {
+            return "\(self.rawValue)-\(project.notificationsApiWikiIdentifier)"
+        }
+    }
+    
     private let project: RemoteNotificationsProject
     private let cookieDomain: String
     init(with apiController: RemoteNotificationsAPIController, modelController: RemoteNotificationsModelController, project: RemoteNotificationsProject, cookieDomain: String) {
@@ -8,6 +20,16 @@ class RemoteNotificationsImportOperation: RemoteNotificationsOperation {
     }
     
     override func execute() {
+        
+        //Confirm we haven't already imported for this language before kicking off import
+        let key = LibraryKey.completedImportFlags.fullKeyForProject(project)
+        let isAlreadyImported = libraryBool(for: key)
+        
+        guard !isAlreadyImported else {
+            finish()
+            return
+        }
+        
         importNotifications()
     }
     
@@ -43,14 +65,49 @@ class RemoteNotificationsImportOperation: RemoteNotificationsOperation {
                     
                     guard let newContinueId = result?.continueId,
                           newContinueId != continueId else {
+                        self.markLanguageAsImportCompleted()
                         self.finish()
                         return
                     }
-
+                    
                     self.importNotifications(continueId: newContinueId)
                 })
             } catch let error {
                 self.finish(with: error)
+            }
+        }
+    }
+    
+    private func markLanguageAsImportCompleted() {
+        let key = LibraryKey.completedImportFlags.fullKeyForProject(project)
+        setLibraryBool(true, for: key)
+    }
+}
+
+//MARK: Library Key Value helpers
+
+private extension RemoteNotificationsImportOperation {
+    func libraryBool(for key: String) -> Bool {
+        
+        var result: Bool = false
+        let backgroundContext = self.modelController.newBackgroundContext()
+        backgroundContext.performAndWait {
+            result = backgroundContext.wmf_numberValue(forKey: key)?.boolValue ?? false
+        }
+        
+        return result
+    }
+    
+    func setLibraryBool(_ bool: Bool, for key: String) {
+        
+        let backgroundContext = self.modelController.newBackgroundContext()
+        backgroundContext.perform {
+            let nsNumber = NSNumber(value: bool)
+            backgroundContext.wmf_setValue(nsNumber, forKey: key)
+            do {
+                try backgroundContext.save()
+            } catch let error {
+                DDLogError("Error saving RemoteNotificationsImportOperation backgroundContext for library keys: \(error)")
             }
         }
     }
