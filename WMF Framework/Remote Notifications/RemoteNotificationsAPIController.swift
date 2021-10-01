dac3e930c7f81f2959129e880bae814c24939ea4
Toni Sevener <tsevener@wikimedia.org>
2021-10-01 10:56:18 -0500

    Add specific handling for talk page notification types, make sure everything is localized properly

--- WMF Framework/Remote Notifications/RemoteNotificationsAPIController.swift
+++ WMF Framework/Remote Notifications/RemoteNotificationsAPIController.swift
@@ -115,6 +115,14 @@ public class RemoteNotificationsAPIController: Fetcher {
             public var pushContentText: String? {
                 return self.message?.header?.removingHTML
             }
+            
+            public var namespaceKey: Int? {
+                return self.title?.namespaceKey
+            }
+            
+            public var titleFull: String? {
+                return self.title?.full
+            }
 
             enum CodingKeys: String, CodingKey {
                 case wiki
