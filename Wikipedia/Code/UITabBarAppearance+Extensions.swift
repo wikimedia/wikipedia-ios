096e06a26906e6de7cca5a714963ae80be31e95e
Toni Sevener <tsevener@wikimedia.org>
2021-11-09 14:46:01 -0600

    Use appearance APIs to fix iOS15 background color bugs in nav bars and tab bars

---
+++ Wikipedia/Code/UITabBarAppearance+Extensions.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+@objc extension UITabBarAppearance {
+    @objc static func appearanceForTheme(_ theme: Theme) -> UITabBarAppearance {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = theme.colors.chromeBackground
+        
+        let itemAppearance = UITabBarItemAppearance()
+        let normalStateAppearance = itemAppearance.normal
+        normalStateAppearance.titleTextAttributes = theme.tabBarTitleTextAttributes
+        normalStateAppearance.badgeTextAttributes = theme.tabBarItemBadgeTextAttributes
+        normalStateAppearance.badgeBackgroundColor = theme.colors.accent
+        normalStateAppearance.iconColor = theme.colors.unselected
+        let selectedStateAppearance = itemAppearance.selected
+        selectedStateAppearance.titleTextAttributes = theme.tabBarSelectedTitleTextAttributes
+        
+        appearance.stackedLayoutAppearance = itemAppearance
+        appearance.inlineLayoutAppearance = itemAppearance
+        appearance.compactInlineLayoutAppearance = itemAppearance
+
+        return appearance
+    }
+}
