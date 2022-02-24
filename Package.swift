10eec25bb04e0bb720ee20a4e542fa832f593ea6
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-24 11:30:47 +0000

    added a swift package manager and removed unused boilerplate

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "WikipediaKit",
+    products: [
+        .library(
+            name: "WikipediaKit",
+            targets: ["WikipediaKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "WikipediaKit",
+            dependencies: []),
+    ]
+)
---
+++ Tests/wikipedia-iosTests/wikipedia_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import wikipedia_ios
+
+final class wikipedia_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(wikipedia_ios().text, "Hello, World!")
+    }
+}
