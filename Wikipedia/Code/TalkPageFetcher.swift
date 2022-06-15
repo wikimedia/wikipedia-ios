a0ba74508391b66694f91a77719dfb8bfdeedf64
mazevedo <mazevedo@wikimedia.org>
2022-06-15 13:48:27 -0300

    Add new TalkPAge response objects

--- Wikipedia/Code/TalkPageFetcher.swift
+++ Wikipedia/Code/TalkPageFetcher.swift
@@ -1,6 +1,32 @@
 import Foundation
 import WMF
 
+class TalkPageResponse: Codable {
+    let pageinfo: [TalkPageThreadItems]
+    
+    enum CodingKeys: String, CodingKey {
+        case pageinfo = "discussiontoolspageinfo"
+    }
+}
+
+class TalkPageThreadItems: Codable {
+    let threadItemsHTML: [TalkPageItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case threadItemsHTML = "threaditemshtml"
+    }
+}
+
+class TalkPageItem: Codable {
+    let type: String
+    let level: Int
+    let id: String
+    let html: String
+    let headingLevel: Int
+    let placeholderHeading: Bool
+    let replies: [TalkPageItem]
+}
+
 class TalkPageFetcher: Fetcher {
     
 }
