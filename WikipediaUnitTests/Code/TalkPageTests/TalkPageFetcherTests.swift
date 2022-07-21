5308ac325ab1ef0e7c49c895dfd4dfdf9c679f11
mazevedo <mazevedo@wikimedia.org>
2022-07-21 11:21:47 -0300

    Add unit tests for TalkPagefetcher post topic and post reply methods

--- WikipediaUnitTests/Code/TalkPageTests/TalkPageFetcherTests.swift
+++ WikipediaUnitTests/Code/TalkPageTests/TalkPageFetcherTests.swift
@@ -2,6 +2,7 @@ import XCTest
 
 @testable import Wikipedia
 @testable import WMF
+import SwiftUI
 
 
 final class TalkPageFetcherTests: XCTestCase {
@@ -13,7 +14,6 @@ final class TalkPageFetcherTests: XCTestCase {
         let expectation = expectation(description: "Waiting for completion")
         
         fetcher.fetchTalkPageContent(talkPageTitle: "Talk:Archaeoindris", siteURL: siteURL) { result in
-            
             expectation.fulfill()
             
             switch result {
@@ -22,9 +22,40 @@ final class TalkPageFetcherTests: XCTestCase {
             case .failure:
                 XCTFail("Expected Success")
             }
-            
         }
         wait(for: [expectation], timeout: 5)
+    }
+    
+    func testPostReply() {
+        
+        let fetcher = MockTalkPageFetcher()
+        let siteURL = URL(string: "https://en.wikipedia.org")!
+        
+        fetcher.postReply(talkPageTitle: "User_talk:Username", siteURL: siteURL, commentId: "commentid", comment: "comment") { _ in }
+        XCTAssertTrue(fetcher.postReplyWasCalled)
+    }
+    
+    func testPostTopic() {
+        let fetcher = MockTalkPageFetcher()
+        let siteURL = URL(string: "https://en.wikipedia.org")!
+        
+        fetcher.postTopic(talkPageTitle: "User_talk:Username", siteURL: siteURL, topicTitle: "Title", topicBody: "body") { _ in }
+        XCTAssertTrue(fetcher.postTopicWasCalled)
+    }
+}
+
+class MockTalkPageFetcher: TalkPageFetcher {
+    
+    var postReplyWasCalled = false
+    var postTopicWasCalled = false
+    
+    override func postReply(talkPageTitle: String, siteURL: URL, commentId: String, comment: String, completion: @escaping(Result<[AnyHashable: Any], Error>) -> Void) {
+        
+        postReplyWasCalled = true
+    }
+    
+    override func postTopic(talkPageTitle: String, siteURL: URL, topicTitle: String, topicBody: String, completion: @escaping(Result<[AnyHashable: Any], Error>) -> Void) {
+        postTopicWasCalled = true
     }
     
 }
