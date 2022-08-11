87341e7c1a8dd1d7c19e039496fac105c1430691
Deepak Mantena <staykids@users.noreply.github.com>
2022-08-11 15:25:13 -0700

    Update view model with sample data to populate header.

--- Wikipedia/Code/TalkPageViewModel.swift
+++ Wikipedia/Code/TalkPageViewModel.swift
@@ -2,10 +2,26 @@ import Foundation
 
 final class TalkPageViewModel {
 
+    // MARK: - Nested Types
+
+    enum PageType {
+        case article
+        case user
+    }
+
     // MARK: - Properties
 
     private let pageTitle: String
     private let siteURL: URL
+
+    // TODO: - Populate from data controller
+    var pageType = PageType.user
+    let talkPageTitle: String = "Page title"
+    var description: String? = "This is the page description"
+    var leadImage: UIImage? = UIImage(systemName: "text.bubble.fill")
+    var coffeeRollText: NSAttributedString? = NSAttributedString(string: "This is the coffee roll")
+    var projectSourceImage: UIImage? = UIImage(named: "notifications-project-mediawiki")
+    var projectLanguage: String? = "EN"
 
     // MARK: - Lifecycle
 
