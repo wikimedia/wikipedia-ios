b3834abefc680c20217d134561f89222a8ce0228
Toni Sevener <tsevener@wikimedia.org>
2022-08-24 14:05:53 -0500

    Fix compile error

--- Wikipedia/Code/VanishAccountCustomUIHostingController.swift
+++ Wikipedia/Code/VanishAccountCustomUIHostingController.swift
@@ -22,6 +22,7 @@ class VanishAccountCustomUIHostingController: UIHostingController<VanishAccountC
     
     @MainActor required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
