f27150aa25ccf280d0c78af29ac7cccad78188b1
Toni Sevener <tsevener@wikimedia.org>
2022-08-24 14:05:34 -0500

    Remove unused method

--- Wikipedia/Code/VanishAccountCustomUIHostingController.swift
+++ Wikipedia/Code/VanishAccountCustomUIHostingController.swift
@@ -22,13 +22,6 @@ class VanishAccountCustomUIHostingController: UIHostingController<VanishAccountC
     
     @MainActor required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
