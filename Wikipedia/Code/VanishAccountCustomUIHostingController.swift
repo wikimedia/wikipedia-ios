847076b2f9fd7746e9e7c35abe4e76838cb0f683
mazevedo <mazevedo@wikimedia.org>
2022-08-24 13:49:17 -0300

    Remove unused code

--- Wikipedia/Code/VanishAccountCustomUIHostingController.swift
+++ Wikipedia/Code/VanishAccountCustomUIHostingController.swift
@@ -1,23 +1,16 @@
 import SwiftUI
 import UIKit
+import WMF
 
 class VanishAccountCustomUIHostingController: UIHostingController<VanishAccountContentView> {
     
-    var shouldShowPopUp = false
-    var userInput = String()
-    
     init(title: String, theme: Theme, username: String) {
         super.init(rootView: VanishAccountContentView(theme: theme, username: username))
-        rootView.userInput = userInput
         self.title = title
     }
     
     @MainActor required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(UIApplication.didBecomeActiveNotification)
     }
     
 }
