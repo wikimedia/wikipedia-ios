80578a87aafd933fed5bcae2e1f6e3c3bbf5fc79
mazevedo <mazevedo@wikimedia.org>
2022-08-25 10:52:43 -0300

    Remove screen rotation code, add scrollview

--- Wikipedia/Code/VanishAccountPopUpAlertView.swift
+++ Wikipedia/Code/VanishAccountPopUpAlertView.swift
@@ -21,46 +21,50 @@ struct VanishAccountPopUpAlertView: View {
     }
     
     var body: some View {
-        ZStack {
-            if isVisible {
+        GeometryReader { proxy in
+            ZStack {
                 Color.black.opacity(isVisible ? 0.3 : 0).edgesIgnoringSafeArea(.all)
-                VStack(alignment: .center, spacing: 0) {
-                    Text(LocalizedStrings.title)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 45, alignment: .center)
-                        .font(Font(titleFont))
-                        .foregroundColor(Color(theme.colors.primaryText))
-                        .padding(10)
-                    Image("vanish-account-two-tone")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 85, height: 85, alignment: .center)
-                    BulletListView(theme: theme)
-                        .background(Color(theme.colors.paperBackground))
-                        .padding([.top, .leading, .trailing], 20)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Divider()
-                    Button(action: {
-                        withAnimation(.linear(duration: 0.3)) {
-                            userInput = ""
-                            isVisible = false
+                if isVisible {
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        
+                        
+                        VStack(alignment: .center, spacing: 0) {
+                            Text(LocalizedStrings.title)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 45, alignment: .center)
+                                .font(Font(titleFont))
+                                .foregroundColor(Color(theme.colors.primaryText))
+                                .padding(10)
+                            Image("vanish-account-two-tone")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 85, height: 85, alignment: .center)
+                            BulletListView(theme: theme)
+                                .background(Color(theme.colors.paperBackground))
+                                .padding([.top, .leading, .trailing], 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Divider()
+                            Button(action: {
+                                withAnimation(.linear(duration: 0.3)) {
+                                    userInput = ""
+                                    isVisible = false
+                                }
+                            }, label: {
+                                Text(CommonStrings.okTitle)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 43, alignment: .center)
+                                    .foregroundColor(Color(theme.colors.link))
+                                    .font(Font(titleFont))
+                            }).buttonStyle(PlainButtonStyle())
                         }
-                    }, label: {
-                        Text(CommonStrings.okTitle)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 43, alignment: .center)
-                            .foregroundColor(Color(theme.colors.link))
-                            .font(Font(titleFont))
-                    }).buttonStyle(PlainButtonStyle())
+                        .frame(maxWidth: thisWidth)
+                        .background(Color(theme.colors.paperBackground))
+                        .cornerRadius(14)
+                    }
                 }
-                .frame(maxWidth: thisWidth)
-                .background(Color(theme.colors.paperBackground))
-                .cornerRadius(14)
-                .onRotate { newOr in
-                    orientation = newOr
-                    updateOrientation()
-                }
-            }
+                
+            }.frame(minHeight: proxy.size.height)
         }
     }
     
@@ -91,34 +95,24 @@ struct BulletListView: View {
     var body: some View {
         VStack {
             HStack {
-                BulletView(theme: theme, height: updateHeight(height: 52))
+                BulletView(theme: theme, height: 52)
                 Text(LocalizedStrings.firstItem)
                     .font(Font(bodyFont))
-                    .frame(maxWidth: .infinity, minHeight: updateHeight(height: 90), alignment: .leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
             HStack {
-                BulletView(theme: theme, height: updateHeight(height: 40))
+                BulletView(theme: theme, height: 40)
                 Text(LocalizedStrings.secondItem)
                     .font(Font(bodyFont))
                     .frame(maxWidth: .infinity, alignment: .leading)
             }
             HStack {
-                BulletView(theme: theme, height: updateHeight(height: 44))
+                BulletView(theme: theme, height: 44)
                 Text("\(LocalizedStrings.thirdItem) [\(LocalizedStrings.linkTitle)](https://en.wikipedia.org/wiki/Wikipedia:Courtesy_vanishing)")
                     .font(Font(bodyFont))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.bottom, 10)
             }
-        }.onRotate { newOr in
-            orientation = newOr
-        }
-    }
-    
-    func updateHeight(height: CGFloat) -> CGFloat {
-        if orientation.isLandscape {
-            return height - 30
-        } else {
-            return height
         }
     }
     
@@ -138,22 +132,4 @@ struct BulletView: View {
         }.frame(maxHeight: height, alignment: .leading)
     }
     
-}
-
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-    
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
-    }
-}
-
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
-    }
 }
