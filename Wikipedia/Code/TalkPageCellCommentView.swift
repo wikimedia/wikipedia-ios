0b6be4a24e61c7bb46857c8c6bc213b66a863090
Deepak Mantena <staykids@users.noreply.github.com>
2022-09-09 15:53:27 -0700

    Add comment label and reply depth width and height constraints.

--- Wikipedia/Code/TalkPageCellCommentView.swift
+++ Wikipedia/Code/TalkPageCellCommentView.swift
@@ -19,6 +19,7 @@ final class TalkPageCellCommentView: SetupView {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
         stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
         stackView.spacing = 4
         return stackView
     }()
@@ -28,7 +29,11 @@ final class TalkPageCellCommentView: SetupView {
         label.adjustsFontForContentSizeCategory = true
         label.font = UIFont.wmf_scaledSystemFont(forTextStyle: .body, weight: .regular, size: 16)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.setContentCompressionResistancePriority(.required, for: .horizontal)
+        // TODO: - Something off with these...
+        label.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
         label.numberOfLines = 0
         return label
     }()
@@ -44,7 +49,9 @@ final class TalkPageCellCommentView: SetupView {
         button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 2, bottom: 0, right: -2)
 
         button.setContentHuggingPriority(.required, for: .horizontal)
+        button.setContentHuggingPriority(.required, for: .vertical)
         button.setContentCompressionResistancePriority(.required, for: .horizontal)
+        button.setContentCompressionResistancePriority(.required, for: .vertical)
 
         button.titleLabel?.font = UIFont.wmf_scaledSystemFont(forTextStyle: .body, weight: .semibold, size: 15)
         button.titleLabel?.adjustsFontForContentSizeCategory = true
@@ -53,7 +60,7 @@ final class TalkPageCellCommentView: SetupView {
         return button
     }()
 
-    lazy var replyDepthView = TalkPageCellCommentDepthIndicator(depth: 0)
+    lazy var replyDepthView = TalkPageCellReplyDepthIndicator(depth: 0)
     
     weak var viewModel: TalkPageCellCommentViewModel?
     weak var replyDelegate: TalkPageCellReplyDelegate?
@@ -68,11 +75,24 @@ final class TalkPageCellCommentView: SetupView {
         verticalStackView.addArrangedSubview(commentLabel)
         verticalStackView.addArrangedSubview(replyButton)
 
+        let replyDepthWidthConstraint = replyDepthView.widthAnchor.constraint(lessThanOrEqualTo: horizontalStackView.widthAnchor, constant: 1/2)
+        replyDepthWidthConstraint.priority = .required
+
+        let replyDepthHeightConstraint = replyDepthView.heightAnchor.constraint(equalTo: horizontalStackView.heightAnchor)
+        replyDepthHeightConstraint.priority = .required
+
+        let commentLabelWidthConstraint = commentLabel.widthAnchor.constraint(greaterThanOrEqualTo: horizontalStackView.widthAnchor, multiplier: 1/2)
+        commentLabelWidthConstraint.priority = .required
+
         NSLayoutConstraint.activate([
             horizontalStackView.topAnchor.constraint(equalTo: topAnchor),
             horizontalStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             horizontalStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            replyDepthWidthConstraint,
+            replyDepthHeightConstraint,
+            commentLabelWidthConstraint
         ])
     }
 
@@ -81,7 +101,7 @@ final class TalkPageCellCommentView: SetupView {
     func configure(viewModel: TalkPageCellCommentViewModel) {
         self.viewModel = viewModel
         commentLabel.text = viewModel.text
-        replyDepthView.label.text = " \(viewModel.replyDepth) "
+        replyDepthView.configure(viewModel: viewModel)
     }
     
     // MARK: - Actions
