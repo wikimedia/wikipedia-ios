99bdbddc6a3d8acb9adcb40bbeec02128bec1318
mazevedo <mazevedo@wikimedia.org>
2022-09-16 17:10:10 -0300

    Add comment

--- Wikipedia/Code/NSMutableAttributedString+RemoveNewLine.swift
+++ Wikipedia/Code/NSMutableAttributedString+RemoveNewLine.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 extension NSMutableAttributedString {
+    /// Removes unnecessary `\n` initial new line characters
     public func removingInitialNewlineCharacters() -> Self {
         if string.first == "\n" {
             while string.first == "\n" {
