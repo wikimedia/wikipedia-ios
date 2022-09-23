8a8604f4b25357b61c00f4686de533258e8fcb8f
Toni Sevener <tsevener@wikimedia.org>
2022-09-22 19:53:55 -0500

    Force new talk pages on

--- WMF Framework/FeatureFlags.swift
+++ WMF Framework/FeatureFlags.swift
@@ -3,6 +3,9 @@ import Foundation
 public struct FeatureFlags {
     
     public static var needsNewTalkPage: Bool {
+        
+        return true
+        
         #if WMF_STAGING
             return true
         #else
