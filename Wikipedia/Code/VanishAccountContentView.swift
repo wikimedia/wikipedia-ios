f0f424ce7c77c54dcea8dcb7a610eb223e8bccfe
Deepak Mantena <staykids@users.noreply.github.com>
2022-10-21 09:47:32 -0700

    Update vanishing description text.

--- Wikipedia/Code/VanishAccountContentView.swift
+++ Wikipedia/Code/VanishAccountContentView.swift
@@ -5,7 +5,7 @@ struct VanishAccountContentView: View {
     
     enum LocalizedStrings {
         static let title = WMFLocalizedString("vanish-account-title", value: "Vanishing process", comment: "Title for the vanishing process screen")
-        static let description = WMFLocalizedString("vanish-account-description", value: "Vanishing is a last resort and should only be used when you wish to stop editing forever and also to hide as many of your past associations as possible.\n\nTo initiate the vanishing process please provide the following", comment: "Description for the vanishing process")
+        static let description = WMFLocalizedString("vanish-account-description", value: "To initiate the vanishing process please provide the following", comment: "Description for the vanishing process")
         static let usernameFieldTitle = CommonStrings.usernameFieldTitle
         static let additionalInformationFieldTitle = WMFLocalizedString("vanish-account-additional-information-field", value: "Additional information", comment: "Title for the additional information form field")
         static let additionalInformationFieldPlaceholder = WMFLocalizedString("vanish-account-additional-information-placeholder", value: "Optional", comment: "Placeholder for the additional information form field")
