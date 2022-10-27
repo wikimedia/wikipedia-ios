1f85c926a19c6c8c2fb7786f2f9ee3c0d9f4aa0c
Deepak Mantena <staykids@users.noreply.github.com>
2022-10-27 13:18:48 -0700

    Add `TalkPageFindInPageState` to manage search results state.

---
+++ Wikipedia/Code/TalkPageFindInPageState.swift
@@ -0,0 +1,86 @@
+import Foundation
+
+final class TalkPageFindInPageState {
+
+    // MARK: - Properties
+
+    let searchController = TalkPageFindInPageSearchController()
+    var keyboardBar: FindAndReplaceKeyboardBar?
+
+    var selectedIndex: Int = -1 {
+        didSet {
+            updateView()
+        }
+    }
+
+    var selectedMatch: TalkPageFindInPageSearchController.SearchResult? {
+        guard matches.indices.contains(selectedIndex) else {
+            return nil
+        }
+
+        return matches[selectedIndex]
+    }
+
+    var matches: [TalkPageFindInPageSearchController.SearchResult] = [] {
+        didSet {
+            updateView()
+        }
+    }
+
+    // MARK: - Public
+
+    /// Next result, looping to start of matches if at end
+    func next() {
+        guard !matches.isEmpty else {
+            return
+        }
+
+        guard selectedIndex < matches.count - 1 else {
+            selectedIndex = 0
+            return
+        }
+
+        selectedIndex += 1
+    }
+
+    /// Previous result, looping to end of matches if at beginning
+    func previous() {
+        guard !matches.isEmpty else {
+            return
+        }
+
+        guard selectedIndex > 0 else {
+            selectedIndex = matches.count - 1
+            return
+        }
+
+        selectedIndex -= 1
+    }
+
+    func reset(_ topics: [TalkPageCellViewModel]) {
+        matches = []
+        selectedIndex = -1
+        keyboardBar?.reset()
+        topics.forEach {
+            $0.highlightText = nil
+            $0.activeHighlightResult = nil
+        }
+
+    }
+
+    func search(term: String, in topics: [TalkPageCellViewModel]) {
+        selectedIndex = -1
+        matches = searchController.search(term: term, in: topics)
+        topics.forEach {
+            $0.highlightText = matches.isEmpty ? nil : term
+            $0.activeHighlightResult = nil
+        }
+    }
+
+    // MARK: - Private
+
+    private func updateView() {
+        keyboardBar?.updateMatchCounts(index: selectedIndex, total: UInt(matches.count))
+    }
+
+}
