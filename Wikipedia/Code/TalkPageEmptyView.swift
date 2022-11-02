28ddc0edea596e3287771d18d8c919be21bc4b3e
mazevedo <mazevedo@wikimedia.org>
2022-11-02 13:42:01 -0300

    Adjust empty view padding

--- Wikipedia/Code/TalkPageEmptyView.swift
+++ Wikipedia/Code/TalkPageEmptyView.swift
@@ -124,22 +124,23 @@ final class TalkPageEmptyView: SetupView {
 
         scrollView.addSubview(stackView)
 
-        stackView.addArrangedSubview(VerticalSpacerView.spacerWith(space: 20))
+        stackView.addArrangedSubview(VerticalSpacerView.spacerWith(space: 12))
 
         stackView.addArrangedSubview(imageStack)
-        imageStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 36))
+        imageStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 64))
         imageStack.addArrangedSubview(imageView)
-        imageStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 36))
+        imageStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 64))
 
         stackView.addArrangedSubview(headerStack)
-        headerStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 51))
+        headerStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 32))
         headerStack.addArrangedSubview(headerLabel)
-        headerStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 51))
+        headerStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 32))
 
         stackView.addArrangedSubview(bodyStack)
-        bodyStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 51))
+        bodyStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 32))
         bodyStack.addArrangedSubview(bodyLabel)
-        bodyStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 51))
+        bodyStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 32))
+
 
         stackView.addArrangedSubview(buttonStack)
         buttonStack.addArrangedSubview(HorizontalSpacerView.spacerWith(space: 20))
@@ -148,9 +149,9 @@ final class TalkPageEmptyView: SetupView {
 
         stackView.addArrangedSubview(VerticalSpacerView.spacerWith(space: 20))
 
-        stackView.setCustomSpacing(32, after: imageStack)
-        stackView.setCustomSpacing(16, after: headerStack)
-        stackView.setCustomSpacing(28, after: bodyStack)
+        stackView.setCustomSpacing(8, after: imageStack)
+        stackView.setCustomSpacing(12, after: headerStack)
+        stackView.setCustomSpacing(16, after: bodyStack)
 
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: topAnchor),
