8f331b818159e9da958d46c0c13f66c904162cf1
Toni Sevener <tsevener@wikimedia.org>
2022-12-14 10:21:56 -0600

    reorder

--- Wikipedia/Code/TalkPageArchivesHostingController.swift
+++ Wikipedia/Code/TalkPageArchivesHostingController.swift
@@ -3,9 +3,9 @@ import UIKit
 
 class TalkPageArchivesHostingController: CustomNavigationViewHostingController<TalkPageArchivesView> {
     
-    let redView = TempShiftingView(color: .red, order: 0)
+    let redView = TempShiftingView(color: .red, order: 2)
     let blueView = TempShiftingView(color: .blue, order: 1)
-    let greenView = TempShiftingView(color: .green, order: 2)
+    let greenView = TempShiftingView(color: .green, order: 0)
     let barView = ShiftingNavigationBarView(order: 0)
     
     override var customNavigationViewSubviews: [CustomNavigationViewShiftingSubview] {
