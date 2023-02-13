8a919c830bc5627f9f6fa69a19c5b17a62c48dcd
Toni Sevener <tsevener@wikimedia.org>
2023-02-13 08:41:00 -0600

    Make script executable and confirm deploy succeeded first

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,5 +1,12 @@
  #!/bin/sh
 
-BUILD_TAG="betas/${CI_BUILD_NUMBER}"
-git tag $BUILD_TAG
-git push --tags https://${GITHUB_PAT}@github.com/wmf_apps_ci/https://github.com/wikimedia/wikipedia-ios.git
+if [[${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH}]]; then
+	BUILD_TAG="betas/${CI_BUILD_NUMBER}"
+	git tag $BUILD_TAG
+	git push --tags https://${GITHUB_PAT}@github.com/wmf_apps_ci/https://github.com/wikimedia/wikipedia-ios.git
+	echo "Successfully tagged ${BUILD_TAG}"
+	exit 0
+else
+	echo "Deploy failed, not adding tag."
+	exit 1
+fi
