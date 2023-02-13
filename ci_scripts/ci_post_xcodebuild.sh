b446455a192808752e2aaf6808a7751ddb0861b9
Toni Sevener <tsevener@wikimedia.org>
2023-02-13 08:54:52 -0600

    Fix script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,12 +1,12 @@
  #!/bin/sh
 
-if [[${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH}]]; then
+if [[ ${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH} ]]; then
 	BUILD_TAG="betas/${CI_BUILD_NUMBER}"
 	git tag $BUILD_TAG
 	git push --tags https://${GITHUB_PAT}@github.com/wmf_apps_ci/https://github.com/wikimedia/wikipedia-ios.git
 	echo "Successfully tagged ${BUILD_TAG}"
 	exit 0
 else
-	echo "Deploy failed, not adding tag."
+	echo "Failure adding tag."
 	exit 1
 fi
