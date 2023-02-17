9edc5d5c7889da72140352ba5fcb84fadd68dbcc
Deepak Mantena <staykids@users.noreply.github.com>
2023-02-17 10:14:59 -0800

    Set footer container top/bottom constraints to 16pts.

--- Wikipedia/Code/EditNoticesView.swift
+++ Wikipedia/Code/EditNoticesView.swift
@@ -226,8 +226,8 @@ final class EditNoticesView: SetupView {
 
             footerStack.leadingAnchor.constraint(equalTo: footerContainer.readableContentGuide.leadingAnchor, constant: 20),
             footerStack.trailingAnchor.constraint(equalTo: footerContainer.readableContentGuide.trailingAnchor, constant: -20),
-            footerStack.topAnchor.constraint(equalTo: footerContainer.topAnchor, constant: 20),
-            footerStack.bottomAnchor.constraint(equalTo: footerContainer.readableContentGuide.bottomAnchor)
+            footerStack.topAnchor.constraint(equalTo: footerContainer.topAnchor, constant: 16),
+            footerStack.bottomAnchor.constraint(equalTo: footerContainer.safeAreaLayoutGuide.bottomAnchor, constant: -16)
         ])
     }
 
