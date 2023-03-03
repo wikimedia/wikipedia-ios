a990e469e150259895b63ad9cc52b3e5877c94c8
mazevedo <mazevedo@wikimedia.org>
2023-03-03 14:38:44 -0300

    Update post-build script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,12 +1,17 @@
  #!/bin/sh
 
-if [[ ${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH} ]]; then
-	BUILD_TAG="betas/${CI_BUILD_NUMBER}"
-	git tag $BUILD_TAG
-	git push --tags https://${GITHUB_USERNAME}:${GITHUB_PAT}@github.com/wikimedia/wikipedia-ios.git
-	echo "Successfully tagged ${BUILD_TAG}"
+# Stop running the script in case a command returns
+# a nonzero exit code.
+set -e
+
+if [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
+	./tag_script_xcodebuild.sh
+	echo "Execute tag script."
 	exit 0
-else
-	echo "Failure adding tag."
-	exit 1
+else if [[ ${CI_WORKFLOW} == "Wikipedia Experimental Build" ]]; then
+	echo "Do not execute tag script."
+	exit 0
 fi
+
+echo "Post-build scrip failed."
+exit 1
