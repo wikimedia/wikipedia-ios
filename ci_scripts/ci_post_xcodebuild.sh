20573031c70b8b430c4a53424c1d739f2ae8f4fb
mazevedo <mazevedo@wikimedia.org>
2023-03-06 18:45:16 -0300

    Remove unused condition

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -8,9 +8,6 @@ if [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
 	./tag_script_xcodebuild.sh
 	echo "Execute tag script."
 	exit 0
-elif [[ ${CI_WORKFLOW} == "Wikipedia Experimental Build" ]]; then
-	echo "Experimental script."
-	exit 0
 else
 	echo "Do not execute tag script."
 	exit 0
