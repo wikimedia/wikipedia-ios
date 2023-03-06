04327731781dc4e5fcfe562bf6f9d961d0d65b36
mazevedo <mazevedo@wikimedia.org>
2023-03-06 09:09:02 -0300

    Fix syntax hopefully

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -8,10 +8,10 @@ if [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
 	./tag_script_xcodebuild.sh
 	echo "Execute tag script."
 	exit 0
-else if [[ ${CI_WORKFLOW} == "Wikipedia Experimental Build" ]]; then
+elif [[ ${CI_WORKFLOW} == "Wikipedia Experimental Build" ]]; then
+	echo "Experimental script."
+	exit 0
+else
 	echo "Do not execute tag script."
 	exit 0
 fi
-
-echo "Post-build scrip failed."
-exit 1
