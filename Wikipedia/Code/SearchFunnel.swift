5a9c5cdf9fa1a604455a1bed0677182c4bb1013b
mazevedo <mazevedo@wikimedia.org>
2023-03-13 15:23:36 -0300

    Send language info on search funnel

--- Wikipedia/Code/SearchFunnel.swift
+++ Wikipedia/Code/SearchFunnel.swift
@@ -30,45 +30,52 @@
         let number_of_results: Int?
         let time_to_display_results: Int?
         let session_token: String
+        let wiki_id: String?
     }
 
-    func logEvent(action: Action, source: String, position: Int? = nil, searchType: WMFSearchType? = nil, numberOfResults: Int? = nil, timeToDisplay: Int? = nil) {
+    var searchLanguage: String? {
+        let userDefaults = UserDefaults.standard
+        let lang = userDefaults.wmf_currentSearchContentLanguageCode()
+        return lang
+    }
+
+    func logEvent(action: Action, source: String, position: Int? = nil, searchType: WMFSearchType? = nil, numberOfResults: Int? = nil, timeToDisplay: Int? = nil, wikiId: String?) {
         guard let searchSessionToken else { return }
-        let event = Event(action: action, source: source, position: position, search_type: searchType?.rawValue, number_of_results: numberOfResults, time_to_display_results: timeToDisplay, session_token: searchSessionToken)
+        let event = Event(action: action, source: source, position: position, search_type: searchType?.rawValue, number_of_results: numberOfResults, time_to_display_results: timeToDisplay, session_token: searchSessionToken, wiki_id: wikiId)
         EventPlatformClient.shared.submit(stream: .search, event: event)
     }
 
     func logSearchStart(source: String) {
         _searchSessionToken = nil
-        logEvent(action: .start, source: source)
+        logEvent(action: .start, source: source, wikiId: searchLanguage)
     }
 
     func logSearchAutoSwitch(source: String) {
-        logEvent(action: .autoSwitch, source: source)
+        logEvent(action: .autoSwitch, source: source, wikiId: searchLanguage)
     }
 
     func logSearchDidYouMean(source: String) {
-        logEvent(action: .didYouMean, source: source)
+        logEvent(action: .didYouMean, source: source, wikiId: searchLanguage)
     }
 
     func logSearchResultTap(position: Int, source: String) {
-        logEvent(action: .click, source: source, position: position)
+        logEvent(action: .click, source: source, position: position, wikiId: searchLanguage)
     }
 
     func logSearchCancel(source: String) {
-        logEvent(action: .cancel, source: source)
+        logEvent(action: .cancel, source: source, wikiId: searchLanguage)
     }
 
     func logSearchLangSwitch(source: String) {
-        logEvent(action: .langSwitch, source: source)
+        logEvent(action: .langSwitch, source: source, wikiId: searchLanguage)
     }
 
     func logSearchResults(with type: WMFSearchType, resultCount: Int, elapsedTime: Double, source: String) {
-        logEvent(action: .results, source: source, searchType: type, numberOfResults: resultCount, timeToDisplay: Int(elapsedTime * 1000))
+        logEvent(action: .results, source: source, searchType: type, numberOfResults: resultCount, timeToDisplay: Int(elapsedTime * 1000), wikiId: searchLanguage)
     }
 
     func logShowSearchError(with type: WMFSearchType, elapsedTime: Double, source: String) {
-        logEvent(action: .error, source: source, searchType: type, timeToDisplay: Int(elapsedTime * 1000))
+        logEvent(action: .error, source: source, searchType: type, timeToDisplay: Int(elapsedTime * 1000), wikiId: searchLanguage)
     }
 
 }
