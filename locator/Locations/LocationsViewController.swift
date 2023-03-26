af36a7302cce930fbdae789d6715d0bbb34d1e9c
Dzmitry Navak <d_navak@flo.health>
2023-03-27 01:50:54 +0300

    feat: create empty app without storyboard

---
+++ locator/Locations/LocationsViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ locator/MainCoordinator.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+/// Main coordinator use for routing logic, can be extended with nested coordinatiors with app growth
+final class MainCoordinator {
+    private(set) var topViewController: UIViewController
+    
+    init() {
+        let initialVC = LocationsViewController()
+        self.topViewController = UINavigationController(rootViewController: initialVC)
+        
+        setAppearance()
+    }
+    
+    private func setAppearance() {
+        UINavigationBar.appearance().isTranslucent = true
+    }
+}
