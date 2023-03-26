115c4c42a95219415497ce3c1624396829ba2adf
Dzmitry Navak <d_navak@flo.health>
2023-03-26 16:50:59 +0300

    test: add dictionary to location tests

---
+++ WikipediaUnitTests/Code/CLLocation+WMFDictionaryTests.m
@@ -0,0 +1,31 @@
+#import <XCTest/XCTest.h>
+#import "CLLocation+WMFDictionary.h"
+
+@interface CLLocation_WMFDictionaryTests : XCTestCase
+
+@property (nonatomic, strong) NSDictionary<NSString *, NSNumber *> *empty;
+@property (nonatomic, strong) NSDictionary<NSString *, NSNumber *> *notFull;
+@property (nonatomic, strong) NSDictionary<NSString *, NSNumber *> *correct;
+@property (nonatomic, strong) CLLocation *correctLocation;
+
+@end
+
+@implementation CLLocation_WMFDictionaryTests
+
+- (void)setUp {
+    [super setUp];
+    self.correctLocation = [[CLLocation alloc] initWithLatitude: 37.783821 longitude: -122.400264];
+    self.correct = @{@"lat": @37.783821, @"long": @-122.400264};
+    self.notFull = @{@"lat": @37.783821};
+    self.empty = @{};
+}
+
+- (void)testShouldReturnNilForEmptyDictionary {
+    XCTAssertNil([CLLocation locationWithDictionary:self.empty]);
+}
+
+- (void)testShouldReturnNilForNotFullDictionary {
+    XCTAssertNil([CLLocation locationWithDictionary:self.notFull]);
+}
+
+@end
