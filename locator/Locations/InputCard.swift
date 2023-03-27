8878c3ff75ad828402bd0c1011165cabce882fde
Dzmitry Navak <d_navak@flo.health>
2023-03-27 07:01:21 +0300

    feat: limit incorrect input in location field

--- locator/Locations/InputCard.swift
+++ locator/Locations/InputCard.swift
@@ -28,7 +28,7 @@ extension InputCard {
     }
 }
 
-final class InputCardView: UITextField & UIContentView {
+final class InputCardView: UITextField & UIContentView & UITextFieldDelegate {
     var configuration: UIContentConfiguration {
         didSet {
             configure(configuration: configuration)
@@ -38,7 +38,6 @@ final class InputCardView: UITextField & UIContentView {
     init(configuration initialConfiguration: InputCard) {
         self.configuration = initialConfiguration
         super.init(frame: .zero)
-        textContentType = .location
         placeholder = "location, eg: 32.123,25.345"
         text = initialConfiguration.cardPayload.location
         var buttonConfiguration = UIButton.Configuration.borderless()
@@ -50,10 +49,32 @@ final class InputCardView: UITextField & UIContentView {
         })
         leftView = button
         leftViewMode = .always
+        keyboardType = .numbersAndPunctuation
+        self.delegate = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet(charactersIn: "0123456789., ")
+        let characterSet = CharacterSet(charactersIn: string)
+        guard allowedCharacters.isSuperset(of: characterSet) else {
+            return false
+        }
+        
+        // Check if the replacement text contains more than one comma or two dots
+        let existingText = self.text ?? ""
+        let updatedText = (existingText as NSString).replacingCharacters(in: range, with: string)
+        let numbers = updatedText.components(separatedBy: ",")
+        let commaCount = numbers.count - 1
+        let isDoubleDot = numbers.contains { $0.components(separatedBy: ".").count > 2 }
+        guard commaCount < 2, !isDoubleDot else {
+            return false
+        }
+        
+        return true
     }
     
     func configure(configuration: UIContentConfiguration) {
