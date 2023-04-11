7cc5acf4656f46938b91fce86d7227bb1e7e16f9
Toni Sevener <tsevener@wikimedia.org>
2023-04-11 15:35:24 -0500

    Try again

--- ci_scripts/copy_sourceroot.sh
+++ ci_scripts/copy_sourceroot.sh
@@ -1,6 +1,8 @@
  #!/bin/sh
  
- cd ../WikipediaUnitTests/
+ set -e
+ 
+ cd WikipediaUnitTests/
  plutil -replace SourceRoot -string $CI_WORKSPACE Info.plist
  plutil -p Info.plist
  echo "CI_WORKSPACE value successfully copied into Info.plist SourceRoot key."
