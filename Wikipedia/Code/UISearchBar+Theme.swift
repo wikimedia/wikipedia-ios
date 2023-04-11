259263e4018c1b7d6854ca5f4886f334f6657003
Sandeep Kumar <sandeepkrofc@gmail.com>
2023-04-12 01:56:04 +0530

    Adding support to configure placeholder text

--- Wikipedia/Code/UISearchBar+Theme.swift
+++ Wikipedia/Code/UISearchBar+Theme.swift
@@ -8,5 +8,12 @@ extension UISearchBar: Themeable {
         }
         searchTextPositionAdjustment = UIOffset(horizontal: 7, vertical: 0)
         setSearchFieldBackgroundImage(theme.searchFieldBackgroundImage, for: .normal)
+        
+        if let placeholder = searchTextField.placeholder {
+            let placeholderAttributes = [NSAttributedString.Key.foregroundColor: theme.colors.secondaryText]
+            searchTextField.attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                                attributes: placeholderAttributes)
+        }
+        searchTextField.leftView?.tintColor = theme.colors.secondaryText
     }
 }
