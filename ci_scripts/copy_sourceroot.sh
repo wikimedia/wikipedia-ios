3278687cc955fa79e897b0597ada637f6fc2b09c
Toni Sevener <tsevener@wikimedia.org>
2023-04-11 15:06:51 -0500

    Try copying alternative SourceRoot

---
+++ ci_scripts/copy_sourceroot.sh
@@ -0,0 +1,7 @@
+ #!/bin/sh
+ 
+ cd ../WikipediaUnitTests/
+ plutil -replace SourceRoot -string $CI_WORKSPACE Info.plist
+ plutil -p Info.plist
+ echo "CI_WORKSPACE value successfully copied into Info.plist SourceRoot key."
+ exit 0
