00c15fd48d83be13a883797e293afd9b419e6051
Toni Sevener <tsevener@wikimedia.org>
2023-04-11 15:43:10 -0500

    Revert "Try again"
    This reverts commit 7cc5acf4656f46938b91fce86d7227bb1e7e16f9.

--- ci_scripts/copy_sourceroot.sh
+++ ci_scripts/copy_sourceroot.sh
@@ -1,8 +1,6 @@
  #!/bin/sh
  
- set -e
- 
- cd WikipediaUnitTests/
+ cd ../WikipediaUnitTests/
  plutil -replace SourceRoot -string $CI_WORKSPACE Info.plist
  plutil -p Info.plist
  echo "CI_WORKSPACE value successfully copied into Info.plist SourceRoot key."
