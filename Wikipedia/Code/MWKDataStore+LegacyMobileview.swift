8e4ac41208f6a27dac2f23289ea7b560a538e048
Toni Sevener <tsevener@wikimedia.org>
2023-04-24 16:36:45 -0500

    Fix migration failure loop

--- Wikipedia/Code/MWKDataStore+LegacyMobileview.swift
+++ Wikipedia/Code/MWKDataStore+LegacyMobileview.swift
@@ -17,58 +17,58 @@ extension MWKDataStore {
             DDLogError("migrateMobileviewToMobileHTMLIfNecessary - End 1")
             return
         }
+        
+        let disableOfflineConversion = {
+            DispatchQueue.main.async {
+                do {
+                    article.isDownloaded = false
+                    article.isConversionFromMobileViewNeeded = false
+                    try self.save()
+                    DDLogError("migrateMobileviewToMobileHTMLIfNecessary - handleConversionFailure save")
+                } catch let error {
+                    DDLogError("Error updating article: \(error)")
+                }
+            }
+        }
+        
         guard let articleURL = article.url else {
             assertionFailure("Could not get article url")
+            disableOfflineConversion()
             completionHandler(MigrateMobileviewToMobileHTMLIfNecessaryError.noArticleURL)
                 DDLogError("migrateMobileviewToMobileHTMLIfNecessary - End 2")
             return
         }
-
+        
         let articleCacheController = cacheController.articleCache
         let articleFolderURL = URL(fileURLWithPath: path(forArticleURL: articleURL))
         guard let legacyArticle = LegacyArticle(articleFolderURL: articleFolderURL) else {
+            disableOfflineConversion()
             completionHandler(MigrateMobileviewToMobileHTMLIfNecessaryError.noLegacyArticleData)
                     DDLogError("migrateMobileviewToMobileHTMLIfNecessary - End 3")
             return
         }
+        
+        let removeArticleMobileviewSavedDataFolder = {
+            // Remove old mobileview saved data folder for this article
+            do {
+                try FileManager.default.removeItem(atPath: self.path(forArticleURL: articleURL))
+            } catch {
+                DDLogError("Could not remove mobileview folder for articleURL: \(articleURL)")
+            }
+        }
 
         mobileviewConverter.convertMobileviewSavedDataToMobileHTML(articleURL: articleURL, article: legacyArticle) { (result, error) in
-            let removeArticleMobileviewSavedDataFolder = {
-                // Remove old mobileview saved data folder for this article
-                do {
-                    try FileManager.default.removeItem(atPath: self.path(forArticleURL: articleURL))
-                } catch {
-                    DDLogError("Could not remove mobileview folder for articleURL: \(articleURL)")
-                }
-            }
-            
-            let handleConversionFailure = {
-                // No need to keep mobileview section html if conversion failed, so ok to remove section data
-                // because we're setting `isDownloaded` next so saved article fetching will re-download from
-                // new mobilehtml endpoint.
-                removeArticleMobileviewSavedDataFolder()
-
-                // If conversion failed above for any reason set "article.isDownloaded" to false so normal fetching logic picks it up
-                DispatchQueue.main.async {
-                    do {
-                        article.isDownloaded = false
-                        article.isConversionFromMobileViewNeeded = false
-                        try self.save()
-                        DDLogError("migrateMobileviewToMobileHTMLIfNecessary - handleConversionFailure save")
-                    } catch let error {
-                        DDLogError("Error updating article: \(error)")
-                    }
-                }
-            }
             
             guard error == nil, let result = result else {
-                handleConversionFailure()
+                removeArticleMobileviewSavedDataFolder()
+                disableOfflineConversion()
                 completionHandler(error)
                 DDLogError("migrateMobileviewToMobileHTMLIfNecessary - End 4")
                 return
             }
             guard let mobileHTML = result as? String else {
-                handleConversionFailure()
+                removeArticleMobileviewSavedDataFolder()
+                disableOfflineConversion()
                 completionHandler(MigrateMobileviewToMobileHTMLIfNecessaryError.noMobileHTML)
                 DDLogError("migrateMobileviewToMobileHTMLIfNecessary - End 5")
                 return
