8a09f178d490c0613d5c500948510b8327e462af
Deepak Mantena <staykids@users.noreply.github.com>
2023-05-16 13:25:34 -0700

    Add feature flags for watchlist and updated diffs.

--- WMF Framework/FeatureFlags.swift
+++ WMF Framework/FeatureFlags.swift
@@ -5,5 +5,21 @@ public struct FeatureFlags {
     public static var needsNewTalkPage: Bool {
         return true
     }
+
+    public static var watchlistEnabled: Bool {
+        #if WMF_STAGING
+        return true
+        #else
+        return false
+        #endif
+    }
+
+    public static var updatedDiffsEnabled: Bool {
+        #if WMF_STAGING
+        return true
+        #else
+        return false
+        #endif
+    }
     
 }
