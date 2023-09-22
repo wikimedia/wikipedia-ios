d22e93a442ab7aa64fe824e2dd744500260e36c3
Toni Sevener <tsevener@wikimedia.org>
2023-09-22 18:41:23 -0500

    Add build script for copying merchant ID and payments API key to appropriate files

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -8,7 +8,21 @@ if [[ ${CI_WORKFLOW} == "Run Tests" ]]; then
 	./copy_sourceroot.sh
 	echo "Execute copy source root."
 	exit 0
-else
-	echo "Did not execute copy source root."
-	exit 0
 fi
+
+EntitlementsFile="${CI_WORKSPACE}/Wikipedia/Wikipedia.entitlements"
+
+if [[ ${CI_WORKFLOW} == "Weekly Staging Build" ]]; then
+    InfoPListFile="${CI_WORKSPACE}/Wikipedia/Staging-Info.plist"
+    ./copy_environment_vars.sh EntitlementsFile InfoPListFile $MERCHANT_ID $PAYMENTS_API_KEY
+    echo "Execute copy merchant IDs."
+    exit 0
+    
+elif [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
+    InfoPListFile="${CI_WORKSPACE}/Wikipedia/Wikipedia-Info.plist"
+    ./copy_environment_vars.sh EntitlementsFile InfoPListFile $MERCHANT_ID $PAYMENTS_API_KEY
+fi
+
+    
+
+exit 0
