37e9ce87ccae358d8a0ddc2a4dbfd47bfd206601
Toni Sevener <tsevener@wikimedia.org>
2023-09-29 09:41:26 -0500

    Don't change donate config fetch

--- Wikipedia/Code/WMFAnnouncementsContentSource+Extensions.swift
+++ Wikipedia/Code/WMFAnnouncementsContentSource+Extensions.swift
@@ -8,18 +8,16 @@ extension WMFAnnouncementsContentSource {
         let fundraisingCampaignDataController = WKFundraisingCampaignDataController()
         let currentDate = Date.now
         fundraisingCampaignDataController.fetchConfig(countryCode: countryCode, currentDate: currentDate) { result in
-            
-            if fundraisingCampaignDataController.hasActivelyRunningCampaigns(countryCode: countryCode, currentDate: currentDate) {
-                
-                guard let paymentsAPIKey = Bundle.main.wmf_paymentsAPIKey() else {
-                    return
-                }
-                
-                let donateDataController = WKDonateDataController()
-                donateDataController.fetchConfigs(for: countryCode, paymentsAPIKey: paymentsAPIKey) { result in
 
-                }
-            }
+        }
+        
+        guard let paymentsAPIKey = Bundle.main.wmf_paymentsAPIKey() else {
+            return
+        }
+        
+        let donateDataController = WKDonateDataController()
+        donateDataController.fetchConfigs(for: countryCode, paymentsAPIKey: paymentsAPIKey) { result in
+
         }
     }
 }
