b4217df9587520212abc189a9ba6c3c72f9d43f2
Toni Sevener <tsevener@wikimedia.org>
2023-10-05 13:04:07 -0500

    Add isDonationURL extension for logging

--- WMF Framework/URL+Extensions.swift
+++ WMF Framework/URL+Extensions.swift
@@ -4,4 +4,8 @@ public extension URL {
     var isThankYouDonationURL: Bool {
         return self.host == "thankyou.wikipedia.org" || self.host == "thankyou.wikimedia.org"
     }
+    
+    var isDonationURL: Bool {
+        return self.host == "donate.wikipedia.org" || self.host == "donate.wikimedia.org"
+    }
 }
