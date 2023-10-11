f0471987790165b17fb9f05fe3f47634d99c4123
Toni Sevener <tsevener@wikimedia.org>
2023-10-11 09:58:34 -0500

    Wrap in donation URL check to reduce change risk

--- Wikipedia/Code/SinglePageWebViewController.swift
+++ Wikipedia/Code/SinglePageWebViewController.swift
@@ -105,7 +105,9 @@ class SinglePageWebViewController: ViewController {
             let searchItem = AppSearchBarButtonItem.newAppSearchBarButtonItem
             navigationItem.setRightBarButtonItems([searchItem, safariItem], animated: false)
 
-            navigationBar.isInteractiveHidingEnabled = false
+            if url.isDonationURL {
+                navigationBar.isInteractiveHidingEnabled = false
+            }
             setupWButton()
         }
 
