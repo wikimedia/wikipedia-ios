b76de52603837f98d7b2e7dea9a7724875c3d949
Toni Sevener <tsevener@wikimedia.org>
2023-12-15 14:36:20 -0600

    Apply strikethrough selection state to input view

--- Components/Sources/Components/Components/Editors/Common Views/Input Views/Main/Grouped Toolbar Table Item/WKEditorToolbarGroupedView.swift
+++ Components/Sources/Components/Components/Editors/Common Views/Input Views/Main/Grouped Toolbar Table Item/WKEditorToolbarGroupedView.swift
@@ -49,6 +49,18 @@ class WKEditorToolbarGroupedView: WKEditorToolbarView {
         strikethroughButton.setImage(WKSFSymbolIcon.for(symbol: .strikethrough), for: .normal)
         strikethroughButton.addTarget(self, action: #selector(tappedStrikethrough), for: .touchUpInside)
         strikethroughButton.accessibilityLabel = WKSourceEditorLocalizedStrings.current?.accessibilityLabelButtonStrikethrough
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateButtonSelectionState(_:)), name: Notification.WKSourceEditorSelectionState, object: nil)
+    }
+    
+    // MARK: - Notifications
+        
+    @objc private func updateButtonSelectionState(_ notification: NSNotification) {
+        guard let selectionState = notification.userInfo?[Notification.WKSourceEditorSelectionStateKey] as? WKSourceEditorSelectionState else {
+            return
+        }
+
+        strikethroughButton.isSelected = selectionState.isStrikethrough
     }
     
     // MARK: - Button Actions
