8d73c22e17d56492cb2799b5123ce78ac6646965
Toni Sevener <tsevener@wikimedia.org>
2023-12-15 10:09:06 -0600

    Allow ability to toggle starting formatting string with wildcard

--- Components/Sources/Components/Components/Editors/Source Editor/Formatter Extensions/WKSourceEditorReference+ButtonActions.swift
+++ Components/Sources/Components/Components/Editors/Source Editor/Formatter Extensions/WKSourceEditorReference+ButtonActions.swift
@@ -3,6 +3,6 @@ import ComponentsObjC
 
 extension WKSourceEditorFormatterReference {
     func toggleReferenceFormatting(action: WKSourceEditorFormatterButtonAction, in textView: UITextView) {
-        toggleFormatting(startingFormattingString: "<ref>", endingFormattingString: "</ref>", action: action, in: textView)
+        toggleFormatting(startingFormattingString: "<ref>", wildcardStartingFormattingString: "<ref*>", endingFormattingString: "</ref>", action: action, in: textView)
     }
 }
