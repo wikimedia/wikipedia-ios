1ea06abde4cbfa7aef6485c6eedf1a816530a466
Toni Sevener <tsevener@wikimedia.org>
2024-01-18 14:03:42 -0600

    Add tests

--- Components/Tests/ComponentsTests/WKSourceEditorFormatterTests.swift
+++ Components/Tests/ComponentsTests/WKSourceEditorFormatterTests.swift
@@ -13,8 +13,9 @@ final class WKSourceEditorFormatterTests: XCTestCase {
     var listFormatter: WKSourceEditorFormatterList!
     var headingFormatter: WKSourceEditorFormatterHeading!
     var strikethroughFormatter: WKSourceEditorFormatterStrikethrough!
+    var findAndReplaceFormatter: WKSourceEditorFormatterFindAndReplace!
     var formatters: [WKSourceEditorFormatter] {
-        return [baseFormatter, templateFormatter, boldItalicsFormatter, listFormatter, headingFormatter, strikethroughFormatter]
+        return [baseFormatter, templateFormatter, boldItalicsFormatter, listFormatter, headingFormatter, strikethroughFormatter, findAndReplaceFormatter]
     }
 
     override func setUpWithError() throws {
@@ -25,6 +26,10 @@ final class WKSourceEditorFormatterTests: XCTestCase {
         self.colors.orangeForegroundColor = WKTheme.light.editorOrange
         self.colors.purpleForegroundColor = WKTheme.light.editorPurple
         self.colors.greenForegroundColor = WKTheme.light.editorGreen
+        self.colors.matchForegroundColor = WKTheme.light.editorMatchForeground
+        self.colors.matchBackgroundColor = WKTheme.light.editorMatchBackground
+        self.colors.selectedMatchBackgroundColor = WKTheme.light.editorSelectedMatchBackground
+        self.colors.replacedMatchBackgroundColor = WKTheme.light.editorReplacedMatchBackground
         
         self.fonts = WKSourceEditorFonts()
         self.fonts.baseFont = WKFont.for(.body, compatibleWith: traitCollection)
@@ -43,6 +48,7 @@ final class WKSourceEditorFormatterTests: XCTestCase {
         self.listFormatter = WKSourceEditorFormatterList(colors: colors, fonts: fonts)
         self.headingFormatter = WKSourceEditorFormatterHeading(colors: colors, fonts: fonts)
         self.strikethroughFormatter = WKSourceEditorFormatterStrikethrough(colors: colors, fonts: fonts)
+        self.findAndReplaceFormatter = WKSourceEditorFormatterFindAndReplace(colors: colors, fonts: fonts)
     }
 
     override func tearDownWithError() throws {
@@ -1144,5 +1150,89 @@ final class WKSourceEditorFormatterTests: XCTestCase {
         XCTAssertEqual(base2Range.length, 8, "Incorrect base formatting")
         XCTAssertEqual(base2Attributes[.font] as! UIFont, fonts.baseFont, "Incorrect base formatting")
         XCTAssertEqual(base2Attributes[.foregroundColor] as! UIColor, colors.baseForegroundColor, "Incorrect base formatting")
+    }
+    
+    func testFind() {
+        let string = "Find a '''word''' and highlight that word."
+        let mutAttributedString = NSMutableAttributedString(string: string)
+
+        for formatter in formatters {
+            formatter.addSyntaxHighlighting(to: mutAttributedString, in: NSRange(location: 0, length: string.count))
+        }
+        
+        findAndReplaceFormatter.startMatchSession(withFullAttributedString: mutAttributedString, searchText: "word")
+        findAndReplaceFormatter.highlightNextMatch(inFullAttributedString: mutAttributedString, afterRangeValue: nil)
+        
+        // "Find a "
+        var base1Range = NSRange(location: 0, length: 0)
+        let base1Attributes = mutAttributedString.attributes(at: 0, effectiveRange: &base1Range)
+        
+        // '''
+        var boldOpenRange = NSRange(location: 0, length: 0)
+        let boldOpenAttributes = mutAttributedString.attributes(at: 7, effectiveRange: &boldOpenRange)
+        
+        // word
+        var match1Range = NSRange(location: 0, length: 0)
+        let match1Attributes = mutAttributedString.attributes(at: 10, effectiveRange: &match1Range)
+        
+        // '''
+        var boldCloseRange = NSRange(location: 0, length: 0)
+        let boldCloseAttributes = mutAttributedString.attributes(at: 14, effectiveRange: &boldCloseRange)
+
+        // " and highlight that "
+        var base2Range = NSRange(location: 0, length: 0)
+        let base2Attributes = mutAttributedString.attributes(at: 17, effectiveRange: &base2Range)
+        
+        // word
+        var match2Range = NSRange(location: 0, length: 0)
+        let match2Attributes = mutAttributedString.attributes(at: 37, effectiveRange: &match2Range)
+        
+        // "."
+        var base3Range = NSRange(location: 0, length: 0)
+        let base3Attributes = mutAttributedString.attributes(at: 41, effectiveRange: &base3Range)
+
+        // "Find a "
+        XCTAssertEqual(base1Range.location, 0, "Incorrect base formatting")
+        XCTAssertEqual(base1Range.length, 7, "Incorrect base formatting")
+        XCTAssertEqual(base1Attributes[.font] as! UIFont, fonts.baseFont, "Incorrect base formatting")
+        XCTAssertEqual(base1Attributes[.foregroundColor] as! UIColor, colors.baseForegroundColor, "Incorrect base formatting")
+        
+        // "'''"
+        XCTAssertEqual(boldOpenRange.location, 7, "Incorrect bold formatting")
+        XCTAssertEqual(boldOpenRange.length, 3, "Incorrect bold formatting")
+        XCTAssertEqual(boldOpenAttributes[.font] as! UIFont, fonts.baseFont, "Incorrect bold formatting")
+        XCTAssertEqual(boldOpenAttributes[.foregroundColor] as! UIColor, colors.orangeForegroundColor, "Incorrect bold formatting")
+        
+        // "word"
+        XCTAssertEqual(match1Range.location, 10, "Incorrect match formatting")
+        XCTAssertEqual(match1Range.length, 4, "Incorrect match formatting")
+        XCTAssertEqual(match1Attributes[.font] as! UIFont, fonts.boldFont, "Incorrect match formatting")
+        XCTAssertEqual(match1Attributes[.foregroundColor] as! UIColor, colors.matchForegroundColor, "Incorrect bmatchase formatting")
+        XCTAssertEqual(match1Attributes[.backgroundColor] as! UIColor, colors.selectedMatchBackgroundColor, "Incorrect match formatting")
+
+        // "'''"
+        XCTAssertEqual(boldCloseRange.location, 14, "Incorrect bold formatting")
+        XCTAssertEqual(boldCloseRange.length, 3, "Incorrect bold formatting")
+        XCTAssertEqual(boldCloseAttributes[.font] as! UIFont, fonts.baseFont, "Incorrect bold formatting")
+        XCTAssertEqual(boldCloseAttributes[.foregroundColor] as! UIColor, colors.orangeForegroundColor, "Incorrect bold formatting")
+        
+        // " and highlight that "
+        XCTAssertEqual(base2Range.location, 17, "Incorrect base formatting")
+        XCTAssertEqual(base2Range.length, 20, "Incorrect base formatting")
+        XCTAssertEqual(base2Attributes[.font] as! UIFont, fonts.baseFont, "Incorrect base formatting")
+        XCTAssertEqual(base2Attributes[.foregroundColor] as! UIColor, colors.baseForegroundColor, "Incorrect base formatting")
+        
+        // "word"
+        XCTAssertEqual(match2Range.location, 37, "Incorrect match formatting")
+        XCTAssertEqual(match2Range.length, 4, "Incorrect match formatting")
+        XCTAssertEqual(match2Attributes[.font] as! UIFont, fonts.baseFont, "Incorrect match formatting")
+        XCTAssertEqual(match2Attributes[.foregroundColor] as! UIColor, colors.matchForegroundColor, "Incorrect bmatchase formatting")
+        XCTAssertEqual(match2Attributes[.backgroundColor] as! UIColor, colors.matchBackgroundColor, "Incorrect match formatting")
+        
+        // "."
+        XCTAssertEqual(base3Range.location, 41, "Incorrect base formatting")
+        XCTAssertEqual(base3Range.length, 1, "Incorrect base formatting")
+        XCTAssertEqual(base3Attributes[.font] as! UIFont, fonts.baseFont, "Incorrect base formatting")
+        XCTAssertEqual(base3Attributes[.foregroundColor] as! UIColor, colors.baseForegroundColor, "Incorrect base formatting")
     }
 }
--- Components/Tests/ComponentsTests/WKSourceEditorTextFrameworkMediatorTests.swift
+++ Components/Tests/ComponentsTests/WKSourceEditorTextFrameworkMediatorTests.swift
@@ -9,7 +9,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
         mediator.updateColorsAndFonts()
         return mediator
     }()
-
+    
     func testBoldItalicsButtonSelectionState() throws {
         
         let text = "One\nTwo '''Three ''Four'' Five''' Six ''Seven'' Eight\nNine"
@@ -83,7 +83,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testClosingBoldSelectionStateCursor() throws {
         let text = "One '''Two''' Three"
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         let selectionStates = mediator.selectionState(selectedDocumentRange: NSRange(location: 10, length: 0))
         XCTAssertTrue(selectionStates.isBold)
     }
@@ -91,7 +91,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testClosingItalicsSelectionStateCursor() throws {
         let text = "One ''Two'' Three"
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         let selectionStates = mediator.selectionState(selectedDocumentRange: NSRange(location: 9, length: 0))
         XCTAssertTrue(selectionStates.isItalics)
     }
@@ -99,7 +99,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testHorizontalTemplateButtonSelectionStateCursor() throws {
         let text = "Testing simple {{Currentdate}} template example."
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "Testing"
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 4, length: 0))
         XCTAssertFalse(selectionStates1.isHorizontalTemplate)
@@ -116,7 +116,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testHorizontalTemplateButtonSelectionStateRange() throws {
         let text = "Testing simple {{Currentdate}} template example."
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "Testing"
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 4, length: 3))
         XCTAssertFalse(selectionStates1.isHorizontalTemplate)
@@ -133,7 +133,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testVerticalTemplateStartButtonSelectionStateCursor() throws {
         let text = "{{Infobox officeholder"
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "Testing"
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 4, length: 0))
         XCTAssertFalse(selectionStates1.isHorizontalTemplate)
@@ -142,7 +142,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testVerticalTemplateParameterButtonSelectionStateCursor() throws {
         let text = "| genus = Felis"
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "Testing"
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 4, length: 0))
         XCTAssertFalse(selectionStates1.isHorizontalTemplate)
@@ -151,7 +151,7 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testVerticalTemplateEndButtonSelectionStateCursor() throws {
         let text = "}}"
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "Testing"
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 1, length: 0))
         XCTAssertFalse(selectionStates1.isHorizontalTemplate)
@@ -208,12 +208,12 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testHorizontalTemplateButtonSelectionStateFormattedRange() throws {
         let text = "Testing inner formatted {{cite web | url=https://en.wikipedia.org | title = The '''Free''' Encyclopedia}} template example."
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         // "cite web | url=https://en.wikipedia.org | title = The '''Free''' Encyclopedia"
         let selectionStates = mediator.selectionState(selectedDocumentRange: NSRange(location: 26, length: 77))
         XCTAssertTrue(selectionStates.isHorizontalTemplate)
     }
-
+    
     func testHeadingSelectionState() throws {
         
         let text = "== Test =="
@@ -313,11 +313,11 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
         XCTAssertFalse(selectionStates2.isSubheading3)
         XCTAssertTrue(selectionStates2.isSubheading4)
     }
-
+    
     func testStrikethroughSelectionState() throws {
         let text = "Testing <s>Strikethrough</s> Testing."
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 0, length: 7))
         let selectionStates2 = mediator.selectionState(selectedDocumentRange: NSRange(location: 11, length: 13))
         let selectionStates3 = mediator.selectionState(selectedDocumentRange: NSRange(location: 29, length: 7))
@@ -329,12 +329,40 @@ final class WKSourceEditorTextFrameworkMediatorTests: XCTestCase {
     func testStrikethroughSelectionStateCursor() throws {
         let text = "Testing <s>Strikethrough</s> Testing."
         mediator.textView.attributedText = NSAttributedString(string: text)
-
+        
         let selectionStates1 = mediator.selectionState(selectedDocumentRange: NSRange(location: 3, length: 0))
         let selectionStates2 = mediator.selectionState(selectedDocumentRange: NSRange(location: 17, length: 0))
         let selectionStates3 = mediator.selectionState(selectedDocumentRange: NSRange(location: 33, length: 0))
         XCTAssertFalse(selectionStates1.isStrikethrough)
         XCTAssertTrue(selectionStates2.isStrikethrough)
         XCTAssertFalse(selectionStates3.isStrikethrough)
+    }
+    
+    func testFindWithResults() throws {
+        let text = "Find a '''word''' and highlight that word."
+        mediator.textView.attributedText = NSAttributedString(string: text)
+        
+        mediator.findStart(text: "word")
+        guard let formatter = mediator.findAndReplaceFormatter else {
+            XCTFail("Missing find formatter.")
+            return
+        }
+        
+        XCTAssertEqual(formatter.selectedMatchIndex, 0, "Find - Incorrect selected match index")
+        XCTAssertEqual(formatter.matchCount, 2, "Find - Incorrect match count")
+    }
+    
+    func testFindWithoutResults() throws {
+        let text = "Find a '''word''' and highlight that word."
+        mediator.textView.attributedText = NSAttributedString(string: text)
+        
+        mediator.findStart(text: "cat")
+        guard let formatter = mediator.findAndReplaceFormatter else {
+            XCTFail("Missing find formatter.")
+            return
+        }
+        
+        XCTAssertEqual(formatter.selectedMatchIndex, NSNotFound, "Find - Incorrect selected match index")
+        XCTAssertEqual(formatter.matchCount, 0, "Find - Incorrect match count")
     }
 }
