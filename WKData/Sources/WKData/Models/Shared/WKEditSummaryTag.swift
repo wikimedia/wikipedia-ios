648be2dc45aaf8b2467a598a4fa227c3251c0b74
Toni Sevener <tsevener@wikimedia.org>
2024-02-05 22:06:21 -0600

    Add edit summary tag enum to WKData

---
+++ WKData/Sources/WKData/Models/Shared/WKEditSummaryTag.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/// Hashtags appended to edit summaries throughout the app, for identification in mediawiki history.
+public enum WKEditSummaryTag: String {
+    case articleSectionSourceEditor = "#article_section_source_editor"
+    case articleFullSourceEditor = "#article_full_source_editor"
+    case articleSelectSourceEditor = "#article_seelct_source_editor"
+    case talkFullSourceEditor = "#talk_full_source_editor"
+    case articleDescriptionAdd = "#article_description_add"
+    case articleDescriptionChange = "#article_description_change"
+    case articleHistoryDiffUndo = "#article_history_diff_undo"
+    case articleHistoryDiffRollback = "#article_history_diff_rollback"
+    case watchlistDiffUndo = "#watchlist_diff_undo"
+    case watchlistDiffRollback = "#watchlist_diff_rollback"
+    case talkReply = "#talk_reply"
+    case talkTopic = "#talk_topic"
+}
