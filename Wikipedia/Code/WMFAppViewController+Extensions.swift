1b164a0f2a051067353ef48c4a11e2597fae930a
Deepak Mantena <staykids@users.noreply.github.com>
2024-02-16 14:31:25 -0800

    Recompute localized Wikipedia titles after user adds new language.

--- Wikipedia/Code/WMFAppViewController+Extensions.swift
+++ Wikipedia/Code/WMFAppViewController+Extensions.swift
@@ -283,14 +283,6 @@ extension WMFAppViewController: WKWatchlistDelegate {
     }
 
     public func watchlistUserDidTapAddLanguage(from viewController: UIViewController, viewModel: WKWatchlistFilterViewModel) {
-        displayAddLanguageSettingsForWatchlistFilters(from: viewController) {
-            // TODO
-            // Handle reloading filter view model projects when changing languages
-            viewModel.reload()
-        }
-    }
-
-    public func displayAddLanguageSettingsForWatchlistFilters(from viewController: UIViewController, completion: @escaping () -> Void) {
         let languagesController = WMFLanguagesViewController(nibName: "WMFLanguagesViewController", bundle: nil)
         languagesController.title = CommonStrings.wikipediaLanguages
         languagesController.apply(theme)
@@ -298,6 +290,25 @@ extension WMFAppViewController: WKWatchlistDelegate {
         languagesController.showAllLanguages = true
         languagesController.showPreferredLanguages = false
         languagesController.showNonPreferredLanguages = false
+
+        languagesController.userLanguageSelectionBlock = { [weak self, weak viewModel] in
+            guard let self = self else { return }
+
+            // From `ViewControllerRouter`
+            let dataStore = self.dataStore
+            let appLanguages = dataStore.languageLinkController.preferredLanguages
+            var localizedProjectNames = appLanguages.reduce(into: [WKProject: String]()) { result, language in
+                guard let wikimediaProject = WikimediaProject(siteURL: language.siteURL, languageLinkController: dataStore.languageLinkController), let wkProject = wikimediaProject.wkProject else {
+                    return
+                }
+
+                result[wkProject] = wikimediaProject.projectName(shouldReturnCodedFormat: false)
+            }
+            localizedProjectNames[.wikidata] = WikimediaProject.wikidata.projectName(shouldReturnCodedFormat: false)
+            localizedProjectNames[.commons] = WikimediaProject.commons.projectName(shouldReturnCodedFormat: false)
+
+            viewModel?.reloadWikipedias(localizedProjectNames: localizedProjectNames)
+        }
 
         let navigationController = WMFThemeableNavigationController(rootViewController: languagesController, theme: theme)
         viewController.present(navigationController, animated: true)
@@ -308,7 +319,9 @@ extension WMFAppViewController: WKWatchlistDelegate {
 extension WMFAppViewController: WMFLanguagesViewControllerDelegate {
 
     public func languagesController(_ controller: WMFLanguagesViewController, didSelectLanguage language: MWKLanguageLink) {
-        //
+        dataStore.languageLinkController.appendPreferredLanguage(language)
+        controller.userLanguageSelectionBlock?()
+        controller.dismiss(animated: true)
     }
 
 }
