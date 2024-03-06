b6db18439c2afd523e9b1eaaefb078e9b647e55d
mazevedo <mazevedo@wikimedia.org>
2024-03-06 18:01:42 -0300

    Add view to bottom vc

--- Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationBottomSheetViewController.swift
+++ Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationBottomSheetViewController.swift
@@ -1,12 +1,14 @@
 import UIKit
 import WKData
 
-final public class WKBottomSheetViewController: WKCanvasViewController {
+final public class WKImageRecommendationBottomSheetViewController: WKCanvasViewController {
 
-    public var viewModel: WKImageRecommendationBottomSheetViewModel
+    public var viewModel: WKImageRecommendationsViewModel
+    public var imageData: ImageRecommendationData
 
-    public init(viewModel: WKImageRecommendationBottomSheetViewModel) {
+    public init(viewModel: WKImageRecommendationsViewModel, imageData: ImageRecommendationData) {
         self.viewModel = viewModel
+        self.imageData = imageData
         super.init()
     }
 
@@ -17,7 +19,39 @@ final public class WKBottomSheetViewController: WKCanvasViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let viewModel = populateImageSheetRecViewModel(for: imageData) {
+            let newView = WKImageRecommendationBottomSheetView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), viewModel: viewModel)
+            addComponent(newView, pinToEdges: true)
+        }
 
+    }
+
+    public override func loadView() {
+        super.loadView()
+
+
+    }
+
+    func populateImageSheetRecViewModel(for image: ImageRecommendationData?) -> WKImageRecommendationBottomSheetViewModel? {
+
+        if let image {
+            let viewModel = WKImageRecommendationBottomSheetViewModel(
+                pageId: image.pageId,
+                headerTitle: viewModel.localizedStrings.bottomSheetTitle,
+                headerIcon: UIImage(),
+                image: UIImage(), // populatewithThumb and not init
+                imageLink: image.fullUrl,
+                thumbLink: image.thumbUrl,
+                imageTitle: image.filename,
+                imageDescription: image.description,
+                yesButtonTitle: viewModel.localizedStrings.yesButtonTitle,
+                noButtonTitle: viewModel.localizedStrings.noButtonTitle,
+                notSureButtonTitle: viewModel.localizedStrings.notSureButtonTitle
+            )
+            return viewModel
+        }
+
+        return nil
     }
 
 }
