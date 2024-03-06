dcb39aed1a50ff77c9c3b765b286c858b9978930
mazevedo <mazevedo@wikimedia.org>
2024-03-06 18:02:25 -0300

    Update bottom sheet view model

--- Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationBottomSheetViewModel.swift
+++ Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationBottomSheetViewModel.swift
@@ -3,29 +3,31 @@ import WKData
 
 public struct WKImageRecommendationBottomSheetViewModel {
 
+    let pageId: Int
     let headerTitle: String
     let headerIcon: UIImage?
     let image: UIImage
     let imageLink: String
+    let thumbLink: String
     let imageTitle: String
-    let imageDescription: String
+    let imageDescription: String?
     let yesButtonTitle: String
     let noButtonTitle: String
     let notSureButtonTitle: String
-    var imageRecommendations: [WKImageRecommendation]?
-    var pageID: String
 
-    public init(headerTitle: String, headerIcon: UIImage?, image: UIImage, imageLink: String, imageTitle: String, imageDescription: String, yesButtonTitle: String, noButtonTitle: String, notSureButtonTitle: String, pageID: String) {
+    public init(pageId: Int, headerTitle: String, headerIcon: UIImage?, image: UIImage, imageLink: String, thumbLink: String, imageTitle: String, imageDescription: String?, yesButtonTitle: String, noButtonTitle: String, notSureButtonTitle: String) {
+        self.pageId = pageId
         self.headerTitle = headerTitle
         self.headerIcon = headerIcon
         self.image = image
         self.imageLink = imageLink
+        self.thumbLink = thumbLink
         self.imageTitle = imageTitle
         self.imageDescription = imageDescription
         self.yesButtonTitle = yesButtonTitle
         self.noButtonTitle = noButtonTitle
         self.notSureButtonTitle = notSureButtonTitle
-        self.pageID = pageID
+
     }
 
 }
