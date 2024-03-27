2d5ce831445b9e90ded324082b1a3e969aa1809b
Toni Sevener <tsevener@wikimedia.org>
2024-03-27 15:39:05 -0500

    Make pageTitle and fileName available from WKImageRecommendationData

--- Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationsViewModel.swift
+++ Components/Sources/Components/Components/Suggested Edits/Image Recommendations/WKImageRecommendationsViewModel.swift
@@ -54,18 +54,22 @@ public final class WKImageRecommendationsViewModel: ObservableObject {
 
     public struct WKImageRecommendationData {
         public let pageId: Int
+        public let pageTitle: String
         public let image: String
         public let filename: String
+        public let displayFilename: String
         public let thumbUrl: String
         public let fullUrl: String
         public let description: String?
         public let descriptionURL: String
         public let reason: String
 
-        public init(pageId: Int, image: String, filename: String, thumbUrl: String, fullUrl: String, description: String?, descriptionURL: String, reason: String) {
+        public init(pageId: Int, pageTitle: String, image: String, filename: String, displayFilename: String, thumbUrl: String, fullUrl: String, description: String?, descriptionURL: String, reason: String) {
             self.pageId = pageId
+            self.pageTitle = pageTitle
             self.image = image
             self.filename = filename
+            self.displayFilename = displayFilename
             self.thumbUrl = thumbUrl
             self.fullUrl = fullUrl
             self.description = description
@@ -228,8 +232,10 @@ public final class WKImageRecommendationsViewModel: ObservableObject {
                 let metadata = firstImage.metadata
                 let imageRecommendation = WKImageRecommendationData(
                     pageId: page.pageid,
+                    pageTitle: page.title,
                     image: firstImage.image,
-                    filename: firstImage.displayFilename,
+                    filename: firstImage.image,
+                    displayFilename: firstImage.displayFilename,
                     thumbUrl: metadata.thumbUrl,
                     fullUrl: metadata.fullUrl,
                     description: metadata.description,
