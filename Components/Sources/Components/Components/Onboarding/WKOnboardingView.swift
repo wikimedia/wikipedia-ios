1667c48a3f0afed5061ce64679112fa8f77c5aec
Deepak Mantena <staykids@users.noreply.github.com>
2024-04-03 13:01:40 -0700

    Pin onboarding buttons to bottom of view and adjust spacing.

--- Components/Sources/Components/Components/Onboarding/WKOnboardingView.swift
+++ Components/Sources/Components/Components/Onboarding/WKOnboardingView.swift
@@ -18,37 +18,50 @@ public struct WKOnboardingView: View {
 
     // MARK: - Lifecycle
 
-    public var body: some View {
-        ZStack {
-            Color(appEnvironment.theme.paperBackground)
-                .ignoresSafeArea()
-            ScrollView(showsIndicators: true) {
-                VStack {
-                    Text(viewModel.title)
-                        .font(Font(WKFont.for(.boldTitle1)))
-                        .foregroundColor(Color(appEnvironment.theme.text))
-                        .padding([.bottom, .top], 44)
-                        .multilineTextAlignment(.center)
-                    ForEach(1...viewModel.cells.count, id:\.self) { cell in
-                        VStack {
-                            WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
-                                .padding([.bottom, .trailing], 20)
-                        }
-                    }
-                    Spacer()
-                    
-                    WKLargeButton(configuration: .primary, title: viewModel.primaryButtonTitle, action: primaryButtonAction)
-                        .padding([.top], 16)
+	var content: some View {
+		Group {
+			ScrollView(showsIndicators: true) {
+				VStack {
+					Text(viewModel.title)
+						.font(Font(WKFont.for(.boldTitle1)))
+						.foregroundColor(Color(appEnvironment.theme.text))
+						.padding(.bottom, 44)
+						.padding(.top, 44 + sizeClassPadding)
+						.multilineTextAlignment(.center)
+					ForEach(1...viewModel.cells.count, id:\.self) { cell in
+						VStack {
+							WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
+								.padding(.bottom, 24)
+								.padding(.trailing, 20)
+						}
+					}
+				}
+				.padding(.bottom, 125)
+			}
+			ZStack(alignment: .bottom, content: {
+				VStack {
+					WKLargeButton(configuration: .primary, title: viewModel.primaryButtonTitle, action: primaryButtonAction)
+						.padding([.top], 16)
 
-                    VStack {
-                        if let secondaryTitle = viewModel.secondaryButtonTitle {
-                            let configuration = WKSmallButton.Configuration(style: .quiet)
-                            WKSmallButton(configuration: configuration, title: secondaryTitle, action: secondaryButtonAction)
-                        }
-                    }
-                }
-                .padding(sizeClassPadding)
-            }
-        }
+					if let secondaryTitle = viewModel.secondaryButtonTitle {
+						let configuration = WKSmallButton.Configuration(style: .quiet)
+						WKSmallButton(configuration: configuration, title: secondaryTitle, action: secondaryButtonAction)
+					}
+				}
+			})
+		}
+		.padding([.leading, .trailing, .bottom], sizeClassPadding)
+	}
+
+    public var body: some View {
+		ZStack(alignment: .bottom) {
+			Color(appEnvironment.theme.paperBackground).ignoresSafeArea()
+			content
+		}		
     }
+
+}
+
+#Preview {
+	WKOnboardingView(viewModel: .init(title: "Onboarding View", cells: [.init(icon: .add, title: "Title 1", subtitle: "Subtitle 1"), .init(icon: .checkmark, title: "Title 2", subtitle: "Subtitle 2")], primaryButtonTitle: "Primary Button", secondaryButtonTitle: "Secondary Button"))
 }
