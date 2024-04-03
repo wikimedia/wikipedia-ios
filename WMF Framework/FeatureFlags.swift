1fc5f0749e788146143abb0c9f278e54d9e12317
Toni Sevener <tsevener@wikimedia.org>
2024-04-03 16:43:56 -0500

    Add feature flag to allow triggering empty state from Test Wiki

--- WMF Framework/FeatureFlags.swift
+++ WMF Framework/FeatureFlags.swift
@@ -22,11 +22,26 @@ public struct FeatureFlags {
         return false
     #endif
     }
+    
+    // Bypasses card display conditional (50+ edits on primary app wiki, not blocked, wiki has recommendations)
+    // This allows for easier design review on Experimental app
+    public static var forceImageRecommendationsExploreCard: Bool {
+        
+    #if WMF_EXPERIMENTAL
+        return true
+    #else
+        return false
+    #endif
+    }
 }
 
 @objc public class WMFFeatureFlags: NSObject {
     
     @objc public static var needsImageRecommendations: Bool {
         return FeatureFlags.needsImageRecommendations
+    }
+    
+    @objc public static var forceImageRecommendationsExploreCard: Bool {
+        return FeatureFlags.forceImageRecommendationsExploreCard
     }
 }
