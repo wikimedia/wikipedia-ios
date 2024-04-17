b935e76cfe6104aff035d8c7cc082230cff41af2
Toni Sevener <tsevener@wikimedia.org>
2024-04-17 15:50:16 -0500

    Update data controller to handle tooltips user defaults flag

--- WKData/Sources/WKData/Data Controllers/Image Recommendations/WKImageRecommendationsDataController.swift
+++ WKData/Sources/WKData/Data Controllers/Image Recommendations/WKImageRecommendationsDataController.swift
@@ -7,9 +7,10 @@ public class WKImageRecommendationsDataController {
 
 	struct OnboardingStatus: Codable {
 		var hasPresentedOnboardingModal: Bool
+        var hasPresentedOnboardingTooltips: Bool
 
 		static var `default`: OnboardingStatus {
-			return OnboardingStatus(hasPresentedOnboardingModal: false)
+            return OnboardingStatus(hasPresentedOnboardingModal: false, hasPresentedOnboardingTooltips: false)
 		}
 	}
 
@@ -39,6 +40,16 @@ public class WKImageRecommendationsDataController {
 			try? userDefaultsStore?.save(key: WKUserDefaultsKey.imageRecommendationsOnboarding.rawValue, value: currentOnboardingStatus)
 		}
 	}
+    
+    public var hasPresentedOnboardingTooltips: Bool {
+        get {
+            return onboardingStatus.hasPresentedOnboardingTooltips
+        } set {
+            var currentOnboardingStatus = onboardingStatus
+            currentOnboardingStatus.hasPresentedOnboardingTooltips = newValue
+            try? userDefaultsStore?.save(key: WKUserDefaultsKey.imageRecommendationsOnboarding.rawValue, value: currentOnboardingStatus)
+        }
+    }
     
     // MARK: - PUT Send Feedback
     
