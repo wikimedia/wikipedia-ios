2effbd0443b6771c1fb2d4c08e190411402e3b32
Toni Sevener <tsevener@wikimedia.org>
2024-04-17 22:48:36 -0500

    Improve spacing

--- Components/Sources/Components/Components/Shared/Feature Announcement/WKFeatureAnnouncementView.swift
+++ Components/Sources/Components/Components/Shared/Feature Announcement/WKFeatureAnnouncementView.swift
@@ -18,39 +18,45 @@ struct WKFeatureAnnouncementView: View {
         return nil
     }
     
+    func spacingForAvailableHeight(_ height: CGFloat) -> CGFloat {
+        return max(height * 0.04, 8)
+    }
+    
     var body: some View {
-        ZStack {
-            Color(appEnvironment.theme.paperBackground)
-                .ignoresSafeArea()
-            ScrollView(.vertical) {
-                VStack(spacing: 40) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            Spacer()
-                            Button(
-                                action: { viewModel.closeButtonAction?() },
-                                label: {
-                                closeImage
-                            })
-                            .foregroundColor(Color(uiColor: appEnvironment.theme.icon))
+        GeometryReader { geometry in
+            ZStack {
+                Color(appEnvironment.theme.paperBackground)
+                    .ignoresSafeArea()
+                ScrollView(.vertical) {
+                    VStack(spacing: spacingForAvailableHeight(geometry.size.height)) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            HStack {
+                                Spacer()
+                                Button(
+                                    action: { viewModel.closeButtonAction?() },
+                                    label: {
+                                        closeImage
+                                    })
+                                .foregroundColor(Color(uiColor: appEnvironment.theme.icon))
+                            }
+                            Text(viewModel.title)
+                                .font(Font(WKFont.for(.boldTitle3)))
+                                .foregroundColor(Color(appEnvironment.theme.text))
+                            Text(viewModel.body)
+                                .font(Font(WKFont.for(.body)))
+                                .foregroundColor(Color(appEnvironment.theme.text))
                         }
-                        Text(viewModel.title)
-                            .font(Font(WKFont.for(.boldTitle3)))
-                            .foregroundColor(Color(appEnvironment.theme.text))
-                        Text(viewModel.body)
-                            .font(Font(WKFont.for(.body)))
-                            .foregroundColor(Color(appEnvironment.theme.text))
+                        if let image = viewModel.image {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 132, height: 118)
+                                .foregroundColor(imageColor)
+                        }
+                        WKLargeButton(configuration: .primary, title: viewModel.primaryButtonTitle, action: viewModel.primaryButtonAction)
                     }
-                    if let image = viewModel.image {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 132, height: 118)
-                            .foregroundColor(imageColor)
-                    }
-                    WKLargeButton(configuration: .primary, title: viewModel.primaryButtonTitle, action: viewModel.primaryButtonAction)
+                    .padding()
                 }
-                .padding()
             }
         }
     }
