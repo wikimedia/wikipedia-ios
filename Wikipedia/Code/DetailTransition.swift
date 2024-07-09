a80124c301a0dc39c1bc2556a0d59a4bcfc863d6
Toni Sevener <tsevener@wikimedia.org>
2024-07-09 11:36:13 -0500

    Fix tab bar issues

--- Wikipedia/Code/DetailTransition.swift
+++ Wikipedia/Code/DetailTransition.swift
@@ -127,41 +127,6 @@ class DetailTransition: NSObject, UIViewControllerAnimatedTransitioning {
             toSnapshot.transform = transform
         }
         
-        let totalHeight = containerView.bounds.size.height
-        let tabBar = self.detailSourceViewController.tabBarController?.tabBar
-        let tabBarSnapshot: UITabBar?
-        if let tb = tabBar {
-            tabBarSnapshot = UITabBar(frame: tb.frame)
-            var selectedItem: UITabBarItem? = nil
-            let copiedItems: [UITabBarItem]? = tb.items?.compactMap { (item: UITabBarItem) -> UITabBarItem in
-                let copiedItem = UITabBarItem(title: item.title, image: item.image, selectedImage: item.selectedImage)
-                copiedItem.badgeValue = item.badgeValue
-                if item === tb.selectedItem {
-                    selectedItem = copiedItem
-                }
-                return copiedItem
-            }
-            tabBarSnapshot?.items = copiedItems
-            tabBarSnapshot?.apply(theme: theme)
-            tabBarSnapshot?.selectedItem = selectedItem
-        } else {
-            tabBarSnapshot = nil
-        }
-        
-        let tabBarDeltaY = totalHeight - (tabBar?.frame.minY ?? totalHeight)
-        let tabBarHiddenTransform = CGAffineTransform(translationX: 0, y: tabBarDeltaY)
-        if let tb = tabBar, let tbs = tabBarSnapshot {
-            tabBar?.alpha = 0
-            tbs.alpha = 1
-            tbs.frame = CGRect(x: 0, y: containerView.frame.height - tb.frame.height, width: tb.frame.width, height: tb.frame.height) // hack, it's already positioned off screen here
-            if isEnteringDetail {
-                tbs.transform = .identity
-            } else {
-                tbs.transform = tabBarHiddenTransform
-            }
-            containerView.addSubview(tbs)
-        }
-
         let duration = self.transitionDuration(using: transitionContext)
         UIView.animateKeyframes(withDuration: duration, delay: 0, options: [], animations: {
             toSnapshot.transform = .identity
@@ -172,19 +137,7 @@ class DetailTransition: NSObject, UIViewControllerAnimatedTransitioning {
                 toSnapshot.alpha = 1
                 fromSnapshot.transform = transform.inverted()
             }
-            if let tbs = tabBarSnapshot {
-                if isEnteringDetail {
-                    tbs.transform = tabBarHiddenTransform
-                } else {
-                    tbs.transform = .identity
-                }
-            }
         }) { (finished) in
-            if let tbs = tabBarSnapshot {
-                tbs.transform = .identity
-                tbs.removeFromSuperview()
-            }
-            tabBar?.alpha = 1
             backgroundView.removeFromSuperview()
             toSnapshot.removeFromSuperview()
             fromSnapshot.removeFromSuperview()
