7bb88e3c4bac217787c7019161f6210a4960a648
mazevedo <mazevedo@wikimedia.org>
2024-07-10 17:07:20 -0300

    use new MediaWiki API to retrive related pages, persist related pages, fetch related articles

--- WMF Framework/RelatedResponse.swift
+++ WMF Framework/RelatedResponse.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 struct RelatedResponse: Codable {
     let batchcomplete: Bool
     let continueInfo: ContinueInfo?
@@ -43,11 +42,56 @@ struct Page: Codable {
     let lastrevid: Int
     let length: Int
     let varianttitles: [String: String]?
+
 }
 
-struct Thumbnail: Codable {
+public struct Thumbnail: Codable {
     let source: String?
     let width: Int?
     let height: Int?
 }
 
+
+@objc(WMFRelatedPage)
+public class RelatedPage: NSObject {
+    public let pageId: Int
+    public let ns: Int
+    public let title: String
+    public let index: Int
+    public let thumbnail: Thumbnail?
+    public let articleDescription: String?
+    public let descriptionSource: String?
+    public let contentModel: String
+    public let pageLanguage: String
+    public let pageLanguageHtmlCode: String
+    public let pageLanguageDir: String
+    public let touched: String
+    public let lastRevId: Int
+    public let length: Int
+
+    var languageVariantCode: String?
+
+    var articleURL: URL?
+
+    var key: WMFInMemoryURLKey? {
+        articleURL?.wmf_languageVariantCode = languageVariantCode
+        return articleURL?.wmf_inMemoryKey
+    }
+
+    public init(pageId: Int, ns: Int, title: String, index: Int, thumbnail: Thumbnail?, articleDescription: String?, descriptionSource: String?, contentModel: String, pageLanguage: String, pageLanguageHtmlCode: String, pageLanguageDir: String, touched: String, lastRevId: Int, length: Int) {
+        self.pageId = pageId
+        self.ns = ns
+        self.title = title
+        self.index = index
+        self.thumbnail = thumbnail
+        self.articleDescription = articleDescription
+        self.descriptionSource = descriptionSource
+        self.contentModel = contentModel
+        self.pageLanguage = pageLanguage
+        self.pageLanguageHtmlCode = pageLanguageHtmlCode
+        self.pageLanguageDir = pageLanguageDir
+        self.touched = touched
+        self.lastRevId = lastRevId
+        self.length = length
+    }
+}
--- WMF Framework/RelatedSearchFetcher.swift
+++ WMF Framework/RelatedSearchFetcher.swift
@@ -2,14 +2,12 @@ import Foundation
 
 @objc(WMFRelatedSearchFetcher)
 final class RelatedSearchFetcher: Fetcher {
-    private struct RelatedPages: Decodable {
-        let pages: [ArticleSummary]?
-    }
 
-    @objc func fetchRelatedArticles(forArticleWithURL articleURL: URL?, completion: @escaping (Error?, [WMFInMemoryURLKey: ArticleSummary]?) -> Void) {
+    @objc func fetchRelatedArticles(forArticleWithURL articleURL: URL?, completion: @escaping (Error?, [WMFInMemoryURLKey: RelatedPage]?) -> Void) {
         guard
             let articleURL = articleURL,
-            let articleTitle = articleURL.percentEncodedPageTitleForPathComponents
+            let articleTitle = articleURL.percentEncodedPageTitleForPathComponents,
+            let siteURL = articleURL.wmf_site
         else {
             completion(Fetcher.invalidParametersError, nil)
             return
@@ -34,12 +32,75 @@ final class RelatedSearchFetcher: Fetcher {
 
         performDecodableMediaWikiAPIGET(for: articleURL, with: queryParams) { (result: Result<RelatedResponse, Error>) in
             switch result {
-            case .success(let success):
-                print(success)
+            case .success(let result):
+                guard let relatedPages = self.getRelatedPages(from: result, siteURL: siteURL), relatedPages.count > 0 else {
+                    return
+                }
+
+                let relatedPagesWithUniqueURLKeys: [(WMFInMemoryURLKey, RelatedPage)] = relatedPages.compactMap { (summary) -> (WMFInMemoryURLKey, RelatedPage)? in
+
+                    if let variant = articleURL.wmf_languageVariantCode {
+                        summary.languageVariantCode = variant
+                    }
+
+                    guard let articleKey = summary.key else {
+                        return nil
+                    }
+                    return (articleKey, summary)
+                }
+                completion(nil, Dictionary(uniqueKeysWithValues: relatedPagesWithUniqueURLKeys))
+
             case .failure:
                 completion(Fetcher.unexpectedResponseError, nil)
             }
         }
 
     }
+
+    func getArticleURLFromTitle(title: String, siteURL: URL) -> URL? {
+        return siteURL.wmf_URL(withTitle: title)
+    }
+
+    private func getRelatedPages(from response: RelatedResponse, siteURL: URL) -> [RelatedPage]? {
+
+        var pages: [RelatedPage] = []
+
+        guard let query = response.query else {
+            return nil
+        }
+
+        for page in query.pages {
+            let variantTitles = getVariantTitles(page.varianttitles)
+            let firstVariantTitle = variantTitles?.first?.title
+
+            var pageTitle = page.title
+            if let firstVariantTitle {
+                pageTitle = firstVariantTitle
+            }
+            let item = RelatedPage(pageId: page.pageid, ns: page.ns, title: pageTitle, index: page.index, thumbnail: page.thumbnail, articleDescription: page.description, descriptionSource: page.descriptionsource, contentModel: page.contentmodel, pageLanguage: page.pagelanguage, pageLanguageHtmlCode: page.pagelanguagehtmlcode, pageLanguageDir: page.pagelanguagedir, touched: page.touched, lastRevId: page.lastrevid, length: page.length)
+            item.articleURL = getArticleURLFromTitle(title: pageTitle, siteURL: siteURL)
+            pages.append(item)
+        }
+        return pages
+    }
+
+    private func getVariantTitles(_ variantTitles: [String: String]?) -> [VariantTitle]? {
+
+        var languageTitles: [VariantTitle] = []
+
+        guard let variantTitles else { return nil }
+
+        for (variant, title) in variantTitles {
+            let languageTitle = VariantTitle(variant: variant, title: title)
+            languageTitles.append(languageTitle)
+        }
+
+        return languageTitles
+    }
+
+    private struct VariantTitle {
+        let variant: String
+        let title: String
+    }
+
 }
