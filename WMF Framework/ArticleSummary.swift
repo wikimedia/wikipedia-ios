53b6166e0e4ed853661da9b040e25fe4258ab7fb
Toni Sevener <tsevener@wikimedia.org>
2024-07-15 14:24:35 -0500

    Generate memberwise initializers for ArticleSummary
    - Allows us to init directly instead of only using Codable

--- WMF Framework/ArticleSummary.swift
+++ WMF Framework/ArticleSummary.swift
@@ -3,7 +3,9 @@ import Foundation
 /// Represents the response structure of an article summary from the Page Content Service /page/summary endpoint
 @objc(WMFArticleSummary)
 public class ArticleSummary: NSObject, Codable {
+    
     @objc public class Namespace: NSObject, Codable {
+        
         let id: Int?
         let text: String?
 
@@ -12,6 +14,11 @@ public class ArticleSummary: NSObject, Codable {
                 return nil
             }
             return NSNumber(value: id)
+        }
+        
+        internal init(id: Int? = nil, text: String? = nil) {
+            self.id = id
+            self.text = text
         }
     }
     let id: Int64?
@@ -62,15 +69,41 @@ public class ArticleSummary: NSObject, Codable {
     var key: WMFInMemoryURLKey? {
         return articleURL?.wmf_inMemoryKey // don't use contentURLs.desktop?.page directly as it needs to be standardized
     }
+    
+    internal init(id: Int64? = nil, wikidataID: String? = nil, revision: String? = nil, timestamp: String? = nil, index: Int? = nil, namespace: ArticleSummary.Namespace? = nil, title: String? = nil, displayTitle: String? = nil, articleDescription: String? = nil, extract: String? = nil, extractHTML: String? = nil, thumbnail: ArticleSummaryImage? = nil, original: ArticleSummaryImage? = nil, coordinates: ArticleSummaryCoordinates? = nil, languageVariantCode: String? = nil, contentURLs: ArticleSummaryContentURLs) {
+        self.id = id
+        self.wikidataID = wikidataID
+        self.revision = revision
+        self.timestamp = timestamp
+        self.index = index
+        self.namespace = namespace
+        self.title = title
+        self.displayTitle = displayTitle
+        self.articleDescription = articleDescription
+        self.extract = extract
+        self.extractHTML = extractHTML
+        self.thumbnail = thumbnail
+        self.original = original
+        self.coordinates = coordinates
+        self.languageVariantCode = languageVariantCode
+        self.contentURLs = contentURLs
+    }
 }
 
 @objc(WMFArticleSummaryImage)
 class ArticleSummaryImage: NSObject, Codable {
+    
     let source: String
     let width: Int
     let height: Int
     var url: URL? {
         return URL(string: source)
+    }
+    
+    internal init(source: String, width: Int, height: Int) {
+        self.source = source
+        self.width = width
+        self.height = height
     }
 }
 
@@ -86,6 +119,11 @@ class ArticleSummaryURLs: NSObject, Codable {
 class ArticleSummaryContentURLs: NSObject, Codable {
     let desktop: ArticleSummaryURLs?
     let mobile: ArticleSummaryURLs?
+    
+    internal init(desktop: ArticleSummaryURLs? = nil, mobile: ArticleSummaryURLs? = nil) {
+        self.desktop = desktop
+        self.mobile = mobile
+    }
 }
 
 @objc(WMFArticleSummaryCoordinates)
