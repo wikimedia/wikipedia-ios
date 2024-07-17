0f2c329a846287883cd6d9cac523ec5a809c2937
Toni Sevener <tsevener@wikimedia.org>
2024-07-17 11:24:42 -0500

    Add safety to search bold title

--- Wikipedia/Code/ArticleCollectionViewCell.swift
+++ Wikipedia/Code/ArticleCollectionViewCell.swift
@@ -30,8 +30,10 @@ open class ArticleCollectionViewCell: CollectionViewCell, SwipeableCell, BatchEd
             if let boldString = _titleBoldedString, let boldFont {
                 let boldUIFont = WKFont.for(boldFont)
                 let boldAttributedString = NSMutableAttributedString(string: boldString)
-                let range = NSRange(location: 0, length: boldAttributedString.length)
-                attributedTitle.addAttribute(.font, value: boldUIFont, range: range)
+                let range = (attributedTitle.string as NSString).range(of: boldString, options: .caseInsensitive)
+                if range.location != NSNotFound {
+                    attributedTitle.addAttribute(.font, value: boldUIFont, range: range)
+                }
             }
             titleLabel.attributedText = attributedTitle
         } else {
