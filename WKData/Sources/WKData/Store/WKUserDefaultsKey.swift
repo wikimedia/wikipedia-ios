db8971b59d0cff19997c63f1212b6ce93bbf5309
mazevedo <mazevedo@wikimedia.org>
2024-07-22 15:05:43 -0300

    Add alt ext developer settings toggle

--- WKData/Sources/WKData/Store/WKUserDefaultsKey.swift
+++ WKData/Sources/WKData/Store/WKUserDefaultsKey.swift
@@ -2,4 +2,5 @@ enum WKUserDefaultsKey: String {
     case watchlistFilterSettings = "watchlist-filter-settings"
 	case imageRecommendationsOnboarding = "image-recommendations-onboarding"
     case developerSettingsDoNotPostImageRecommendationsEdit = "dev-settings-do-not-post-image-rec-edit"
+    case developerSettingsEnableAltTextExperiment = "dev-settings-enable-alt-text"
 }
