8a0bf16ef748e0b99dc8d41057868cdcdeece6e0
mazevedo <mazevedo@wikimedia.org>
2024-07-31 18:16:06 -0300

    Add basic view elements

--- Components/Sources/Components/Components/Suggested Edits/Alt Text Experiment/AltTextExperimentModalSheetView.swift
+++ Components/Sources/Components/Components/Suggested Edits/Alt Text Experiment/AltTextExperimentModalSheetView.swift
@@ -18,6 +18,7 @@ final class AltTextExperimentModalSheetView: WKComponentView {
         stackView.setContentHuggingPriority(.required, for: .vertical)
         stackView.setContentCompressionResistancePriority(.required, for: .vertical)
         stackView.alignment = .fill
+        stackView.spacing = padding
         stackView.axis = .vertical
         return stackView
     }()
@@ -27,7 +28,19 @@ final class AltTextExperimentModalSheetView: WKComponentView {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.setContentHuggingPriority(.required, for: .vertical)
         stackView.setContentCompressionResistancePriority(.required, for: .vertical)
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    private lazy var imageAndTitleStackView:  UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.setContentHuggingPriority(.required, for: .vertical)
+        stackView.setContentCompressionResistancePriority(.required, for: .vertical)
         stackView.distribution = .fill
+        stackView.spacing = basePadding
         stackView.alignment = .fill
         stackView.axis = .horizontal
         return stackView
@@ -41,7 +54,7 @@ final class AltTextExperimentModalSheetView: WKComponentView {
     }()
 
     private lazy var iconImageView: UIImageView = {
-        let icon = WKSFSymbolIcon.for(symbol: .plusCircleFill) //temp waiting for design
+        let icon = WKSFSymbolIcon.for(symbol: .plusCircleFill) // temp waiting for design
         let imageView = UIImageView(image: icon)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.setContentCompressionResistancePriority(.required, for: .vertical)
@@ -57,16 +70,31 @@ final class AltTextExperimentModalSheetView: WKComponentView {
         label.setContentHuggingPriority(.required, for: .vertical)
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        label.font = WKFont.for(.boldTitle3)
         return label
     }()
 
-    private lazy var textField: UITextField = {
-        let textfield = UITextField(frame: .zero)
+    private lazy var textView: UITextView = {
+        let textfield = UITextView(frame: .zero)
         textfield.translatesAutoresizingMaskIntoConstraints = false
-//        textfield.addTarget(self, action: #selector(titleTextFieldChanged), for: .editingChanged)
+        textfield.layer.cornerRadius = 10
         return textfield
     }()
+
+    private lazy var placeholder: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+
+    private let basePadding: CGFloat = 8
+    private let padding: CGFloat = 16
 
     // MARK: Lifecycle
 
@@ -76,11 +104,11 @@ final class AltTextExperimentModalSheetView: WKComponentView {
         setup()
     }
 
-    let padding: CGFloat = 10
-
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: Methods
 
     public override func appEnvironmentDidChange() {
         super.appEnvironmentDidChange()
@@ -88,39 +116,72 @@ final class AltTextExperimentModalSheetView: WKComponentView {
     }
 
     func updateColors() {
-        backgroundColor = theme.paperBackground
+        backgroundColor = theme.midBackground
         titleLabel.textColor = theme.text
+        textView.backgroundColor = theme.paperBackground
         iconImageView.tintColor = theme.link
+        nextButton.setTitleColor(theme.link, for: .normal)
+        placeholder.textColor = theme.secondaryText
     }
 
     func configure() {
         updateColors()
+        titleLabel.text = viewModel?.localizedStrings.title
+        nextButton.setTitle(viewModel?.localizedStrings.buttonTitle, for: .normal)
+        placeholder.text = viewModel?.localizedStrings.textViewPlaceholder
+        
+        titleLabel.font = WKFont.for(.boldTitle3, compatibleWith: traitCollection)
+        nextButton.titleLabel?.font = WKFont.for(.semiboldHeadline, compatibleWith: traitCollection)
+        placeholder.font = WKFont.for(.callout, compatibleWith: traitCollection)
     }
 
     func setup() {
         configure()
 
-        headerStackView.addArrangedSubview(iconImageContainerView)
-        headerStackView.addArrangedSubview(titleLabel)
+        textView.addSubview(placeholder)
+        iconImageContainerView.addSubview(iconImageView)
 
-        stackView.addSubview(textField)
+        imageAndTitleStackView.addArrangedSubview(iconImageContainerView)
+        imageAndTitleStackView.addArrangedSubview(titleLabel)
+
+        headerStackView.addArrangedSubview(imageAndTitleStackView)
+        headerStackView.addArrangedSubview(nextButton)
+
+        stackView.addArrangedSubview(headerStackView)
+        stackView.addArrangedSubview(textView)
 
         scrollView.addSubview(stackView)
-        stackView.addArrangedSubview(headerStackView)
-
         addSubview(scrollView)
 
         NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            topAnchor.constraint(equalTo: scrollView.topAnchor),
-            bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            scrollView.contentLayoutGuide.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
 
             stackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: padding),
             stackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: -padding),
-            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: padding),
+            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: basePadding),
             stackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor, constant: -padding),
-        ])
 
+            textView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            textView.heightAnchor.constraint(equalToConstant: 125),
+
+            nextButton.heightAnchor.constraint(equalToConstant:44),
+
+            placeholder.topAnchor.constraint(equalTo: textView.topAnchor, constant: basePadding),
+            placeholder.leadingAnchor.constraint(equalTo: textView.leadingAnchor, constant: basePadding),
+
+
+            iconImageContainerView.centerXAnchor.constraint(equalTo: iconImageView.centerXAnchor),
+            iconImageView.leadingAnchor.constraint(equalTo: iconImageContainerView.leadingAnchor),
+            iconImageView.trailingAnchor.constraint(equalTo: iconImageContainerView.trailingAnchor),
+            iconImageView.topAnchor.constraint(equalTo: iconImageContainerView.topAnchor),
+            iconImageView.bottomAnchor.constraint(equalTo: iconImageContainerView.bottomAnchor),
+            iconImageContainerView.topAnchor.constraint(equalTo: titleLabel.topAnchor)
+        ])
     }
 }
