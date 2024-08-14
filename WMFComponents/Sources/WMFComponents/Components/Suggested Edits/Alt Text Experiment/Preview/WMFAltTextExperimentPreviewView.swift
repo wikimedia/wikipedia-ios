6c48a2ab44c5d39fa83e07fb34533cd3dfd350a9
mazevedo <mazevedo@wikimedia.org>
2024-08-14 15:21:40 -0300

    Add size to alt text view spacer

--- WMFComponents/Sources/WMFComponents/Components/Suggested Edits/Alt Text Experiment/Preview/WMFAltTextExperimentPreviewView.swift
+++ WMFComponents/Sources/WMFComponents/Components/Suggested Edits/Alt Text Experiment/Preview/WMFAltTextExperimentPreviewView.swift
@@ -13,35 +13,38 @@ public struct WMFAltTextExperimentPreviewView: View {
         }
 
     public var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            VStack(alignment: .leading) {
-                Image(uiImage: viewModel.image)
-                    .frame(width: UIScreen.main.bounds.size.width, height: 300)
-                    .aspectRatio(contentMode: .fit)
-                WMFAltTextPreviewCell(title: viewModel.localizedStrings.altTextTitle, subtitle: viewModel.altText, theme: appEnvironment.theme)
-                if let caption = viewModel.caption {
-                    WMFAltTextPreviewCell(title: viewModel.localizedStrings.altTextTitle, subtitle: caption, theme: appEnvironment.theme)
+        GeometryReader { geometry in
+            ScrollView(.vertical, showsIndicators: true) {
+                VStack(alignment: .leading) {
+                    Image(uiImage: viewModel.image)
+                        .frame(width: UIScreen.main.bounds.size.width, height: 300)
+                        .aspectRatio(contentMode: .fit)
+                    WMFAltTextPreviewCell(title: viewModel.localizedStrings.altTextTitle, subtitle: viewModel.altText, theme: appEnvironment.theme)
+                    if let caption = viewModel.caption {
+                        WMFAltTextPreviewCell(title: viewModel.localizedStrings.altTextTitle, subtitle: caption, theme: appEnvironment.theme)
+                    }
                 }
-            }
-            Spacer()
-            HStack {
-                Image("license-cc")
-                    .renderingMode(.template)
-                    .colorMultiply(Color(appEnvironment.theme.secondaryText))
-                    .padding([.trailing], 12)
-                if let attributedString {
-                    Text(attributedString)
-                        .font(Font(WMFFont.for(.footnote)))
-                        .foregroundColor(Color(appEnvironment.theme.secondaryText))
-                        .multilineTextAlignment(.leading)
-                } else {
-                    Text(viewModel.localizedStrings.footerText)
-                        .font(Font(WMFFont.for(.footnote)))
-                        .foregroundColor(Color(appEnvironment.theme.secondaryText))
-                        .multilineTextAlignment(.leading)
+                Spacer()
+                    .frame(idealHeight: geometry.size.height/5)
+                HStack {
+                    Image("license-cc")
+                        .renderingMode(.template)
+                        .colorMultiply(Color(appEnvironment.theme.secondaryText))
+                        .padding([.trailing], 12)
+                    if let attributedString {
+                        Text(attributedString)
+                            .font(Font(WMFFont.for(.footnote)))
+                            .foregroundColor(Color(appEnvironment.theme.secondaryText))
+                            .multilineTextAlignment(.leading)
+                    } else {
+                        Text(viewModel.localizedStrings.footerText)
+                            .font(Font(WMFFont.for(.footnote)))
+                            .foregroundColor(Color(appEnvironment.theme.secondaryText))
+                            .multilineTextAlignment(.leading)
+                    }
                 }
+                .padding(16)
             }
-            .padding(16)
         }
     }
 }
