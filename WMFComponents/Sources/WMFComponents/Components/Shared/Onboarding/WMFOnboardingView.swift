afddd232aa422b70a1e7cb267b28a2fdbe68793b
Toni Sevener <tsevener@wikimedia.org>
2024-08-19 08:54:16 -0500

    Flash scroll indicators in onboarding view

--- WMFComponents/Sources/WMFComponents/Components/Shared/Onboarding/WMFOnboardingView.swift
+++ WMFComponents/Sources/WMFComponents/Components/Shared/Onboarding/WMFOnboardingView.swift
@@ -17,29 +17,51 @@ public struct WMFOnboardingView: View {
     }
     
     @ScaledMetric var scrollViewBottomInset = 125.0
+    
+    @State private var flashScrollIndicators: Bool = false
 
     // MARK: - Lifecycle
+    
+    @available(iOS 17.0, *)
+    var flashingScrollView: some View {
+        ScrollView(showsIndicators: true) {
+            scrollViewContent
+        }
+        .scrollIndicatorsFlash(trigger: flashScrollIndicators)
+    }
+    
+    var scrollView: some View {
+        ScrollView(showsIndicators: true) {
+            scrollViewContent
+        }
+    }
+    
+    var scrollViewContent: some View {
+        VStack {
+            Text(viewModel.title)
+                .font(Font(WMFFont.for(.boldTitle1)))
+                .foregroundColor(Color(appEnvironment.theme.text))
+                .padding(.bottom, 44)
+                .padding(.top, 44 + sizeClassPadding)
+                .multilineTextAlignment(.center)
+            ForEach(1...viewModel.cells.count, id:\.self) { cell in
+                VStack {
+                    WMFOnboardingCell(viewModel: viewModel.cells[cell - 1])
+                        .padding(.bottom, 24)
+                        .padding(.trailing, 20)
+                }
+            }
+        }
+        .padding(EdgeInsets(top: 0, leading: sizeClassPadding, bottom: scrollViewBottomInset, trailing: sizeClassPadding))
+    }
 
 	var content: some View {
         ZStack(alignment: .bottom, content: {
-			ScrollView(showsIndicators: true) {
-				VStack {
-					Text(viewModel.title)
-						.font(Font(WMFFont.for(.boldTitle1)))
-						.foregroundColor(Color(appEnvironment.theme.text))
-                        .padding(.bottom, 44)
-                        .padding(.top, 44 + sizeClassPadding)
-						.multilineTextAlignment(.center)
-					ForEach(1...viewModel.cells.count, id:\.self) { cell in
-						VStack {
-							WMFOnboardingCell(viewModel: viewModel.cells[cell - 1])
-								.padding(.bottom, 24)
-								.padding(.trailing, 20)
-						}
-					}
-				}
-                .padding(EdgeInsets(top: 0, leading: sizeClassPadding, bottom: scrollViewBottomInset, trailing: sizeClassPadding))
-			}
+            if #available(iOS 17.0, *) {
+                flashingScrollView
+            } else {
+                scrollView
+            }
             VStack(spacing: 20) {
                 WMFLargeButton(configuration: .primary, title: viewModel.primaryButtonTitle, action: primaryButtonAction)
                     .padding([.top], 16)
@@ -54,13 +76,17 @@ public struct WMFOnboardingView: View {
                 Color(appEnvironment.theme.paperBackground).ignoresSafeArea()
             }
         })
-		
 	}
 
     public var body: some View {
         content
             .background {
                 Color(appEnvironment.theme.paperBackground).ignoresSafeArea()
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    flashScrollIndicators.toggle()
+                }
             }
     }
 
