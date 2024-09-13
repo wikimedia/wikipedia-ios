7b6b24ec0d94ff701b41f173bfc09cb4d378442c
mazevedo <mazevedo@wikimedia.org>
2024-09-13 15:18:23 -0300

    Fix EMFSmallButtonDelegate naming issues

--- WMFComponents/Sources/WMFComponents/Components/Shared/Buttons/WMFSmallMenuButtonDelegate.swift
+++ WMFComponents/Sources/WMFComponents/Components/Shared/Buttons/WMFSmallMenuButtonDelegate.swift
@@ -3,8 +3,7 @@ import UIKit
 public protocol WMFSmallMenuButtonDelegate: AnyObject {
     func wmfMenuButton(_ sender: WMFSmallMenuButton, didTapMenuItem item: WMFSmallMenuButton.MenuItem)
     func wmfMenuButtonDidTap(_ sender: WMFSmallMenuButton)
-
-	func wmfSwiftUIMenuButtonUserDidTap(configuration: WMFSmallMenuButton.Configuration, item: WMFSmallMenuButton.MenuItem?)
+    func wmfSwiftUIMenuButtonUserDidTap(configuration: WMFSmallMenuButton.Configuration, item: WMFSmallMenuButton.MenuItem?)
     func wmfSwiftUIMenuButtonUserDidTapAccessibility(configuration: WMFSmallMenuButton.Configuration, item: WMFSmallMenuButton.MenuItem?)
 }
 
