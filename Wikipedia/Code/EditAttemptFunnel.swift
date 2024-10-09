c79c2eecfcde9de52936c5e621ce7521f19471a4
Toni Sevener <tsevener@wikimedia.org>
2024-10-09 14:58:44 -0500

    Add user_is_temp separately to EditAttemptFunnel

--- Wikipedia/Code/EditAttemptFunnel.swift
+++ Wikipedia/Code/EditAttemptFunnel.swift
@@ -20,6 +20,7 @@ public final class EditAttemptFunnel {
         let platform: String
         let user_editcount: Int
         let user_id: Int
+        let user_is_temp: Bool
         let version: Int
         let page_title: String?
         let page_ns: Int?
@@ -37,6 +38,10 @@ public final class EditAttemptFunnel {
         case saveFailure = "saveFailure"
         case abort = "abort"
     }
+    
+    private var isTemp: Bool {
+        return MWKDataStore.shared().authenticationManager.authStateIsTemporary
+    }
 
     private func logEvent(pageURL: URL, action: EditAction, revisionId: Int? = nil) {
         let editorInterface = "wikitext"
@@ -47,7 +52,7 @@ public final class EditAttemptFunnel {
         
         let appInstallID = UserDefaults.standard.wmf_appInstallId
 
-        let event = Event(action: action, editing_session_id: "", app_install_id: appInstallID, editor_interface: editorInterface, integration: integrationID, mw_version: "", platform: platform, user_editcount: 0, user_id: userId, version: 1, page_title: pageURL.wmf_title, page_ns: pageURL.namespace?.rawValue, revision_id: revisionId)
+        let event = Event(action: action, editing_session_id: "", app_install_id: appInstallID, editor_interface: editorInterface, integration: integrationID, mw_version: "", platform: platform, user_editcount: 0, user_id: userId, user_is_temp: isTemp, version: 1, page_title: pageURL.wmf_title, page_ns: pageURL.namespace?.rawValue, revision_id: revisionId)
         
         let container = EventContainer(event: event)
         EventPlatformClient.shared.submit(stream: .editAttempt, event: container, needsMinimal: true)
