2a30959b326fab0a0d54131a5fa3713687c667d4
Toni Sevener <tsevener@wikimedia.org>
2024-10-22 09:04:58 -0500

    Add missing entry point logging

--- WMFComponents/Sources/WMFComponents/Components/Profile/WMFProfileViewModel.swift
+++ WMFComponents/Sources/WMFComponents/Components/Profile/WMFProfileViewModel.swift
@@ -263,6 +263,7 @@ enum ProfileState {
                 isLoadingDonateConfigs: false,
                 action: {
                     coordinatorDelegate?.handleProfileAction(.showYearInReview)
+                    coordinatorDelegate?.handleProfileAction(.logYearInReviewTap)
                 }
             )
             
