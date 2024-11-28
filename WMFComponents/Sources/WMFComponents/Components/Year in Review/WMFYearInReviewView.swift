3e4186fadce5c644cfff8b1a8707cd2c887685d9
mazevedo <mazevedo@wikimedia.org>
2024-11-28 14:35:41 -0300

    Add secondary style to WMFLarge button for better tap area

--- WMFComponents/Sources/WMFComponents/Components/Year in Review/WMFYearInReviewView.swift
+++ WMFComponents/Sources/WMFComponents/Components/Year in Review/WMFYearInReviewView.swift
@@ -159,11 +159,12 @@ public struct WMFYearInReviewView: View {
                     viewModel.getStarted()
                 }
             }
-            WMFSmallButton(configuration: configuration, title: viewModel.localizedStrings.firstSlideLearnMore) {
+
+            WMFLargeButton(configuration: .secondary, title: viewModel.localizedStrings.firstSlideLearnMore) {
                 viewModel.loggingDelegate?.logYearInReviewIntroDidTapLearnMore()
                 viewModel.coordinatorDelegate?.handleYearInReviewAction(.introLearnMore)
-                // TODO: Implement hide this feature
             }
+
         }
     }
 }
