99f11a95f67f600bb0d2237b139c17e17ad84491
Toni Sevener <tsevener@wikimedia.org>
2024-12-16 23:02:30 -0600

    Always prefer large titles

--- WMFComponents/Sources/WMFComponents/Extensions/UIViewController+NavigationBarUtils.swift
+++ WMFComponents/Sources/WMFComponents/Extensions/UIViewController+NavigationBarUtils.swift
@@ -94,6 +94,7 @@ public extension WMFNavigationBarConfiguring where Self: UIViewController {
     ///   - closeButtonConfig: Config for close button. If provided, a leading X button will be added to navigation bar, which will dismiss the view controller when tapped
     func configureNavigationBar(titleConfig: WMFNavigationBarTitleConfig, closeButtonConfig: WMFNavigationBarCloseButtonConfig?, profileButtonConfig: WMFNavigationBarProfileButtonConfig?, searchBarConfig: WMFNavigationBarSearchConfig?, hideNavigationBarOnScroll: Bool) {
         
+        navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.setNavigationBarHidden(false, animated: true)
         navigationController?.hidesBarsOnSwipe = hideNavigationBarOnScroll
         
@@ -112,14 +113,12 @@ public extension WMFNavigationBarConfiguring where Self: UIViewController {
         
         switch titleConfig.alignment {
         case .centerCompact:
-            // navigationController?.navigationBar.prefersLargeTitles = false
             navigationItem.largeTitleDisplayMode = .never
             if let customTitleView = titleConfig.customView {
                 navigationItem.titleView = customTitleView
                 themeNavigationBarCustomCenteredTitleView()
             }
         case .leadingCompact:
-            navigationController?.navigationBar.prefersLargeTitles = false
             navigationItem.largeTitleDisplayMode = .never
             navigationItem.titleView = UIView()
             if let customTitleView = titleConfig.customView {
@@ -137,7 +136,6 @@ public extension WMFNavigationBarConfiguring where Self: UIViewController {
                 themeNavigationBarLeadingTitleView()
             }
         case .leadingLarge:
-            navigationController?.navigationBar.prefersLargeTitles = true
             navigationItem.largeTitleDisplayMode = .always
             if let customTitleView = titleConfig.customView {
                 navigationItem.titleView = customTitleView
@@ -146,7 +144,6 @@ public extension WMFNavigationBarConfiguring where Self: UIViewController {
             }
             navigationItem.leftBarButtonItem = nil
         case .hidden:
-            navigationController?.navigationBar.prefersLargeTitles = false
             navigationItem.largeTitleDisplayMode = .never
             navigationItem.titleView = UIView()
         }
