9c193434589ce9ea4479dde9300150493f37f4c0
chandra9302 <39931486+chandra9302@users.noreply.github.com>
2025-02-04 21:27:40 -0800

    Fix T385397: Fixes the issue of navigation bar hiding on tab switch resulting in content visiblity under navigatoin bar in Explore tab.

--- Wikipedia/Code/ExploreViewController.swift
+++ Wikipedia/Code/ExploreViewController.swift
@@ -53,6 +53,8 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
     
     var topSafeAreaOverlayHeightConstraint: NSLayoutConstraint?
     var topSafeAreaOverlayView: UIView?
+    
+    private var presentingSearchResults: Bool = false
 
     // MARK: - Lifecycle
 
@@ -199,7 +201,7 @@ class ExploreViewController: ColumnarCollectionViewController, ExploreCardViewCo
             searchBarPlaceholder: WMFLocalizedString("search-field-placeholder-text", value: "Search Wikipedia", comment: "Search field placeholder text"),
             showsScopeBar: false, scopeButtonTitles: nil)
         
-        configureNavigationBar(titleConfig: titleConfig, closeButtonConfig: nil, profileButtonConfig: profileButtonConfig, searchBarConfig: searchConfig, hideNavigationBarOnScroll: true)
+        configureNavigationBar(titleConfig: titleConfig, closeButtonConfig: nil, profileButtonConfig: profileButtonConfig, searchBarConfig: searchConfig, hideNavigationBarOnScroll: !presentingSearchResults)
     }
     
     @objc func updateProfileButton() {
@@ -2179,10 +2181,12 @@ extension ExploreViewController: YearInReviewBadgeDelegate {
 extension ExploreViewController: UISearchControllerDelegate {
     
     func willPresentSearchController(_ searchController: UISearchController) {
+        presentingSearchResults = true
         navigationController?.hidesBarsOnSwipe = false
     }
     
     func didDismissSearchController(_ searchController: UISearchController) {
+        presentingSearchResults = false
         navigationController?.hidesBarsOnSwipe = true
     }
 }
