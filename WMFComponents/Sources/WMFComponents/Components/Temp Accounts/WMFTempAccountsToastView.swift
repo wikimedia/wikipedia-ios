b2180dab51202b845f9620b03af6633fbd29b0cd
golson-wmf <golson@wikimedia.org>
2025-02-25 16:06:41 -0500

    Present toast

--- WMFComponents/Sources/WMFComponents/Components/Temp Accounts/WMFTempAccountsToastView.swift
+++ WMFComponents/Sources/WMFComponents/Components/Temp Accounts/WMFTempAccountsToastView.swift
@@ -2,22 +2,47 @@ import SwiftUI
 import WMFData
 
 public struct WMFTempAccountsToastView: View {
-    var viewModel: WMFTempAccountsToastViewModel
+    @ObservedObject var appEnvironment = WMFAppEnvironment.current
+    @ObservedObject var viewModel: WMFTempAccountsToastViewModel
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+
+    var theme: WMFTheme {
+        return appEnvironment.theme
+    }
     
     public init(viewModel: WMFTempAccountsToastViewModel) {
         self.viewModel = viewModel
     }
     
+    private var titleStyles: HtmlUtils.Styles {
+        HtmlUtils.Styles(font: WMFFont.for(.subheadline), boldFont: WMFFont.for(.boldSubheadline), italicsFont: WMFFont.for(.subheadline), boldItalicsFont: WMFFont.for(.subheadline), color: theme.text, linkColor: theme.link, lineSpacing: 1)
+    }
+    
     public var body: some View {
-        VStack {
-            Image("close")
-            HStack {
-                Image("alert")
-                Text("words")
+        VStack(spacing: 12) {
+            if let xCircleFill = WMFSFSymbolIcon.for(symbol: .xCircleFill) {
+                Image(uiImage: xCircleFill)
+                    .frame(maxWidth: .infinity, alignment: .topTrailing)
             }
-            Button("Read more", action: {
-                
-            })
+            VStack(spacing: 8) {
+                HStack(spacing: 8) {
+                    if let exclamationPointCircle = WMFIcon.exclamationPointCircle {
+                        Image(uiImage: exclamationPointCircle)
+                            .foregroundStyle(Color(theme.editorMatchForeground))
+                    }
+                    WMFHtmlText(html: viewModel.title, styles: titleStyles)
+                        .lineLimit(2)
+                }
+                WMFSmallButton(configuration: WMFSmallButton.Configuration(style: .neutral), title: viewModel.readMoreButtonTitle, action: {
+                    viewModel.didTapReadMore()
+                })
+            }
+            .padding(.vertical, 12)
+            Divider()
         }
+        .frame(maxWidth: .infinity)
+        .ignoresSafeArea()
+        .background(Color(theme.midBackground))
     }
 }
--- WMFComponents/Sources/WMFComponents/Components/Temp Accounts/WMFTempAccountsToastViewModel.swift
+++ WMFComponents/Sources/WMFComponents/Components/Temp Accounts/WMFTempAccountsToastViewModel.swift
@@ -5,9 +5,13 @@ import WMFData
 public class WMFTempAccountsToastViewModel: ObservableObject {
     public var didTapReadMore: () -> Void
     public var didTapClose: () -> Void
+    public var title: String
+    public var readMoreButtonTitle: String
     
-    public init(didTapReadMore: @escaping () -> Void, didTapClose: @escaping () -> Void) {
+    public init(didTapReadMore: @escaping () -> Void, didTapClose: @escaping () -> Void, title: String, readMoreButtonTitle: String) {
         self.didTapReadMore = didTapReadMore
         self.didTapClose = didTapClose
+        self.title = title
+        self.readMoreButtonTitle = readMoreButtonTitle
     }
 }
