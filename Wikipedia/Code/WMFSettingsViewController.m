11ca83a961756ce1687cdb0f50fee5b296c46ae2
Toni Sevener <tsevener@wikimedia.org>
2025-07-29 18:14:58 -0500

    Revert "Keep settings item"
    This reverts commit 941527b33af61982f4d56bc62888e72938d17d79.

--- Wikipedia/Code/WMFSettingsViewController.m
+++ Wikipedia/Code/WMFSettingsViewController.m
@@ -590,10 +590,12 @@ static NSString *const WMFSettingsURLDonation = @"https://donate.wikimedia.org/?
                              [WMFSettingsMenuItem itemForType:WMFSettingsMenuItemType_Search]];
     NSMutableArray *items = [NSMutableArray arrayWithArray:commonItems];
     [items addObject:[WMFSettingsMenuItem itemForType:WMFSettingsMenuItemType_ExploreFeed]];
-
+    
     if ([[WMFYearInReviewDataController dataControllerForObjectiveC] shouldShowYearInReviewSettingsItemWithCountryCode:NSLocale.currentLocale.countryCode primaryAppLanguageCode:self.dataStore.languageLinkController.appLanguage.languageCode]) {
+#if DEBUG
         [items addObject:[WMFSettingsMenuItem itemForType:WMFSettingsMenuItemType_YearInReview]];
         [[NSUserDefaults standardUserDefaults] wmf_setShowYirSettingToggle:YES];
+#endif
     } else {
         [[NSUserDefaults standardUserDefaults] wmf_setShowYirSettingToggle:NO];
     }
