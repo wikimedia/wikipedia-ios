32bd6202a980342db4d56b69050ede82535a185a
Toni Sevener <tsevener@wikimedia.org>
2025-12-18 15:14:38 -0600

    Revert "Fail again"
    This reverts commit 98363c5cec0d1dac145b283ae8698b4157a9c764.

--- Wikipedia/Code/CollectionViewUpdater.swift
+++ Wikipedia/Code/CollectionViewUpdater.swift
@@ -227,11 +227,3 @@ class CollectionViewUpdater<T: NSFetchRequestResult>: NSObject, NSFetchedResults
     }
     
 }
-
-extension CollectionViewUpdater {
-    func replaceFetchedObjects(with objects: [T]) {
-        // Only used for initial load to avoid duplicates
-        // Note: We don't replace the FRC itself, just reload the collection view
-        collectionView.reloadData()
-    }
-}
