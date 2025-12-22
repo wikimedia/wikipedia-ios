5b23f0dcbe0bc6e9edfe5bc51b242baf75e2eaca
Toni Sevener <tsevener@wikimedia.org>
2025-12-22 17:50:11 -0600

    Add resources to Swift package

--- WMFLocalizations/Package.swift
+++ WMFLocalizations/Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WMFLocalizations"),
+            name: "WMFLocalizations",
+            resources: [.process("Resources")]),
         .testTarget(
             name: "WMFLocalizationsTests",
             dependencies: ["WMFLocalizations"]
