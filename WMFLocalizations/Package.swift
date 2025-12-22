d039ad0b04b0060866ebe300d857ac862b88b68c
Toni Sevener <tsevener@wikimedia.org>
2025-12-22 16:01:37 -0600

    Copy iOS Native Localizations files into package

--- WMFLocalizations/Package.swift
+++ WMFLocalizations/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WMFLocalizations",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
