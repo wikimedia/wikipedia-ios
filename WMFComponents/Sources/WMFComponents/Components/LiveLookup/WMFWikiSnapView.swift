b9934ff7a18f603912d7aad80f8c11e212cde8c1
golson <golson@wikimedia.org>
2026-01-20 15:43:00 -0500

    dismiss

--- WMFComponents/Sources/WMFComponents/Components/LiveLookup/WMFWikiSnapView.swift
+++ WMFComponents/Sources/WMFComponents/Components/LiveLookup/WMFWikiSnapView.swift
@@ -3,11 +3,26 @@ import Vision
 import SwiftUI
 
 public struct WMFWikiSnapView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     public init() {
         
     }
-    
     public var body: some View {
-        Text("Live lookup")
+        NavigationStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+            }
+            .navigationTitle("WikiSnap")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
+
 }
