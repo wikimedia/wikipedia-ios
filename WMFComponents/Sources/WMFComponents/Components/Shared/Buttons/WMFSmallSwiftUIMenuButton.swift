fef96dc66d3930f62f695077238f686106d11ff0
mazevedo <mazevedo@wikimedia.org>
2026-02-13 13:09:42 -0300

    Update button style

--- WMFComponents/Sources/WMFComponents/Components/Shared/Buttons/WMFSmallSwiftUIMenuButton.swift
+++ WMFComponents/Sources/WMFComponents/Components/Shared/Buttons/WMFSmallSwiftUIMenuButton.swift
@@ -2,54 +2,60 @@ import SwiftUI
 
 public struct WMFSmallSwiftUIMenuButton: View {
 
-	@ObservedObject var appEnvironment = WMFAppEnvironment.current
+    @ObservedObject var appEnvironment = WMFAppEnvironment.current
 
-	public let configuration: WMFSmallMenuButton.Configuration
-	public weak var menuButtonDelegate: WMFSmallMenuButtonDelegate?
+    public let configuration: WMFSmallMenuButton.Configuration
+    public weak var menuButtonDelegate: WMFSmallMenuButtonDelegate?
 
-	public init(configuration: WMFSmallMenuButton.Configuration, menuButtonDelegate: WMFSmallMenuButtonDelegate?) {
-		self.configuration = configuration
-		self.menuButtonDelegate = menuButtonDelegate
-	}
+    public init(configuration: WMFSmallMenuButton.Configuration, menuButtonDelegate: WMFSmallMenuButtonDelegate?) {
+        self.configuration = configuration
+        self.menuButtonDelegate = menuButtonDelegate
+    }
 
-	public var body: some View {
-			Menu(content: {
-				ForEach(configuration.menuItems) { menuItem in
-					Button(action: {
-                        if UIAccessibility.isVoiceOverRunning {
-                            menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTapAccessibility(configuration: configuration, item: menuItem)
-                        } else {
-                            menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTap(configuration: configuration, item: menuItem)
+    public var body: some View {
+        Menu(content: {
+            ForEach(configuration.menuItems) { menuItem in
+                Button(action: {
+                    if UIAccessibility.isVoiceOverRunning {
+                        menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTapAccessibility(configuration: configuration, item: menuItem)
+                    } else {
+                        menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTap(configuration: configuration, item: menuItem)
+                    }
+                }) {
+                    HStack {
+                        Text(menuItem.title)
+                        Spacer()
+                        if let image = menuItem.image {
+                            Image(uiImage: image)
                         }
-					}) {
-						HStack {
-							Text(menuItem.title)
-								.foregroundColor(Color(appEnvironment.theme[keyPath: configuration.primaryColor]))
-							Spacer()
-							Image(uiImage: menuItem.image ?? UIImage())
-						}
-					}
-				}
-			}, label: {
-				HStack {
-					Image(uiImage: configuration.image ?? UIImage())
-						.foregroundColor(Color(appEnvironment.theme[keyPath: configuration.primaryColor]))
-					Spacer()
-						.frame(width: 8)
-					Text(configuration.title ?? "")
-						.lineLimit(1)
-						.foregroundColor(Color(appEnvironment.theme[keyPath: configuration.primaryColor]))
-						.font(Font(WMFFont.for(.boldFootnote)))
-				}
-				.padding([.leading, .trailing], 8)
-				.padding([.top, .bottom], 8)
-				.background(Color(appEnvironment.theme[keyPath: configuration.primaryColor].withAlphaComponent(0.15)))
-			})
-			.highPriorityGesture(TapGesture().onEnded {
-				menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTap(configuration: configuration, item: nil)
-			})
-			.cornerRadius(8)
-	}
-	
+                    }
+                }
+            }
+        }, label: {
+            HStack(spacing: 6) {
+                if let image = configuration.image {
+                    Image(uiImage: image)
+                        .foregroundColor(Color(appEnvironment.theme[keyPath: configuration.primaryColor]))
+                }
+                if let title = configuration.title {
+                    Text(title)
+                        .lineLimit(1)
+                        .font(Font(WMFFont.for(.boldFootnote)))
+                }
+            }
+            .foregroundColor(Color(appEnvironment.theme[keyPath: configuration.primaryColor]))
+            .padding(.vertical, 8)
+            .padding(.horizontal, 8)
+            .frame(height: 32)
+            .background(
+                Capsule()
+                    .fill(Color(appEnvironment.theme[keyPath: configuration.primaryColor]).opacity(0.15))
+            )
+            .clipShape(Capsule())
+        })
+        .highPriorityGesture(TapGesture().onEnded {
+            menuButtonDelegate?.wmfSwiftUIMenuButtonUserDidTap(configuration: configuration, item: nil)
+        })
+    }
 }
 
