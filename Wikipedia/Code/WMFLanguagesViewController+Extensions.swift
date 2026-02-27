8bc1edbcc70b79cccaf795df43bcb204070509ac
Toni Sevener <tsevener@wikimedia.org>
2026-02-27 15:41:48 -0600

    Language edge effect fixes

--- Wikipedia/Code/WMFLanguagesViewController+Extensions.swift
+++ Wikipedia/Code/WMFLanguagesViewController+Extensions.swift
@@ -6,7 +6,24 @@ extension WMFLanguagesViewController: WMFNavigationBarConfiguring {
     @objc func configureNavigationBarFromObjC(title: String) {
         let titleConfig: WMFNavigationBarTitleConfig = WMFNavigationBarTitleConfig(title: title, customView: nil, alignment: .centerCompact)
         let closeButtonConfig = WMFLargeCloseButtonConfig(imageType: .plainX, target: self, action: #selector(closeButtonPressed), alignment: .leading)
-        configureNavigationBar(titleConfig: titleConfig, closeButtonConfig: closeButtonConfig, profileButtonConfig: nil, tabsButtonConfig: nil, searchBarConfig: nil, hideNavigationBarOnScroll: false)
+        
+        let searchBarConfig: WMFNavigationBarSearchConfig?
+        if !self.hideLanguageFilter {
+            let placeholder = WMFLocalizedString("article-languages-filter-placeholder", value: "Find language", comment: "Filter languages text box placeholder text.")
+            searchBarConfig = WMFNavigationBarSearchConfig(
+                searchResultsController: nil,
+                searchControllerDelegate: nil,
+                searchResultsUpdater: nil,
+                searchBarDelegate: self,
+                searchBarPlaceholder: placeholder,
+                showsScopeBar: false,
+                scopeButtonTitles: nil
+            )
+        } else {
+            searchBarConfig = nil
+        }
+        
+        configureNavigationBar(titleConfig: titleConfig, closeButtonConfig: closeButtonConfig, profileButtonConfig: nil, tabsButtonConfig: nil, searchBarConfig: searchBarConfig, hideNavigationBarOnScroll: false)
     }
     
     @objc private func closeButtonPressed() {
