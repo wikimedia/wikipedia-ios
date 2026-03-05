7fed1ebc9fe28276ad861b5b592c495fd52be8fe
Toni Sevener <tsevener@wikimedia.org>
2026-03-05 15:31:35 -0600

    Improvement for iPad

--- Wikipedia/Code/WMFAppViewControllerTipWrapper.swift
+++ Wikipedia/Code/WMFAppViewControllerTipWrapper.swift
@@ -8,18 +8,44 @@ import SwiftUI
     fileprivate var tipObservationTask: Task<Void, Never>?
     weak var tooltipVC: TipUIPopoverViewController?
     
+    lazy var tabSearchTargetViewIPad: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     @objc func listenForTooltips(appViewController: WMFAppViewController, tabBarItem: UITabBarItem) {
     
+        // It seems like tabBarItem does not have a recognizable frame on iPad for TipUIPopoverViewController to point to. We are going to add a fake view that is about the area of the center of the tab bar, and remove the popover arrows.
+        
+        var targetViewIPad: UIView? = nil
+        if UIDevice.current.userInterfaceIdiom == .pad && appViewController.traitCollection.horizontalSizeClass == .regular {
+            appViewController.view.addSubview(tabSearchTargetViewIPad)
+            NSLayoutConstraint.activate([
+                appViewController.view.centerXAnchor.constraint(equalTo: tabSearchTargetViewIPad.centerXAnchor, constant: 0),
+                appViewController.view.topAnchor.constraint(equalTo: tabSearchTargetViewIPad.topAnchor, constant: -140),
+                tabSearchTargetViewIPad.widthAnchor.constraint(equalToConstant: 1),
+                tabSearchTargetViewIPad.heightAnchor.constraint(equalToConstant: 1)
+            ])
+            targetViewIPad = tabSearchTargetViewIPad
+        }
+
         self.tipObservationTask =  Task { @MainActor [weak self, weak appViewController] in
             guard let self, let appViewController else { return }
             
             for await status in tip.statusUpdates {
                 if status == .available {
-                    let popoverController = TipUIPopoverViewController(tip, sourceItem: tabBarItem)
+                    let popoverController = TipUIPopoverViewController(tip, sourceItem: targetViewIPad ?? tabBarItem)
+                    
+                    if UIDevice.current.userInterfaceIdiom == .pad && appViewController.traitCollection.horizontalSizeClass == .regular {
+                        popoverController.popoverPresentationController?.permittedArrowDirections = []
+                    }
                     self.tooltipVC = popoverController
                     appViewController.present(popoverController, animated: true) {
                         popoverController.presentationController?.delegate = self
                     }
+                    
                 } else if case .invalidated = status {
                     tooltipVC?.presentationController?.delegate = nil
                     tooltipVC?.dismiss(animated: true)
