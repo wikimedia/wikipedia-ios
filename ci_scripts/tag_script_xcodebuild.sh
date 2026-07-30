39dfa178ea770d9510232d363f8c00c756c0ed80
Toni Sevener <tonisevener@gmail.com>
2026-07-29 20:09:03 -0500

    Have Xcode Cloud workflows increment build numbers based on tags

--- ci_scripts/tag_script_xcodebuild.sh
+++ ci_scripts/tag_script_xcodebuild.sh
@@ -1,7 +1,16 @@
  #!/bin/sh
 
+if [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
+	TAG_PREFIX="betas"
+elif [[ ${CI_WORKFLOW} == "Experimental Build" ]]; then
+	TAG_PREFIX="alphas"
+else
+	echo "Unrecognized workflow for tagging: ${CI_WORKFLOW}"
+	exit 1
+fi
+
 if [[ ${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH} ]]; then
-	BUILD_TAG="betas/${CI_BUILD_NUMBER}"
+	BUILD_TAG="${TAG_PREFIX}/${CI_BUILD_NUMBER}"
 	git tag $BUILD_TAG
 	git push --tags https://${GITHUB_USERNAME}:${GITHUB_PAT}@github.com/wikimedia/wikipedia-ios.git
 	echo "Successfully tagged ${BUILD_TAG}"
