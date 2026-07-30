b7458a038f684abd35ca2bbf5775ecbe7a695902
Toni Sevener <tonisevener@gmail.com>
2026-07-30 13:52:34 -0500

    Xcode Cloud fix - don't use CI_BUILD_NUMBER

--- ci_scripts/tag_script_xcodebuild.sh
+++ ci_scripts/tag_script_xcodebuild.sh
@@ -1,16 +1,24 @@
  #!/bin/sh
 
+set -e
+
 if [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
 	TAG_PREFIX="betas"
+	BUILD_PLIST="../Wikipedia/Wikipedia-Info.plist"
 elif [[ ${CI_WORKFLOW} == "Experimental Build" ]]; then
 	TAG_PREFIX="alphas"
+	BUILD_PLIST="../Wikipedia/Experimental-Info.plist"
 else
 	echo "Unrecognized workflow for tagging: ${CI_WORKFLOW}"
 	exit 1
 fi
 
 if [[ ${CI_XCODEBUILD_EXIT_CODE} == 0 && ! -z ${CI_APP_STORE_SIGNED_APP_PATH} ]]; then
-	BUILD_TAG="${TAG_PREFIX}/${CI_BUILD_NUMBER}"
+	# Read the build number back from the plist rather than trusting
+	# CI_BUILD_NUMBER, which is Xcode Cloud's own internal counter and
+	# never reflects what ci_pre_xcodebuild.sh actually computed/set here.
+	BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "${BUILD_PLIST}")
+	BUILD_TAG="${TAG_PREFIX}/${BUILD_NUMBER}"
 	git tag $BUILD_TAG
 	git push --tags https://${GITHUB_USERNAME}:${GITHUB_PAT}@github.com/wikimedia/wikipedia-ios.git
 	echo "Successfully tagged ${BUILD_TAG}"
