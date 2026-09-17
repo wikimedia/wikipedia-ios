9b2cf83f245ca66c77dba0c5e26c8989f97a7f72
Toni Sevener <tsevener@wikimedia.org>
2026-09-17 17:10:01 -0500

    Whitespace nit

--- Wikipedia/Code/SettingsTabViewController.swift
+++ Wikipedia/Code/SettingsTabViewController.swift
@@ -179,6 +179,7 @@ public final class WMFSettingsHostingController: WMFComponentHostingController<W
             DonateFunnel.shared.logYearInReviewSettingsDidAppear()
         }
     }
+
     // MARK: - Navigation Bar Configuration
 
     private func configureNavigationBar() {
