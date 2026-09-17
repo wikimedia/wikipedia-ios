a9b547627076a779f24d8dbbfc00154d6e5fa4be
Grey Olson <golson@wikimedia.org>
2026-09-17 13:10:53 -0400

    Move logic for YIR announcement to its own file

--- WMFLocalizations/Sources/WMFNativeLocalizations/CommonStrings.swift
+++ WMFLocalizations/Sources/WMFNativeLocalizations/CommonStrings.swift
@@ -703,8 +703,6 @@ public class CommonStrings: NSObject {
 
     public static let notNowTitle = WMFLocalizedString("not-now-title", value: "Not now", comment: "Text for the dismiss button across various features.")
 
-    public static let youCanAccessYIR = WMFLocalizedString("year-in-review-intro-exit-toast-title", value: "You can access your Year in Review later in Profile.", comment: "Toast displayed to user after the exit Year in Review on the intro slide.")
-
     public static let logoCaption = WMFLocalizedString("year-in-review-highlights-share-logo-caption", value: "Wikipedia logo", comment: "Caption for Wikipedia logo on sharable version of the highlights year in review slide")
 
     public static let todayTitle = WMFLocalizedString("today-title", value: "Today", comment: "Title for today section on article view history")
